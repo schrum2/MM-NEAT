cf97f969c82315be9104563875044826a4afb6ab
schrum2 <schrum2@utexas.edu>
2016-02-23 23:58:47 -0600

    These are batch files for launching simple experiments. They will need to be changed to launch the correct jar file.

---
+++ logGhostLocs.bat
@@ -0,0 +1,3 @@
+REM Usage:   postBestWatch.bat <experiment directory> <log prefix> <run type> <run number> <number of trials per individual>
+REM Example: postBestWatch.bat onelifeconflict OneLifeConflict OneModule 0 5
+java -jar "dist/MONE.jar" runNumber:%4 parallelEvaluations:false logGhostLocOnPowerPill:true experiment:edu.utexas.cs.nn.experiment.BestNetworkExperiment base:%1 log:%2-%3 saveTo:%3 trials:%5 watch:false showNetworks:false io:false netio:false onlyWatchPareto:true printFitness:true animateNetwork:false monitorInputs:false modePheremone:false
---
+++ postParetoFrontWatch.bat
@@ -0,0 +1,3 @@
+REM Usage:   postParetoFrontWatch.bat <experiment directory> <log prefix> <run type> <run number> <number of trials per individual>
+REM Example: postParetoFrontWatch.bat onelifeconflict OneLifeConflict OneModule 0 5
+java -jar "dist/MONE.jar" runNumber:%4 experiment:edu.utexas.cs.nn.experiment.LoadAndWatchExperiment base:%1 log:%2-%3 saveTo:%3 trials:%5 watch:true showNetworks:true io:false netio:false onlyWatchPareto:true printFitness:true animateNetwork:false monitorInputs:true
