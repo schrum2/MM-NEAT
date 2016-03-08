6d281ad1839d728c021dc8cca8cf970169621711
Rollinsa <rollinsa@southwestern.edu>
2016-03-08 16:56:30 -0600

    Added comments to Organism.java
    Added descriptive comments for Organism class.

--- src/edu/utexas/cs/nn/evolution/Organism.java
+++ src/edu/utexas/cs/nn/evolution/Organism.java
@@ -3,21 +3,29 @@ package edu.utexas.cs.nn.evolution;
 import edu.utexas.cs.nn.evolution.genotypes.Genotype;
 
 /**
- *
+ * Stores a genotype, which can be replaced as needed.
  * @author Jacob Schrum
  */
 public class Organism<T> {
 
+	// Store a genotype that produces a phenotype T
     protected Genotype<T> genotype;
 
+    // Constructor
     public Organism(Genotype<T> genotype) {
         this.genotype = genotype;
     }
 
+    // Getter
     public Genotype<T> getGenotype() {
         return genotype;
     }
 
+    /**
+     * Replace the genotype of the organism so the same organism
+     * can be evaluated in the domain, but with a new genotype.
+     * @param newGenotype The new genotype
+     */
     public void replaceGenotype(Genotype<T> newGenotype) {
         this.genotype = newGenotype;
     }
