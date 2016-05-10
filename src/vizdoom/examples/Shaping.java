16c730f7a7e0f6d794691780633ded868c2e1526
schrum2 <schrum2@utexas.edu>
2016-05-10 00:24:05 -0500

    Added VizDoom
    Added the executable for vizdoom, the pre-compiled dll, and the jar file
    with the code that connects to it, and the basic vizdoom java examples
    provided initially. The next big step is to create a task linked to
    vizdoom so agents can be evolved in this domain.

---
+++ src/vizdoom/examples/Shaping.java
@@ -0,0 +1,85 @@
+package vizdoom.examples;
+import vizdoom.*;
+
+import java.util.*;
+import java.lang.*;
+
+public class Shaping {
+
+    public static void main (String[] args) {
+        SpecifyDLL.specifyDLLPath();        
+
+        System.out.println("\n\nSHAPING EXAMPLE\n");
+
+
+        // Create DoomGame instance. It will run the game and communicate with you.
+        DoomGame game = new DoomGame();
+
+        // Health gathering scenario has scripted shaping reward.
+        game.loadConfig("vizdoom/examples/config/health_gathering.cfg");
+        game.setViZDoomPath("vizdoom/bin/vizdoom");
+
+        // Sets path to doom2 iwad resource file which contains the actual doom game-> Default is "./doom2.wad".
+        game.setDoomGamePath("vizdoom/scenarios/freedoom2.wad");
+        //game.setDoomGamePath("vizdoom/scenarios/doom2.wad");   // Not provided with environment due to licences.
+
+
+
+        // Sets resolution. Default is 320X240
+        game.setScreenResolution(ScreenResolution.RES_320X240);
+
+        // Initialize the game. Further configuration won't take any effect from now on.
+        game.init();
+
+        List<int[]> actions = new ArrayList<int[]>();
+        actions.add(new int[] {1, 0, 1});
+        actions.add(new int[] {0, 1, 1});
+        actions.add(new int[] {0, 0, 1});
+
+        Random ran = new Random();
+
+        // Run this many episodes
+        int episodes = 10;
+
+        // Use this to remember last shaping reward value.
+        double lastTotalShapingReward = 0;
+        for (int i = 0; i < episodes; ++i) {
+
+            System.out.println("Episode #" + (i + 1));
+
+            // Starts a new episode. It is not needed right after init() but it doesn't cost much and the loop is nicer.
+            game.newEpisode();
+
+            while (!game.isEpisodeFinished()) {
+
+                // Get the state
+                GameState s = game.getState();
+
+                // Make random action and get reward
+                double r = game.makeAction(actions.get(ran.nextInt(3)));
+
+
+                // Retrieve the shaping reward
+                int _ssr = game.getGameVariable(GameVariable.USER1);    // Get value of scripted variable
+                double ssr = game.DoomFixedToDouble(_ssr);              // If value is in DoomFixed format project it to double
+                double sr = ssr - lastTotalShapingReward;
+                lastTotalShapingReward = ssr;
+
+                System.out.println("State #" + s.number);
+                System.out.println("Health: " + s.gameVariables[0]);
+                System.out.println("Action reward: " + r);
+                System.out.println("Action shaping reward: " + sr);
+                System.out.println("=====================");
+
+            }
+
+        System.out.println( "Episode finished.");
+        System.out.println("Total reward: " + game.getTotalReward());
+        System.out.println ("************************");
+
+        }
+
+        // It will be done automatically in destructor but after close You can init it again with different settings.
+        game.close();
+    }
+}
---
+++ src/vizdoom/examples/Spectator.java
@@ -0,0 +1,78 @@
+package vizdoom.examples;
+import vizdoom.*;
+
+import java.util.*;
+import java.lang.*;
+
+public class Spectator {
+
+    public static void main (String[] args) {
+        SpecifyDLL.specifyDLLPath();
+        
+        DoomGame game= new DoomGame();
+        // Choose scenario config file you wish to watch.
+        // Don't load two configs cause the second will overwrite the first one.
+        // Multiple config files are ok but combining these ones doesn't make much sense.
+
+        //game.loadConfig("vizdoom/examples/config/basic.cfg");
+        //game.loadConfig("vizdoom/examples/config/deadly_corridor.cfg");
+        game.loadConfig("vizdoom/examples/config/deathmatch.cfg");
+        //game.loadConfig("vizdoom/examples/config/defend_the_center.cfg");
+        //game.loadConfig("vizdoom/examples/config/defend_the_line.cfg");
+        //game.loadConfig("vizdoom/examples/config/health_gathering.cfg");
+        //game.loadConfig("vizdoom/examples/config/my_way_home.cfg");
+        //game.loadConfig("vizdoom/examples/config/predict_position.cfg");
+        //game.loadConfig("vizdoom/examples/config/take_cover.cfg");
+
+        game.setScreenResolution(ScreenResolution.RES_640X480);
+
+        // Select game and map You want to use.
+        game.setDoomGamePath("vizdoom/scenarios/freedoom2.wad");
+
+        //game.setDoomGamePath("vizdoom/scenarios/doom2.wad");
+        game.setViZDoomPath("vizdoom/bin/vizdoom_nosound");
+
+        //Adds mouse support:
+        game.addAvailableButton(Button.TURN_LEFT_RIGHT_DELTA);
+
+        // Enables spectator mode, so you can play. Agent is supposed to watch you playing and learn from it.
+        game.setWindowVisible(true);
+        game.setMode(Mode.SPECTATOR);
+        game.init();
+
+        int episodes = 10;
+        for (int i=0;i<episodes;i++){
+
+            System.out.println("Episode #" +(i+1));
+
+            game.newEpisode();
+            while (! game.isEpisodeFinished()){
+                GameState s = game.getState();
+                int[] img = s.imageBuffer;
+                int[] misc = s.gameVariables;
+
+                game.advanceAction();
+                boolean[] a = game.getLastAction();
+                double r = game.getLastReward();
+
+                System.out.println("State #"+s.number);
+                System.out.println("Game Variables: "+Arrays.toString(misc));
+                System.out.println("Action: "+ Arrays.toString(a));
+                System.out.println("Reward: "+r);
+                System.out.println("=====================");
+            }
+
+            System.out.println("episode finished!");
+            System.out.println("Total reward:"+ game.getTotalReward());
+            System.out.println("************************");
+
+            try {
+                Thread.sleep(2000);
+            }
+            catch(InterruptedException ex){
+                Thread.currentThread().interrupt();
+            }
+        }
+        game.close();
+    }
+}
