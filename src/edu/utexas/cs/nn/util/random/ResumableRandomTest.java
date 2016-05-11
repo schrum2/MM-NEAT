6145148e295af21fd0f50bb3074710a3d1dae7b1
leg2015 <gillespl@southwestern.edu>
2016-05-11 14:15:56 -0500

    Revised JUnit Test for Resumable Random class
    Made changes to JUnit test and added a second test for the hard set seed
    method

--- src/edu/utexas/cs/nn/util/random/ResumableRandomTest.java
+++ src/edu/utexas/cs/nn/util/random/ResumableRandomTest.java
@@ -8,31 +8,48 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
+ * This is a JUnit test for the core methods of the Resumable Random class, getSeed which 
+ * gets the current seed of the Random, and hardSetSeed which sets the seed to a new value.
+ * 
  * @author gillespl
  *
  */
 public class ResumableRandomTest {
 
+	static final int SIZE = 100;
+	static final long seed = 10;
 	ResumableRandom randomGenerator = new ResumableRandom();
+	
 	
 	/**
 	 * Tests whether or not the method can get and reset the seed for a resumable random number generator
 	 */
 	@Test
-	public void test() {
+	public void test_getSeed() {
+
 		long seed = randomGenerator.getSeed();
-		int[] test1 = new int[100];
+		int[] test1 = new int[SIZE];
 		for(int i = 0; i < test1.length; i++) {
 			test1[i] = randomGenerator.nextInt();
 		}
 		randomGenerator.hardSetSeed(seed);
-		int[] test2 = new int[100];
+		int[] test2 = new int[SIZE];
 		for(int i = 0; i < test2.length; i++) {
 			test2[i] = randomGenerator.nextInt();
 		}
 		for(int i = 0; i < test1.length && i < test2.length; i++) {
 			assertEquals(test1[i], test2[i]);
 		}
+	}
+	
+	@Test
+	public void test_hardSetSeed() {
+		ResumableRandom r2 = new ResumableRandom();
+		randomGenerator.hardSetSeed(seed);
+		long l1 = randomGenerator.getSeed();
+		r2.hardSetSeed(seed);
+		long l2 = r2.getSeed();
+		assertEquals(l1, l2);
 	}
 
 }
