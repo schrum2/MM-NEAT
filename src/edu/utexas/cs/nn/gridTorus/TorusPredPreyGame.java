00dbeab1f5d25b7b9ee25704fc0b497a5d70567e
Rollinsa <rollinsa@southwestern.edu>
2016-05-12 16:50:11 -0500

    Revert "Tweaked TorusPredPreyTask"
    This reverts commit 1e0e33e23485cea886deeafd04303f1f46b07699.

--- src/edu/utexas/cs/nn/gridTorus/TorusPredPreyGame.java
+++ src/edu/utexas/cs/nn/gridTorus/TorusPredPreyGame.java
@@ -97,10 +97,6 @@ public class TorusPredPreyGame {
         return time;
     }
     
-    public int getTimeLimit(){
-    	return timeLimit;
-    }
-    
     /**
      * moves all the agents along the x and y coordinates
      * @param moves a grid of all possible moves for an agent
--- src/edu/utexas/cs/nn/tasks/gridTorus/TorusEvolvedPreyVsStaticPredatorsTask.java
+++ src/edu/utexas/cs/nn/tasks/gridTorus/TorusEvolvedPreyVsStaticPredatorsTask.java
@@ -1,44 +1,120 @@
-
 package edu.utexas.cs.nn.tasks.gridTorus;
 
+/**
+ * Imports needed parts to initialize the task.
+ */
 import edu.utexas.cs.nn.evolution.genotypes.Genotype;
+import edu.utexas.cs.nn.evolution.nsga2.tug.TUGTask;
+import edu.utexas.cs.nn.gridTorus.TorusPredPreyGame;
+import edu.utexas.cs.nn.gridTorus.TorusWorldExec;
 import edu.utexas.cs.nn.gridTorus.controllers.AggressivePredatorController;
 import edu.utexas.cs.nn.gridTorus.controllers.TorusPredPreyController;
 import edu.utexas.cs.nn.MMNEAT.MMNEAT;
 import edu.utexas.cs.nn.networks.Network;
+import edu.utexas.cs.nn.networks.NetworkTask;
+import edu.utexas.cs.nn.parameters.CommonConstants;
 import edu.utexas.cs.nn.parameters.Parameters;
+import edu.utexas.cs.nn.tasks.NoisyLonerTask;
+import edu.utexas.cs.nn.util.datastructures.Pair;
 
 /**
  *
- * @author Rollinsa
+ * @author Jacob Schrum, Gabby Gonzalez
  * The following class sets up tasks for learning agents and NPCs.
  */
-public class TorusEvolvedPreyVsStaticPredatorsTask<T extends Network> extends TorusPredPreyTask<T> {
+public class TorusEvolvedPreyVsStaticPredatorsTask<T extends Network> extends NoisyLonerTask<T> implements TUGTask, NetworkTask {
+	/**
+	 * Initializes the controllers and world to be used.
+	 */
+	private TorusPredPreyController[] staticAgents;
+	private TorusWorldExec exec;
 
+	/**
+	 * Extends NoisyLonerTask to make a generalized task for evolving a brain with static enemies.
+	 * Enemies are created with the parameter being torusPredators and initially all aggressive.
+	 */
 	public TorusEvolvedPreyVsStaticPredatorsTask() {
-		super(true); 
-
-		MMNEAT.registerFitnessFunction("Time Alive"); 
-	}
-
-	@Override
-	public TorusPredPreyController[] getPredAgents(Genotype<T> individual) {
+		super(); 
 		int numPredators = Parameters.parameters.integerParameter("torusPredators"); 
-		TorusPredPreyController[] staticAgents = new TorusPredPreyController[numPredators];
+		staticAgents = new TorusPredPreyController[numPredators];
 		for(int i = 0; i < numPredators; i++) {
 			staticAgents[i] = new AggressivePredatorController(); 
 		}
-		return staticAgents;	
+		MMNEAT.registerFitnessFunction("Time Alive"); 
 	}
 
+	/**
+	 * Used to evaluated the fitness score of the brain agent genotype. Additionally, initializes the game to do this.
+	 * one or more prey is evolved while predators are static
+	 * @param genotype and num (evaluation being performed)
+	 * @return pair of fitness and states from evaluation
+	 */
 	@Override
-	public TorusPredPreyController[] getPreyAgents(Genotype<T> individual) {
-		int numPrey = Parameters.parameters.integerParameter("torusPreys");
-		TorusPredPreyController[] evolvedAgents = new TorusPredPreyController[numPrey];    	
-		for(int i = 0; i < numPrey; i++){
-			//false to indicate that this is not a predator, but a prey
-			evolvedAgents[i] = new NNTorusPredPreyAgent<T>(individual, false).getController(); 
+	public Pair<double[], double[]> oneEval(Genotype<T> individual, int num) {
+		int numPreys = Parameters.parameters.integerParameter("torusPreys");
+		TorusPredPreyController[] evolvedAgents = new TorusPredPreyController[numPreys];    	
+		for(int i = 0; i < numPreys; i++){
+			//false to indicate that this is not a predator
+			NNTorusPredPreyAgent<T> preyAgent = new NNTorusPredPreyAgent<T>(individual, false);
+			evolvedAgents[i] = preyAgent.getController(); 
 		}
-		return evolvedAgents;
+		exec = new TorusWorldExec(); 
+		TorusPredPreyGame game; 
+		if (CommonConstants.watch) { 
+			game = exec.runGameTimed(staticAgents, evolvedAgents, true); 
+		} else {
+			game = exec.runExperiment(staticAgents, evolvedAgents);
+		}
+		double[] oneTrialFitness = new double[]{game.getTime()}; 
+		double[] otherStats = new double[0];
+		return new Pair<double[], double[]>(oneTrialFitness, otherStats); 
+	}
+
+	/**
+	 * Returns the current number of objectives for the agent that initialized minScores, i.e. brain agent.
+	 */
+	public int numObjectives() { 
+		return minScores().length; 
+	}
+
+	/**
+	 * Goals set to zeroes, since objectives are positive
+	 */
+	public double[] startingGoals() {
+		return minScores();
+	}
+
+	/**
+	 * Initialize the array minScores for the brain agent.
+	 */
+	@Override
+	public double[] minScores() { 
+		return new double[]{0};
+	}
+
+	/**
+	 * Accesses the labels for the sensors used in network visualization.
+	 */
+	public String[] sensorLabels() { 
+		return NNTorusPredPreyController.sensorLabels(staticAgents.length);
+	}
+
+	/**
+	 * Accesses the outputs that will be used by an agent, i.e. the movement directions.
+	 */
+	public String[] outputLabels() { 
+		//this class only evolves the prey, so only the prey actions (whether do nothing action is enabled for prey or not)
+		//matter, and the predator do nothing action option does not
+		//return new String[]{"UP", "RIGHT", "DOWN", "LEFT"};
+		return Parameters.parameters.booleanParameter("allowDoNothingActionForPreys") ?
+				new String[]{"UP", "RIGHT", "DOWN", "LEFT", "NOTHING"} :
+					new String[]{"UP", "RIGHT", "DOWN", "LEFT"};
+	}
+
+	/**
+	 * Accesses the time stamps for the current game being executed, use for evaluation purposes.
+	 */
+	public double getTimeStamp() {
+		return exec.game.getTime();
 	}
 }
