80f05e3d151ddb2c8c979c7b8c0b615ab2df17b9
leg2015 <gillespl@southwestern.edu>
2016-05-12 13:28:15 -0500

    Changed file output
    Added a command line parameter so JUnit tests don't write an output file
    to the computer

--- src/edu/utexas/cs/nn/evolution/mutation/tweann/MMDTest.java
+++ src/edu/utexas/cs/nn/evolution/mutation/tweann/MMDTest.java
@@ -38,7 +38,7 @@ public class MMDTest {
 	 */
 	@Before
 	public void setUp() throws Exception {
-		Parameters.initializeParameterCollections(new String[]{"io:false","recurrency:false","mmdRate:1.0"});
+		Parameters.initializeParameterCollections(new String[]{"io:false", "netio:false", "recurrency:false","mmdRate:1.0"});
 		MMNEAT.loadClasses();
 		tg1 = new TWEANNGenotype(MMNEAT.networkInputs, MMNEAT.networkOutputs, 0);
 		tg2 = new TWEANNGenotype(MMNEAT.networkInputs, MMNEAT.networkOutputs, 0);
