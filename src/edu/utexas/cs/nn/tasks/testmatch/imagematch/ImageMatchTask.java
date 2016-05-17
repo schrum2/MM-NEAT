7993bb206842dfd3230968d4f3d2ca3968fb756c
leg2015 <gillespl@southwestern.edu>
2016-05-17 11:53:12 -0500

    JUnit and comment for ImageMatchTask
    Finished JUnit test and commenting for JUnit test and class. Also fixed
    a bug in getTestPairs class so image match task now properly works

--- src/edu/utexas/cs/nn/tasks/testmatch/imagematch/ImageMatchTask.java
+++ src/edu/utexas/cs/nn/tasks/testmatch/imagematch/ImageMatchTask.java
@@ -1,6 +1,5 @@
 package edu.utexas.cs.nn.tasks.testmatch.imagematch;
 
-
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.*;
@@ -8,9 +7,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-
 import javax.imageio.*;
-
 import edu.utexas.cs.nn.MMNEAT.MMNEAT;
 import edu.utexas.cs.nn.evolution.EvolutionaryHistory;
 import edu.utexas.cs.nn.evolution.genotypes.Genotype;
@@ -41,9 +38,10 @@ public class ImageMatchTask<T extends Network> extends MatchDataTask<T> {
 	private static final int hIndex = 0;
 	private static final int sIndex = 1;
 	private static final int bIndex = 2;
-	private static final double MAX_COLOR_INTENSITY = 255.0;//this variable needed to scale RGB values to a 0-1 range
+	private static final double BIAS = 1.0;//a common input used in CPPNs
+	private static final double SQRT2 = Math.sqrt(2);
 	private BufferedImage img = null;
-	private int imageHeight, imageWidth;
+	public int imageHeight, imageWidth;
 
 	/**
 	 * Default task constructor
@@ -61,30 +59,36 @@ public class ImageMatchTask<T extends Network> extends MatchDataTask<T> {
 		try {//throws and exception if filename is not valid
 			img = ImageIO.read(new File(IMAGE_MATCH_PATH + "\\" + filename));
 		} catch (IOException e) {
+			System.out.println("Could not load image: " + filename);
+			System.exit(1);
 		}
 		imageHeight = img.getHeight();
 		imageWidth = img.getWidth();
 	}
 
+	/**
+	 * Evaluate method. If watch is true, puts the image and network into a JFrame with
+	 * option to save both as a bmp. Else, the super evaluate method is called.
+	 */
 	public Score<T> evaluate(Genotype<T> individual) {
 		if(CommonConstants.watch) {
 			Network n = individual.getPhenotype();
 			double[] hsb = null;
 			BufferedImage child = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
-
-			for(int x = 0; x < imageWidth; x++) {
+			for(int x = 0; x < imageWidth; x++) {//scans across whole image
 				for(int y = 0; y < imageHeight; y++) {
-					double[] input = {x,y,0,1};
-					ImageMatchTask.scale(input, imageWidth, imageHeight);
-					ILocated2D distance = new Tuple2D(input[0], input[1]);
-					input[DISTANCE_COORDINATE_IND] = distance.distance(new Tuple2D(0,0)) * Math.sqrt(2);
+					double[] input = {x, y, 0, BIAS};
+					ImageMatchTask.scale(input, imageWidth, imageHeight);//properly scales the coordinates
+					ILocated2D distance = new Tuple2D(input[X_COORDINATE_INDEX], input[Y_COORDINATE_INDEX]);
+					input[DISTANCE_COORDINATE_IND] = distance.distance(new Tuple2D(0,0)) * SQRT2;//computes euclidian distance
 					hsb = n.process(input);
+					//network outputs computed on hsb, not rgb scale because creates better images
 					Color childColor = Color.getHSBColor((float) hsb[hIndex], (float) Math.max(0,Math.min(hsb[sIndex],1)), (float) Math.abs(hsb[bIndex]));
-					child.setRGB(x, y, childColor.getRGB());
+					child.setRGB(x, y, childColor.getRGB());//set back to RGB to draw picture to JFrame
 				}
 			}
 
-
+			//draws picture and network to JFrame
 			DrawingPanel parentPanel = new DrawingPanel(imageWidth, imageHeight, "target");
 			DrawingPanel childPanel = new DrawingPanel(imageWidth, imageHeight, "output");
 			childPanel.setLocation(imageWidth + IMAGE_PLACEMENT, 0);
@@ -93,7 +97,7 @@ public class ImageMatchTask<T extends Network> extends MatchDataTask<T> {
 
 			parentGraphics.drawRenderedImage(img, null);
 			childGraphics.drawRenderedImage(child, null);
-			Scanner scan = new Scanner(System.in);
+			Scanner scan = new Scanner(System.in);//Scanner allows picture and network to be saved as bmp
 			System.out.println("Save image? y/n");
 			if(scan.next().equals("y")) {
 				System.out.println("enter filename");
@@ -104,7 +108,7 @@ public class ImageMatchTask<T extends Network> extends MatchDataTask<T> {
 			parentPanel.dispose();
 			childPanel.dispose();
 		}
-		return super.evaluate(individual);
+		return super.evaluate(individual);//if watch=false
 	}
 
 	/**
@@ -112,7 +116,7 @@ public class ImageMatchTask<T extends Network> extends MatchDataTask<T> {
 	 */
 	@Override
 	public String[] sensorLabels() {
-		return new String[]{"X-coordinate", "Y-coordinate"};
+		return new String[]{"X-coordinate", "Y-coordinate", "distance from center", "bias"};
 	}
 
 	/**
@@ -120,45 +124,91 @@ public class ImageMatchTask<T extends Network> extends MatchDataTask<T> {
 	 */
 	@Override
 	public String[] outputLabels() {
-		return new String[]{"r-value", "g-value", "b-value"};
+		return new String[]{"hue-value", "saturation-value", "brightness-value"};
 	}
 
+	/**
+	 * Returns number of inputs to network
+	 */
 	@Override
 	public int numInputs() {
 		return 4;
 	}
 
+	/**
+	 * Returns number of outputs from network
+	 */
 	@Override
 	public int numOutputs() {
 		return 3;
 	}
 
+	/**
+	 * Gets the expected input and output from picture for network to compare against
+	 * 
+	 * @return an ArrayList of all the pairs of expected inputs and outputs
+	 */
 	@Override
 	public ArrayList<Pair<double[], double[]>> getTrainingPairs() {
 		ArrayList<Pair<double[], double[]>> pairs = new ArrayList<Pair<double[], double[]>>();
 		for(int i = 0; i < imageWidth; i++) {
 			for(int j = 0; j < imageHeight; j++) {
-				Color color = new Color(img.getRGB((i/imageWidth)*2 - 1, (j/ imageHeight)*2 - 1));
-				pairs.add(new Pair<double[], double[]>(new double[]{i, j}, new double[]
-						{color.getRed()/MAX_COLOR_INTENSITY, color.getGreen()/MAX_COLOR_INTENSITY, color.getBlue()/MAX_COLOR_INTENSITY}));
+				Color color = new Color(img.getRGB(i, j));
+				float[] hsb = new float[numOutputs()];
+				ILocated2D distance = new Tuple2D(i, j);
+				Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), hsb);
+				pairs.add(new Pair<double[], double[]>(new double[]{ImageMatchTask.scale(i, imageWidth), 
+						ImageMatchTask.scale(j, imageHeight), distance.distance(new Tuple2D(0,0)) * Math.sqrt(2), BIAS},
+						new double[] {hsb[hIndex], hsb[sIndex], hsb[bIndex]}));
 			}
 		}
 		return pairs;
 	}
 
+	/**
+	 * Scales X-Y coordinates to where origin is at center of picture, not top left corner
+	 * 
+	 * @param toScale double array of x and y coordinate, with x-coordinate at[0] and y at [1]
+	 * @param imageWidth width of image
+	 * @param imageHeight height of image
+	 */
 	public static void scale(double[] toScale, int imageWidth, int imageHeight) {
-		toScale[X_COORDINATE_INDEX] = (toScale[0]/imageWidth)*2 - 1;
-		toScale[Y_COORDINATE_INDEX] = (toScale[1]/imageHeight)*2 - 1;
+		if(toScale[X_COORDINATE_INDEX] == 0 && toScale[Y_COORDINATE_INDEX] == 0) {
+			toScale[X_COORDINATE_INDEX] = -1;
+			toScale[Y_COORDINATE_INDEX] = -1;
+		} else if(toScale[X_COORDINATE_INDEX] == 0 && toScale[Y_COORDINATE_INDEX] != 0){
+			toScale[X_COORDINATE_INDEX] = -1;
+			toScale[Y_COORDINATE_INDEX] = (toScale[Y_COORDINATE_INDEX]/imageHeight)*2 - 1;
+		} else if(toScale[Y_COORDINATE_INDEX] == 0 && toScale[X_COORDINATE_INDEX] != 0){
+			toScale[Y_COORDINATE_INDEX]= -1;
+			toScale[X_COORDINATE_INDEX] = (toScale[X_COORDINATE_INDEX]/imageWidth)*2 - 1;
+		} else {
+		toScale[X_COORDINATE_INDEX] = (toScale[X_COORDINATE_INDEX]/imageWidth)*2 - 1;
+		toScale[Y_COORDINATE_INDEX] = (toScale[Y_COORDINATE_INDEX]/imageHeight)*2 - 1;
+		}
 	}
-
+	/**
+	 * Scales either x or y coordinate to where origin is at center of picture, not top left corner
+	 * @param toScale coordinate to be scaled
+	 * @param imageSize either imageHeight or imageWidth, depending on whether toScale is x or y coordinate
+	 * 
+	 * @return scaled coordinate
+	 */
+	public static int scale(int toScale, int imageSize) {
+		return (toScale/imageSize)*2 - 1;
+	}
+	
+	/**
+	 * main method used to create a random CPPN image.
+	 */
 	public static void main(String[] args) {
-
 		randomCPPNimage();	
-
 	}
-
-
-
+	
+	/**
+	 * Creates a random image of given size and numMutations and puts it in JFrame
+	 *  with option to save image and network as a bmp
+	 */
 	public static void randomCPPNimage() {
 
 		MMNEAT.clearClasses();
--- src/edu/utexas/cs/nn/tasks/testmatch/imagematch/ImageMatchTaskTest.java
+++ src/edu/utexas/cs/nn/tasks/testmatch/imagematch/ImageMatchTaskTest.java
@@ -1,96 +1,113 @@
 package edu.utexas.cs.nn.tasks.testmatch.imagematch;
 
 import static org.junit.Assert.*;
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.image.*;
-import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Scanner;
-
-import javax.imageio.*;
-
+import org.junit.Before;
+import org.junit.Test;
 import edu.utexas.cs.nn.MMNEAT.MMNEAT;
 import edu.utexas.cs.nn.evolution.EvolutionaryHistory;
-import edu.utexas.cs.nn.evolution.genotypes.Genotype;
-import edu.utexas.cs.nn.evolution.genotypes.TWEANNGenotype;
-import edu.utexas.cs.nn.graphics.DrawingPanel;
-import edu.utexas.cs.nn.networks.Network;
 import edu.utexas.cs.nn.networks.TWEANN;
-import edu.utexas.cs.nn.parameters.CommonConstants;
 import edu.utexas.cs.nn.parameters.Parameters;
-import edu.utexas.cs.nn.scores.Score;
-import edu.utexas.cs.nn.tasks.testmatch.MatchDataTask;
 import edu.utexas.cs.nn.util.datastructures.Pair;
 import edu.utexas.cs.nn.util.util2D.ILocated2D;
 import edu.utexas.cs.nn.util.util2D.Tuple2D;
-import java.awt.image.BufferedImage;
-
-import org.junit.Before;
-import org.junit.Test;
-
+/**
+ * JUnit test class for the ImageMatchTask
+ * 
+ * @author gillespl
+ *
+ */
 public class ImageMatchTaskTest {
 
 	public static final String IMAGE_MATCH_PATH = "data\\imagematch";
-	private static final int IMAGE_PLACEMENT = 200;
 	private static final int X_COORDINATE_INDEX = 0;
 	private static final int Y_COORDINATE_INDEX = 1;
-	private static final int DISTANCE_COORDINATE_IND = 2;
-	private static final int hIndex = 0;
-	private static final int sIndex = 1;
-	private static final int bIndex = 2;
-	private static final double MAX_COLOR_INTENSITY = 255.0;//this variable needed to scale RGB values to a 0-1 range
-	private BufferedImage img = null;
-	private int imageHeight, imageWidth;
+	private static final int BIAS_IND = 3;
+	private static final double BIAS = 1.0;//input to neural network
+	public int imageHeight, imageWidth;
 	
-//	MMNEAT.clearClasses();
-//	EvolutionaryHistory.setInnovation(0);
-//	EvolutionaryHistory.setHighestGenotypeId(0);
-//	Parameters.initializeParameterCollections(new String[]{"io:false", "netio:false", "allowMultipleFunctions:true", "recurrency:false", "includeHalfLinearPiecewiseFunction:true", "includeSawtoothFunction:true"});
-//	MMNEAT.loadClasses();
+	ImageMatchTask<TWEANN> test;
 	
-	ImageMatchTask test = new ImageMatchTask("Tiny");
-	
+	/**
+	 * Instantiates ImageMatchTask
+	 * 
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
+		MMNEAT.clearClasses();
+		EvolutionaryHistory.setInnovation(0);
+		EvolutionaryHistory.setHighestGenotypeId(0);
+		Parameters.initializeParameterCollections(new String[]{"io:false", "netio:false", "allowMultipleFunctions:true", "recurrency:false", "includeHalfLinearPiecewiseFunction:true", "includeSawtoothFunction:true"});
+		MMNEAT.loadClasses();
+		test = new ImageMatchTask<TWEANN>("Tiny.png");
+		imageHeight = test.imageHeight;
+		imageWidth = test.imageWidth;
 	}
 
+	/**
+	 * Tests the getter functions of ImageMatchTask
+	 */
 	@Test
 	public void test_getterFunctions() {
 		String[] outputLabels = test.outputLabels();
 		String[] sensorLabels = test.sensorLabels();
 		assertTrue(sensorLabels[0].equals("X-coordinate"));
-		assertTrue(outputLabels[0].equals("r-value"));
+		assertTrue(outputLabels[0].equals("hue-value"));
 		assertTrue(test.numInputs() == 4);
 		assertTrue(test.numOutputs() == 3);
 	}
 	
+	/**
+	 * Tests proper values are stored in getTrainingPairs method
+	 */
 	@Test
 	public void test_getTrainingPairs() {
 		ArrayList<Pair<double[], double[]>> pair = test.getTrainingPairs();
-		assertTrue(pair.get(pair.size()-1).t1[0] == imageHeight);
-		assertTrue(pair.get(pair.size()-1).t1[1] == imageHeight);
-		assertTrue(pair.get(pair.size() -1).t2.length == 3);
+		System.out.print(Arrays.toString(pair.get(pair.size()-1).t1));
+		assertEquals(pair.get(pair.size()-1).t1[X_COORDINATE_INDEX], ImageMatchTask.scale(0, imageHeight), .001);
+		assertEquals(pair.get(pair.size()-1).t1[Y_COORDINATE_INDEX], ImageMatchTask.scale(0, imageHeight), .001);
+		assertEquals(pair.get(pair.size()-1).t1[BIAS_IND], BIAS, .001);
+		assertEquals(pair.get(pair.size() -1).t2.length, test.numOutputs(), .001);
+	}
+	
+	/**
+	 * Tests the distance calculation is correct. Employs a 
+	 * helper method from Tuple2D class
+	 */
+	@Test
+	public void test_distance() {
+		ILocated2D distance = new Tuple2D(imageWidth, imageHeight);
+		double euclidianDistance = Math.sqrt(Math.pow(imageWidth, 2) + Math.pow(imageHeight, 2));
+		assertTrue(euclidianDistance == distance.distance(new Tuple2D(0,0)));
 	}
 
+	/**
+	 * Tests the scale function works
+	 */
 	@Test
 	public void test_Scale() {
 		double[] leftTopCorner = {0,0};
-		test.scale(leftTopCorner, imageWidth, imageHeight);
+		ImageMatchTask.scale(leftTopCorner, imageWidth, imageHeight);
 		double[] leftBottomCorner = {0, imageHeight};
-		test.scale(leftBottomCorner, imageWidth, imageHeight);
+		ImageMatchTask.scale(leftBottomCorner, imageWidth, imageHeight);
 		double[] rightTopCorner = {imageWidth, 0};
-		test.scale(rightTopCorner, imageWidth, imageHeight);
+		ImageMatchTask.scale(rightTopCorner, imageWidth, imageHeight);
 		double [] rightBottomCorner = {imageWidth, imageHeight};
-		test.scale(rightBottomCorner, imageWidth, imageHeight);
-		double[] center = {imageWidth/2, imageHeight/2};
-		test.scale(center, imageWidth, imageHeight);
-		assertTrue(leftTopCorner[0] == -1 && leftTopCorner[1] == -1);
-		assertTrue(leftBottomCorner[0] == -1 && leftBottomCorner[1] == 1);
-		assertTrue(rightTopCorner[0] == 1 && rightTopCorner[1] == -1);
-		assertTrue(rightBottomCorner[0] == 1 && rightBottomCorner[1] == 1);
-		assertTrue(center[0] == 0 && center[1] == 0);
-		
+		ImageMatchTask.scale(rightBottomCorner, imageWidth, imageHeight);
+		double[] center = {imageWidth/2.0, imageHeight/2.0};
+		ImageMatchTask.scale(center, imageWidth, imageHeight);
+		assertEquals(leftTopCorner[X_COORDINATE_INDEX], -1, .01); 
+		assertEquals(leftTopCorner[Y_COORDINATE_INDEX], -1, .01);
+		assertEquals(leftBottomCorner[X_COORDINATE_INDEX], -1, .01);
+		assertEquals(leftBottomCorner[Y_COORDINATE_INDEX], 1, .01);
+		assertEquals(rightTopCorner[X_COORDINATE_INDEX], 1, .01); 
+		assertEquals(rightTopCorner[Y_COORDINATE_INDEX], -1, .01);
+		assertEquals(rightBottomCorner[X_COORDINATE_INDEX], 1, .01);
+		assertEquals(rightBottomCorner[Y_COORDINATE_INDEX], 1, .01);
+		assertEquals(center[X_COORDINATE_INDEX], 0, .01); 
+		assertEquals(center[Y_COORDINATE_INDEX], 0, .001);
+
 	}
 }
