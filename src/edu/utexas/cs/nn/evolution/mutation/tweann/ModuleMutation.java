67d62450b68de0e85ba529aa0a6f485ee3b856da
leg2015 <gillespl@southwestern.edu>
2016-05-17 16:42:28 -0500

    Commented module mutation class

--- src/edu/utexas/cs/nn/evolution/mutation/tweann/ModuleMutation.java
+++ src/edu/utexas/cs/nn/evolution/mutation/tweann/ModuleMutation.java
@@ -9,39 +9,51 @@ import edu.utexas.cs.nn.util.random.RandomNumbers;
 import java.util.Arrays;
 
 /**
+ * an abstract class for mutating modules.
+ * 
  * @author Jacob Schrum
  */
 public abstract class ModuleMutation extends TWEANNMutation {
 
+	/**
+	 * Constructor for a moduleMutation	
+	 * 
+	 * @param rate rate of mutation occurrences
+	 */
     public ModuleMutation(String rate) {
         super(rate);
     }
 
+    /**
+     * Mutates a TWEANN genotype
+     * 
+     * @param genotype the genotype to be mutated
+     */
     public void mutate(Genotype<TWEANN> genotype) {
-        if (CommonConstants.weakenBeforeModeMutation) {
-            // 0.5 is weakening proportion ... make param
+        if (CommonConstants.weakenBeforeModeMutation) {//weakens modules if needed
             ((TWEANNGenotype) genotype).weakenAllModules(Parameters.parameters.doubleParameter("weakenPortion"));
             if(infoTracking != null) {
                 infoTracking.append("WEAKEN ");
             }
         }
-        // Option to freeze existing network before adding new mode
+        // Option to freeze existing network before adding new module
         if (CommonConstants.freezeBeforeModeMutation) {
             ((TWEANNGenotype) genotype).freezeNetwork();
             if(infoTracking != null) {
                 infoTracking.append("FREEZE ");
             }
         }
+        //this is the method that changes between mutation methods
         addModule((TWEANNGenotype) genotype);
         // This code only matters for hierarchical multitask networks.
         if(CommonConstants.hierarchicalMultitask) {
             int originalModules = ((TWEANNGenotype) genotype).moduleAssociations.length;
-            int[] newModeAssociations = new int[originalModules + 1]; // One module added
-            System.arraycopy(((TWEANNGenotype) genotype).moduleAssociations, 0, newModeAssociations, 0, originalModules); // Copy over old module associations
-            int hierarchicalModes = CommonConstants.multitaskModules;
-            newModeAssociations[originalModules] = RandomNumbers.randomGenerator.nextInt(hierarchicalModes); // Assign to random multitask mode
-            ((TWEANNGenotype) genotype).moduleAssociations = newModeAssociations;
-            infoTracking.append("Assoc: ").append(Arrays.toString(newModeAssociations)).append(" ");
+            int[] newmoduleAssociations = new int[originalModules + 1]; // One module added
+            System.arraycopy(((TWEANNGenotype) genotype).moduleAssociations, 0, newmoduleAssociations, 0, originalModules); // Copy over old module associations
+            int hierarchicalmodules = CommonConstants.multitaskModules;
+            newmoduleAssociations[originalModules] = RandomNumbers.randomGenerator.nextInt(hierarchicalmodules); // Assign to random multitask module
+            ((TWEANNGenotype) genotype).moduleAssociations = newmoduleAssociations;
+            infoTracking.append("Assoc: ").append(Arrays.toString(newmoduleAssociations)).append(" ");
         }
     }
 
