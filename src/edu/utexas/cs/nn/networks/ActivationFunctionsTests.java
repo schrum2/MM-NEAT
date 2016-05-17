faa088c0c07c11ef92fc2e831e3dcea9058efcac
leg2015 <gillespl@southwestern.edu>
2016-05-17 09:34:30 -0500

    JUnit testing Activation Functions
    added JUnit tests for new activation functions

--- src/edu/utexas/cs/nn/networks/ActivationFunctionsTests.java
+++ src/edu/utexas/cs/nn/networks/ActivationFunctionsTests.java
@@ -19,6 +19,16 @@ public class ActivationFunctionsTests {
 	}
 	
 	@Test
+	public void test_keypoints_sawtooth() {
+	double[] keyActivationInputs = {-1, -1.5, 0, 0.7, 1, 1.666};
+	double[] keyActivationAnswers = {0, .5, 0, .7, 0, .666};
+	assertEquals(keyActivationInputs.length, keyActivationAnswers.length);
+	for(int i = 0; i < keyActivationAnswers.length; i++) {
+		assertEquals(keyActivationAnswers[i], ActivationFunctions.sawtooth(keyActivationInputs[i]), .00001);
+	}
+	}
+	
+	@Test
 	public void test_keypoints_tanh() {
 		// outputs of tanh for inputs from keyActivationPoints
 		double[] keyActivationAnswers = {-1, 0, 1};
@@ -38,6 +48,16 @@ public class ActivationFunctionsTests {
 		}
 	}
 
+	@Test
+	public void test_keypoints_halfLinear() {
+		double[] keyActivationAnswers = {0, 0, .5, 1, 1};
+		double[] keyActivationInputs = {-100, 0, .5, 1, 100};
+		assertEquals(keyActivationAnswers.length, keyActivationInputs.length);
+		for(int i = 0; i < keyActivationAnswers.length; i++) {
+			assertEquals(keyActivationAnswers[i], ActivationFunctions.halfLinear(keyActivationInputs[i]), 0.001);
+		}
+	}
+	
 	@Test
 	public void test_keypoints_quickSigmoid() {
 		// outputs of quickSigmoid for inputs from keyActivationPoints
@@ -60,6 +80,7 @@ public class ActivationFunctionsTests {
 		}
 	}
 
+	
 	/**
 	 * sigmoid should be roughly equal to quickSigmoid
 	 */
