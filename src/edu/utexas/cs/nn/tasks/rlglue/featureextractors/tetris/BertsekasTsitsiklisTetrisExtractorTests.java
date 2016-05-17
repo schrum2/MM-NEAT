5c68f7054d157068b8d7dc664c55b210d621c54f
Gab <gonzale9@southwestern.edu>
2016-05-17 14:20:38 -0500

    added BertsekasTsitsiklisTetrisExtractorTests
    added a file to test the BersekasTsitsiklis feature extractor and see
    that everything works properly

---
+++ src/edu/utexas/cs/nn/tasks/rlglue/featureextractors/tetris/BertsekasTsitsiklisTetrisExtractorTests.java
@@ -0,0 +1,13 @@
+package edu.utexas.cs.nn.tasks.rlglue.featureextractors.tetris;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BertsekasTsitsiklisTetrisExtractorTests {
+
+	@Test
+	public void testing() {
+		
+	}
+}
