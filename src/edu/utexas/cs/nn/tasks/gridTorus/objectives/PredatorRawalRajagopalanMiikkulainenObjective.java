98145b2967686c13d04818d327c5265b0a4a3b45
Rollinsa <rollinsa@southwestern.edu>
2016-05-19 11:05:40 -0500

    Fixed bug with the Predator RRM fitness function
    4/5 IS ZERO!!!!!!!!!!!!!!!!!!!!!

--- src/edu/utexas/cs/nn/tasks/gridTorus/objectives/PredatorRawalRajagopalanMiikkulainenObjective.java
+++ src/edu/utexas/cs/nn/tasks/gridTorus/objectives/PredatorRawalRajagopalanMiikkulainenObjective.java
@@ -25,23 +25,24 @@ public class PredatorRawalRajagopalanMiikkulainenObjective <T extends Network> e
 
 	public static final double NO_PREY_SCORE = 25;
 
+	public PredatorRawalRajagopalanMiikkulainenObjective() {
+		//because of the equation for the fitness function given by the RRM paper, there must be two prey
+		if(Parameters.parameters.integerParameter("torusPreys") != 2){
+			throw new IllegalArgumentException("The number of prey must be two for this fitness function");
+		}
+	}
+	
 	@Override
 	/**
 	 * Find the score of the predator based on if all prey died or not
 	 */
 	public double fitness(Organism<T> individual) {
 		
-		
 		TorusAgent[] prey = game.getPrey();
 		TorusAgent[] preds = game.getPredators();
 		int numPrey = prey.length;
 		int numPreds = preds.length;
 		double numCaught = 0;
-		
-		//because of the equation for the fitness function given by the RRM paper, there must be two prey
-		if(numPrey != 2){
-			throw new IllegalArgumentException("The number of prey must be two for this fitness function");
-		}
 		
 		for(TorusAgent p : prey){
 			if(p == null){
@@ -66,7 +67,7 @@ public class PredatorRawalRajagopalanMiikkulainenObjective <T extends Network> e
 		//make d essentially its inverse so that less distance is encouraged
 		d = 1 - d;
 		
-		double WEIGHT = (NO_PREY_SCORE * (4/5));
+		double WEIGHT = (NO_PREY_SCORE * (4.0/5));
 		//now we have d = d, n = numPrey, c = numCaught
 		//------ (20c / n) + (2c) + (20d / n) ------ (generalized for variable number of prey/preds)
 		return ((WEIGHT * numCaught) / numPrey) + (2 * numCaught) + ((WEIGHT * d) / numPrey);
