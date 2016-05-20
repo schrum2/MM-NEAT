f2c80beca543cca85d6a3e35bbabb858ffc8caea
leg2015 <gillespl@southwestern.edu>
2016-05-20 17:02:07 -0500

    started junit testing for statistic utilities

---
+++ src/edu/utexas/cs/nn/util/stats/StatisticsUtilitiesTest.java
@@ -0,0 +1,159 @@
+package edu.utexas.cs.nn.util.stats;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StatisticsUtilitiesTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testPercentile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMedian() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArgmaxDoubleArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArgmaxIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArgmaxIntArrayInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArgminDoubleArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArgminIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSoftmax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProbabilistic() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDistributionDoubleArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDistributionIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModeDoubleArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModeDoubleArrayDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMaximumDoubleArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMaximumCollectionOfInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMaximumLongArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMinimum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAverage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSumIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSumDoubleArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSumCollectionOfInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPopulationStandardDeviation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSampleStandardDeviation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPopulationVariance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSampleVariance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSumOfSquares() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInstantaneousErrorEnergy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInstantaneousTotalErrorEnergy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAverageSquaredErrorEnergy() {
+		fail("Not yet implemented");
+	}
+
+}
