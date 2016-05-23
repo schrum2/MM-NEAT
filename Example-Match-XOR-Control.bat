337603ae77f4c67ce4179c1a3ee1aa7daf992901
schrum2 <schrum2@utexas.edu>
2016-05-23 13:10:54 -0500

    More batch file moving and renaming

--- Example-Match-XOR-Control.bat
+++
@@ -1,1 +0,0 @@
-java -jar dist/MM-NEATv2.jar runNumber:0 randomSeed:0 base:match trials:1 maxGens:100 mu:100 io:true netio:true mating:true fs:true task:edu.utexas.cs.nn.tasks.testmatch.functions.XORTask log:XOR-Control saveTo:Control
