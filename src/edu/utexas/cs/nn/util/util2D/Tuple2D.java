3c8578d2348aeb0499f39e0879f04c4bbcceba99
leg2015 <gillespl@southwestern.edu>
2016-05-23 17:45:57 -0500

    comment and JUnit test for Tuple2D class

--- src/edu/utexas/cs/nn/util/util2D/Tuple2D.java
+++ src/edu/utexas/cs/nn/util/util2D/Tuple2D.java
@@ -9,109 +9,186 @@ import java.awt.geom.Point2D;
  */
 public class Tuple2D extends Point2D.Double implements ILocated2D {
 
-    /**
-     * Construct using x,y coordinates
-     * @param x x-coordinate
-     * @param y y-coordinate
-     */
-    public Tuple2D(double x, double y) {
-        super(x, y);
-    }
+	/**
+	 * added to removed compiler complaints
+	 */
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Is the location at the origin?
-     * @return Whether both coordinates are 0
-     */
-    public boolean isZero() {
-        return (x == 0 && y == 0);
-    }
+	/**
+	 * Construct using x,y coordinates
+	 * 
+	 * @param x
+	 *            x-coordinate
+	 * @param y
+	 *            y-coordinate
+	 */
+	public Tuple2D(double x, double y) {
+		super(x, y);
+	}
 
-    /**
-     * Euclidean distance of (x,y) coordinate from the origin,
-     * a.k.a. length of vector.
-     * @return length of vector to (x,y)
-     */
-    public double length() {
-        return Math.sqrt((x * x) + (y * y));
-    }
+	/**
+	 * Is the location at the origin?
+	 * 
+	 * @return Whether both coordinates are 0
+	 */
+	public boolean isZero() {
+		return (x == 0 && y == 0);
+	}
 
-    /**
-     * Parallel vector with length/magnitude of 1.
-     * @return Corresponding unit vector.
-     */
-    public Tuple2D normalize() {
-        double len = length();
-        return new Tuple2D(x / len, y / len);
-    }
+	/**
+	 * Euclidean distance of (x,y) coordinate from the origin, a.k.a. length of
+	 * vector.
+	 * 
+	 * @return length of vector to (x,y)
+	 */
+	public double length() {
+		return Math.sqrt((x * x) + (y * y));
+	}
 
-    public double angle() {
-        double angle = x == 0 ? Math.PI / 2 : Math.acos(Math.abs(this.normalize().x));
-        if (x < 0) {
-            if (y < 0) {
-                angle = Math.PI + angle;
-            } else if (y > 0) {
-                angle = Math.PI - angle;
-            } else if (y == 0) {
-                angle = Math.PI;
-            }
-        } else if (x > 0) {
-            if (y < 0) {
-                angle = (2 * Math.PI) - angle;
-            }
-        } else {
-            if (y < 0) {
-                angle = (3 * Math.PI) / 2;
-            } else if (y > 0) {
-                angle = Math.PI / 2;
-            } else {
-                angle = 0;
-            }
-        }
-        return angle;
-    }
+	/**
+	 * Parallel vector with length/magnitude of 1.
+	 * 
+	 * @return Corresponding unit vector.
+	 */
+	public Tuple2D normalize() {
+		double len = length();
+		return new Tuple2D(x / len, y / len);
+	}
 
-    public Tuple2D midpoint(Tuple2D position) {
-        return add(position).div(2);
-    }
+	/**
+	 * returns the angle in radians from the positive x-axis to the the tuple2D
+	 * using the math conventions that counterclockwise is positive, clockwise
+	 * is negative and 2PI is the max angle
+	 * 
+	 * @return angle in radians
+	 */
+	public double angle() {
+		double angle = x == 0 ? Math.PI / 2 : Math.acos(Math.abs(this.normalize().x));
+		if (x < 0) {
+			if (y < 0) {
+				angle = Math.PI + angle;
+			} else if (y > 0) {
+				angle = Math.PI - angle;
+			} else if (y == 0) {
+				angle = Math.PI;
+			}
+		} else if (x > 0) {
+			if (y < 0) {
+				angle = (2 * Math.PI) - angle;
+			}
+		} else {
+			if (y < 0) {
+				angle = (3 * Math.PI) / 2;
+			} else if (y > 0) {
+				angle = Math.PI / 2;
+			} else {
+				angle = 0;
+			}
+		}
+		return angle;
+	}
 
-    public Tuple2D add(Tuple2D position) {
-        return new Tuple2D(x + position.x, y + position.y);
-    }
+	/**
+	 * midpoint from other tuple to tuple in quesiton
+	 * 
+	 * @param position
+	 *            other tuple
+	 * @return tuple at midpoint between the two tuples
+	 */
+	public Tuple2D midpoint(Tuple2D position) {
+		return add(position).div(2);
+	}
 
-    public Tuple2D sub(ILocated2D rhs) {
-        return new Tuple2D(this.x - rhs.getX(), this.y - rhs.getY());
-    }
+	/**
+	 * adds two tuples position
+	 * 
+	 * @param position
+	 *            other tuple
+	 * @return tuple that is result of adding two tuples
+	 */
+	public Tuple2D add(Tuple2D position) {
+		return new Tuple2D(x + position.x, y + position.y);
+	}
 
-    public Tuple2D mult(double i) {
-        return new Tuple2D(x * i, y * i);
-    }
+	/**
+	 * subtracts the location of two tuples
+	 * 
+	 * @param rhs
+	 *            location of other ILocated2D
+	 * @return tuple that is result of subtracting two tuples
+	 */
+	public Tuple2D sub(ILocated2D rhs) {
+		return new Tuple2D(this.x - rhs.getX(), this.y - rhs.getY());
+	}
 
-    public Tuple2D div(double i) {
-        return new Tuple2D(x / i, y / i);
-    }
+	/**
+	 * multiplies a tuple by a variable
+	 * 
+	 * @param i
+	 *            number to multiply tuple by
+	 * @return location of multiplied tuple
+	 */
+	public Tuple2D mult(double i) {
+		return new Tuple2D(x * i, y * i);
+	}
 
-    public Tuple2D getPosition() {
-        return this;
-    }
+	/**
+	 * divides a tuple by a variable
+	 * 
+	 * @param i
+	 *            number to divide tuple by
+	 * @return location of divided tuple
+	 */
+	public Tuple2D div(double i) {
+		return new Tuple2D(x / i, y / i);
+	}
 
-    public Tuple2D rotate(double radians) {
-        return new Tuple2D(
-                (x * Math.cos(radians)) - (y * Math.sin(radians)),
-                (x * Math.sin(radians)) + (y * Math.cos(radians)));
-    }
+	/**
+	 * returns a tuple that is identical to tuple in question
+	 */
+	public Tuple2D getPosition() {
+		return this;
+	}
 
-    public double distance(ILocated2D other) {
-        return distance(new Point2D.Double(other.getX(), other.getY()));
-    }
+	/**
+	 * rotates a given tuple around origin by given radians
+	 * 
+	 * @param radians
+	 *            radians to rotate by
+	 * @return rotated tuple
+	 */
+	public Tuple2D rotate(double radians) {
+		return new Tuple2D((x * Math.cos(radians)) - (y * Math.sin(radians)),
+				(x * Math.sin(radians)) + (y * Math.cos(radians)));
+	}
 
-    @Override
-    public String toString() {
-        return "(" + x + "," + y + ")";
-    }
+	/**
+	 * distance from one tuple to another
+	 */
+	public double distance(ILocated2D other) {
+		return distance(new Point2D.Double(other.getX(), other.getY()));
+	}
 
-    public double angleBetweenTargets(ILocated2D a, ILocated2D b) {
-        double angle1 = a.getPosition().sub(this).angle();
-        double angle2 = b.getPosition().sub(this).angle();
-        return Math.abs(angle1 - angle2);
-    }
+	/**
+	 * overrides default toString method
+	 */
+	@Override
+	public String toString() {
+		return "(" + x + "," + y + ")";
+	}
+
+	/**
+	 * angle between two tuples
+	 * 
+	 * @param a
+	 *            first tuple
+	 * @param b
+	 *            second tuple
+	 * @return angle (in radians) between tuples a and b
+	 */
+	public double angleBetweenTargets(ILocated2D a, ILocated2D b) {
+		double angle1 = a.getPosition().sub(this).angle();
+		double angle2 = b.getPosition().sub(this).angle();
+		return Math.abs(angle1 - angle2);
+	}
 }
---
+++ src/edu/utexas/cs/nn/util/util2D/Tuple2DTest.java
@@ -0,0 +1,137 @@
+package edu.utexas.cs.nn.util.util2D;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Tuple2DTest {
+
+	public static final double ERROR = .0001;
+	@Test
+	public void testTuple2D() {
+		Tuple2D test = new Tuple2D(1, 2);
+		assertEquals(test.x, 1, ERROR);
+		assertEquals(test.y,  2, ERROR);
+	}
+
+	@Test
+	public void testIsZero() {
+		Tuple2D test = new Tuple2D(1, 2);
+		Tuple2D otherTest = new Tuple2D(0,0);
+		assertFalse(test.isZero());
+		assertTrue(otherTest.isZero());
+	}
+
+	@Test
+	public void testLength() {
+		Tuple2D test = new Tuple2D(3, 4);
+		assertEquals(test.length(), 5, ERROR);
+	}
+
+	@Test
+	public void testNormalize() {
+		Tuple2D test = new Tuple2D(3, 4);
+		double testX = 3.0/5;
+		double testY = 4.0/5;
+		test = test.normalize();
+		assertEquals(testX, test.x, ERROR);
+		assertEquals(testY, test.y, ERROR);
+	}
+
+	@Test
+	public void testAngle() {
+		Tuple2D test = new Tuple2D(1,1);
+		assertEquals(test.angle(), Math.PI/4, ERROR);
+	}
+
+	@Test
+	public void testMidpoint() {
+		Tuple2D test = new Tuple2D(1,1);
+		Tuple2D zero = new Tuple2D(0,0);
+		test = test.midpoint(zero);
+		Tuple2D otherTest = new Tuple2D(.5, .5);
+		assertEquals(test.x, otherTest.x, ERROR);
+		assertEquals(test.y, otherTest.y, ERROR);
+		assertTrue(test.midpoint(test).equals(otherTest));
+	}
+
+	@Test
+	public void testAdd() {
+		Tuple2D test = new Tuple2D(2, 2);
+		Tuple2D something = new Tuple2D(1,1);
+		test.add(something);
+		Tuple2D otherTest = new Tuple2D(2, 2);
+		assertEquals(test.x, otherTest.x, ERROR);
+		assertEquals(test.y, otherTest.y, ERROR);
+		assertTrue(test.equals(otherTest));
+	}
+
+	@Test
+	public void testSub() {
+		Tuple2D test = new Tuple2D(1, 1);
+		test = test.sub(test);
+		assertTrue(test.isZero());
+	}
+
+	@Test
+	public void testMult() {
+		Tuple2D test = new Tuple2D(2, 3);
+		test = test.mult(2);
+		assertEquals(test.x, 4, ERROR);
+		assertEquals(test.y, 6, ERROR);
+	}
+
+	@Test
+	public void testDiv() {
+		Tuple2D test = new Tuple2D(4, 2);
+		test = test.div(2);
+		assertEquals(test.x, 2, ERROR);
+		assertEquals(test.y, 1, ERROR);
+	}
+
+	@Test
+	public void testGetPosition() {
+		Tuple2D test = new Tuple2D(1, 1);
+		Tuple2D otherTest = test.getPosition();
+		assertTrue(test.equals(otherTest));
+	}
+
+	@Test
+	public void testEquals() {
+		Tuple2D test = new Tuple2D(1, 1);
+		Tuple2D otherTest = new Tuple2D(1, 1);
+		assertTrue(test.equals(otherTest));
+	}
+	@Test
+	public void testRotate() {
+		Tuple2D test = new Tuple2D(1, 0);
+		Tuple2D otherTest = new Tuple2D(1, 0);
+		test.rotate(Math.PI/2);
+		assertEquals(test, otherTest);
+	}
+
+	@Test
+	public void testDistanceILocated2D() {
+		Tuple2D test = new Tuple2D(1, 1);
+		ILocated2D otherTest = new Tuple2D(0,0);
+		assertEquals(test.distance(otherTest), Math.sqrt(2), ERROR);
+	}
+
+	@Test
+	public void testToString() {
+		Tuple2D test = new Tuple2D(1, 1);
+		String tostring = test.toString();
+		String copy = "(1.0,1.0)";
+		assertEquals(tostring, copy);
+	}
+
+	@Test
+	public void testAngleBetweenTargets() {
+		Tuple2D test = new Tuple2D(0,0);
+		Tuple2D a = new Tuple2D(1,1);
+		Tuple2D b = new Tuple2D(1,0);
+		double angle = test.angleBetweenTargets(a, b);
+		assertEquals(angle, Math.PI/4, ERROR);
+	}
+
+}
