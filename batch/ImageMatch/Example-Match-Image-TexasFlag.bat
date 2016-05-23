bf16c2eaf0ba01e6ccb283c0f1c9319a90e807d7
leg2015 <gillespl@southwestern.edu>
2016-05-23 12:00:56 -0500

    moved imagematch batch files to its own file
    cleaned up directory by moving all imagematch tasks to a separate folder

--- batch/ImageMatch/Example-Match-Image-TexasFlag.bat
+++ batch/ImageMatch/Example-Match-Image-TexasFlag.bat
@@ -1,1 +1,3 @@
+cd ..
+cd ..
 java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:match trials:1 maxGens:50 mu:100 io:true netio:true mating:true fs:false task:edu.utexas.cs.nn.tasks.testmatch.imagematch.ImageMatchTask log:Image-TexasFlag saveTo:TexasFlag matchImageFile:TEXASSSSSS.png allowMultipleFunctions:true ftype:0 watch:false netChangeActivationRate:0.3 saveAllChampions:true
--- batch/ImageMatch/Example-Match-Image-sunset1.bat
+++ batch/ImageMatch/Example-Match-Image-sunset1.bat
@@ -1,1 +1,3 @@
+cd ..
+cd ..
 java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:match trials:1 maxGens:1000 mu:100 io:true netio:true mating:true fs:false task:edu.utexas.cs.nn.tasks.testmatch.imagematch.ImageMatchTask log:Image-sunset1 saveTo:sunset1 matchImageFile:sunset1.png allowMultipleFunctions:true ftype:0 watch:false netChangeActivationRate:0.3 overrideImageSize:false imageHeight:200 imageWidth:300 saveAllChampions:true
