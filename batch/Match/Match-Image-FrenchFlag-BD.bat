72353b8251dfdaffcf609562e0ae0906bb0dd8ee
leg2015 <gillespl@southwestern.edu>
2016-05-24 14:16:00 -0500

    Added behavioral diversity batch files to image match batch files
    also changed naming scheme for batch files

---
+++ batch/Match/Match-Image-FrenchFlag-BD.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:match trials:1 maxGens:500 mu:100 io:true netio:true mating:true fs:false task:edu.utexas.cs.nn.tasks.testmatch.imagematch.ImageMatchTask log:FrenchFlag-BD saveTo:BD matchImageFile:TeenyTinyFrenchFlag.png allowMultipleFunctions:true ftype:0 watch:false netChangeActivationRate:0.3 overrideImageSize:true imageHeight:198 imageWidth:300 ea:edu.utexas.cs.nn.evolution.nsga2.bd.BDNSGA2 behaviorCharacterization:edu.utexas.cs.nn.evolution.nsga2.bd.characterizations.GeneralNetworkCharacterization
