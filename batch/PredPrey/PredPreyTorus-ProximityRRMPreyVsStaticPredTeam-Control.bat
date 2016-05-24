69ccf2a4689ff8829eec012200365421687d6c6a
Rollinsa <rollinsa@southwestern.edu>
2016-05-24 16:44:57 -0500

    Added batch files for new proximity sensor mode

---
+++ batch/PredPrey/PredPreyTorus-ProximityRRMPreyVsStaticPredTeam-Control.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:torus trials:10 maxGens:200 mu:100 io:true netio:true mating:false fs:false task:edu.utexas.cs.nn.tasks.gridTorus.TorusEvolvedPreyVsStaticPredatorsTask log:RRMPreyVsStaticPredTeam-Control saveTo:Control allowDoNothingActionForPredators:true torusPreys:2 torusPredators:3 staticPredatorController:edu.utexas.cs.nn.gridTorus.controllers.AggressivePredatorController preyRRM:true preyMaximizeTotalTime:false torusSenseByProximity:true torusSenseTeammates:true
