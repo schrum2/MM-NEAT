20f48c791f3e1c41f1ce5e9882331cecd6473f1d
leg2015 <gillespl@southwestern.edu>
2016-05-24 16:32:35 -0500

    fiddled some more with image match batch files
    added in different files for whether or not behavioral diversity is to
    be used

--- batch/Match/Match-Image-sunset2-Control.bat
+++ batch/Match/Match-Image-sunset2-Control.bat
@@ -1,3 +1,3 @@
 cd ..
 cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:match trials:1 maxGens:1000 mu:100 io:true netio:true mating:true fs:false task:edu.utexas.cs.nn.tasks.testmatch.imagematch.ImageMatchTask log:Image-sunset2 saveTo:sunset2 matchImageFile:sunset2.png allowMultipleFunctions:true ftype:0 watch:false netChangeActivationRate:0.3 overrideImageSize:false imageHeight:133 imageWidth:200 saveAllChampions:true
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:sunset2 trials:1 maxGens:1000 mu:100 io:true netio:true mating:true fs:false task:edu.utexas.cs.nn.tasks.testmatch.imagematch.ImageMatchTask log:sunset2-Control saveTo:Control matchImageFile:sunset2.png allowMultipleFunctions:true ftype:0 watch:false netChangeActivationRate:0.3 overrideImageSize:false imageHeight:133 imageWidth:200 saveAllChampions:true
