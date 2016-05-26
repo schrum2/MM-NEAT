d48e39ccfeffacd9a95ac1fc5996dc44e1f9442f
Gab <Serenamidori@gmail.com>
2016-05-26 16:53:03 -0500

    Methods added to VizDoom
    These methods added are used to get row information for inputs without
    using magic numbers. Additionally, it's been found that a good number of
    screen resolutions won't work with these methods, magic numbers or
    otherwise, so they are listed in asserts that they should not be used.

--- src/edu/utexas/cs/nn/tasks/vizdoom/VizDoomBasicShootTask.java
+++ src/edu/utexas/cs/nn/tasks/vizdoom/VizDoomBasicShootTask.java
@@ -37,9 +37,9 @@ public class VizDoomBasicShootTask<T extends Network> extends VizDoomTask<T> {
     	MMNEAT.loadClasses();
     	VizDoomBasicShootTask<TWEANN> vd = new VizDoomBasicShootTask<TWEANN>();
     	TWEANNGenotype individual = new TWEANNGenotype();
-    	System.out.println(CommonConstants.trials);
     	System.out.println(vd.evaluate(individual));
     	System.out.println(vd.evaluate(individual));
+    	vd.finalCleanup();
     }
 
 	@Override
@@ -49,7 +49,56 @@ public class VizDoomBasicShootTask<T extends Network> extends VizDoomTask<T> {
 
 	@Override
 	public double[] getInputs(GameState s) {
-		// TODO Auto-generated method stub
-		return null;
+		int width = game.getScreenWidth();
+		int height = game.getScreenHeight();
+		int row = 61; // MAGIC NUMBER, change when introducing ratio method in VizDoomTask
+		int color = RED_INDEX;
+    	double[] result = new double[width];
+    	int index = row * width * 3;
+		for(int y = 0; y < height; y++) {
+        	int bufferPos = 0;
+        	for(int x = 0; x < width; x++) {
+        		int c = index + bufferPos + color;
+                result[x] = (s.imageBuffer[c]) / 255.0;
+        		bufferPos += 3;
+        	}
+        }
+		return result;
+	}
+
+	/**
+	 * This method takes the given game information to send back the appropriate row number to get the inputs from.
+	 * This is done based on Screen Resolution ratios. The calculations are hard coded, but tested and gave reliable rows when displayed.
+	 */
+	@Override
+	public int getRow() {
+		float first;
+    	int second = 0;
+		if(game.getScreenWidth()/4 == game.getScreenHeight()/3){ // ratio is 4:3
+			System.out.println("Ratio is 4:3");
+			first = (float) (game.getScreenWidth() * 0.3825);
+    		System.out.println(first);
+    		second = Math.round(first);    		
+    		System.out.println(second);
+    	} else if(game.getScreenWidth()/16 == game.getScreenHeight()/10){ // ratio is 16:10
+    		System.out.println("Ratio is 16:10");
+			first = (float) (game.getScreenWidth() * 0.32); // CHANGE THIS
+    		System.out.println(first);
+    		second = Math.round(first);    		
+    		System.out.println(second);
+    	} else if(game.getScreenWidth()/16 == game.getScreenHeight()/9){ // ratio is 16:9
+    		System.out.println("Ratio is 16:9");
+			first = (float) (game.getScreenWidth() * 0.29); // CHANGE THIS
+    		System.out.println(first);
+    		second = Math.round(first);    		
+    		System.out.println(second);
+    	} else { // ratio is 5:4
+    		System.out.println("Ratio is 5:4");
+			first = (float) (game.getScreenWidth() * 0.41); // CHANGE THIS
+    		System.out.println(first);
+    		second = Math.round(first);    		
+    		System.out.println(second);
+    	} 
+		return second;
 	}
 }
