c8c6479e325fbdf6fd4637448c2a933552769247
leg2015 <gillespl@southwestern.edu>
2016-06-02 13:58:40 -0500

    Added package for new evolutionary algorithm for picbreeder task

---
+++ src/edu/utexas/cs/nn/evolution/selectiveBreeding/SelectiveBreedingEA.java
@@ -0,0 +1,11 @@
+package edu.utexas.cs.nn.evolution.selectiveBreeding;
+
+import edu.utexas.cs.nn.evolution.SinglePopulationGenerationalEA;
+
+public class SelectiveBreedingEA implements SinglePopulationGenerationalEA<T> {
+
+	public SelectiveBreedingEA() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
