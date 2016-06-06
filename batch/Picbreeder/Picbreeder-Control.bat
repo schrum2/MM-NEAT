7bc9f7a4c4c5db216fea9ab09ee5fb6d3790867c
leg2015 <gillespl@southwestern.edu>
2016-06-06 14:47:33 -0500

    changed batch files

--- batch/Picbreeder/Picbreeder-Control.bat
+++ batch/Picbreeder/Picbreeder-Control.bat
@@ -1,2 +1,3 @@
 cd ..
 cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:picbreeder trials:1 mu:20 maxGens:500 io:true netio:true mating:true fs:false task:edu.utexas.cs.nn.tasks.picbreeder.PicbreederTask log:Picbreeder-Control saveTo:Control allowMultipleFunctions:true ftype:0 watch:false netChangeActivationRate:0.3 recurrency:false ea:edu.utexas.cs.nn.evolution.selectiveBreeding.SelectiveBreedingEA
