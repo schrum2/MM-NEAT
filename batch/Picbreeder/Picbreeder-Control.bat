803e3b676c86cd5e2ebe451eb41ca394e4f36a49
leg2015 <gillespl@southwestern.edu>
2016-06-06 14:47:15 -0500

    okay got the batch file to work

--- batch/Picbreeder/Picbreeder-Control.bat
+++ batch/Picbreeder/Picbreeder-Control.bat
@@ -1,3 +1,2 @@
 cd ..
 cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:picbreeder trials:1 mu:20 maxGens:500 io:true netio:true mating:true fs:false task:edu.utexas.cs.nn.tasks.picbreeder.PicbreederTask log:Picbreeder-Control saveTo:Control allowMultipleFunctions:true ftype:0 watch:false netChangeActivationRate:0.3 recurrency:false saveAllChampions:true logTWEANNData:true logMutationAndLineage:true ea:edu.utexas.cs.nn.evolution.selectiveBreeding.SelectiveBreedingEA
