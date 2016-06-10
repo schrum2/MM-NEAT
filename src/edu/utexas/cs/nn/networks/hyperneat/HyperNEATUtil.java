85ea347f55788c5cac7ec83e26f5036986554216
leg2015 <gillespl@southwestern.edu>
2016-06-10 16:39:36 -0500

    comments for hyperneatutil and test

--- src/edu/utexas/cs/nn/networks/hyperneat/HyperNEATUtil.java
+++ src/edu/utexas/cs/nn/networks/hyperneat/HyperNEATUtil.java
@@ -17,16 +17,38 @@ import edu.utexas.cs.nn.util.datastructures.Pair;
 //draw grid on substrate
 //every time refreshed, draw new rectangles on top of old rectangles
 //need a variable that scales rectangles down just a bit so it doesn't erase grid
-
+/**
+ * Util class containing methods used by hyperNEAT and its tasks
+ * 
+ * @author Lauren Gillespie
+ *
+ */
 public class HyperNEATUtil {
 
+	//size of grid in substrate drawing. Can be changed/turned into a param if need be
 	public final static int SUBS_GRID_SIZE = 30;
 
+	/**
+	 * Draws substrate as a grid with squares representing each substrate coordinate
+	 * @param s substrate
+	 * @param nodes list of substrate nodes TODO: use to extract color of square
+	 * @param c color of squares
+	 * @return drawing panel containing substrate drawing
+	 */
 	public static DrawingPanel drawSubstrate(Substrate s, ArrayList<Node> nodes, Color c) { 
 		DrawingPanel p = new DrawingPanel(s.size.t1 * SUBS_GRID_SIZE, s.size.t2 * SUBS_GRID_SIZE, s.name);
 		return drawSubstrate(p, s, nodes, c); // updates existing panel
 	}
 
+	/**
+	 * Overloaded. Draws substrate as a grid with squares representing each substrate coordinate
+	 *  
+	 * @param dp drawing panel which substrate is going to be drawn onto
+	 * @param s substate in question
+	 * @param nodes list of substrate nodes TODO: use to extract color of square
+	 * @param c color
+	 * @return drawing panel containing drawing of substrate
+	 */
 	// Call inside of TWEANN.process at end
 	public static DrawingPanel drawSubstrate(DrawingPanel dp, Substrate s, ArrayList<Node> nodes, Color c) { 
 		for(int i = 0; i < s.size.t1; i ++) {
@@ -37,6 +59,12 @@ public class HyperNEATUtil {
 		}
 		return dp;
 	}
+	
+	/**
+	 * Draws grid around squares so they are more easily distinguishable
+	 * @param p drawingPanel
+	 * @param size size of substrate
+	 */
 	private static void drawGrid(DrawingPanel p, Pair<Integer, Integer> size) {
 		// Loop through columns and rows to draw black lines
 		p.getGraphics().setBackground(Color.white);
@@ -48,6 +76,12 @@ public class HyperNEATUtil {
 		}	
 	}
 
+	/**
+	 * Draws squares representing a coordinate location from substrate
+	 * @param p drawing panel
+	 * @param size size of substrate
+	 * @param c color of square
+	 */
 	private static void drawCoord(DrawingPanel p, Pair<Integer, Integer> size, Color c) { 
 		for(int i = 0; i <= size.t1; i++) {
 			for(int j = 0; j <= size.t2; j++) {
