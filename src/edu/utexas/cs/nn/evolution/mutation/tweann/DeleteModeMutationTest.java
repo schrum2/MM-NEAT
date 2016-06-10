8e7599e6c5a082ff9e8589154c20e55cb56bfdb8
leg2015 <gillespl@southwestern.edu>
2016-06-10 11:08:09 -0500

    more junit tests

--- src/edu/utexas/cs/nn/evolution/mutation/tweann/DeleteModeMutationTest.java
+++ src/edu/utexas/cs/nn/evolution/mutation/tweann/DeleteModeMutationTest.java
@@ -1,24 +1,47 @@
 package edu.utexas.cs.nn.evolution.mutation.tweann;
 
-import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.utexas.cs.nn.MMNEAT.MMNEAT;
+import edu.utexas.cs.nn.evolution.EvolutionaryHistory;
+import edu.utexas.cs.nn.evolution.genotypes.TWEANNGenotype;
+import edu.utexas.cs.nn.parameters.Parameters;
+
 public class DeleteModeMutationTest {
 
+	TWEANNGenotype tg1;
+	TWEANNGenotype tg2;
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception {Parameters.initializeParameterCollections(new String[] { "io:false", "netio:false", "recurrency:false", "mmdRate:0.1", "deleteLinkRate:0.0" });
+	MMNEAT.loadClasses();
+	tg1 = new TWEANNGenotype(5, 3, 0);
+	MMNEAT.genotype = tg1.copy();
+	EvolutionaryHistory.initArchetype(0);
+
+	final int MUTATIONS1 = 30;
+
+	for (int i = 0; i < MUTATIONS1; i++) {
+		tg1.mutate();
+	}
+	tg2 = (TWEANNGenotype) tg1.copy();
+	tg1.moduleDuplication();
+	tg1.moduleDuplication();
 	}
 
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception {	
+	MMNEAT.clearClasses();
+	tg1 = null;
+	tg2 = null;
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		System.out.println("no longer a useful mutation");
 	}
 
 }
--- src/edu/utexas/cs/nn/evolution/mutation/tweann/FullyConnectedModuleMutationTest.java
+++ src/edu/utexas/cs/nn/evolution/mutation/tweann/FullyConnectedModuleMutationTest.java
@@ -6,16 +6,32 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.utexas.cs.nn.MMNEAT.MMNEAT;
+import edu.utexas.cs.nn.evolution.EvolutionaryHistory;
+import edu.utexas.cs.nn.evolution.genotypes.TWEANNGenotype;
+import edu.utexas.cs.nn.parameters.Parameters;
+
 public class FullyConnectedModuleMutationTest {
 
+	TWEANNGenotype tg1;
+	TWEANNGenotype tg2;
+	
 	@Before
-	public void setUp() throws Exception {
-	}
+	public void setUp() throws Exception {Parameters.initializeParameterCollections(new String[] { "io:false", "netio:false", "recurrency:false", "mmdRate:0.1", "deleteLinkRate:0.0" });
+	MMNEAT.loadClasses();
+	tg1 = new TWEANNGenotype(5, 3, 0);
+	MMNEAT.genotype = tg1.copy();
+	EvolutionaryHistory.initArchetype(0);
 
-	@After
-	public void tearDown() throws Exception {
-	}
+	final int MUTATIONS1 = 30;
 
+	for (int i = 0; i < MUTATIONS1; i++) {
+		tg1.mutate();
+	}
+	tg2 = (TWEANNGenotype) tg1.copy();
+	tg1.moduleDuplication();
+	tg1.moduleDuplication();
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");
