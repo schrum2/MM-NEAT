c3839ffcbf356b8b12f46982260d567fb98d795e
Rollinsa <rollinsa@southwestern.edu>
2016-06-13 11:36:59 -0500

    Generalized launchMultiple
    Generalized the launch multiple batch file so that the user specifies
    the starting and ending run numbers. Also allowed the batch file to be
    able to handle two different sets of experiments in one run. Now I will
    create a batch file that will do all of the same things as this one,
    except that it will run the experiments asynchronously rather than
    simultaneously

--- launchMultiple.bat
+++ launchMultiple.bat
@@ -1,9 +1,15 @@
-REM Usage  : launchMultiple.bat <batch subdir> <batch file> <num runs>
-REM example: launchMultiple.bat RLGlue RLGlue-CartPole.bat 3
-set /a "last=%3-1"
-FOR /L %%A IN (0,1,%last%) DO (
+REM Usage  : launchMultiple.bat <batch subdir> <batch file> <starting run> <ending run> <batch subdir> <batch file> <starting run> <ending run>
+REM example: launchMultiple.bat RLGlue RLGlue-CartPole.bat 0 2
+REM example: launchMultiple.bat RLGlue RLGlue-CartPole.bat 0 2 PredPrey TorusPredPrey-TorusPred-CoOpMultiCCQ.bat 1 7
+REM example: launchMultiple.bat PredPrey TorusPredPrey-TorusPred-CoOpMultiCCQ.bat 0 9 PredPrey TorusPredPrey-TorusPred-CoOpMultiCCQ.bat 10 19
+set argCount=0
+for %%x in (%*) do set /A argCount+=1
   cd batch
   cd %1
-  %2 %%A
-)
+FOR /L %%A IN (%3,1,%4) DO %2 %%A
+IF /I %argCount% EQU 8 cd ..
+IF /I %argCount% EQU 8 cd %5
+IF /I %argCount% EQU 8 FOR /L %%x IN (%7,1,%8) DO %6 %%x
+cd ..
+cd ..
 ECHO "All done!"
