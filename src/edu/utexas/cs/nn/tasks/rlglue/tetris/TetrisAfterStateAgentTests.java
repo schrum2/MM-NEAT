beb8846753389a076fe231e5c46a3b6b004128d2
Gab <Serenamidori@gmail.com>
2016-06-13 11:28:31 -0500

    Added to Action Sequence JUnit testing
    Currently testing for correct actionList being returned and used for
    each action, but can't figure out how to test that actions are actually
    being taken as the state never changes.

--- src/edu/utexas/cs/nn/tasks/rlglue/tetris/TetrisAfterStateAgentTests.java
+++ src/edu/utexas/cs/nn/tasks/rlglue/tetris/TetrisAfterStateAgentTests.java
@@ -10,7 +10,11 @@ import org.rlcommunity.rlglue.codec.types.Observation;
 
 import edu.utexas.cs.nn.MMNEAT.MMNEAT;
 import edu.utexas.cs.nn.evolution.EvolutionaryHistory;
+import edu.utexas.cs.nn.evolution.genotypes.TWEANNGenotype;
+import edu.utexas.cs.nn.networks.Network;
+import edu.utexas.cs.nn.parameters.CommonConstants;
 import edu.utexas.cs.nn.parameters.Parameters;
+import edu.utexas.cs.nn.tasks.rlglue.RLGlueTask;
 import edu.utexas.cs.nn.tasks.rlglue.featureextractors.tetris.BertsekasTsitsiklisTetrisExtractor;
 
 public class TetrisAfterStateAgentTests {
@@ -26,6 +30,8 @@ public class TetrisAfterStateAgentTests {
 		EvolutionaryHistory.setInnovation(0);
 		EvolutionaryHistory.setHighestGenotypeId(0);
 		Parameters.initializeParameterCollections(new String[] { "io:false", "netio:false", "recurrency:false",
+				"rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris",
+				"task:edu.utexas.cs.nn.tasks.rlglue.tetris.TetrisTask", "rlGlueAgent:edu.utexas.cs.nn.tasks.rlglue.tetris.TetrisAfterStateAgent",
 				"rlGlueExtractor:edu.utexas.cs.nn.tasks.rlglue.featureextractors.tetris.BertsekasTsitsiklisTetrisExtractor" });
 		MMNEAT.loadClasses();
 	}
@@ -77,16 +83,59 @@ public class TetrisAfterStateAgentTests {
 
 	@Test
 	public void action_sequence() {
+		CommonConstants.randomArgMaxTieBreak = false;
 		TetrisAfterStateAgent afterStateAgent = new TetrisAfterStateAgent();
 		TetrisState testState = new TetrisState();
+		afterStateAgent.policy = new Network() {
+			@Override
+			public int numInputs() {
+				return MMNEAT.networkInputs;
+			}
+			@Override
+			public int numOutputs() {
+				return MMNEAT.networkOutputs;
+			}
+			@Override
+			public int effectiveNumOutputs() {
+				return MMNEAT.networkOutputs;
+			}
+			@Override
+			public double[] process(double[] inputs) {
+				return new double[]{1};
+			}
+			@Override
+			public void flush() {
+			}
+			@Override
+			public boolean isMultitask() {
+				return false;
+			}
+			@Override
+			public void chooseMode(int mode) {				
+			}
+			@Override
+			public int lastModule() {
+				return 0;
+			}
+			@Override
+			public double[] moduleOutput(int mode) {
+				return null;
+			}
+			@Override
+			public int numModules() {
+				return 1;
+			}
+			@Override
+			public int[] getModuleUsage() {
+				return null;
+			}
+		};
 		BertsekasTsitsiklisTetrisExtractor BTTE = new BertsekasTsitsiklisTetrisExtractor();
 		// line piece
 		testState.worldState[166] = 1;
 		testState.worldState[167] = 1;
 		testState.worldState[168] = 1;
 		testState.worldState[169] = 1;
-		testState.currentX += 2;
-		testState.currentY += 14;
 		// S piece
 		testState.worldState[171] = 1;
 		testState.worldState[172] = 1;
@@ -107,11 +156,45 @@ public class TetrisAfterStateAgentTests {
 		testState.worldState[185] = 1;
 		testState.worldState[175] = 1;
 		testState.worldState[186] = 1;
-
+		//piece at spawn
+		
+		System.out.println("x: " + testState.currentX);
+		System.out.println("y: " + testState.currentY);
 		Observation o = testState.get_observation();
 		assertTrue(afterStateAgent.currentActionList.isEmpty());
 		Action a = afterStateAgent.getAction(o);
 		assertFalse(afterStateAgent.currentActionList.isEmpty());
-
+		assertEquals(afterStateAgent.currentActionList.size(), 4);
+		assertEquals(a.getInt(0), TetrisState.RIGHT); // 1
+		testState = afterStateAgent.observationToTetrisState(o);
+		System.out.println("x: " + testState.currentX);
+		System.out.println("y: " + testState.currentY);
+		
+		Action b = afterStateAgent.getAction(o);
+		assertEquals(afterStateAgent.currentActionList.size(), 3);
+		assertEquals(b.getInt(0), TetrisState.RIGHT); // 1
+		testState = afterStateAgent.observationToTetrisState(o);
+		System.out.println("x: " + testState.currentX);
+		System.out.println("y: " + testState.currentY);
+		
+		Action c = afterStateAgent.getAction(o);
+		assertEquals(afterStateAgent.currentActionList.size(), 2);
+		assertEquals(c.getInt(0), TetrisState.CW); // 2
+		testState = afterStateAgent.observationToTetrisState(o);
+		System.out.println("x: " + testState.currentX);
+		System.out.println("y: " + testState.currentY);
+		
+		Action d = afterStateAgent.getAction(o);
+		assertEquals(afterStateAgent.currentActionList.size(), 1);
+		assertEquals(d.getInt(0), TetrisState.FALL); // 5
+		System.out.println("x: " + testState.currentX);
+		System.out.println("y: " + testState.currentY);
+		
+		Action e = afterStateAgent.getAction(o);
+		assertEquals(afterStateAgent.currentActionList.size(), 0);
+		assertEquals(e.getInt(0), TetrisState.NONE); // 4
+		System.out.println("x: " + testState.currentX);
+		System.out.println("y: " + testState.currentY);
+		
 	}
 }
