fd9161c8248a49b8c582c6ca8e134fef072f9750
Gab <Serenamidori@gmail.com>
2016-06-14 15:08:37 -0500

    Added VizDoom batch files for testing
    Added a variety of batch files that can be copied or deleted later,
    these are currently being tested on the server

--- batch/VizDoom/VizDoom-Basic-Control.bat
+++ batch/VizDoom/VizDoom-Basic-Control.bat
@@ -1,3 +1,3 @@
 cd ..
 cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoombasic trials:10 maxGens:100 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomBasicShootTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:Basic-Control saveTo:Control gameWad:freedoom2.wad watch:false doomFullScreenInput:true stepByStepVizDoom:false
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoombasic trials:10 maxGens:100 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomBasicShootTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:Basic-Control saveTo:Control gameWad:freedoom2.wad watch:false stepByStepVizDoom:false
---
+++ batch/VizDoom/VizDoom-TakeCover-SmudgeSmall.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoomtakecover trials:10 maxGens:100 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomTakeCoverTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:TakeCover-Control saveTo:Control gameWad:freedoom2.wad watch:false stepByStepVizDoom:false doomEpisodeLength:10000 doomInputPixelSmudge:2
