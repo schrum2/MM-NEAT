4c9ef6e5fe5c9efb401489eaf881b66de1aa6a73
leg2015 <gillespl@southwestern.edu>
2016-06-15 16:46:43 -0500

    added some more junit tests to tweann mutation package

--- src/edu/utexas/cs/nn/evolution/mutation/tweann/MeltThenFreezePolicyMutationTest.java
+++ src/edu/utexas/cs/nn/evolution/mutation/tweann/MeltThenFreezePolicyMutationTest.java
@@ -6,19 +6,41 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.utexas.cs.nn.MMNEAT.MMNEAT;
+import edu.utexas.cs.nn.evolution.EvolutionaryHistory;
+import edu.utexas.cs.nn.evolution.genotypes.TWEANNGenotype;
+import edu.utexas.cs.nn.evolution.genotypes.TWEANNGenotype.NodeGene;
+import edu.utexas.cs.nn.parameters.Parameters;
+
 public class MeltThenFreezePolicyMutationTest {
 
+	TWEANNGenotype tg1;
+	MeltThenFreezePolicyMutation mtfpm;
 	@Before
 	public void setUp() throws Exception {
+		Parameters.initializeParameterCollections(new String[] { "io:false", "netio:false", "recurrency:false", "freezePolicyRate:1.0"});
+		MMNEAT.loadClasses();
+		tg1 = new TWEANNGenotype(MMNEAT.networkInputs, MMNEAT.networkOutputs, 0);
+		mtfpm = new MeltThenFreezePolicyMutation();
+		MMNEAT.genotype = tg1.copy();
+		EvolutionaryHistory.initArchetype(0);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		tg1 = null;
+		mtfpm = null;
+		MMNEAT.clearClasses();
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		mtfpm.mutate(tg1);
+		int x = 0;
+		for(NodeGene node : tg1.nodes) {
+			if(node.frozen) x++;
+		}
+		assertTrue(x != 0);
 	}
 
 }
--- src/edu/utexas/cs/nn/evolution/mutation/tweann/MeltThenFreezePreferenceMutationTest.java
+++ src/edu/utexas/cs/nn/evolution/mutation/tweann/MeltThenFreezePreferenceMutationTest.java
@@ -7,23 +7,37 @@ import org.junit.Before;
 import org.junit.Test;
 
 import edu.utexas.cs.nn.MMNEAT.MMNEAT;
+import edu.utexas.cs.nn.evolution.genotypes.TWEANNGenotype;
 import edu.utexas.cs.nn.parameters.Parameters;
 
 public class MeltThenFreezePreferenceMutationTest {
 
+	TWEANNGenotype tg1;
+	MeltThenFreezePreferenceMutation mtfpm;
+	
 	@Before
 	public void setUp() throws Exception {
-		Parameters.initializeParameterCollections(new String[] { "io:false", "netio:false", "recurrency:false", "freezePreferenceRate"});
+		Parameters.initializeParameterCollections(new String[] { "io:false", "netio:false", "recurrency:false", "freezePreferenceRate:1.0"});
 		MMNEAT.loadClasses();
+		tg1 = new TWEANNGenotype(MMNEAT.networkInputs, MMNEAT.networkOutputs, 0);
+		mtfpm = new MeltThenFreezePreferenceMutation();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		tg1 = null;
+		mtfpm = null;
+		MMNEAT.clearClasses();
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		mtfpm.mutate(tg1);
+		int firstPreference = tg1.outputStartIndex() + tg1.neuronsPerModule;
+		for(int i = 0; i < firstPreference; i++) {
+			assertFalse(tg1.nodes.get(i).frozen);
+		}
+		assertTrue(tg1.nodes.get(firstPreference).frozen);
 	}
 
 }
