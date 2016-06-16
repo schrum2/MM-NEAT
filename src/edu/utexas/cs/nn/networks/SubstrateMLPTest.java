b356faa34930313fdc2c6f389b28a25df9a4cbdf
leg2015 <gillespl@southwestern.edu>
2016-06-16 15:22:24 -0500

    added to test

--- src/edu/utexas/cs/nn/networks/SubstrateMLPTest.java
+++ src/edu/utexas/cs/nn/networks/SubstrateMLPTest.java
@@ -13,6 +13,7 @@ import edu.utexas.cs.nn.networks.hyperneat.HyperNEATDummyTask;
 import edu.utexas.cs.nn.networks.hyperneat.Substrate;
 import edu.utexas.cs.nn.parameters.Parameters;
 import edu.utexas.cs.nn.util.datastructures.Pair;
+import edu.utexas.cs.nn.util.datastructures.Triple;
 public class SubstrateMLPTest {
 
 	SubstrateMLP mlp;
@@ -55,7 +56,7 @@ public class SubstrateMLPTest {
 		System.out.println("outputs: " + Arrays.toString(outputs));
 		assertTrue(outputs.length == 4);
 	}
-	
+
 	@Test
 	public void testComplexMLP() {
 		try {
@@ -67,11 +68,60 @@ public class SubstrateMLPTest {
 		MMNEAT.loadClasses();
 		hcppn = new HyperNEATCPPNGenotype();
 		task = (HyperNEATDummyTask<?>) MMNEAT.task;
-		
-		
+
+		Substrate input1 = new Substrate(new Pair<Integer, Integer>(10, 15), Substrate.INPUT_SUBSTRATE, new Triple<Integer, Integer, Integer>(0, 0, 0), "I_0");
+		Substrate input2 = new Substrate(new Pair<Integer, Integer>(20, 7), Substrate.INPUT_SUBSTRATE, new Triple<Integer, Integer, Integer>(2, 0, 0), "I_1");
+		Substrate process01 = new Substrate(new Pair<Integer, Integer>(5, 5), Substrate.PROCCESS_SUBSTRATE, new Triple<Integer, Integer, Integer>(-1, 2, 0), "P_0_0");
+		Substrate process02 = new Substrate(new Pair<Integer, Integer>(5, 5), Substrate.PROCCESS_SUBSTRATE, new Triple<Integer, Integer, Integer>(1, 2, 0), "P_0_1");
+		Substrate process03 = new Substrate(new Pair<Integer, Integer>(8, 8), Substrate.PROCCESS_SUBSTRATE, new Triple<Integer, Integer, Integer>(3, 2, 0), "P_0_2");
+		Substrate process11 = new Substrate(new Pair<Integer, Integer>(2, 3), Substrate.PROCCESS_SUBSTRATE, new Triple<Integer, Integer, Integer>(0, 4, 0), "P_1_0");
+		Substrate process12 = new Substrate(new Pair<Integer, Integer>(4, 7), Substrate.PROCCESS_SUBSTRATE, new Triple<Integer, Integer, Integer>(2, 4, 0), "P_1_1");
+		Substrate output1 = new Substrate(new Pair<Integer, Integer>(4, 4), Substrate.OUTPUT_SUBSTRATE, new Triple<Integer, Integer, Integer>(1, 6, 0), "O_0");
 		ArrayList<Substrate> subs = new ArrayList<Substrate>();
+		subs.add(input1);
+		subs.add(input2);
+		subs.add(process01);
+		subs.add(process02);
+		subs.add(process03);
+		subs.add(process11);
+		subs.add(process12);
+		subs.add(output1);
+		Pair<String, String> conn1 = new Pair<String, String>("I_0", "P_0_0");
+		Pair<String, String> conn2 = new Pair<String, String>("I_1", "P_0_0");
+		Pair<String, String> conn3 = new Pair<String, String>("I_0", "P_0_1");
+		Pair<String, String> conn4 = new Pair<String, String>("I_1", "P_0_1");
+		Pair<String, String> conn5 = new Pair<String, String>("I_0", "P_0_2");
+		Pair<String, String> conn6 = new Pair<String, String>("I_1", "P_0_2");
+		Pair<String, String> conn7 = new Pair<String, String>("P_0_0", "P_1_0");
+		Pair<String, String> conn8 = new Pair<String, String>("P_0_0", "P_1_1");
+		Pair<String, String> conn9 = new Pair<String, String>("P_0_1", "P_1_0");
+		Pair<String, String> conn10 = new Pair<String, String>("P_0_1", "P_1_1");
+		Pair<String, String> conn11 = new Pair<String, String>("P_0_2", "P_1_0");
+		Pair<String, String> conn12 = new Pair<String, String>("P_0_2", "P_1_1");
+		Pair<String, String> conn13 = new Pair<String, String>("P_1_0", "O_0");
+		Pair<String, String> conn14 = new Pair<String, String>("P_1_1", "O_0");
 		ArrayList<Pair<String, String>> connections = new ArrayList<Pair<String, String>>();
+		connections.add(conn1);
+		connections.add(conn2);
+		connections.add(conn3);
+		connections.add(conn4);
+		connections.add(conn5);
+		connections.add(conn6);
+		connections.add(conn7);
+		connections.add(conn8);
+		connections.add(conn9);
+		connections.add(conn10);
+		connections.add(conn11);
+		connections.add(conn12);
+		connections.add(conn13);
+		connections.add(conn14);
+		double[] inputs = new double[10 * 15 + 20 * 7];
+		for(int i = 0; i < inputs.length; i++) {
+			inputs[i] = i;
+		}
 		mlp = new SubstrateMLP(subs, connections,  hcppn.getCPPN());
+		double[] outputs = mlp.process(inputs);
+		assertTrue(outputs.length == 16);
 	}
 
 }
