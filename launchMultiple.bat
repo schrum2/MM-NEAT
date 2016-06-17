b1844d854498cb5d254c8439d1b4663da8350a75
Rollinsa <rollinsa@southwestern.edu>
2016-06-17 09:38:06 -0500

    Fixed errors with changing directories in launchMultiple batch files. Restarted server experiments

--- launchMultiple.bat
+++ launchMultiple.bat
@@ -1,15 +1,9 @@
-REM Usage  : launchMultiple.bat <batch subdir> <batch file> <starting run> <ending run> <batch subdir> <batch file> <starting run> <ending run>
+REM Usage  : launchMultiple.bat <batch subdir> <batch file> <starting run> <ending run> 
 REM example: launchMultiple.bat RLGlue RLGlue-Tetris-ExtendedBT.bat 0 2
-REM example: launchMultiple.bat RLGlue RLGlue-Tetris-ExtendedBT.bat 0 2 PredPrey TorusPredPrey-TorusPred-CoOpMultiCCQ.bat 1 7
-REM example: launchMultiple.bat PredPrey TorusPredPrey-TorusPred-CoOpMultiCCQ.bat 0 4 PredPrey TorusPredPrey-TorusPred-CoOpMultiCCQ.bat 5 9
-set argCount=0
-for %%x in (%*) do set /A argCount+=1
+REM example: launchMultiple.bat PredPrey TorusPredPrey-TorusPred-CoOpMultiCCQ.bat 1 7
+FOR /L %%A IN (%3,1,%4) DO (
   cd batch
   cd %1
-FOR /L %%A IN (%3,1,%4) DO %2 %%A
-IF /I %argCount% EQU 8 cd ..
-IF /I %argCount% EQU 8 cd %5
-IF /I %argCount% EQU 8 FOR /L %%x IN (%7,1,%8) DO %6 %%x
-cd ..
-cd ..
+  %2 %%A
+)
 ECHO "All done!"
