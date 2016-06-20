4a94abb23a8f8ff8c05cd6f70c4d7965a46b32eb
Gab <Serenamidori@gmail.com>
2016-06-20 15:29:15 -0500

    Small Batch File Additions for VizDoom
    We may not need these in the future

---
+++ batch/VizDoom/VizDoom-Basic-HNSmudge.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoombasic trials:2 maxGens:100 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomBasicShootTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:Basic-HNSmudge saveTo:HNSmudge gameWad:freedoom2.wad watch:false doomInputPixelSmudge:12 doomSmudgeStat:edu.utexas.cs.nn.util.stats.MostExtreme hyperNEAT:true genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype allowMultipleFunctions:true ftype:1 netChangeActivationRate:0.3 doomInputStartX:0 doomInputStartY:60 doomInputHeight:48 doomInputWidth:200
