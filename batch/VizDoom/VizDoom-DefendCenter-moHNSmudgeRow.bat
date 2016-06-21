2f141110f304019b91222d9f8d0b7347b5abffef
Gab <Serenamidori@gmail.com>
2016-06-21 16:24:33 -0500

    Batch file for multi-objective HyperNEAT VizDoom DefendCenter

---
+++ batch/VizDoom/VizDoom-DefendCenter-moHNSmudgeRow.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoomdefendcenter trials:5 maxGens:100 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomDefendCenterTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:DefendCenter-Hyper saveTo:Hyper gameWad:freedoom2.wad watch:false stepByStep:false doomEpisodeLength:2100 doomInputStartX:0 doomInputStartY:70 doomInputHeight:15 doomInputWidth:200 doomInputPixelSmudge:5 doomSmudgeStat:edu.utexas.cs.nn.util.stats.MostExtreme moVizDoom:true hyperNEAT:true genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype allowMultipleFunctions:true ftype:1 netChangeActivationRate:0.3
