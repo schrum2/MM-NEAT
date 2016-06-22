4581b77d5273fba1ab22e7917bc66eaa2fec393c
Gab <Serenamidori@gmail.com>
2016-06-22 15:54:27 -0500

    Updated mario batch files

--- batch/Mario/Mario-Basic-Control.bat
+++ batch/Mario/Mario-Basic-Control.bat
@@ -1,3 +1,3 @@
 cd ..
 cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:mario trials:5 maxGens:100 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.mario.MarioTask cleanOldNetworks:true fs:false log:Mario-Basic saveTo:Basic watch:false marioInputStartX:-3 marioInputStartY:-2 marioInputWidth:12 marioInputHeight:5 showMarioInputs:false
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:mario trials:5 maxGens:100 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.mario.MarioTask cleanOldNetworks:true fs:false log:Mario-Control saveTo:Control watch:false marioInputStartX:-3 marioInputStartY:-2 marioInputWidth:12 marioInputHeight:5 showMarioInputs:false
