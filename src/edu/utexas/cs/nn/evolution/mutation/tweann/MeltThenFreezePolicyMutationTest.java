471ac45aab51d6da9c9a640503faba35238d5f7a
leg2015 <gillespl@southwestern.edu>
2016-06-24 10:39:41 -0500

    Tried to get melt then freeze policy mutation junit working but was unsuccessful

--- src/edu/utexas/cs/nn/evolution/mutation/tweann/MeltThenFreezePolicyMutationTest.java
+++ src/edu/utexas/cs/nn/evolution/mutation/tweann/MeltThenFreezePolicyMutationTest.java
@@ -2,6 +2,8 @@ package edu.utexas.cs.nn.evolution.mutation.tweann;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,20 +12,24 @@ import edu.utexas.cs.nn.MMNEAT.MMNEAT;
 import edu.utexas.cs.nn.evolution.EvolutionaryHistory;
 import edu.utexas.cs.nn.evolution.genotypes.TWEANNGenotype;
 import edu.utexas.cs.nn.evolution.genotypes.TWEANNGenotype.NodeGene;
+import edu.utexas.cs.nn.networks.TWEANN.Node;
 import edu.utexas.cs.nn.parameters.Parameters;
 
-public class MeltThenFreezePolicyMutationTest {
+public class MeltThenFreezePolicyMutationTest {//TODO
 
 	TWEANNGenotype tg1;
 	MeltThenFreezePolicyMutation mtfpm;
 	@Before
 	public void setUp() throws Exception {
-		Parameters.initializeParameterCollections(new String[] { "io:false", "netio:false", "recurrency:false", "freezePolicyRate:1.0"});
+		Parameters.initializeParameterCollections(new String[] { "io:false", "netio:false", "recurrency:false", "freezePolicyRate:1.0", "mmrRate:1.0"});
 		MMNEAT.loadClasses();
 		tg1 = new TWEANNGenotype(MMNEAT.networkInputs, MMNEAT.networkOutputs, 0);
 		mtfpm = new MeltThenFreezePolicyMutation();
 		MMNEAT.genotype = tg1.copy();
 		EvolutionaryHistory.initArchetype(0);
+		MMR mmr = new MMR();
+		mmr.mutate(tg1);
+		assertEquals(tg1.numModules, 2);
 	}
 
 	@After
@@ -35,12 +41,24 @@ public class MeltThenFreezePolicyMutationTest {
 
 	@Test
 	public void test() {
-		mtfpm.mutate(tg1);
-		int x = 0;
-		for(NodeGene node : tg1.nodes) {
-			if(node.frozen) x++;
+		int numPolicy = 2 * tg1.numOut;
+		ArrayList<NodeGene> nodes = tg1.nodes;
+		for(int i = 0; i < tg1.nodes.size(); i++) {
+			NodeGene node = nodes.get(i);
+			if(node.ntype == Node.NTYPE_INPUT || node.ntype == Node.NTYPE_HIDDEN) {
+				node.freeze();
+				assertEquals(node.frozen, true);
+			}
 		}
-		assertTrue(x != 0);
+		mtfpm.mutate(tg1);
+		 int numFrozen = 0;
+		for(NodeGene node : nodes) {
+			if(node.frozen) {
+				numFrozen++;
+			}
+		}
+		System.out.println("num outputs: " + tg1.numOut + " num policy as calculated: " + numPolicy);
+		assertEquals(numFrozen, tg1.numOut);
 	}
 
 }
