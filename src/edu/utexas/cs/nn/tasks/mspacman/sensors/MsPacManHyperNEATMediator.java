d100e5a8c4496057625757c31995eb12706dcf79
leg2015 <gillespl@southwestern.edu>
2016-06-24 15:54:26 -0500

    Mediator class for ms PacMan and hyperNEAT

--- src/edu/utexas/cs/nn/tasks/mspacman/sensors/MsPacManHyperNEATMediator.java
+++ src/edu/utexas/cs/nn/tasks/mspacman/sensors/MsPacManHyperNEATMediator.java
@@ -1,8 +1,25 @@
 package edu.utexas.cs.nn.tasks.mspacman.sensors;
 
+import edu.utexas.cs.nn.tasks.mspacman.sensors.blocks.hyperneat.SubstrateGhostSensorBlock;
+import edu.utexas.cs.nn.tasks.mspacman.sensors.blocks.hyperneat.SubstratePacManSensorBlock;
+import edu.utexas.cs.nn.tasks.mspacman.sensors.blocks.hyperneat.SubstratePillsSensorBlock;
+import edu.utexas.cs.nn.tasks.mspacman.sensors.blocks.hyperneat.SubstratePowerPillSensorBlock;
+/**
+ * Mediator class for hyperNEAT msPacMan.
+ *  Defines the sensor blocks, inputs and their components
+ * @author Lauren Gillespie
+ *
+ */
 public class MsPacManHyperNEATMediator extends BlockLoadedInputOutputMediator {
 
+	/**
+	 * Constructor simply aggregates all the
+	 *  necessary sensor blocks together
+	 */
 	public MsPacManHyperNEATMediator() {
-		// TODO
+		blocks.add(new SubstratePillsSensorBlock());
+		blocks.add(new SubstratePowerPillSensorBlock());
+		blocks.add(new SubstrateGhostSensorBlock());
+		blocks.add(new SubstratePacManSensorBlock());
 	}
 }
