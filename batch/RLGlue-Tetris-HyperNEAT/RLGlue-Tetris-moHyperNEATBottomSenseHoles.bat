eb67cf79f4da471840b861af2ba183c7b704a6bb
leg2015 <gillespl@southwestern.edu>
2016-06-24 10:06:28 -0500

    Moved more batch files around to new folders
    split the 1D and bottom tetris files

--- batch/RLGlue-Tetris-HyperNEAT/RLGlue-Tetris-moHyperNEATBottomSenseHoles.bat
+++
@@ -1,9 +0,0 @@
-cd ..
-cd ..
-setlocal enabledelayedexpansion
-set string=java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:tetris trials:3 maxGens:500 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.rlglue.tetris.HyperNEATTetrisTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:RL-moHyperNEATBottomSenseHoles saveTo:moHyperNEATBottomSenseHoles rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris rlGlueExtractor:edu.utexas.cs.nn.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor tetrisTimeSteps:true tetrisBlocksOnScreen:true rlGlueAgent:edu.utexas.cs.nn.tasks.rlglue.tetris.TetrisAfterStateAgent hyperNEAT:true genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype allowMultipleFunctions:true ftype:1 netChangeActivationRate:0.3 substrateMapping:edu.utexas.cs.nn.networks.hyperneat.BottomSubstrateMapping senseHolesDifferently:true
-set /a r=%1
-set /a r=!r!+4156
-set string=!string! rlGluePort:
-set string=!string!!r!
-call !string!
