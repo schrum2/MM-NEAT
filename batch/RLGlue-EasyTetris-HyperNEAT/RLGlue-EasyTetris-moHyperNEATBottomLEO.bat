9d4558da4d9283cea71c578743aa1ed4e587b33c
schrum2 <schrum2@utexas.edu>
2016-06-23 23:58:32 -0500

    HyperNEAT uses bias by default
    A previous commit made evolveHyperNEATBias true by default, so I changed
    and removed batch files accordingly.
    
    Also made a batch file to use split inputs for the raw extractor in
    plain NEAT (not HyperNEAT)

---
+++ batch/RLGlue-EasyTetris-HyperNEAT/RLGlue-EasyTetris-moHyperNEATBottomLEO.bat
@@ -0,0 +1,9 @@
+cd ..
+cd ..
+setlocal enabledelayedexpansion
+set string=java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:easytetris trials:3 maxGens:500 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.rlglue.tetris.HyperNEATTetrisTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:EasyRL-moHyperNEATBottomLEO saveTo:moHyperNEATBottomLEO rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris rlGlueExtractor:edu.utexas.cs.nn.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor tetrisTimeSteps:true tetrisBlocksOnScreen:false rlGlueAgent:edu.utexas.cs.nn.tasks.rlglue.tetris.TetrisAfterStateAgent hyperNEAT:true genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype allowMultipleFunctions:true ftype:1 netChangeActivationRate:0.3 absenceNegative:false substrateMapping:edu.utexas.cs.nn.networks.hyperneat.BottomSubstrateMapping tetrisAllowTri:false tetrisAllowSShape:false tetrisAllowZShape:false senseHolesDifferently:false extraHNTetrisLinks:false leo:true
+set /a r=%1
+set /a r=!r!+4216
+set string=!string! rlGluePort:
+set string=!string!!r!
+call !string!
---
+++ batch/RLGlue-Tetris-HyperNEAT/RLGlue-Tetris-moHyperNEAT1DBottomStretchedTanh.bat
@@ -0,0 +1,9 @@
+cd ..
+cd ..
+setlocal enabledelayedexpansion
+set string=java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:tetris trials:3 maxGens:500 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.rlglue.tetris.HyperNEATTetrisTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:RL-moHyperNEAT1DBottomStretchedTanh saveTo:moHyperNEAT1DBottomStretchedTanh rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris rlGlueExtractor:edu.utexas.cs.nn.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor tetrisTimeSteps:true tetrisBlocksOnScreen:true rlGlueAgent:edu.utexas.cs.nn.tasks.rlglue.tetris.TetrisAfterStateAgent hyperNEAT:true genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype allowMultipleFunctions:true ftype:17 netChangeActivationRate:0.3 senseHolesDifferently:true substrateMapping:edu.utexas.cs.nn.networks.hyperneat.Bottom1DSubstrateMapping splitRawTetrisInputs:false
+set /a r=%1
+set /a r=!r!+4126
+set string=!string! rlGluePort:
+set string=!string!!r!
+call !string!
