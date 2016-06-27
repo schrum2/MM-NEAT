44a76df3b74972ce3c2887f82e8d2cd0e3b118d2
Jacob Schrum <schrum2@southwestern.edu>
2016-06-27 09:30:46 -0500

    Brought batch files for vizdoom shoot basic up to date

--- batch/VizDoom/VizDoom-Basic-Smudge.bat
+++ batch/VizDoom/VizDoom-Basic-Smudge.bat
@@ -1,3 +1,3 @@
 cd ..
 cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoombasic trials:2 maxGens:10 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomBasicShootTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:Basic-Smudge saveTo:Smudge gameWad:freedoom2.wad watch:false doomInputPixelSmudge:5 doomSmudgeStat:edu.utexas.cs.nn.util.stats.MostExtreme
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoombasic trials:10 maxGens:500 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomBasicShootTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:Basic-Smudge saveTo:Smudge gameWad:freedoom2.wad watch:false doomInputPixelSmudge:5 doomSmudgeStat:edu.utexas.cs.nn.util.stats.MostExtreme doomInputStartX:0 doomInputStartY:60 doomInputHeight:48 doomInputWidth:200
