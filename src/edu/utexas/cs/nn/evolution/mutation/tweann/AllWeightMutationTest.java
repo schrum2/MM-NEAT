fade2de2eb10666e5791443a0bf76a1123a32c96
Gab <Serenamidori@gmail.com>
2016-06-28 14:28:05 -0500

    Updated AllWeightMutationTests
    Fixed the test so that is used the AllWeightMutation mutate() function.
    Also made sure the asserts and for loops made sense.

--- src/edu/utexas/cs/nn/evolution/mutation/tweann/AllWeightMutationTest.java
+++ src/edu/utexas/cs/nn/evolution/mutation/tweann/AllWeightMutationTest.java
@@ -3,6 +3,7 @@ package edu.utexas.cs.nn.evolution.mutation.tweann;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.After;
 import org.junit.Before;
@@ -33,17 +34,31 @@ public class AllWeightMutationTest {
 	@Test
 	public void test() {
 		ArrayList<LinkGene> links = tg1.links;
+		System.out.println("tg1.links: " + links);
+		AllWeightMutation AWM = new AllWeightMutation();
+				
 		double[] linkes = new double[links.size()];
+		System.out.println("  Original: " + links.size());
+		int originalLength = links.size();
 		for(int i = 0; i < links.size(); i++) {
 			linkes[i] = links.get(i).weight;
+			System.out.println("	Weight at " + i + " is " + links.get(i).weight);
 		}
-		for(int i = 0; i < MUTATION_NUM; i ++ ) {
-			tg1.mutate();
+
+		for(int m = 0; m < MUTATION_NUM; m++) {
+			AWM.mutate(tg1);
 		}
-		double[] newLinkes = new double[tg1.links.size()];
-		for(int i = 0; i < tg1.links.size(); i ++) {
-			newLinkes[i] = tg1.links.get(i).weight;
+		
+		double[] newLinkes = new double[links.size()];
+		System.out.println("  Mutated: " + links.size());
+		for(int j = 0; j < links.size(); j++) {
+			newLinkes[j] = links.get(j).weight;
+			System.out.println("	Weight at " + j + " is " + links.get(j).weight);
 		}
+		System.out.println("original links: " + Arrays.toString(linkes));
+		System.out.println("new links: " + Arrays.toString(newLinkes));
+		
+		assertEquals(originalLength, links.size()); // the number of links won't change when mutated
 		assertFalse(linkes.equals(newLinkes));
 	}
 
