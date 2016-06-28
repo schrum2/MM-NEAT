078d4f6f1c43d71000176c10260a6c95573a5a82
leg2015 <gillespl@southwestern.edu>
2016-06-28 15:08:08 -0500

    Added a fullScreen test batch file for hyperNEAT and msPacMan

---
+++ batch/MsPacMan-HyperNEAT/MsPacMan-HyperNEAT-testFullScreen.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:0 randomSeed:0 maxGens:200 mu:25 io:true netio:true mating:true base:HNMsPacMan task:edu.utexas.cs.nn.tasks.mspacman.MsPacManTask cleanOldNetworks:true pacManLevelTimeLimit:8000 pacmanInputOutputMediator:edu.utexas.cs.nn.tasks.mspacman.sensors.MsPacManHyperNEATMediator trials:1 log:HyperNEAT-testFullScreen saveTo:testFullScreen hyperNEAT:true genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype allowMultipleFunctions:true fs:false ftype:1 netChangeActivationRate:0.3 pacManFullScreenOutput:true
