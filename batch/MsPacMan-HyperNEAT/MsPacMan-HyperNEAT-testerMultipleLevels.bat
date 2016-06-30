571f71475c53a4516149eb65f78a0493e0f53753
leg2015 <gillespl@southwestern.edu>
2016-06-30 16:15:44 -0500

    added some new parameters and batch files for msPacMan hyperNEAT

--- batch/MsPacMan-HyperNEAT/MsPacMan-HyperNEAT-testerMultipleLevels.bat
+++
@@ -1,4 +0,0 @@
-cd ..
-cd ..
-java -jar dist/MM-NEATv2.jar runNumber:0 randomSeed:1 maxGens:200 mu:25 io:false netio:false mating:true task:edu.utexas.cs.nn.tasks.mspacman.MsPacManTask cleanOldNetworks:true pacManLevelTimeLimit:8000 pacmanInputOutputMediator:edu.utexas.cs.nn.tasks.mspacman.sensors.MsPacManHyperNEATMediator trials:1 log:HNMsPacMan-tester saveTo:HNMsPacMan hyperNEAT:true genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype allowMultipleFunctions:true fs:false ftype:1 netChangeActivationRate:0.3 watch:true monitorSubstrates:true showNetworks:false showCPPN:true pacManFullScreenOutput:true substrateGridSize:10  pacmanFatalTimeLimit:false pacManLevelTimeLimit:30 stepByStep:true
-  
--- src/edu/utexas/cs/nn/parameters/Parameters.java
+++ src/edu/utexas/cs/nn/parameters/Parameters.java
@@ -284,6 +284,9 @@ public class Parameters {
 		longOptions.add("lastInnovation", 0l, "Highest innovation number used so far");
 		longOptions.add("lastGenotypeId", 0l, "Highest genotype id used so far");
 		// Boolean parameters
+		booleanOptions.add("pacmanFullScreenProcess", false, "full screen process layer");
+		booleanOptions.add("pacmanBothThreatAndEdibleSubstrate", false, "whether or not to have 2 substrates for threat and edible ghosts");
+		booleanOptions.add("pacmanFullScreenPowerInput", false, "full screen input for power pill substrate");
 		booleanOptions.add("pacManFullScreenOutput", false, "Output substrate has a cell for every 5x5 block on pacman screen");
 		booleanOptions.add("moMario", false, "Mario is multiobjective");
 		booleanOptions.add("showCPPN", false, "shows evolved CPPN during post evals");
