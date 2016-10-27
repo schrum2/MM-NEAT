85ebefe673196ed18315ef233c6e42c0e45ddc94
Rollinsa <rollinsa@southwestern.edu>
2016-10-27 18:05:31 -0500

    Got rid of the postProcessNoHV batch file cuz out of date, now does no HV by default with standard postProcess

--- postProcessNoHV.bat
+++
@@ -1,3 +0,0 @@
-REM Usage:   postProcess.bat <experiment directory> <log prefix> <run type> <number of runs>
-REM Example: postProcess.bat onelifeconflict OneLifeConflict OneModule 30
-java -jar "dist/MM-NEATv2.jar" process:%4 base:%1 log:%2-%3 saveTo:%3 processHV:false
