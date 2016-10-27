848b5b62d09d778978d1a8e6298df0fce26cad9c
Rollinsa <rollinsa@southwestern.edu>
2016-10-27 17:46:37 -0500

    Created 6 batch files for ModulesVsSelection Experimental Set (Official).

---
+++ batch/PredPrey-ModulesVsSelection/TorusPredPrey-Team1M.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:toruspred teams:10 maxGens:300 mu:200 io:true netio:true mating:false fs:false task:edu.utexas.cs.nn.tasks.gridTorus.cooperative.CooperativePredatorsVsStaticPreyTask log:TorusPred-Team1M saveTo:Team1M allowDoNothingActionForPredators:true torusPreys:2 torusPredators:3 staticPreyController:edu.utexas.cs.nn.gridTorus.controllers.PreyFleeClosestPredatorController predatorCatchClose:false cooperativeTeamSelection:true torusSenseByProximity:true torusSenseTeammates:true ea:edu.utexas.cs.nn.evolution.nsga2.CoevolutionNSGA2 experiment:edu.utexas.cs.nn.experiment.evolution.LimitedMultiplePopulationGenerationalEAExperiment teamLog:true bestTeamScore:false
