b6acf3a0679cbe75ab341630cd02be232b2c7638
alicequint <quintana@southwestern.edu>
2017-04-25 14:53:36 -0500

    comments in MultiplePopulationGenerationalEA
    added comments to this interface based on the comments in
    SinglePopulationGenerationalEA.java from the same package

--- src/edu/utexas/cs/nn/evolution/MultiplePopulationGenerationalEA.java
+++ src/edu/utexas/cs/nn/evolution/MultiplePopulationGenerationalEA.java
@@ -16,12 +16,32 @@ import java.util.ArrayList;
  */
 public interface MultiplePopulationGenerationalEA extends GenerationalEA {
 
+	/**
+	 * Initializes a sub-population of genotypes corresponding to each example genotype 
+	 * in the provided array list.
+	 * @param examples 
+	 * 		example genotypes that are used to make the starting population of organisms
+	 * @return randomized population
+	 */
 	@SuppressWarnings("rawtypes")
 	public ArrayList<ArrayList<Genotype>> initialPopulations(ArrayList<Genotype> examples);
 
+	/**
+	 * Given a list of all sub-populations, derive the next generation consisting of a new sub-population 
+	 * derived from each input population.
+	 * @param populations
+	 * 			populations to evaluate and evolve
+	 * @return new population of organisms from the next generation
+	 */
 	@SuppressWarnings("rawtypes")
 	public ArrayList<ArrayList<Genotype>> getNextGeneration(ArrayList<ArrayList<Genotype>> populations);
 
+	/**
+	 * Carry out any operations at the end of the experiment (such as logging information about the 
+	 * final population)
+	 * @param populations
+	 * 			final population produced by evolution
+	 */
 	@SuppressWarnings("rawtypes")
 	public void close(ArrayList<ArrayList<Genotype>> populations);
 }
