a0afd4bc008d767e1e0b8b01a692e14910dfd0e3
twerisa <twerasei@southwestern.edu>
2017-04-28 01:06:58 -0500

    Method header comments
    Added method header comments to all methods in
    LimitedSinglePopulationGenerationalEAExperiment.java

--- src/edu/utexas/cs/nn/experiment/evolution/LimitedSinglePopulationGenerationalEAExperiment.java
+++ src/edu/utexas/cs/nn/experiment/evolution/LimitedSinglePopulationGenerationalEAExperiment.java
@@ -16,6 +16,9 @@ public class LimitedSinglePopulationGenerationalEAExperiment<T> extends SinglePo
 
 	private int maxGenerations;
 
+	/**
+	 * Creates a default instance based on parameters loaded from the commandline
+	 */
 	@SuppressWarnings("unchecked")
 	public LimitedSinglePopulationGenerationalEAExperiment() {
 		this((SinglePopulationGenerationalEA<T>) MMNEAT.ea, MMNEAT.genotype,
@@ -23,12 +26,22 @@ public class LimitedSinglePopulationGenerationalEAExperiment<T> extends SinglePo
 				Parameters.parameters.stringParameter("lastSavedDirectory"));
 	}
 
+	/**
+	 * Creates experiment instance with specified parameters.
+	 * @param ea evolutionary algorithm that works on a single population
+	 * @param example template for the initial genotype of the population
+	 * @param maxGenerations number of generations of evolution
+	 * @param lastSavedDir last directory to which genotypes were saved, if saving occurred (null or "" otherwise)
+	 */
 	public LimitedSinglePopulationGenerationalEAExperiment(SinglePopulationGenerationalEA<T> ea, Genotype<T> example, int maxGenerations, String lastSavedDir) {
 		super(ea, example, lastSavedDir);
 		this.maxGenerations = maxGenerations;
 	}
 
-        @Override
+	/**
+	 * Indicates that the experiment should stop once the maxGenerations have been reached
+	 */
+	@Override
 	public boolean shouldStop() {
 		return ea.currentGeneration() >= this.maxGenerations;
 	}
