afefc96c56cc6df79e5f3dce7ae5753051d4d4b2
Alice Quintanilla <quintana@southwestern.edu>
2017-05-10 12:01:55 -0500

    comments and test
    completed comments for DIstance2DComparator.java and started creating a
    test case for it, haven't written the tests yet

--- src/edu/utexas/cs/nn/util/util2D/Distance2DComparator.java
+++ src/edu/utexas/cs/nn/util/util2D/Distance2DComparator.java
@@ -10,11 +10,28 @@ public class Distance2DComparator implements Comparator<ILocated2D> {
 
 	private final ILocated2D reference;
 
+	/**
+	 * constructor
+	 * @param reference
+	 * 				any object that has a 2d location
+	 */
 	public Distance2DComparator(ILocated2D reference) {
 		this.reference = reference;
 	}
-
+	
 	@Override
+	/**
+	 * compares two distances, each between the reference and a given point, 
+	 * 
+	 * @param o1 
+	 * 			object in 2d space 
+	 * @param o2 
+	 * 			another object in 2d space
+	 * @return comparison between (distance between (o1, reference)) and (distance between (o2, reference))
+	 * 		1 : o1 is farther,
+	 * 		0 : they are equally far from reference,
+	 * 	   -1 : o2 is farther.
+	 */
 	public int compare(ILocated2D o1, ILocated2D o2) {
 		if (reference == null || (o1 == null && o2 == null)) {
 			return 0;
---
+++ src/edu/utexas/cs/nn/util/util2D/Distance2DComparatorTest.java
@@ -0,0 +1,43 @@
+package edu.utexas.cs.nn.util.util2D;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Distance2DComparatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		Tuple2D bottomLeft = new Tuple2D(1.0, 1.0);
+		Tuple2D topLeft = new Tuple2D(1.0, 8.0);
+		Tuple2D bottomRight = new Tuple2D(8.0, 1.0);
+		Tuple2D topRight = new Tuple2D(8.0, 8.0);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDistance2DComparator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompare() {
+		fail("Not yet implemented");
+	}
+
+}
