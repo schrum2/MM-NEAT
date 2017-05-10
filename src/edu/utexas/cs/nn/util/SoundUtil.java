75f18869b18bcaf7fe3f7b78d40cf0760a71e083
twerisa <twerasei@southwestern.edu>
2017-05-10 16:41:47 -0500

    #271 cleanup of methods used to play WAV file
    playWAVFile method broken down into multiple smaller methods which have
    been commented. Replaced all try-catches with throws declarations, but
    seems to be creating a necessity for a large amount of exceptions thrown
    in each method. Fix later when I know more about how the code is being
    used?

--- src/edu/utexas/cs/nn/util/SoundUtil.java
+++ src/edu/utexas/cs/nn/util/SoundUtil.java
@@ -6,51 +6,72 @@ import java.io.IOException;
 import javax.sound.sampled.*;
 
 public class SoundUtil {
-	
+
 	private static final String BEARGROWL_WAV = "data/sounds/bear_growl_y.wav";
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
 		playWAVFile(BEARGROWL_WAV);
 	}
-	
-	
-	public static void playClip(Clip audioClip) {
-        audioClip.start();     
-        do {
-            // wait for the playback completes
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-        } while(audioClip.isRunning());      
-	}
-	
+
 	/**
+	 * Plays an input audio clip; exits loop when clip has finished playing.
 	 * 
-	 * @param audio
+	 * @param audioClip clip being played
+	 * @throws InterruptedException if a thread is interrupted while waiting/idling
 	 */
-	public static void playWAVFile(String audio) {
+	public static void playClip(Clip audioClip) throws InterruptedException {
+		audioClip.start();     
+		do { //use do while to allow for audio clip to start before audioClip.isRunning() is checked
+			// wait for the playback to complete
+			Thread.sleep(1000);
+		} while(audioClip.isRunning());      
+	}
+
+	/**
+	 * Converts a file into an audio clip. 
+	 * 
+	 * @param audioFile file to be converted
+	 * @return newly converted audio clip
+	 * @throws UnsupportedAudioFileException if type of audio file cannot be read/recognized as valid
+	 * @throws IOException if an IO operation has failed or been interrupted
+	 * @throws LineUnavailableException if line cannot be found
+	 */
+	public static Clip makeClip(File audioFile) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+		AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile); //converts file into audio stream
+		AudioFormat format = audioStream.getFormat();
+		DataLine.Info info = new DataLine.Info(Clip.class, format);
+		Clip audioClip = (Clip) AudioSystem.getLine(info); //uses necessary formatting to create clip from audio stream
+		audioClip.open(audioStream);  
+		return audioClip; //returns finalized clip
+	}
+
+	/**
+	 * Plays an audio file by converting the input file into a clip and subsequently playing through the contents of the clip
+	 * 
+	 * @param audio input file being played
+	 * @throws UnsupportedAudioFileException if type of audio file cannot be read/recognized as valid
+	 * @throws IOException if an IO operation has failed or been interrupted
+	 * @throws LineUnavailableException if line cannot be found
+	 * @throws InterruptedException if a thread is interrupted while waiting/idling
+	 */
+	public static void playWAVFile(String audio) throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
 		File audioFile = new File(audio);
-        try {
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
-            AudioFormat format = audioStream.getFormat();
-            DataLine.Info info = new DataLine.Info(Clip.class, format);
-            Clip audioClip = (Clip) AudioSystem.getLine(info);
-            audioClip.open(audioStream);      
-            playClip(audioClip);
-            audioClip.close();
-             
-        } catch (UnsupportedAudioFileException ex) {
-            System.out.println("The specified audio file is not supported.");
-            ex.printStackTrace();
-        } catch (LineUnavailableException ex) {
-            System.out.println("Audio line for playing back is unavailable.");
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            System.out.println("Error playing the audio file.");
-            ex.printStackTrace();
-        }
-         
-    }
+		playWAVFile(audioFile);
+	}
+
+	/**
+	 * Reads in audio file instance from previous method, converts it to a clip and plays that clip
+	 * 
+	 * @param audioFile File instance of input string from locally saved file
+	 * @throws UnsupportedAudioFileException if type of audio file cannot be read/recognized as valid
+	 * @throws LineUnavailableException if line cannot be found
+	 * @throws InterruptedException if a thread is interrupted while waiting/idling
+	 * @throws IOException if an IO operation has failed or been interrupted
+	 */
+	public static void playWAVFile(File audioFile) throws UnsupportedAudioFileException, LineUnavailableException, InterruptedException, IOException {
+		Clip audioClip = makeClip(audioFile); //saves converted clip file as a variable
+		playClip(audioClip); //calls playClip() to play file
+
+	}
+
 }
