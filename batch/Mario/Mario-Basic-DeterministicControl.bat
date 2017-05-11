a342ebfcae94974d1498dffa9aa2b239c3a3409d
Alice Quintanilla <quintana@southwestern.edu>
2017-05-11 15:46:04 -0500

    Changed save-location of deterministic Mario to  be distinct from non-deterministic
    whereas before it was log:Mario-Hyper, it is now
    log:Mario-DeterministicHyper, and similar for Control.

--- batch/Mario/Mario-Basic-DeterministicControl.bat
+++ batch/Mario/Mario-Basic-DeterministicControl.bat
@@ -1,3 +1,3 @@
 cd ..
 cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:mario trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.mario.MarioTask cleanOldNetworks:true fs:false log:Mario-Control saveTo:Control watch:false marioInputStartX:-3 marioInputStartY:-2 marioInputWidth:12 marioInputHeight:5 showMarioInputs:false deterministic:true
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:mario trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.mario.MarioTask cleanOldNetworks:true fs:false log:Mario-DeterministicControl saveTo:DeterministicControl watch:false marioInputStartX:-3 marioInputStartY:-2 marioInputWidth:12 marioInputHeight:5 showMarioInputs:false deterministic:true
