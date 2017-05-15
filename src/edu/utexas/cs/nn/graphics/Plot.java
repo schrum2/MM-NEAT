e46aca232ce4e5c855487f34a1a218ff754767f1
twerisa <twerasei@southwestern.edu>
2017-05-15 14:39:11 -0500

    Fixing issues with Plot.java
    Generalized Plot class so that it can be used to create plots in which
    the DrawingPanel is an atypical size/not the size originally specified
    in the original variables of the class. (found this issue after messing
    with creating a visualization of an audio file in edu.utexas.cs.nn.sound

--- src/edu/utexas/cs/nn/graphics/Plot.java
+++ src/edu/utexas/cs/nn/graphics/Plot.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
 
+import edu.utexas.cs.nn.util.MiscUtil;
+
 /**
  *
  * @author Jacob Schrum
@@ -15,12 +17,16 @@ public class Plot {
 	public static final int EDGE = 16;
 	public static final int TOP = 56;
 	public static final int OVAL_DIM = 5;
-
+	
 	public static void linePlot(DrawingPanel panel, double min, double max, ArrayList<Double> scores, Color color) {
 		Graphics g = panel.getGraphics();
+		int height = panel.getFrame().getHeight() - 50; // -50 is to avoid gray panel at bottom of DrawingPanel
+		int width = panel.getFrame().getWidth();		
 		g.setColor(Color.black);
-		g.drawLine(OFFSET, OFFSET, OFFSET, BROWSE_DIM - OFFSET);
-		g.drawLine(OFFSET, BROWSE_DIM - OFFSET, BROWSE_DIM - OFFSET, BROWSE_DIM - OFFSET);
+		// y-axis
+		g.drawLine(OFFSET, OFFSET, OFFSET, height - OFFSET);	
+		// x-axis
+		g.drawLine(OFFSET, height - OFFSET, width - OFFSET, height - OFFSET);
 		double last = scores.get(0);
 		double maxRange = Math.max(max, max - min);
 		double lowerMin = Math.min(0, min);
@@ -28,23 +34,33 @@ public class Plot {
 			g.setColor(color);
 			// g.fillRect(OFFSET + scale((double) i, (double) scores.size()),
 			// OFFSET + invert(scores.get(i), max), 1, 1);
-			int x1 = OFFSET + scale((double) (i - 1), (double) scores.size(), 0);
-			int y1 = OFFSET + invert(last, maxRange, lowerMin);
-			int x2 = OFFSET + scale((double) i, (double) scores.size(), 0);
-			int y2 = OFFSET + invert(scores.get(i), maxRange, lowerMin);
+			int x1 = OFFSET + scale((double) (i - 1), (double) scores.size(), 0, width);
+			int y1 = OFFSET + invert(last, maxRange, lowerMin, height);
+			int x2 = OFFSET + scale((double) i, (double) scores.size(), 0, width);
+			int y2 = OFFSET + invert(scores.get(i), maxRange, lowerMin, height);
+
+			//System.out.println(x1+","+ y1+","+ x2+","+ y2);
 			g.drawLine(x1, y1, x2, y2);
 			g.setColor(Color.black);
-			g.drawString("" + max, OFFSET / 2, OFFSET / 2);
-			g.drawString("" + lowerMin, OFFSET / 2, BROWSE_DIM - (OFFSET / 2));
 			last = scores.get(i);
 		}
+		g.drawString("" + max, OFFSET / 2, OFFSET / 2);
+		g.drawString("" + lowerMin, OFFSET / 2, height - (OFFSET / 2));
 	}
 
 	public static int scale(double x, double max, double min) {
-		return (int) (((x - min) / max) * (BROWSE_DIM - (2 * OFFSET)));
+		return scale(x,max,min,BROWSE_DIM);
+	}
+	
+	public static int scale(double x, double max, double min, int totalWidth) {
+		return (int) (((x - min) / max) * (totalWidth - (2 * OFFSET)));
 	}
 
 	public static int invert(double y, double max, double min) {
-		return (BROWSE_DIM - (2 * OFFSET)) - scale(y, max, min);
+		return invert(y,max,min);
+	}
+	
+	public static int invert(double y, double max, double min, int totalHeight) {
+		return (totalHeight - (2 * OFFSET)) - scale(y, max, min, totalHeight);
 	}
 }
