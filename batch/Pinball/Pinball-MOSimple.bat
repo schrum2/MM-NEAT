94dbf2e5e574c10dfe805cd54f0f68996a0a075d
DarwinJohnson <johnso17@southwestern.edu>
2017-05-16 11:44:37 -0500

    #303 Created two MO Batch Files
    Copied the existing Batch files for Pinball and added the moPinball
    parameter set to true for both of them. The new MO Batch files are named
    Pinball-MOHard and Pinball-MOSimple, respectively.

---
+++ batch/Pinball/Pinball-MOSimple.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:pinball trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.pinball.PinballTask cleanOldNetworks:true fs:false log:Pinball-MOSimple saveTo:MOSimple watch:false pinballConfig:pinball_simple_single.cfg moPinball:true
