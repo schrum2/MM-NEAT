75f6d28ab33cd81212875409272795db5efc664e
twerisa <twerasei@southwestern.edu>
2017-05-16 14:22:15 -0500

    #271 Created Amplitude.java
    Created a new class and moved over the CPPN method and the methods used
    to extract an amplitude array from an audio input stream. Also fixed
    resulting method call errors throughout the package. This class is where
    I will put the methods that save amplitude arrays as WAV and MP3 files,
    and the methods that save CPPNs as WAV and MP3 files.

---
+++ src/edu/utexas/cs/nn/util/sound/Amplitude.java
@@ -0,0 +1,113 @@
+package edu.utexas.cs.nn.util.sound;
+
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+
+import edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype;
+import edu.utexas.cs.nn.networks.Network;
+
+public class Amplitude {
+	
+	//Methods from GT - used to extract amplitude from recorded wave
+	
+	/**
+	 * Method that inputs an AudioInputStream, obtains the format of the stream and forms an array of bytes 
+	 * based on the size of the stream, and returns a method call to extractAmplitudeDataFromAmplitudeByteArray(). 
+	 * 
+	 * @param audioInputStream stream of audio being converted into amplitude data
+	 * @return  method call that extracts amplitude data from byte array formed
+	 */
+	public static int[] extractAmplitudeDataFromAudioInputStream(AudioInputStream audioInputStream) {  
+		AudioFormat format = audioInputStream.getFormat();  
+		byte[] audioBytes = new byte[(int) (audioInputStream.getFrameLength() * format.getFrameSize())];  
+		try {  
+			audioInputStream.read(audioBytes);  
+		} catch (IOException e) {  
+			System.out.println("IOException during reading audioBytes");  
+			e.printStackTrace();  
+		}  
+		return extractAmplitudeDataFromAmplitudeByteArray(format, audioBytes);  //calls method that extracts amplitude data from byte array formed
+	}  
+	
+	/**
+	 * Method that inputs the format of an AudioInputStream as well as the byte array formed from its contents
+	 * and then creates an array of ints containing the amplitude data of the stream. 
+	 * 
+	 * @param format AudioFormat of AudioinputStream
+	 * @param audioBytes byte array formed based on the size of the stream
+	 * @return int array containing amplitude data from stream
+	 */
+	private static int[] extractAmplitudeDataFromAmplitudeByteArray(AudioFormat format, byte[] audioBytes) {  
+		// convert
+		int[]  audioData = null;  
+		if (format.getSampleSizeInBits() == 16) {  
+			int nlengthInSamples = audioBytes.length / 2;  
+			audioData = new int[nlengthInSamples];  
+			if (format.isBigEndian()) {  
+				for (int i = 0; i < nlengthInSamples; i++) {  
+					/* First byte is MSB (high order) */  
+					int MSB = audioBytes[2 * i];  
+					/* Second byte is LSB (low order) */  
+					int LSB = audioBytes[2 * i + 1];  
+					audioData[i] = (MSB << 8 | (255 & LSB));  
+				}  
+			} else {  
+				for (int i = 0; i < nlengthInSamples; i++) {  
+					/* First byte is LSB (low order) */  
+					int LSB = audioBytes[2 * i];  
+					/* Second byte is MSB (high order) */  
+					int MSB = audioBytes[2 * i + 1];  
+					audioData[i] = (MSB << 8 | (255 & LSB));  
+				}  
+			}  
+		} else if (format.getSampleSizeInBits() == 8) {  
+			int nlengthInSamples = audioBytes.length;  
+			audioData = new int[nlengthInSamples];  
+			if (format.getEncoding().toString().startsWith("PCM_SIGN")) {  
+				// PCM_SIGNED  
+				for (int i = 0; i < audioBytes.length; i++) {  
+					audioData[i] = audioBytes[i];  
+				}  
+			} else {  
+				// PCM_UNSIGNED  
+				for (int i = 0; i < audioBytes.length; i++) {  
+					audioData[i] = audioBytes[i] - 128;  
+				}  
+			}  
+		}
+		return audioData;  
+	}
+
+	//CPPN 
+	
+	/**
+	 * Creates a double array of amplitudes using a CPPN. CPPN has three inputs - time, frequency,
+	 * and a bias. It only has one output, which is the amplitude. The array of inputs is looped through 
+	 * so that each index is manipulated by the CPPN, and the output indexes are saved into a result array 
+	 * of doubles. 
+	 * 
+	 * @param CPPN network used to generate amplitude 
+	 * @param length length of sample
+	 * @param frequency Frequency of note being manipulated
+	 * @return array of doubles representing all CPPN-manipulated output amplitudes
+	 */
+	public static double[] amplitudeGenerator(Network CPPN, int length, int frequency) {
+		double[] result = new double[length];
+		for(double time = 0; time < length; time++) {
+			//double[] inputs = new double[]{time/StdAudio.SAMPLE_RATE, Math.sin(2*Math.PI * frequency * time/StdAudio.SAMPLE_RATE), HyperNEATCPPNGenotype.BIAS};
+			//double[] inputs = new double[]{time/StdAudio.SAMPLE_RATE, ActivationFunctions.triangleWave(2*Math.PI * frequency * time/StdAudio.SAMPLE_RATE), HyperNEATCPPNGenotype.BIAS};
+			double[] inputs = new double[]{time/StdAudio.SAMPLE_RATE, 
+					Math.sin(2*Math.PI * frequency * time/StdAudio.SAMPLE_RATE), 
+//					ActivationFunctions.triangleWave(2*Math.PI * frequency * time/StdAudio.SAMPLE_RATE), 
+//					ActivationFunctions.squareWave(2*Math.PI * frequency * time/StdAudio.SAMPLE_RATE), 
+					HyperNEATCPPNGenotype.BIAS};
+			double[] outputs = CPPN.process(inputs);
+			result[(int) time] = outputs[0]; // amplitude
+		}
+		return result;
+	}
+
+
+}
--- src/edu/utexas/cs/nn/util/sound/SoundUtil.java
+++ src/edu/utexas/cs/nn/util/sound/SoundUtil.java
@@ -51,7 +51,7 @@ public class SoundUtil {
 			test.mutate();
 		}
 		Network cppn = test.getCPPN();
-		double[] testArray = amplitudeGenerator(cppn, 60000, 440);
+		double[] testArray = Amplitude.amplitudeGenerator(cppn, 60000, 440);
 		StdAudio.play(testArray);
 		GraphicsUtil.wavePlotFromDoubleArray(testArray);
 		
@@ -66,7 +66,7 @@ public class SoundUtil {
 		byte[] bearNumbers = WAVToByte(BEARGROWL_WAV);
 		//System.out.println(Arrays.toString(bearNumbers));
 		AudioInputStream bearAIS = SoundUtil.byteToAIS(bearNumbers);
-		int[] bearData = SoundUtil.extractAmplitudeDataFromAudioInputStream(bearAIS);
+		int[] bearData = Amplitude.extractAmplitudeDataFromAudioInputStream(bearAIS);
 		//System.out.println(Arrays.toString(bearData));
 
 		byte[] applauseNumbers = WAVToByte(APPLAUSE_WAV);	
@@ -75,7 +75,7 @@ public class SoundUtil {
 		byte[] harpNumbers = WAVToByte(HARP_WAV);
 		//System.out.println(Arrays.toString(harpNumbers));
 		AudioInputStream harpAIS = SoundUtil.byteToAIS(harpNumbers);
-		int[] harpData = SoundUtil.extractAmplitudeDataFromAudioInputStream(harpAIS);
+		int[] harpData = Amplitude.extractAmplitudeDataFromAudioInputStream(harpAIS);
 		System.out.println(Arrays.toString(harpData));
 
 		//System.out.println(stream);
@@ -95,7 +95,7 @@ public class SoundUtil {
 		//playByteAIS(applauseNumbers);
 
 		AudioInputStream applauseAIS = byteToAIS(applauseNumbers);
-		int[] applauseNumbers2 = extractAmplitudeDataFromAudioInputStream(applauseAIS);
+		int[] applauseNumbers2 = Amplitude.extractAmplitudeDataFromAudioInputStream(applauseAIS);
 		//System.out.println(Arrays.toString(applauseNumbers2));
 
 
@@ -291,106 +291,6 @@ public class SoundUtil {
 		Clip byteAudioClip = makeClip(byteAudio);
 		playClip(byteAudioClip);
 	}
-
-	//Methods from GT - used to extract amplitude from recorded wave
-	
-	/**
-	 * Method that inputs an AudioInputStream, obtains the format of the stream and forms an array of bytes 
-	 * based on the size of the stream, and returns a method call to extractAmplitudeDataFromAmplitudeByteArray(). 
-	 * 
-	 * @param audioInputStream stream of audio being converted into amplitude data
-	 * @return  method call that extracts amplitude data from byte array formed
-	 */
-	public static int[] extractAmplitudeDataFromAudioInputStream(AudioInputStream audioInputStream) {  
-		AudioFormat format = audioInputStream.getFormat();  
-		byte[] audioBytes = new byte[(int) (audioInputStream.getFrameLength() * format.getFrameSize())];  
-		try {  
-			audioInputStream.read(audioBytes);  
-		} catch (IOException e) {  
-			System.out.println("IOException during reading audioBytes");  
-			e.printStackTrace();  
-		}  
-		return extractAmplitudeDataFromAmplitudeByteArray(format, audioBytes);  //calls method that extracts amplitude data from byte array formed
-	}  
-	
-	/**
-	 * Method that inputs the format of an AudioInputStream as well as the byte array formed from its contents
-	 * and then creates an array of ints containing the amplitude data of the stream. 
-	 * 
-	 * @param format AudioFormat of AudioinputStream
-	 * @param audioBytes byte array formed based on the size of the stream
-	 * @return int array containing amplitude data from stream
-	 */
-	private static int[] extractAmplitudeDataFromAmplitudeByteArray(AudioFormat format, byte[] audioBytes) {  
-		// convert
-		int[]  audioData = null;  
-		if (format.getSampleSizeInBits() == 16) {  
-			int nlengthInSamples = audioBytes.length / 2;  
-			audioData = new int[nlengthInSamples];  
-			if (format.isBigEndian()) {  
-				for (int i = 0; i < nlengthInSamples; i++) {  
-					/* First byte is MSB (high order) */  
-					int MSB = audioBytes[2 * i];  
-					/* Second byte is LSB (low order) */  
-					int LSB = audioBytes[2 * i + 1];  
-					audioData[i] = (MSB << 8 | (255 & LSB));  
-				}  
-			} else {  
-				for (int i = 0; i < nlengthInSamples; i++) {  
-					/* First byte is LSB (low order) */  
-					int LSB = audioBytes[2 * i];  
-					/* Second byte is MSB (high order) */  
-					int MSB = audioBytes[2 * i + 1];  
-					audioData[i] = (MSB << 8 | (255 & LSB));  
-				}  
-			}  
-		} else if (format.getSampleSizeInBits() == 8) {  
-			int nlengthInSamples = audioBytes.length;  
-			audioData = new int[nlengthInSamples];  
-			if (format.getEncoding().toString().startsWith("PCM_SIGN")) {  
-				// PCM_SIGNED  
-				for (int i = 0; i < audioBytes.length; i++) {  
-					audioData[i] = audioBytes[i];  
-				}  
-			} else {  
-				// PCM_UNSIGNED  
-				for (int i = 0; i < audioBytes.length; i++) {  
-					audioData[i] = audioBytes[i] - 128;  
-				}  
-			}  
-		}
-		return audioData;  
-	}
-
-	//CPPN 
-	
-	/**
-	 * Creates a double array of amplitudes using a CPPN. CPPN has three inputs - time, frequency,
-	 * and a bias. It only has one output, which is the amplitude. The array of inputs is looped through 
-	 * so that each index is manipulated by the CPPN, and the output indexes are saved into a result array 
-	 * of doubles. 
-	 * 
-	 * @param CPPN network used to generate amplitude 
-	 * @param length length of sample
-	 * @param frequency Frequency of note being manipulated
-	 * @return array of doubles representing all CPPN-manipulated output amplitudes
-	 */
-	public static double[] amplitudeGenerator(Network CPPN, int length, int frequency) {
-		double[] result = new double[length];
-		for(double time = 0; time < length; time++) {
-			//double[] inputs = new double[]{time/StdAudio.SAMPLE_RATE, Math.sin(2*Math.PI * frequency * time/StdAudio.SAMPLE_RATE), HyperNEATCPPNGenotype.BIAS};
-			//double[] inputs = new double[]{time/StdAudio.SAMPLE_RATE, ActivationFunctions.triangleWave(2*Math.PI * frequency * time/StdAudio.SAMPLE_RATE), HyperNEATCPPNGenotype.BIAS};
-			double[] inputs = new double[]{time/StdAudio.SAMPLE_RATE, 
-					Math.sin(2*Math.PI * frequency * time/StdAudio.SAMPLE_RATE), 
-//					ActivationFunctions.triangleWave(2*Math.PI * frequency * time/StdAudio.SAMPLE_RATE), 
-//					ActivationFunctions.squareWave(2*Math.PI * frequency * time/StdAudio.SAMPLE_RATE), 
-					HyperNEATCPPNGenotype.BIAS};
-			double[] outputs = CPPN.process(inputs);
-			result[(int) time] = outputs[0]; // amplitude
-		}
-		return result;
-	}
-
 
 
 
