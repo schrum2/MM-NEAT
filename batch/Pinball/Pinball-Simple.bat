d0a515fa62f1573d4766db430f6f3136cda88bf5
DarwinJohnson <johnso17@southwestern.edu>
2017-05-16 10:21:15 -0500

    #126 Simple Batch File Change
    Changed the Pinball-Simple Batch file to save to a directory named
    Simple instead of Control; small artifact left over from the original.

--- batch/Pinball/Pinball-Simple.bat
+++ batch/Pinball/Pinball-Simple.bat
@@ -1,3 +1,2 @@
 cd ..
 cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:pinball trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.pinball.PinballTask cleanOldNetworks:true fs:false log:Pinball-Control saveTo:Control watch:false pinballConfig:pinball_simple_single.cfg
