162db399790840fcfeb96e1948552358a2bd6bef
DarwinJohnson <johnso17@southwestern.edu>
2017-05-16 11:50:13 -0500

    #303 Fixed Minor Error in MO Batch File
    Fixed the Save directory name; was previously called MOHardHard. Now is
    just called MOHard.

--- batch/Pinball/Pinball-MOHard.bat
+++ batch/Pinball/Pinball-MOHard.bat
@@ -1,3 +1,3 @@
 cd ..
 cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:pinball trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.pinball.PinballTask cleanOldNetworks:true fs:false log:Pinball-MOHard saveTo:MOHardHard watch:false pinballConfig:pinball_hard_single.cfg moPinball:true
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:pinball trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.pinball.PinballTask cleanOldNetworks:true fs:false log:Pinball-MOHard saveTo:MOHard watch:false pinballConfig:pinball_hard_single.cfg moPinball:true
