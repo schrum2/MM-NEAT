ed86d69a689371a79b04ef0c1e4abed9c34e8d26
Alice Quintanilla <quintana@southwestern.edu>
2017-05-17 13:58:34 -0500

    Initial work on NN Evaluation Function. Still not finished.

--- src/edu/utexas/cs/nn/tasks/microrts/NNEvaluationFunction.java
+++ src/edu/utexas/cs/nn/tasks/microrts/NNEvaluationFunction.java
@@ -1,62 +1,46 @@
 package edu.utexas.cs.nn.tasks.microrts;
 
+import edu.utexas.cs.nn.evolution.genotypes.Genotype;
+import edu.utexas.cs.nn.networks.Network;
 import micro.ai.evaluation.EvaluationFunction;
 import micro.rts.GameState;
 import micro.rts.PhysicalGameState;
 import micro.rts.units.Unit;
 
-public class NNEvaluationFunction extends EvaluationFunction{
+public class NNEvaluationFunction<T extends Network> extends EvaluationFunction{
+	
+	Network nn;
 	//score modifier for these categories
 	public static float RESOURCE_IN_BASE = 20; //UnitAction defines resource to not include unharvested resources
     public static float RESOURCE_IN_WORKER = 10;
     public static float UNIT_BONUS_MULTIPLIER = 40.0f;
-    public static float BUILDING_MULTIPLIER = -1; //TODO use this somehow
+    public static float BUILDING_MULTIPLIER = -1;
+    
+    public NNEvaluationFunction(Genotype<T> g){
+    	nn = g.getPhenotype();
+    }
     
 	/**
 	 * @param maxplayer - player to be evaluated
 	 * @param minplayer - opponent
 	 * @param gs - specified state of the game
-	 * @return the amount of points that the player has more than the opponent 
-	 * 			(0 is a tie and negative results mean evaluated player is losing)
+	 * @return 
 	 */
 	@Override
 	public float evaluate(int maxplayer, int minplayer, GameState gs) {
-		return base_score(maxplayer,gs) - base_score(minplayer,gs);
+		return -1f;
 	}
 	
-	/**
-	 * determines how good a player is doing in a given GameState
-	 * @param playerID 
-	 * 				controller/agent being evaluated
-	 * @param GameState gs
-	 * 				specified state of the game 
-	 * @return calculated score for this player at the specified game state
-	 */
-	public float base_score(int playerID, GameState gs){
-		PhysicalGameState pgs = gs.getPhysicalGameState();
-		float score = gs.getPlayer(playerID).getResources()*RESOURCE_IN_BASE;
-		for(Unit u:pgs.getUnits()) {
-            if (u.getPlayer()==playerID) {
-                score += u.getResources() * RESOURCE_IN_WORKER;
-                score += UNIT_BONUS_MULTIPLIER * (u.getCost()*u.getHitPoints())/(float)u.getMaxHitPoints(); //may need to change this to give more of a gradient
-            }
-        }
-        return score;
+	private double[] gameStateToArray(GameState gs){
+		double[] board = new double[0];
+		return null;
 	}
 
 	/**
-	 * adds up all remaining unharvested resources
-	 * @param GameState gs
-	 * 				specified state of the game  
-	 * @return total unused resources
+	 * maximum possible thing returned by the evaluation function
 	 */
 	@Override
 	public float upperBound(GameState gs) {
-        PhysicalGameState pgs = gs.getPhysicalGameState();
-        int free_resources = 0;
-        for(Unit u:pgs.getUnits()) {
-            if (u.getPlayer()==-1) free_resources+=u.getResources();
-        }
-		return free_resources;
+        return 1f;
 	}
 }
