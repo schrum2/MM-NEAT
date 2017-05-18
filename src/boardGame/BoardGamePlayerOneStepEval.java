2067ac8e4e4f48483ec2bb51b5664b4c5d58761e
DarwinJohnson <johnso17@southwestern.edu>
2017-05-18 16:50:01 -0500

    #313 Created BoardGamePlayerOneStepEval
    Created a general OneStepEval Player for BoardGames. Will be implemented
    soon.

---
+++ src/boardGame/BoardGamePlayerOneStepEval.java
@@ -0,0 +1,16 @@
+package boardGame;
+
+import edu.utexas.cs.nn.networks.Network;
+
+public class BoardGamePlayerOneStepEval<T extends Network> implements BoardGamePlayer {
+
+	public BoardGamePlayerOneStepEval(){
+	}
+	
+	@Override
+	public BoardGameState takeAction(BoardGameState current) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
