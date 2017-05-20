ae96c2f43988d58f46108b39eaf5e1fa2d2e964c
Jacob Schrum <schrum2@southwestern.edu>
2017-05-20 14:35:01 -0500

    Removed batch files that can't run
    These do cause memory errors, even with a smaller population. Just have
    to use HNSmudge instead of HyperNEAT on the full screen.

--- batch/VizDoom/VizDoom-Basic-HyperNEAT-Watch.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoombasic trials:1 maxGens:100 mu:5 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomBasicShootTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:Basic-HyperNEAT saveTo:HyperNEAT gameWad:freedoom2.wad watch:false stepByStep:false hyperNEAT:true genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype allowMultipleFunctions:true ftype:1 netChangeActivationRate:0.3 watchLastBest:false monitorSubstrates:true showVizDoomInputs:false showCPPN:true substrateGridSize:10 showWeights:true watch:true showNetworks:false
