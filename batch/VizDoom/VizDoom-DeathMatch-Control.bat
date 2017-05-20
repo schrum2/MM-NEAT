cf168b121068bea2ac10a29e5322988ec7997614
Jacob Schrum <schrum2@southwestern.edu>
2017-05-20 14:56:12 -0500

    Reduced pop size and trials in all
    All VizDoom evals are intensive, so to make progress I made the
    population sizes smaller and reduced the number of trials. This is at
    least appropriate for trial and error testing, though any "real"
    experiments we run may need to have these parameters changed again.

--- batch/VizDoom/VizDoom-DeathMatch-Control.bat
+++ batch/VizDoom/VizDoom-DeathMatch-Control.bat
@@ -1,3 +1,3 @@
 cd ..
 cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoomdeathmatch trials:10 maxGens:100 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomDeathMatchTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:DeathMatch-Control saveTo:Control gameWad:freedoom2.wad doomEpisodeLength:4200
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoomdeathmatch trials:5 maxGens:100 mu:10 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomDeathMatchTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:DeathMatch-Control saveTo:Control gameWad:freedoom2.wad doomEpisodeLength:4200 printFitness:true
--- batch/VizDoom/VizDoom-HealthGather-Control.bat
+++ batch/VizDoom/VizDoom-HealthGather-Control.bat
@@ -1,3 +1,3 @@
 cd ..
 cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoomhealthgather trials:10 maxGens:100 mu:50 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomHealthGatherTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:HealthGather-Control saveTo:Control gameWad:freedoom2.wad doomEpisodeLength:10000
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:vizdoomhealthgather trials:5 maxGens:100 mu:10 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.vizdoom.VizDoomHealthGatherTask cleanOldNetworks:true fs:false noisyTaskStat:edu.utexas.cs.nn.util.stats.Average log:HealthGather-Control saveTo:Control gameWad:freedoom2.wad doomEpisodeLength:10000 printFitness:true
