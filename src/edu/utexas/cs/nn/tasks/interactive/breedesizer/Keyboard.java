7dd9735e90d58f4d18a03ac2ba3b76d2e94a8ed5
twerisa <twerasei@southwestern.edu>
2017-05-22 14:57:28 -0500

    #312 Black keys now work!
    Modified mouseClicked() so that black keys can now play. This involved
    adding -1 at regular intervals in the double array of black key
    frequencies so that they would be spaced out properly, and initializing
    a boolean that is set to true if there is a black key in the region, so
    that white key is set as default when the boolean is false. Added extra
    -1s at the end of the double array of black keys to avoid
    IndexOutOfBoundException when playing the top two notes.

--- src/edu/utexas/cs/nn/tasks/interactive/breedesizer/Keyboard.java
+++ src/edu/utexas/cs/nn/tasks/interactive/breedesizer/Keyboard.java
@@ -2,20 +2,16 @@ package edu.utexas.cs.nn.tasks.interactive.breedesizer;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import edu.utexas.cs.nn.networks.Network;
 import edu.utexas.cs.nn.tasks.interactive.InteractiveEvolutionTask;
-import edu.utexas.cs.nn.util.graphics.DrawingPanel;
 import edu.utexas.cs.nn.util.sound.MiscSoundUtil;
 import edu.utexas.cs.nn.util.sound.SoundAmplitudeArrayManipulator;
 
@@ -50,11 +46,11 @@ public class Keyboard extends JFrame implements MouseListener{
 
 	public static final double[] KEYBOARD = new double[]{C3, CSHARP3, D3, DSHARP3, E3, F3, FSHARP3, G3, GSHARP3, A3, ASHARP3, B3, C4, CSHARP4, D4, DSHARP4, E4, F4, FSHARP4, G4, GSHARP4, A4, ASHARP4, B4, C5};
 	public static final double[] WHITE_KEYS = new double[]{C3, D3, E3, F3, G3, A3, B3, C4, D4, E4, F4, G4, A4, B4, C5};
-	public static final double[] BLACK_KEYS = new double[]{CSHARP3, DSHARP3, -1, FSHARP3, GSHARP3, ASHARP3, -1, CSHARP4, DSHARP4, -1, FSHARP4, GSHARP4, ASHARP4};
-	
+	public static final double[] BLACK_KEYS = new double[]{CSHARP3, -1, DSHARP3, -1, -1, -1, FSHARP3, -1, GSHARP3, -1, ASHARP3, -1, -1,  -1, CSHARP4, -1, DSHARP4, -1, -1, -1, FSHARP4, -1, GSHARP4, -1, ASHARP4, -1, -1, -1};
+
 	public static final String[] PRENOTE = {"C3","D3","E3","F3","G3","A3","B3","C4","D4","E4","F4","G4","A4","B4","C5"};
 	public static final String[] PREEXTENDEDNOTE = {"C3","C#3","D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3", "C4","C#4","D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4", "C5"};
-	
+
 	//variables used for drawing keyboard graphics
 	public static final int KEYBOARD_WIDTH = 600;
 	public static final int KEYBOARD_HEIGHT = 200;
@@ -62,104 +58,116 @@ public class Keyboard extends JFrame implements MouseListener{
 	public static final int BLACK_KEY_WIDTH = 20;	
 	public static final int BLACK_KEY_START_WIDTH = 30;
 	public static final int BLACK_KEY_START_HEIGHT = 100;
-	public static final int WINDOW_EDGE_WIDTH = 8; //to account for construction of graphics being 8 pixels off
-	
-	public static final int NOTE_LENGTH_DEFAULT = 60000;
-	
-	
+	public static final int WINDOW_EDGE_WIDTH = 8; //to account for construction of graphics being 8 pixels off (width)
+	public static final int WINDOW_EDGE_HEIGHT = 30; //to account for construction of graphics being 30 pixels off (height)
 
+	public static final int NOTE_LENGTH_DEFAULT = 40000;
+	
+	/**
+	 * Inner class that constructs the graphical piano keyboard interface using JPanel and Graphics.
+	 * 
+	 * @author Isabel Tweraser
+	 *
+	 */
 	class DrawPane extends JPanel{
 		public void paintComponent(Graphics g){
 			//draw on g here e.g.
 			g.setColor(Color.WHITE);
-			g.fillRect(0, 0, KEYBOARD_WIDTH, KEYBOARD_HEIGHT);
+			g.fillRect(0, 0, KEYBOARD_WIDTH, KEYBOARD_HEIGHT); //white background
 			g.setColor(Color.BLACK);
 			for(int i = 0; i < KEYBOARD_WIDTH; i += WHITE_KEY_WIDTH) {
-				g.drawLine(i, 0, i, KEYBOARD_HEIGHT);
+				g.drawLine(i, 0, i, KEYBOARD_HEIGHT); //line divisions for white keys
 			}
 			boolean[] blackKeyPresent = new boolean[]{true,true,false,true,true,true,false}; //follows 2-3 pattern of black keys on standard piano
 			for(int i = 0; BLACK_KEY_START_WIDTH+i*WHITE_KEY_WIDTH < KEYBOARD_WIDTH; i++) {
 				if(blackKeyPresent[i % blackKeyPresent.length])	
-					g.fillRect(BLACK_KEY_START_WIDTH + i*WHITE_KEY_WIDTH, 0, BLACK_KEY_WIDTH, BLACK_KEY_START_HEIGHT);
+					g.fillRect(BLACK_KEY_START_WIDTH + i*WHITE_KEY_WIDTH, 0, BLACK_KEY_WIDTH, BLACK_KEY_START_HEIGHT); //black keys
 			}
-			
-//			for(int i = 30; i < 600; i += 40) {
-//				if(!(i == 110 || i == 270 || i == 400 || i == 650))
-//					g.fillRect(i, 0, 20, 100);
-//			}
 		}
 	}
-
+	
+	/**
+	 * Instance of CPPN used to replicate CPPN generated at different frequencies on computer
+	 */
 	private Network currentCPPN;
-
+	
+	/**
+	 * Keyboard constructor set to desired height and width of window
+	 */
 	public Keyboard() {
 		this(KEYBOARD_WIDTH, KEYBOARD_HEIGHT,"Keyboard");
 
 	}
-
+	
+	/**
+	 * Constructor that sets the size and enables click responses by adding a mouse listener
+	 * 
+	 * @param width of keyboard
+	 * @param height of keyboard
+	 * @param title Desired title of window being constructed
+	 */
 	public Keyboard(int width, int height, String title) {
 		super("Keyboard");
-		//super(width,height,title);
 		setContentPane(new DrawPane());
-		//JFrame frame = new JFrame("Keyboard");
-		//JPanel keyPanel = new JPanel();
-
-		List<String> note = Arrays.asList(PRENOTE);
-
-		List<String> extendedNote = Arrays.asList(PREEXTENDEDNOTE);
-
-		//	    for (String currentNote : note) {
-		//	        JButton key = new JButton(currentNote);
-		//	        key.addActionListener(this);
-		//	        keyPanel.add(key);
-		//	    }
-		//	    
-		//	    for (String currentNote : extendedNote) {
-		//	        JButton key = new JButton(currentNote);
-		//	        key.addActionListener(this);
-		//	        keyPanel.add(key);
-		//	    }
-		//	    
-		//	    frame.add(keyPanel);
-
+		
 		this.setSize(width,height);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.addMouseListener(this);
 
-
 	}
-	//	
-	//	@Override
-	//	public void actionPerformed(ActionEvent e) {
-	//		Object source = e.getSource();
-	//	    if (source instanceof JButton) {
-	//	        JButton but = (JButton) source;
-	//	        String note = but.getText();
-	//	              
-	//	    }		
-	//	}
 	
+	/**
+	 * Used to access current CPPN generated. Called from BreedesizerTask so that current phenotype 
+	 * can be accessed and set to private variable within Keyboard
+	 * 
+	 * @param phenotype input CPPN played in Breedesizer
+	 */
 	public void setCPPN(Network phenotype) {
 		currentCPPN = phenotype;
 	}
-
+	
+	/**
+	 * Details actions that occur (or notes that play) when mouse is clicked. Determined based on screen location
+	 * where mouse is clicked, which is divided by the width of the individual keys. The quotient corresponds to 
+	 * the index of the correct frequency in the array of frequencies that corresponds with either white keys or black keys.
+	 */
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		
+		//System.out.println(e);
+		
+		int indexClicked;
+		double freq;
 		if(currentCPPN == null) return;
 		
-		int indexClicked = (int) (e.getPoint().getX()-WINDOW_EDGE_WIDTH) / WHITE_KEY_WIDTH;
-		double freq = WHITE_KEYS[indexClicked];
-		double[] amplitude = SoundAmplitudeArrayManipulator.amplitudeGenerator(currentCPPN, NOTE_LENGTH_DEFAULT, freq, InteractiveEvolutionTask.getInputMultipliers());
-		MiscSoundUtil.playDoubleArray(amplitude);
+		boolean played = false; //used for top half of keyboard (determines whether to treat areas in top half of keyboard as black or white keys)
+		
+		if(e.getPoint().getY() < BLACK_KEY_START_HEIGHT + WINDOW_EDGE_HEIGHT && e.getPoint().getX() >= (BLACK_KEY_START_WIDTH + WINDOW_EDGE_WIDTH)) { //top half of keyboard
+			indexClicked = (int) (e.getPoint().getX()-WINDOW_EDGE_WIDTH-BLACK_KEY_START_WIDTH) / BLACK_KEY_WIDTH;
+			freq = BLACK_KEYS[indexClicked];
+			if(freq != -1) { //if there is a black key in the region
+				double[] amplitude = SoundAmplitudeArrayManipulator.amplitudeGenerator(currentCPPN, NOTE_LENGTH_DEFAULT, freq, InteractiveEvolutionTask.getInputMultipliers());
+				MiscSoundUtil.playDoubleArray(amplitude);
+				played = true; //black key, so should not be set to white key index
+			}
+		}
+
+		if(!played) {
+			// white key
+			indexClicked = (int) (e.getPoint().getX()-WINDOW_EDGE_WIDTH) / WHITE_KEY_WIDTH;
+			freq = WHITE_KEYS[indexClicked];
+			double[] amplitude = SoundAmplitudeArrayManipulator.amplitudeGenerator(currentCPPN, NOTE_LENGTH_DEFAULT, freq, InteractiveEvolutionTask.getInputMultipliers());
+			MiscSoundUtil.playDoubleArray(amplitude);
+		}
 		
 		System.out.println(e.getPoint());
-		System.out.println(indexClicked);
+		//System.out.println(indexClicked);
 
 	}
 
 	// Not used
+	
 	@Override
 	public void mousePressed(MouseEvent e) {
 	}
