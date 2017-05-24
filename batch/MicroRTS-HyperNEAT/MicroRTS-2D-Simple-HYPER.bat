a76c6fcf998536c98ef3e18ad96caca66f5c7ced
Alice Quintanilla <quintana@southwestern.edu>
2017-05-24 14:50:41 -0500

    new batch files and parameters for agents in MicrorRTSTask!

---
+++ batch/MicroRTS-HyperNEAT/MicroRTS-2D-Simple-HYPER.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:microRTS trials:1 maxGens:500 mu:10 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.microrts.MicroRTSTask cleanOldNetworks:true fs:false log:MicroRTS-2DSimpleHYPER saveTo:2DSimpleHYPER watch:false microRTSEvaluationFunction:edu.utexas.cs.nn.tasks.microrts.evaluation.NN2DEvaluationFunction hyperNEAT:true genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype allowMultipleFunctions:true ftype:1 netChangeActivationRate:0.3
