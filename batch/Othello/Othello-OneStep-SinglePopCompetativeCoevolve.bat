0691d257446c301ce51bb38a1964778a39c79fa9
DarwinJohnson <johnso17@southwestern.edu>
2017-06-02 09:50:02 -0500

    #323 BoardGame Subdir Organized
    The BoardGame subdir has been broken up into several subdirs organized
    by Board Game Task. All existing Batch Files have been slightly re-named
    to clarify their function. In addtion, HyperNEAT SinglePopulation
    CompetativeCoevolution Batch Files have been created for Othello and
    TicTacToe.

---
+++ batch/Othello/Othello-OneStep-SinglePopCompetativeCoevolve.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:othello trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.boardGame.SinglePopulationCompetativeCoevolutionBoardGameTask cleanOldNetworks:true fs:false log:Othello-OneStep-SinglePopCompetativeCoevolve saveTo:SinglePopCompetativeCoevolve boardGame:boardGame.othello.Othello
---
+++ batch/TicTacToe/TicTacToe-OneStepHN-StaticOpponent-Random.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:ttt trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.boardGame.StaticOpponentBoardGameTask cleanOldNetworks:true fs:false log:TTT-OneStepHN-StaticOpponent-Random saveTo:HN-StaticOpponent-Random genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype boardGame:boardGame.ttt.TicTacToe hyperNEAT:true
