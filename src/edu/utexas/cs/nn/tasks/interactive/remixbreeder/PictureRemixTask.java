687c75171ca2de1bd62973d35a43e2312a29813e
twerisa <twerasei@southwestern.edu>
2017-06-05 11:12:28 -0500

    #355 Secondary constructor for PictureRemix
    Created a secondary constructor for PictureRemix that does the same
    thing as the secondary constructor for ImageMatchTask. Not sure if this
    is right, just wasn't sure how to access the dimensions of the image
    otherwise

--- src/edu/utexas/cs/nn/tasks/interactive/remixbreeder/PictureRemixTask.java
+++ src/edu/utexas/cs/nn/tasks/interactive/remixbreeder/PictureRemixTask.java
@@ -1,5 +1,11 @@
 package edu.utexas.cs.nn.tasks.interactive.remixbreeder;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import edu.utexas.cs.nn.networks.Network;
 import edu.utexas.cs.nn.parameters.Parameters;
 import edu.utexas.cs.nn.tasks.interactive.picbreeder.PicbreederTask;
@@ -9,14 +15,28 @@ public class PictureRemixTask<T extends Network> extends PicbreederTask {
 	public static final int CPPN_NUM_INPUTS	= 7;
 	
 	public String inputImage;
+	public int imageHeight;
+	public int imageWidth;
+	
+	private BufferedImage img = null;
 
 	public PictureRemixTask() throws IllegalAccessException {
 		super();
-		inputImage = Parameters.parameters.stringParameter("matchImageFile");
+		inputImage = Parameters.parameters.stringParameter("matchImageFile");	
 		
-		//do I need to use ImageIO and read in the image to access the height and width?
-		
-		
+	}
+	
+	//do I need to use ImageIO and read in the image to access the height and width?
+	
+	public PictureRemixTask(String filename) throws IllegalAccessException{
+		try {// throws and exception if filename is not valid
+			img = ImageIO.read(new File(inputImage + "\\" + filename));
+		} catch (IOException e) {
+			System.out.println("Could not load image: " + filename);
+			System.exit(1);
+		}
+		imageHeight = img.getHeight();
+		imageWidth = img.getWidth();
 	}
 	
 	@Override
