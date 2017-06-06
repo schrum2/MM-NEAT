98302ab115598b4a6eb9a5ffd20634c3e973134f
twerisa <twerasei@southwestern.edu>
2017-06-06 15:55:02 -0500

    #319 Made utility class for animations

---
+++ src/edu/utexas/cs/nn/util/graphics/AnimationUtil.java
@@ -0,0 +1,16 @@
+package edu.utexas.cs.nn.util.graphics;
+
+/**
+ * Series of utility methods used to create and manipulate
+ * animations. Mainly used by AnimationBreederTask
+ * 
+ * @author Isabel Tweraser
+ *
+ */
+public class AnimationUtil {
+
+	
+	//need method that returns Image[] produced by animating CPPN
+	
+	
+}
