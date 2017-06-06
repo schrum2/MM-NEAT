594527496e6d750b36d53c990c0cbeaa57f50a43
DarwinJohnson <johnso17@southwestern.edu>
2017-06-06 13:47:43 -0500

    #353 Replaced Minimax in Batch Files
    All Batch Files that used the MinimaxPlayer now use the
    AlphaBetaMinimaxPlayer. The BoardGame subdirs have been renamed to
    include "BoardGame-" as a prefix so that they're all sorted together. In
    addition, several Checkers Batch Files have been created, using the
    Othello Batch Files as a base. The appropriate edits have been made so
    that the files otherwise run Checkers.

---
+++ batch/BoardGame-Othello/Othello-OneStepHN-StaticOpponent-Minimax-PieceDiff.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:othello trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.boardGame.StaticOpponentBoardGameTask cleanOldNetworks:true fs:false log:Othello-OneStepHN-StaticOpponent-Minimax-PieceDiff saveTo:HN-StaticOpponent-Minimax-PieceDiff boardGame:boardGame.othello.Othello boardGameOpponent:boardGame.BoardGamePlayerMinimaxAlphaBetaPruning boardGameOpponentHeuristic:boardGame.heuristics.PieceDifferentialBoardGameHeuristic genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype hyperNEAT:true
