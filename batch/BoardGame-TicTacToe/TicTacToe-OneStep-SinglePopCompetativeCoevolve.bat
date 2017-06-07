db78d228c83fda475c7a3a21d3903896579eb4ba
DarwinJohnson <johnso17@southwestern.edu>
2017-06-07 12:06:38 -0500

    TicTacToe Batch Files Renamed
    The TicTacToe Batch Files have been renamed to fit the existing name
    convention.

--- batch/BoardGame-TicTacToe/TicTacToe-OneStep-SinglePopCompetativeCoevolve.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:ttt trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.boardGame.SinglePopulationCompetativeCoevolutionBoardGameTask cleanOldNetworks:true fs:false log:TicTacToe-OneStep-SinglePopCompetativeCoevolve saveTo:SinglePopCompetativeCoevolve boardGame:boardGame.ttt.TicTacToe
---
+++ batch/BoardGame-TicTacToe/TicTacToe-OneStepSinglePopCompCoevolve.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:ttt trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.boardGame.SinglePopulationCompetativeCoevolutionBoardGameTask cleanOldNetworks:true fs:false log:TicTacToe-OneStepSinglePopCompCoevolve saveTo:OneStepSinglePopCompCoevolve boardGame:boardGame.ttt.TicTacToe
