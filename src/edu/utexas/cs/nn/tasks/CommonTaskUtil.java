648f04557a86ea66dcbc79127d6cf64c053821b1
DarwinJohnson <johnso17@southwestern.edu>
2017-06-08 09:24:33 -0500

    #368 Created CommonTaskUtil Class
    Created the CommonTaskUtil Class in the edu.utexas.cs.nn.tasks Package.
    Added a static method called getDrawingPanels() that takes in a Genotype
    and returns a Pair of DrawingPanels based on the Genotype received.

---
+++ src/edu/utexas/cs/nn/tasks/CommonTaskUtil.java
@@ -0,0 +1,29 @@
+package edu.utexas.cs.nn.tasks;
+
+import edu.utexas.cs.nn.evolution.genotypes.Genotype;
+import edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype;
+import edu.utexas.cs.nn.evolution.genotypes.TWEANNGenotype;
+import edu.utexas.cs.nn.networks.TWEANN;
+import edu.utexas.cs.nn.util.graphics.DrawingPanel;
+import javafx.util.Pair;
+
+public class CommonTaskUtil {
+	
+	public static Pair<DrawingPanel, DrawingPanel> getDrawingPanels(Genotype gene){
+		
+		DrawingPanel panel = null;
+		DrawingPanel cppnPanel = null;
+		
+		if(gene instanceof TWEANNGenotype){ // Genotype must be a TWEANNGenotype to draw on either Drawing Panel
+
+			panel = new DrawingPanel(TWEANN.NETWORK_VIEW_DIM, TWEANN.NETWORK_VIEW_DIM, "Evolved Network " + gene.getId());
+			
+			if(gene instanceof HyperNEATCPPNGenotype){ // Genotype must be a HyperNEATCPPNGenotype to draw the cppnPanel
+				cppnPanel = new DrawingPanel(500, 500, "Evolved CPPN");
+			}
+		}
+		
+		return new Pair<DrawingPanel, DrawingPanel>(panel, cppnPanel);
+	}
+	
+}
