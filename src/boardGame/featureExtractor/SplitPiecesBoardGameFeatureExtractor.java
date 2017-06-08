fc0532633a4b2279d0acf5e78cd4f612d32063fb
DarwinJohnson <johnso17@southwestern.edu>
2017-06-08 16:52:30 -0500

    #322 Added FeatureLabels to SplitPiecesExctractor
    Implemented the getFeatureLabels() method in the
    SplitPiecesFeatureExtractor.

--- src/boardGame/featureExtractor/SplitPiecesBoardGameFeatureExtractor.java
+++ src/boardGame/featureExtractor/SplitPiecesBoardGameFeatureExtractor.java
@@ -8,13 +8,16 @@ import boardGame.BoardGameState;
 public class SplitPiecesBoardGameFeatureExtractor<T extends BoardGameState> implements BoardGameFeatureExtractor<T> {
 
 	static final double EMPTY = -1; // Value typically used to represent an Empty Space in the Board Games
-	
+	static int descriptionLength;
+	static int numPlayers;
 	@Override
 	public double[] getFeatures(T bgs) {
 
 		double[] description = bgs.getDescriptor(); // Stores a Description of the Board Game
 		double[] result = new double[bgs.getNumPlayers()*description.length]; // Stores the Pieces for every Player; each Player has a segment equal to the length of the Description
-		
+		descriptionLength = description.length;
+		numPlayers = bgs.getNumPlayers();
+				
 		List<Double> usedPieces = new ArrayList<Double>(); // Keeps track of which Pieces have been extracted
 		
 		int resultIndex = 0; // Keeps track of which Index in result to add Pieces to
@@ -40,8 +43,16 @@ public class SplitPiecesBoardGameFeatureExtractor<T extends BoardGameState> impl
 
 	@Override
 	public String[] getFeatureLabels() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String[] labels = new String[numPlayers*descriptionLength];
+		
+		for(int i = 0; i < numPlayers; i++){
+			for(int j = 0; j < descriptionLength; j++){
+				labels[i*j] = "P" + i + " space: " + j;
+			}
+		}
+		
+		return labels;
 	}
 
 }
