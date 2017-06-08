f1d687761934c35b08407c4911c45bebf7121c23
Alice Quintanilla <quintana@southwestern.edu>
2017-06-08 15:38:25 -0500

    was supposed to be in last commit

--- src/edu/utexas/cs/nn/tasks/microrts/iterativeevolution/CompetitiveEnemySequence.java
+++ src/edu/utexas/cs/nn/tasks/microrts/iterativeevolution/CompetitiveEnemySequence.java
@@ -1,5 +1,7 @@
 package edu.utexas.cs.nn.tasks.microrts.iterativeevolution;
 
+import java.util.ArrayList;
+
 import micro.ai.RandomBiasedAI;
 import micro.ai.abstraction.partialobservability.POLightRush;
 import micro.ai.abstraction.partialobservability.POWorkerRush;
@@ -16,17 +18,31 @@ import micro.ai.mcts.naivemcts.NaiveMCTS;
 public class CompetitiveEnemySequence implements EnemySequence{
 
 	private static final int gensPerEnemy = 20;
-	
+	private ArrayList<AI> appropriateEnemies = new ArrayList<>();
+	private boolean parent = true;
+
 	private final AI[] enemies = new AI[]{
 			new RandomBiasedAI(), //used in competition
 			new POWorkerRush(), //used in competition
-			new POLightRush(),  //used in competition
+			new POLightRush(), //used in competition
 			new NaiveMCTS(), //used in competition
-		};
-	
+	};
+
 	@Override
-	public AI getAppropriateEnemy(int generation) {
-		return enemies[Math.min(generation/gensPerEnemy, enemies.length-1)]; 
+	public ArrayList<AI> getAppropriateEnemy(int generation) {
+		if(growingSet){
+			if(parent){
+				if(generation % gensPerEnemy == 0 && appropriateEnemies.size() < enemies.length){
+					appropriateEnemies.add(enemies[generation*gensPerEnemy]);
+					parent = false;
+				}
+			} else { //eval children
+				parent = true;
+			}
+			return appropriateEnemies;
+		}else
+			appropriateEnemies.add(enemies[Math.min(generation/gensPerEnemy, enemies.length-1)]);
+		return appropriateEnemies;
 	}
 
 }
