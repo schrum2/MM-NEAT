38b61a49e501e4fd64504f40ab1be307ba5ff7f8
DarwinJohnson <johnso17@southwestern.edu>
2017-06-09 11:42:38 -0500

    #361 Cleaned up Othello Batch Files
    The Othello Batch Files have been modified to all use the
    WeightedPieceCounter Heursitic. In addition, the Coevolution Tasks that
    previously used OneStepEval now use Minimax with AlphaBetaPruning.
    Similar clean-ups will be done with the other BoardGame Domains as
    progress is made with Fitness Functions, Heuristics, and Player
    creation.

--- batch/BoardGame-Othello/Othello-HNStaticAlphaBetaPD.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-cd ..
-java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:othello trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.boardGame.StaticOpponentBoardGameTask cleanOldNetworks:true fs:false log:Othello-HNStaticAlphaBetaPD saveTo:HNStaticAlphaBetaPD boardGame:boardGame.othello.Othello boardGameOpponent:boardGame.agents.treesearch.BoardGamePlayerMinimaxAlphaBetaPruning boardGameOpponentHeuristic:boardGame.heuristics.PieceDifferentialBoardGameHeuristic boardGamePlayer:boardGame.agents.treesearch.BoardGamePlayerMinimaxAlphaBetaPruning genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype hyperNEAT:true
