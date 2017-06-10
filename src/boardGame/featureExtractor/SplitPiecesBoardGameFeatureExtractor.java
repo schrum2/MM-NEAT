23d34ec245ae59ecda38ce679f5c6e98ef8a7027
Jacob Schrum <schrum2@southwestern.edu>
2017-06-09 22:59:00 -0500

    Changed static fields to non-static
    Was not sure why these were static. Hopefully changing them does not
    cause a problem.
    @DarwinJohnson

--- src/boardGame/featureExtractor/SplitPiecesBoardGameFeatureExtractor.java
+++ src/boardGame/featureExtractor/SplitPiecesBoardGameFeatureExtractor.java
@@ -7,9 +7,10 @@ import boardGame.BoardGameState;
 
 public class SplitPiecesBoardGameFeatureExtractor<T extends BoardGameState> implements BoardGameFeatureExtractor<T> {
 
-	static final double EMPTY = -1; // Value typically used to represent an Empty Space in the Board Games
-	static int descriptionLength;
-	static int numPlayers;
+	public static final double EMPTY = -1; // Value typically used to represent an Empty Space in the Board Games
+	int descriptionLength;
+	int numPlayers;
+	
 	@Override
 	public double[] getFeatures(T bgs) {
 
@@ -17,41 +18,33 @@ public class SplitPiecesBoardGameFeatureExtractor<T extends BoardGameState> impl
 		double[] result = new double[bgs.getNumPlayers()*description.length]; // Stores the Pieces for every Player; each Player has a segment equal to the length of the Description
 		descriptionLength = description.length;
 		numPlayers = bgs.getNumPlayers();
-				
+
 		List<Double> usedPieces = new ArrayList<Double>(); // Keeps track of which Pieces have been extracted
-		
+
 		int resultIndex = 0; // Keeps track of which Index in result to add Pieces to
-		
+
 		do{ // Adds the Pieces from the Board to the result Array
-		
-		double currentPiece = EMPTY; // Used to keep track of which piece is being extracted; initialized to EMPTY
-
-		for(double d : description){ // Cycles through the Description and adds to the result Array
-
-			if(currentPiece == EMPTY && !usedPieces.contains(d)){ // Hasn't been changed yet, and hasn't been used before
-				currentPiece = d;
-				usedPieces.add(d); // Adds the value to usedPieces; unable to re-use this value
+			double currentPiece = EMPTY; // Used to keep track of which piece is being extracted; initialized to EMPTY
+			for(double d : description){ // Cycles through the Description and adds to the result Array
+				if(currentPiece == EMPTY && !usedPieces.contains(d)){ // Hasn't been changed yet, and hasn't been used before
+					currentPiece = d;
+					usedPieces.add(d); // Adds the value to usedPieces; unable to re-use this value
+				}
+				if(d == EMPTY || d == currentPiece) result[resultIndex++] = d; // If Empty Space or currentPiece, add it to result
 			}
-			
-			if(d == EMPTY || d == currentPiece) result[resultIndex++] = d; // If Empty Space or currentPiece, add it to result
-		}
-		
-		}while(resultIndex < result.length); // Keeps adding to result until result is full
-		
+		} while(resultIndex < result.length); // Keeps adding to result until result is full
+
 		return result;
 	}
 
 	@Override
 	public String[] getFeatureLabels() {
-		
 		String[] labels = new String[numPlayers*descriptionLength];
-		
 		for(int i = 0; i < numPlayers; i++){
 			for(int j = 0; j < descriptionLength; j++){
 				labels[i*j] = "P" + i + " space: " + j;
 			}
 		}
-		
 		return labels;
 	}
 
