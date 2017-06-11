4ccfbc86a25c0eadfd64de5808aad0a4bc0899dc
Jacob Schrum <schrum2@southwestern.edu>
2017-06-11 11:40:10 -0500

    Fixed typo

--- batch/Experiments-2015-GECCO-MsPacMan/LAUNCHALL.bat
+++ batch/Experiments-2015-GECCO-MsPacMan/LAUNCHALL.bat
@@ -10,7 +10,7 @@ start launchMultiple.bat Experiments-2015-GECCO-MsPacMan MsPacMan-ImprisonConfli
 start launchMultiple.bat Experiments-2015-GECCO-MsPacMan MsPacMan-ImprisonConflict-Multitask.bat 0 29
 start launchMultiple.bat Experiments-2015-GECCO-MsPacMan MsPacMan-ImprisonConflict-MMD.bat 0 29
 REM Blended domain
-start launchMultiple.bat Experiments-2015-GECCO--MsPacMan MsPacMan-OneLifeConflict-OneModule.bat 0 29
+start launchMultiple.bat Experiments-2015-GECCO-MsPacMan MsPacMan-OneLifeConflict-OneModule.bat 0 29
 start launchMultiple.bat Experiments-2015-GECCO-MsPacMan MsPacMan-OneLifeConflict-TwoModules.bat 0 29
 start launchMultiple.bat Experiments-2015-GECCO-MsPacMan MsPacMan-OneLifeConflict-ThreeModules.bat 0 29
 start launchMultiple.bat Experiments-2015-GECCO-MsPacMan MsPacMan-OneLifeConflict-TwoModuleMultitask.bat 0 29
