bd9e393f01778ee90e4f03f6a953fa9d2bc9ea02
twerisa <twerasei@southwestern.edu>
2017-06-12 10:40:17 -0500

    #360 Loading files in Breedesizer
    Added code that loads files in Breedesizer and fixed an issue with
    mismatched indexes that resulted (two repondToClick options were
    responding to the exact same index value). This seems to work for
    choosing a new file, but the code crashes if a file with more than one
    track because of the nature of our MIDI playback code. Need to fix this
    so it accounts for multiple tracks

--- src/edu/utexas/cs/nn/tasks/interactive/breedesizer/BreedesizerTask.java
+++ src/edu/utexas/cs/nn/tasks/interactive/breedesizer/BreedesizerTask.java
@@ -49,6 +49,8 @@ public class BreedesizerTask<T extends Network> extends InteractiveEvolutionTask
 
 	public static final int CPPN_NUM_INPUTS	= 3;
 	public static final int CPPN_NUM_OUTPUTS = 1;
+	
+	private static final int FILE_LOADER_CHECKBOX_INDEX = CHECKBOX_IDENTIFIER_START - (CPPN_NUM_INPUTS+CPPN_NUM_OUTPUTS);
 
 	Keyboard keyboard;
 	protected JSlider clipLength;
@@ -110,6 +112,12 @@ public class BreedesizerTask<T extends Network> extends InteractiveEvolutionTask
 			top.add(playWithMIDI);
 		}
 		initializationComplete = true;
+		
+		JButton fileLoadButton = new JButton();
+		fileLoadButton.setText("ChooseNewSound");
+		fileLoadButton.setName("" + FILE_LOADER_CHECKBOX_INDEX);
+		fileLoadButton.addActionListener(this);
+		top.add(fileLoadButton);
 	}
 
 	/**
@@ -125,9 +133,23 @@ public class BreedesizerTask<T extends Network> extends InteractiveEvolutionTask
 		super.respondToClick(itemID);
 		// Play original sound if they click the button
 		if(itemID == (CHECKBOX_IDENTIFIER_START - inputMultipliers.length)) {
+			System.out.println("checkbox identifier - # of input multipliers: " + (CHECKBOX_IDENTIFIER_START - inputMultipliers.length));
+			System.out.println("file loader checkbox index: " + FILE_LOADER_CHECKBOX_INDEX);
 			if(!justStopped) { // Pressing original button can stop playback too
 				midiPlay = MIDIUtil.playMIDIWithCPPNFromString(Parameters.parameters.stringParameter("remixMIDIFile"), currentCPPN);
 			}
+		}
+		super.respondToClick(itemID);
+		if(itemID == FILE_LOADER_CHECKBOX_INDEX) {
+			JFileChooser chooser = new JFileChooser();//used to get new file
+			chooser.setApproveButtonText("Open");
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("MIDI Files", "mid");
+			chooser.setFileFilter(filter);
+			int returnVal = chooser.showOpenDialog(frame);
+			if(returnVal == JFileChooser.APPROVE_OPTION) {//if the user decides to save the image
+				Parameters.parameters.setString("remixMIDIFile", chooser.getCurrentDirectory() + "\\" + chooser.getSelectedFile().getName());
+			}
+			resetButtons();
 		}
 	}
 	
