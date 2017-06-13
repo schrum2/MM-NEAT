043d65433366ee23b73f7a8c273c1e2c497529e7
twerisa <twerasei@southwestern.edu>
2017-06-13 14:24:28 -0500

    Minor explanatory comments on RealValuedGenotype

--- src/edu/utexas/cs/nn/evolution/genotypes/RealValuedGenotype.java
+++ src/edu/utexas/cs/nn/evolution/genotypes/RealValuedGenotype.java
@@ -16,23 +16,44 @@ public class RealValuedGenotype extends NumericArrayGenotype<Double> {
 	public RealValuedGenotype(ArrayList<Double> genes) {
 		super(genes);
 	}
-
+	
+	/**
+	 * Constructs a RealValuedGenotype from an input double array
+	 * 
+	 * @param genes representative double array of genes
+	 */
 	public RealValuedGenotype(double[] genes) {
 		super(ArrayUtil.primitiveDoubleArrayToDoubleArray(genes));
 	}
-
+	
+	/**
+	 * Constructs an array of random values based on an input size
+	 * 
+	 * @param size desired size of array
+	 */
 	public RealValuedGenotype(int size) {
 		this(RandomNumbers.randomArray(size));
 	}
 
+	/**
+	 * Uses constructor to create an ArrayList<Double> of the 
+	 * genes
+	 */
 	public Genotype<ArrayList<Double>> copy() {
 		return new RealValuedGenotype(genes);
 	}
-
+	
+	/**
+	 * Creates new genotype of the same size as the original
+	 * array of genes
+	 */
 	public Genotype<ArrayList<Double>> newInstance() {
 		return new RealValuedGenotype(genes.size());
 	}
-
+	
+	/**
+	 * Mutates genotype
+	 */
 	public void mutate() {
 		new PerturbMutation(genes.size()).mutate(this);
 	}
