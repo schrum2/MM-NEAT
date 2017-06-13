aae2857a0afe2475e060093b7af0e9850dcf5e27
DarwinJohnson <johnso17@southwestern.edu>
2017-06-13 16:18:43 -0500

    Comments for FitnessFunction and FeatureExtractor
    Added Header Comments to the BoardGameFitnessFunction and
    BoardGameFeatureExtractor Interfaces.

--- src/boardGame/featureExtractor/BoardGameFeatureExtractor.java
+++ src/boardGame/featureExtractor/BoardGameFeatureExtractor.java
@@ -4,6 +4,12 @@ import boardGame.BoardGameState;
 
 public interface BoardGameFeatureExtractor<T extends BoardGameState> {
 	
+	/**
+	 * Returns a Double Array representing a given BoardGameState
+	 * 
+	 * @param bgs BoardGameState to be evaluated
+	 * @return Double Array representing the given BoardGameState
+	 */
 	public double[] getFeatures(T bgs);
 	
 	/**
--- src/boardGame/fitnessFunction/BoardGameFitnessFunction.java
+++ src/boardGame/fitnessFunction/BoardGameFitnessFunction.java
@@ -4,11 +4,32 @@ import boardGame.BoardGameState;
 
 public interface BoardGameFitnessFunction<T extends BoardGameState> {
 	
+	/**
+	 * Returns the Fitness of a given Board Game Agent after a single Board Game
+	 * 
+	 * @return Double representing the Fitness of a given Board Game Agent
+	 */
 	public double getFitness();
 	
+	/**
+	 * Updates a given Board Game Agent's Fitness over the course
+	 * of one Board Game
+	 * 
+	 * @param bgs Updated Board Game State to be evaluated
+	 * @param index Index representing the Board Game Agent to be evaluated
+	 */
 	public void updateFitness(T bgs, int index);
 	
+	/**
+	 * Returns the name of the Fitness Function being used
+	 * 
+	 * @return String containing the name of the Fitness Function
+	 */
 	public String getFitnessName();
 	
+	/**
+	 * Resets the Fitness Function to 0 to avoid
+	 * accumulating scores over multiple generations
+	 */
 	public void reset();
 }
