decfa183cc00bf41c5c3db8251237f11012c3eff
twerisa <twerasei@southwestern.edu>
2017-06-19 12:03:26 -0500

    #407 added drawTriangles() method
    Fixed variable access issues I was having with JPanel and JSliders and
    created a method called drawTriangles() that renders an input list of
    triangles as a cube within a graphics instance.

--- src/edu/utexas/cs/nn/tasks/interactive/objectbreeder/Construct3DObject.java
+++ src/edu/utexas/cs/nn/tasks/interactive/objectbreeder/Construct3DObject.java
@@ -40,143 +40,12 @@ public class Construct3DObject {
 		@SuppressWarnings("serial")
 		JPanel renderPanel = new JPanel() {
 			public void paintComponent(Graphics g) {
-				int vertexNum = 100;
-				Graphics2D g2 = (Graphics2D) g;
-				g2.setColor(Color.BLACK);
-				g2.fillRect(0, 0, getWidth(), getHeight());
-
-				Vertex center = new Vertex(0,0,0);
-				
+				Vertex center = new Vertex(0,0,0);				
 				List<Triangle> tris = cubeConstructor(center, 100.0, Color.RED);
-
-//				List<Triangle> tris = new ArrayList<>();
-//				tris.add(new Triangle(center.add(new Vertex(vertexNum, vertexNum, vertexNum)),
-//						center.add(new Vertex(vertexNum, vertexNum, -vertexNum)),
-//						center.add(new Vertex(-vertexNum, vertexNum, -vertexNum)),
-//						Color.ORANGE));				
-//				tris.add(new Triangle(center.add(new Vertex(-vertexNum, vertexNum, vertexNum)),
-//						center.add(new Vertex(vertexNum, vertexNum, vertexNum)),
-//						center.add(new Vertex(-vertexNum, vertexNum, -vertexNum)),
-//						Color.ORANGE));
-//
-//				tris.add(new Triangle(center.add(new Vertex(-vertexNum, vertexNum, vertexNum)),
-//						center.add(new Vertex(-vertexNum, -vertexNum, vertexNum)),
-//						center.add(new Vertex(vertexNum, vertexNum, vertexNum)),
-//						Color.GREEN));
-//				tris.add(new Triangle(center.add(new Vertex(vertexNum, vertexNum, vertexNum)),
-//						center.add(new Vertex(-vertexNum, -vertexNum, vertexNum)),
-//						center.add(new Vertex(vertexNum, -vertexNum, vertexNum)),
-//						Color.GREEN));
-//
-//				tris.add(new Triangle(center.add(new Vertex(vertexNum, vertexNum, vertexNum)),
-//						center.add(new Vertex(vertexNum, vertexNum, -vertexNum)),
-//						center.add(new Vertex(vertexNum, -vertexNum, vertexNum)),
-//						Color.RED));
-//				tris.add(new Triangle(center.add(new Vertex(vertexNum, -vertexNum, -vertexNum)),
-//						center.add(new Vertex(vertexNum, -vertexNum, vertexNum)),
-//						center.add(new Vertex(vertexNum, vertexNum, -vertexNum)),
-//						Color.RED));
-//
-//				tris.add(new Triangle(center.add(new Vertex(-vertexNum, -vertexNum, vertexNum)),
-//						center.add(new Vertex(-vertexNum, -vertexNum, -vertexNum)),
-//						center.add(new Vertex(vertexNum, -vertexNum, vertexNum)),
-//						Color.BLUE));
-//				tris.add(new Triangle(center.add(new Vertex(vertexNum, -vertexNum, -vertexNum)),
-//						center.add(new Vertex(vertexNum, -vertexNum, vertexNum)),
-//						center.add(new Vertex(-vertexNum, -vertexNum, -vertexNum)),
-//						Color.BLUE));
-//
-//				tris.add(new Triangle(center.add(new Vertex(-vertexNum, -vertexNum, -vertexNum)),
-//						center.add(new Vertex(-vertexNum, vertexNum, -vertexNum)),
-//						center.add(new Vertex(vertexNum, -vertexNum, -vertexNum)),
-//						Color.WHITE));
-//				tris.add(new Triangle(center.add(new Vertex(vertexNum, vertexNum, -vertexNum)),
-//						center.add(new Vertex(vertexNum, -vertexNum, -vertexNum)),
-//						center.add(new Vertex(-vertexNum, vertexNum, -vertexNum)),
-//						Color.WHITE));
-//
-//				tris.add(new Triangle(center.add(new Vertex(-vertexNum, -vertexNum, -vertexNum)),
-//						center.add(new Vertex(-vertexNum, vertexNum, -vertexNum)),
-//						center.add(new Vertex(-vertexNum, -vertexNum, vertexNum)),
-//						Color.YELLOW));
-//				tris.add(new Triangle(center.add(new Vertex(-vertexNum, vertexNum, vertexNum)),
-//						center.add(new Vertex(-vertexNum, -vertexNum, vertexNum)),
-//						center.add(new Vertex(-vertexNum, vertexNum, -vertexNum)),
-//						Color.YELLOW));
-
+				Graphics2D g2 = (Graphics2D) g;
 				double heading = Math.toRadians(headingSlider.getValue());
-				Matrix3 headingTransform = new Matrix3(new double[] {
-						Math.cos(heading), 0, -Math.sin(heading),
-						0, 1, 0,
-						Math.sin(heading), 0, Math.cos(heading)
-				});
 				double pitch = Math.toRadians(pitchSlider.getValue());
-				Matrix3 pitchTransform = new Matrix3(new double[] {
-						1, 0, 0,
-						0, Math.cos(pitch), Math.sin(pitch),
-						0, -Math.sin(pitch), Math.cos(pitch)
-				});
-				Matrix3 transform = headingTransform.multiply(pitchTransform);
-
-				BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
-
-				double[] zBuffer = new double[img.getWidth() * img.getHeight()];
-				// initialize array with extremely far away depths
-				for (int q = 0; q < zBuffer.length; q++) {
-					zBuffer[q] = Double.NEGATIVE_INFINITY;
-				}
-
-				for (Triangle t : tris) {
-					Vertex v1 = transform.transform(t.v1);
-					v1.x += getWidth() / 2;
-					v1.y += getHeight() / 2;
-					Vertex v2 = transform.transform(t.v2);
-					v2.x += getWidth() / 2;
-					v2.y += getHeight() / 2;
-					Vertex v3 = transform.transform(t.v3);
-					v3.x += getWidth() / 2;
-					v3.y += getHeight() / 2;
-
-					Vertex ab = new Vertex(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
-					Vertex ac = new Vertex(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z);
-					Vertex norm = new Vertex(
-							ab.y * ac.z - ab.z * ac.y,
-							ab.z * ac.x - ab.x * ac.z,
-							ab.x * ac.y - ab.y * ac.x
-							);
-					double normalLength = Math.sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z);
-					norm.x /= normalLength;
-					norm.y /= normalLength;
-					norm.z /= normalLength;
-
-					double angleCos = Math.abs(norm.z);
-
-					int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
-					int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
-					int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
-					int maxY = (int) Math.min(img.getHeight() - 1, Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
-
-					double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
-
-					for (int y = minY; y <= maxY; y++) {
-						for (int x = minX; x <= maxX; x++) {
-							double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
-							double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
-							double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
-							if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
-								double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
-								int zIndex = y * img.getWidth() + x;
-								if (zBuffer[zIndex] < depth) {
-									img.setRGB(x, y, getShade(t.color, angleCos).getRGB());
-									zBuffer[zIndex] = depth;
-								}
-							}
-						}
-					}
-
-				}
-
-				g2.drawImage(img, 0, 0, null);
+				drawTriangles(g2,tris,getWidth(), getHeight(), heading, pitch);
 			}
 		};
 		pane.add(renderPanel, BorderLayout.CENTER);
@@ -186,6 +55,93 @@ public class Construct3DObject {
 
 		frame.setSize(400, 400);
 		frame.setVisible(true);
+	}
+	
+	/**
+	 * Renders list of triangles as a cube. 
+	 * 
+	 * @param g2 graphics instance
+	 * @param tris representative list of triangles
+	 * @param width width of image
+	 * @param height height of image
+	 * @param heading input heading value for JSlider
+	 * @param pitch input pitch value for JSlider
+	 */
+	protected static void drawTriangles(Graphics2D g2, List<Triangle> tris, int width, int height, double heading, double pitch) {
+		g2.setColor(Color.BLACK);
+		g2.fillRect(0, 0, width, height);
+
+		Matrix3 headingTransform = new Matrix3(new double[] {
+				Math.cos(heading), 0, -Math.sin(heading),
+				0, 1, 0,
+				Math.sin(heading), 0, Math.cos(heading)
+		});
+		Matrix3 pitchTransform = new Matrix3(new double[] {
+				1, 0, 0,
+				0, Math.cos(pitch), Math.sin(pitch),
+				0, -Math.sin(pitch), Math.cos(pitch)
+		});
+		Matrix3 transform = headingTransform.multiply(pitchTransform);
+
+		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+		double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+		// initialize array with extremely far away depths
+		for (int q = 0; q < zBuffer.length; q++) {
+			zBuffer[q] = Double.NEGATIVE_INFINITY;
+		}
+
+		for (Triangle t : tris) {
+			Vertex v1 = transform.transform(t.v1);
+			v1.x += width / 2;
+			v1.y += height / 2;
+			Vertex v2 = transform.transform(t.v2);
+			v2.x += width / 2;
+			v2.y += height / 2;
+			Vertex v3 = transform.transform(t.v3);
+			v3.x += width / 2;
+			v3.y += height / 2;
+
+			Vertex ab = new Vertex(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
+			Vertex ac = new Vertex(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z);
+			Vertex norm = new Vertex(
+					ab.y * ac.z - ab.z * ac.y,
+					ab.z * ac.x - ab.x * ac.z,
+					ab.x * ac.y - ab.y * ac.x
+					);
+			double normalLength = Math.sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z);
+			norm.x /= normalLength;
+			norm.y /= normalLength;
+			norm.z /= normalLength;
+
+			double angleCos = Math.abs(norm.z);
+
+			int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+			int maxX = (int) Math.min(img.getWidth() - 1, Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+			int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+			int maxY = (int) Math.min(img.getHeight() - 1, Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+
+			double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+
+			for (int y = minY; y <= maxY; y++) {
+				for (int x = minX; x <= maxX; x++) {
+					double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+					double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+					double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+					if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+						double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+						int zIndex = y * img.getWidth() + x;
+						if (zBuffer[zIndex] < depth) {
+							img.setRGB(x, y, getShade(t.color, angleCos).getRGB());
+							zBuffer[zIndex] = depth;
+						}
+					}
+				}
+			}
+
+		}
+
+		g2.drawImage(img, 0, 0, null);
 	}
 
 	public static Color getShade(Color color, double shade) {
