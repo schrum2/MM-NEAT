3551d071727a3d9295e863f8ffa4842631a7a01e
DarwinJohnson <johnso17@southwestern.edu>
2017-06-19 13:34:13 -0500

    #411 Tested Othello WPC
    The Othello Weighted Piece Counter Heuristic now has a JUnit Test
    associated with it, and the test currently works. There were some issues
    with rounding errors, but the heuristic itself functions.

--- src/boardGame/heuristics/StaticOthelloWPCHeuristicTest.java
+++ src/boardGame/heuristics/StaticOthelloWPCHeuristicTest.java
@@ -32,7 +32,7 @@ public class StaticOthelloWPCHeuristicTest {
 	OthelloState start;
 	OthelloState test1;
 	OthelloState test2;
-	
+	OthelloState test3;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -61,6 +61,17 @@ public class StaticOthelloWPCHeuristicTest {
 	    							 {E,E,E,E,E,E,E,E}};//  7
 
 	    	  test2 = new OthelloState(board2, B, new ArrayList<Integer>());
+	    	  
+	   int[][] board3 = new int[][]{{W,E,E,E,E,E,E,B}, //  0: Same as the above, but with the Players reversed; should be the same absolute value
+	    							{E,W,E,E,E,E,B,E}, //  1
+	  	    						{E,E,W,E,E,B,E,E}, //  2
+	  	    						{E,E,E,W,B,E,E,E}, //  3
+	  	    						{E,E,E,B,W,E,E,E}, //  4
+	  	    						{E,E,B,E,E,W,E,E}, //  5
+	  	    						{E,B,E,E,E,E,W,E}, //  6
+	  	    						{B,E,E,E,E,E,E,W}};//  7
+
+	  	    	test3 = new OthelloState(board3, B, new ArrayList<Integer>());
 	}
 	
 	@Test
@@ -68,8 +79,9 @@ public class StaticOthelloWPCHeuristicTest {
 		// All evals are from Player 1's perspective; weights for Player 2 are negative
 		assertEquals(0.0, bgh.heuristicEvalution(start), 0.0);
 		// TODO: More Tests
-		assertEquals(0.19002, bgh.heuristicEvalution(test1), 0.0);
-		assertEquals(-0.19002, bgh.heuristicEvalution(test2), 0.0);
+		assertEquals(0.21, bgh.heuristicEvalution(test1), 0.00001);
+		assertEquals(-0.21, bgh.heuristicEvalution(test2), 0.00001);
+		assertEquals(0.0, bgh.heuristicEvalution(test3), 0.00001);
 	}
 
 }
