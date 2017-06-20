35ac9e51bc3e23a23ca3e2d06fc5eff987f350bb
Alice Quintanilla <quintana@southwestern.edu>
2017-06-20 13:19:36 -0500

    added WinLossFitnessFunction
    "it's like a whole new fitness funtion"

--- src/edu/utexas/cs/nn/tasks/microrts/fitness/WinLossFitnessFunction.java
+++ src/edu/utexas/cs/nn/tasks/microrts/fitness/WinLossFitnessFunction.java
@@ -5,21 +5,15 @@ import java.util.ArrayList;
 import edu.utexas.cs.nn.util.datastructures.Pair;
 import micro.rts.GameState;
 
-/**
- * fitness function that uses data tracked in MicroRTSUtility.oneEval()
- * 
- * @author alicequint
- */
-public class ProgressiveFitnessFunction extends RTSFitnessFunction{
-
-	public ProgressiveFitnessFunction(){}
-
+public class WinLossFitnessFunction extends ProgressiveFitnessFunction{
+	
 	/**
 	 * uses information collected over time in MicroRTSUtility.oneEval
 	 */
 	@Override
 	public ArrayList<Pair<double[], double[]>> getFitness(GameState gs) {
-		ArrayList<Pair<double[], double[]>> result = new ArrayList<>(); 
+		ArrayList<Pair<double[], double[]>> result = new ArrayList<>();
+		int winner = gs.winner(); //0:win 1:loss -1:tie
 		double[] fitness = new double[]{
 				normalize(task.getHarvestingEfficiency(1), gameEndTime),
 				normalize(task.getBaseUpTime(1), gameEndTime),
@@ -27,6 +21,9 @@ public class ProgressiveFitnessFunction extends RTSFitnessFunction{
 				// normalize() assumes that the results range from 0 to a maximum number, whereas unit differences is 0 on average
 				// and very unlikely to be anywhere close to the max; +1 makes it so that (-2 to 0) => (-1 to 1)
 				normalize(task.getPercentEnemiesDestroyed(1), 100),
+				
+				(winner == 1 ? -1 : winner + 1), //changes 0:win, -1:tie, 1:loss  => 1:win 0:tie -1:loss (from ai1's perspective)
+				
 		};
 		double[] opponentFitness = new double[fitness.length];
 		if(coevolution){
@@ -35,10 +32,7 @@ public class ProgressiveFitnessFunction extends RTSFitnessFunction{
 			opponentFitness[2] = fitness[2] * -1; // * -1 because each players UnitDifference score will be the Reverse of the other's.
 			opponentFitness[3] = normalize(task.getPercentEnemiesDestroyed(2), 100);
 		}
-
-		int winner = gs.winner(); //0:win 1:loss -1:tie
 		double[]other = new double[]{
-				(winner == 1 ? -1 : winner + 1), //changes 0:win, -1:tie, 1:loss  => 1:win 0:tie -1:loss (from ai1's perspective)
 				gs.getTime(),
 				mapSwitches, //updated when map is switched (belongs to parent)
 				enemySwitches,
@@ -53,22 +47,11 @@ public class ProgressiveFitnessFunction extends RTSFitnessFunction{
 	
 	@Override
 	public String[] getFunctions() {
-			return new String[]{"Harvesting Efficiency","Time Base was Alive","Average Unit Difference","% Enemies Destroyed"};//Fitness
+			return new String[]{"Harvesting Efficiency","Time Base was Alive","Average Unit Difference","% Enemies Destroyed","Win/Loss"};//Fitness
 	}
 	
 	@Override
 	public String[] getOtherScores(){
-		return new String[]{"win?","time game lasted","map switches","enemy switches","number of game state evals"}; //Other
+		return new String[]{"time game lasted","map switches","enemy switches","number of game state evals"}; //Other
 	}
-
-	/**
-	 * precondition: max > 0, min = 0.
-	 * @param value :value to be converted
-	 * @param max largest possible number in this category
-	 * @return value on a scale from -1 to 1 with 1 being max
-	 */
-	protected double normalize(double value, double max){
-		return (2*value - max) / max;
-	}
-
 }
