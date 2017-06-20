3a047c9fe4a5bab42553a197f1302042c0176bf6
twerisa <twerasei@southwestern.edu>
2017-06-20 16:54:20 -0500

    #318 Got 3DObjectbreeder to run
    Overrode a few methods to get 3DObjectBreeder to run. It's producing
    something interesting, but still needs debugging and needs to override
    more things in AnimationBreederTask

--- src/edu/utexas/cs/nn/tasks/interactive/objectbreeder/ThreeDimensionalObjectBreederTask.java
+++ src/edu/utexas/cs/nn/tasks/interactive/objectbreeder/ThreeDimensionalObjectBreederTask.java
@@ -1,6 +1,13 @@
 package edu.utexas.cs.nn.tasks.interactive.objectbreeder;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.FileNotFoundException;
+
+import edu.utexas.cs.nn.MMNEAT.MMNEAT;
+import edu.utexas.cs.nn.networks.Network;
 import edu.utexas.cs.nn.tasks.interactive.animationbreeder.AnimationBreederTask;
+import edu.utexas.cs.nn.util.graphics.AnimationUtil;
 
 /**
  * Interface that interactively evolves three-dimensional
@@ -12,6 +19,15 @@ import edu.utexas.cs.nn.tasks.interactive.animationbreeder.AnimationBreederTask;
  *
  */
 public class ThreeDimensionalObjectBreederTask extends AnimationBreederTask {
+	public static final int CUBE_SIDE_LENGTH = 10;
+	public static final int SHAPE_WIDTH = 10;
+	public static final int SHAPE_HEIGHT = 20;
+	public static final int SHAPE_DEPTH = 10;
+	public static final Color COLOR = Color.RED;
+	
+	public static final int CPPN_NUM_INPUTS = 4;
+	public static final int CPPN_NUM_OUTPUTS = 1;
+					
 
 	public ThreeDimensionalObjectBreederTask() throws IllegalAccessException {
 		super();
@@ -19,8 +35,47 @@ public class ThreeDimensionalObjectBreederTask extends AnimationBreederTask {
 	}
 	
 	@Override
+	public String[] sensorLabels() {
+		return new String[] { "X-coordinate", "Y-coordinate", "Z-coordinate", "bias" };
+	}
+
+	@Override
+	public String[] outputLabels() {
+		return new String[] { "cube present" };
+	}
+	
+	@Override
 	protected String getWindowTitle() {
 		return "3DObjectBreeder";
+	}
+	
+	@Override
+	public int numCPPNInputs() {
+		return CPPN_NUM_INPUTS;
+	}
+
+	@Override
+	public int numCPPNOutputs() {
+		return CPPN_NUM_OUTPUTS;
+	}
+	
+	@Override
+	protected BufferedImage getButtonImage(Network phenotype, int width, int height, double[] inputMultipliers) {
+		// Just get first frame for button. Slightly inefficent though, since all animation frames were pre-computed
+		// TODO: Magic numbers 0, 20, PI, and 17 were used to tilt the shape in an interesting way, but this needs to be replaced eventually.
+		return Construct3DObject.generate3DObjectFromCPPN(phenotype, picSize, picSize, CUBE_SIDE_LENGTH, SHAPE_WIDTH, SHAPE_HEIGHT, SHAPE_DEPTH, COLOR,  0, 20, Math.PI, getInputMultipliers())[17];
+	}
+	
+	/**
+	 * Allows for quick and easy launching without saving any files
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			MMNEAT.main(new String[]{"runNumber:5","randomSeed:5","trials:1","mu:16","maxGens:500","io:false","netio:false","mating:true","task:edu.utexas.cs.nn.tasks.interactive.objectbreeder.ThreeDimensionalObjectBreederTask","allowMultipleFunctions:true","ftype:0","netChangeActivationRate:0.3","cleanFrequency:-1","recurrency:false","ea:edu.utexas.cs.nn.evolution.selectiveBreeding.SelectiveBreedingEA","imageWidth:500","imageHeight:500","imageSize:200"});
+		} catch (FileNotFoundException | NoSuchMethodException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
