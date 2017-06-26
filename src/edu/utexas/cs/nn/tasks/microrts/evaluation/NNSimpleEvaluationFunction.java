a682c7b8ac13585cf5c8107dedc9f754a1879372
schrum2 <schrum2@utexas.edu>
2017-06-26 16:29:58 -0500

    TODO comments for simple eval function
    @alicequint
    Follow the provided instructions in the comments of this commit in order
    to address #433

--- src/edu/utexas/cs/nn/tasks/microrts/evaluation/NNSimpleEvaluationFunction.java
+++ src/edu/utexas/cs/nn/tasks/microrts/evaluation/NNSimpleEvaluationFunction.java
@@ -2,6 +2,7 @@ package edu.utexas.cs.nn.tasks.microrts.evaluation;
 
 import java.util.Arrays;
 
+import edu.utexas.cs.nn.networks.ActivationFunctions;
 import edu.utexas.cs.nn.networks.Network;
 import micro.rts.GameState;
 import micro.rts.PhysicalGameState;
@@ -9,7 +10,9 @@ import micro.rts.units.Unit;
 
 /**
  * original ef for evolved microRTS agents.
- * NOT compatible with Coevolution(TODO) or HyperNEAT(impossible)
+ * 
+ * NOT compatible with Coevolution(TODO) <--- Still true? - Dr. Schrum
+ * or HyperNEAT(impossible)
  * 
  * @author alicequint
  *
@@ -17,6 +20,7 @@ import micro.rts.units.Unit;
  */
 public class NNSimpleEvaluationFunction<T extends Network> extends NNEvaluationFunction<T> {
 
+	// TODO: Make these all static, and refactor so that the variable names are ALL_CAPS
 	private final int worker = 0;
 	private final int light = 1;
 	private final int heavy = 2;
@@ -39,7 +43,7 @@ public class NNSimpleEvaluationFunction<T extends Network> extends NNEvaluationF
 	 */
 	public double[] gameStateToArray(GameState gs){
 		PhysicalGameState pgs = gs.getPhysicalGameState();
-		double[] unitsOnBoard = new double[18]; //number of different variables we are tracking
+		double[] unitsOnBoard = new double[18]; //number of different variables we are tracking // TODO: Make static final int with ALL_CAPS name
 		unitsOnBoard[workerDelta] = 0;
 		unitsOnBoard[mobileDelta] = 0;
 		int workerDeltaUpdates = 0;
@@ -126,6 +130,12 @@ public class NNSimpleEvaluationFunction<T extends Network> extends NNEvaluationF
 				}
 			}
 		}
+		// TODO: It is important to restrict the ranges of all inputs to the range -1 to 1.
+		//       If you are uncertain about the range, then overestimate a bit (though not too much).
+		//		 You can get a general idea of the range by printing out the values.
+		//       You can also apply ActivationFunctions.tanh if the upper/lower bounds seem to trail off to large values,
+		//       But divide by some normalizing value first.
+		
 		//no longer normalized because the less concrete scores dont have the same max
 		//		unitsOnBoard = normalize(unitsOnBoard, pgs.getHeight()*pgs.getWidth());
 		return unitsOnBoard;
