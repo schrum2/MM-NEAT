221cc7f579385e98a49d48220f42cfea8b777bbf
DarwinJohnson <johnso17@southwestern.edu>
2017-06-26 14:42:18 -0500

    Created several new Checkers Batch Files
    Created new SinglePop Checkers Batch Files. These files incorporate the
    10% Random chance, the Hall of Fame, and/or the Advanced Checkers
    Fitness Function. These are combinations of the above processes to be
    used for my poster; I want to compare the effectiveness of each method
    within the domain of Single Population Competative Coevolution Checkers.

---
+++ batch/BoardGame-Checkers/Checkers-HNSinglePopCompCoevolve10Random.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:checkers trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.boardGame.SinglePopulationCompetativeCoevolutionBoardGameTask cleanOldNetworks:true fs:false log:Checkers-HNSinglePopCompCoevolve10Random saveTo:HNSinglePopCompCoevolve10Random boardGame:boardGame.checkers.Checkers genotype:edu.utexas.cs.nn.evolution.genotypes.HyperNEATCPPNGenotype hyperNEAT:true boardGameOpponent:boardGame.agents.treesearch.BoardGamePlayerMinimaxAlphaBetaPruning boardGameOpponentHeuristic:boardGame.heuristics.PieceDifferentialBoardGameHeuristic boardGamePlayer:boardGame.agents.treesearch.BoardGamePlayerMinimaxAlphaBetaPruning minimaxSearchDepth:2 minimaxRandomRate:0.10
