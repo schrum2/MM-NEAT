e58d7e193a58e8542973053b7d3788137518aa00
Alice Quintanilla <quintana@southwestern.edu>
2017-06-26 14:50:51 -0500

    AllOfPlayerTypeSubstrate created

---
+++ src/edu/utexas/cs/nn/tasks/microrts/evaluation/AllOfPlayerTypeSubstrate.java
@@ -0,0 +1,12 @@
+package edu.utexas.cs.nn.tasks.microrts.evaluation;
+
+import micro.rts.GameState;
+
+public class AllOfPlayerTypeSubstrate extends MicroRTSSubstrateInputs{
+
+	@Override
+	double[][] getInputs(GameState gs) {
+		return null;
+	}
+
+}
