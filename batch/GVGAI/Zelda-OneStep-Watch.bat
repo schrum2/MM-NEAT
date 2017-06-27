9e4c4019a43594ce98690a6bea8dec2c52f80097
DarwinJohnson <johnso17@southwestern.edu>
2017-06-27 14:25:53 -0500

    #287 Refactored GVGAI Batch Files
    Changed the Zelda-SinglePlayer Batch File to Zelda-OneStep to reflect
    the Player it uses. Created Zelda-TreeSearch to use the
    GVGAITreeSearchNNPlayer. Also created a Zelda-TreeSearch-Watch Batch
    File to test the visualization of the GVGAI games.

---
+++ batch/GVGAI/Zelda-OneStep-Watch.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:gvgai trials:10 maxGens:500 mu:100 io:true netio:true mating:true task:edu.utexas.cs.nn.tasks.gvgai.GVGAISinglePlayerTask cleanOldNetworks:true fs:false log:Zelda-ZeldaOneStep saveTo:ZeldaOneStep gvgaiGame:zelda gvgaiLevel:0 gvgaiPlayer:edu.utexas.cs.nn.tasks.gvgai.GVGAIOneStepNNPlayer watch:true
