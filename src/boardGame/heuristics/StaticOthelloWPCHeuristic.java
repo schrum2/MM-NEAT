bc72b8c6c91afac325754c36b1039ad06f3db7b4
DarwinJohnson <johnso17@southwestern.edu>
2017-06-28 14:17:01 -0500

    #446 Created Othello WPC Quick Play
    Addeed a Main Method to the OthelloWPCHeuristic class which will create
    an Othello match with a Human vs the WPC Heuristic when launched. This
    is actually identical to the code in the Othello board game class.

--- src/boardGame/heuristics/StaticOthelloWPCHeuristic.java
+++ src/boardGame/heuristics/StaticOthelloWPCHeuristic.java
@@ -1,6 +1,21 @@
 package boardGame.heuristics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardGame.TwoDimensionalBoardGameState;
+import boardGame.agents.BoardGamePlayer;
+import boardGame.agents.BoardGamePlayerHuman2DBoard;
+import boardGame.agents.treesearch.BoardGamePlayerMinimaxAlphaBetaPruning;
+import boardGame.fitnessFunction.BoardGameFitnessFunction;
+import boardGame.fitnessFunction.SimpleWinLoseDrawBoardGameFitness;
+import boardGame.othello.Othello;
+import boardGame.othello.OthelloState;
+import edu.utexas.cs.nn.MMNEAT.MMNEAT;
+import edu.utexas.cs.nn.parameters.Parameters;
+import edu.utexas.cs.nn.tasks.boardGame.BoardGameUtil;
+import edu.utexas.cs.nn.util.ClassCreation;
+import edu.utexas.cs.nn.util.MiscUtil;
 
 public class StaticOthelloWPCHeuristic<T extends TwoDimensionalBoardGameState> extends WeightedPieceCounterHeuristic<T> {
 
@@ -28,6 +43,37 @@ public class StaticOthelloWPCHeuristic<T extends TwoDimensionalBoardGameState> e
 	 */
 	public StaticOthelloWPCHeuristic() {
 		super(WEIGHTS);
+	}
+	
+	public static void main(String[] args){
+		Parameters.initializeParameterCollections(new String[]{"io:false", "netio:false", "task:edu.utexas.cs.nn.tasks.boardGame.StaticOpponentBoardGameTask",
+				"boardGame:boardGame.othello.Othello", "boardGameOpponent:boardGame.agents.BoardGamePlayerHuman2DBoard",
+				"boardGameOpponentHeuristic:boardGame.heuristics.StaticOthelloWPCHeuristic",
+				"boardGamePlayer:boardGame.agents.treesearch.BoardGamePlayerMinimaxAlphaBetaPruning", "watch:true", "minimaxSearchDepth:6"});
+		
+		MMNEAT.loadClasses();
+		
+		Othello bg = null;
+		
+		try {
+			bg = (Othello) ClassCreation.createObject("boardGame");
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		@SuppressWarnings("unchecked")
+		BoardGamePlayer<OthelloState>[] players = new BoardGamePlayer[]{new BoardGamePlayerMinimaxAlphaBetaPruning<OthelloState>(), new BoardGamePlayerHuman2DBoard<OthelloState>()};
+		
+		List<BoardGameFitnessFunction<OthelloState>> scores = new ArrayList<BoardGameFitnessFunction<OthelloState>>();
+		scores.add(new SimpleWinLoseDrawBoardGameFitness<OthelloState>());
+		
+		BoardGameUtil.playGame(bg, players, scores, new ArrayList<>()); // No Other Scores
+		System.out.println("Game Over: Press Enter");
+		
+		MiscUtil.waitForReadStringAndEnterKeyPress();
+		
+		MMNEAT.boardGameViewer.close();
 	}
 	
 }
