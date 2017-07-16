102575a6b710249b80ecd72e3b36d5bbb320dc30
schrum2 <schrum2@utexas.edu>
2017-07-16 16:54:55 -0500

    Added batch files for advanced interface versions of interactive evolution

---
+++ batch/Interactive-Advanced/Picbreeder-Advanced.bat
@@ -0,0 +1,3 @@
+cd ..
+cd ..
+java -jar dist/MM-NEATv2.jar runNumber:%1 randomSeed:%1 base:picbreeder trials:1 mu:16 maxGens:500 io:true netio:true mating:true fs:true task:edu.utexas.cs.nn.tasks.interactive.picbreeder.PicbreederTask log:Picbreeder-Advanced saveTo:Advanced allowMultipleFunctions:true ftype:0 watch:false netChangeActivationRate:0.3 cleanFrequency:-1 recurrency:false saveInteractiveSelections:true simplifiedInteractiveInterface:false saveAllChampions:true cleanOldNetworks:false logTWEANNData:false logMutationAndLineage:true ea:edu.utexas.cs.nn.evolution.selectiveBreeding.SelectiveBreedingEA imageWidth:2000 imageHeight:2000 imageSize:200
