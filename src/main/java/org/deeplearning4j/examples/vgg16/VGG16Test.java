082dc971ada9d2675333278b3ffdb0d62bca134f
schrum2 <schrum2@utexas.edu>
2017-09-15 15:10:00 -0500

    #27 VGG16 not great on Picbreeder pictures?
    I took some actual Picbreeder pictures from the paper "Understanding
    Innovation Engines ..." and saw how well VGG16 classified them, and the
    results were not great. Need to try VGG19, or preferably AlexNet (since
    this is what the original Innovation Engine used)

--- src/main/java/org/deeplearning4j/examples/vgg16/VGG16Test.java
+++ src/main/java/org/deeplearning4j/examples/vgg16/VGG16Test.java
@@ -22,6 +22,25 @@ public class VGG16Test {
 
 	// Consider turning this file into a unit test instead
 	public static void main(String[] args) throws IOException {
+		// From the original Picbreeder
+		NativeImageLoader loader = new NativeImageLoader(224, 224, 3);
+		results(loader.asMatrix(new File("d:/TEMP/FromRealPicbreeder/butterfly.jpg")), true); // VGG16: hair_slide: 69%
+		MiscUtil.waitForReadStringAndEnterKeyPress();
+		results(loader.asMatrix(new File("d:/TEMP/FromRealPicbreeder/fly-fly.jpg")), true); // VGG16: hair_slide: 12%
+		MiscUtil.waitForReadStringAndEnterKeyPress();
+		results(loader.asMatrix(new File("d:/TEMP/FromRealPicbreeder/latte-cup.jpg")), true); // VGG16: cup: 8%
+		MiscUtil.waitForReadStringAndEnterKeyPress();
+		results(loader.asMatrix(new File("d:/TEMP/FromRealPicbreeder/parrot-toucan.jpg")), true); // VGG16: mouse: 15%
+		MiscUtil.waitForReadStringAndEnterKeyPress();
+		results(loader.asMatrix(new File("d:/TEMP/FromRealPicbreeder/race_car-racer.jpg")), true); // VGG16: speedboat: 68%
+		MiscUtil.waitForReadStringAndEnterKeyPress();
+		results(loader.asMatrix(new File("d:/TEMP/FromRealPicbreeder/red_crayon-lipstick.jpg")), true); // VGG16: cup: 14%
+		MiscUtil.waitForReadStringAndEnterKeyPress();
+		results(loader.asMatrix(new File("d:/TEMP/FromRealPicbreeder/red_skull.jpg")), true); // VGG16: crash_helmet: 45%
+		MiscUtil.waitForReadStringAndEnterKeyPress();
+
+		
+		
 //		NativeImageLoader loader = new NativeImageLoader(224, 224, 3);
 //		results(loader.asMatrix(new File("data/imagematch/car.jpg")), true);
 //		MiscUtil.waitForReadStringAndEnterKeyPress();
@@ -34,19 +53,19 @@ public class VGG16Test {
 //		results(loader.asMatrix(new File("data/imagematch/supercreepypersonimage.jpg")), false);
 //		MiscUtil.waitForReadStringAndEnterKeyPress();		
 		
-		Parameters.initializeParameterCollections(new String[]{"runNumber:0","randomSeed:0","trials:1","mu:16","maxGens:500","io:false","netio:false","mating:true","fs:true","task:edu.southwestern.tasks.interactive.picbreeder.PicbreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","recurrency:false","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageSize:224"});
-		MMNEAT.loadClasses();
-		
-		while(true) {
-			TWEANNGenotype tg = new TWEANNGenotype();
-			for(int i = 0; i < 15; i++) {
-				tg.mutate();
-			}
-			BufferedImage image = GraphicsUtil.imageFromCPPN(tg.getPhenotype(), 224, 224);
-			DrawingPanel p = GraphicsUtil.drawImage(image, "test", 224,224);
-			results(ImageNetClassification.bufferedImageToINDArray(image), false);
-			MiscUtil.waitForReadStringAndEnterKeyPress();				
-		}
+//		Parameters.initializeParameterCollections(new String[]{"runNumber:0","randomSeed:0","trials:1","mu:16","maxGens:500","io:false","netio:false","mating:true","fs:true","task:edu.southwestern.tasks.interactive.picbreeder.PicbreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","recurrency:false","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageSize:224"});
+//		MMNEAT.loadClasses();
+//		
+//		while(true) {
+//			TWEANNGenotype tg = new TWEANNGenotype();
+//			for(int i = 0; i < 15; i++) {
+//				tg.mutate();
+//			}
+//			BufferedImage image = GraphicsUtil.imageFromCPPN(tg.getPhenotype(), 224, 224);
+//			DrawingPanel p = GraphicsUtil.drawImage(image, "test", 224,224);
+//			results(ImageNetClassification.bufferedImageToINDArray(image), false);
+//			MiscUtil.waitForReadStringAndEnterKeyPress();				
+//		}
 	}
 	
 	public static void results(INDArray image, boolean preprocess) {
