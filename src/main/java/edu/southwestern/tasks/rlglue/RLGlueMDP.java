585386766446e6e019a2eee2e298df108c96d03a
schrum2 <schrum2@utexas.edu>
2017-10-02 13:05:14 -0500

    #24 Added back episode length restrictions for RL Glue domains
    The RL Glue MDP now ends episodes if the number of steps exceeds a
    threshold, as in my evolution task.
    
    Incidentally, AcroBot was NOT able to learn successful behavior with RL
    Glue, when the episode length restriction was in place (couldn't learn
    it before either). Why is this task so hard? Can compare with Open AI
    Gym performance?

--- src/main/java/edu/southwestern/tasks/rlglue/RLGlueMDP.java
+++ src/main/java/edu/southwestern/tasks/rlglue/RLGlueMDP.java
@@ -5,11 +5,11 @@ import org.deeplearning4j.rl4j.mdp.MDP;
 import org.deeplearning4j.rl4j.space.ArrayObservationSpace;
 import org.deeplearning4j.rl4j.space.DiscreteSpace;
 import org.deeplearning4j.rl4j.space.ObservationSpace;
-import org.rlcommunity.rlglue.codec.RLGlue;
 import org.rlcommunity.rlglue.codec.types.Action;
 import org.rlcommunity.rlglue.codec.types.Reward_observation_terminal;
 
 import edu.southwestern.MMNEAT.MMNEAT;
+import edu.southwestern.parameters.Parameters;
 
 /**
  * Class to treat RL Glue environments as RL4J MDP instances.
@@ -21,6 +21,8 @@ public class RLGlueMDP implements MDP<EncodableObservation, Integer, DiscreteSpa
 	private DiscreteSpace discreteActionSpace;
 	private ArrayObservationSpace<EncodableObservation> observationSpace;
 	private boolean done;
+	private int steps;
+	private static int maxStepsPerEpisode;
 
 	public RLGlueMDP(RLGlueEnvironment environment) {
 		this.environment = environment;
@@ -29,6 +31,8 @@ public class RLGlueMDP implements MDP<EncodableObservation, Integer, DiscreteSpa
 		// Assume each feature is one aspect of an observation
 		this.observationSpace = new ArrayObservationSpace<>(new int[] {MMNEAT.rlGlueExtractor.numFeatures()});
 		this.done = false;
+		this.steps = 0;
+		this.maxStepsPerEpisode = Parameters.parameters.integerParameter("steps");
 	}
 	
 	@Override
@@ -45,6 +49,7 @@ public class RLGlueMDP implements MDP<EncodableObservation, Integer, DiscreteSpa
 	public EncodableObservation reset() {
 		//System.out.println("reset()");
 		done = false;
+		steps = 0;
 		//RLGlue.RL_cleanup();
 		environment.env_cleanup(); // end episode 
 		//RLGlue.RL_init();
@@ -68,8 +73,10 @@ public class RLGlueMDP implements MDP<EncodableObservation, Integer, DiscreteSpa
 		// RL Glue step
 		//System.out.println("\tenv_step("+action+")");
 		Reward_observation_terminal result = environment.env_step(rlGlueAction);
+		// Increase number of setps
+		steps++; // If episodes have a maximum step count
 		// Remember whether episode just finished
-		done = result.isTerminal();
+		done = result.isTerminal() || steps >= maxStepsPerEpisode;
 		// Repackage in MDP's equivalent to Reward_observation_terminal		
 		return new StepReply<>(new EncodableObservation(result.getObservation()), result.getReward(), done, null);
 	}
