4bdc746b0e40976e4eeaab5f98e01a7f636f5f78
schrum2 <schrum2@utexas.edu>
2017-11-13 11:41:17 -0600

    #9 Can compute content loss
    After switching to feedForward as suggested by the DL4J team, I was able
    to get past the Fatal Error and successfully (?) calculate the content
    loss. Now to move on with the rest.

--- src/main/java/edu/southwestern/util/graphics/NeuralStyleTransfer.java
+++ src/main/java/edu/southwestern/util/graphics/NeuralStyleTransfer.java
@@ -2,6 +2,7 @@ package edu.southwestern.util.graphics;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 import org.datavec.image.loader.NativeImageLoader;
 import org.deeplearning4j.nn.api.Layer;
@@ -12,6 +13,8 @@ import org.deeplearning4j.zoo.model.VGG16;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
 import org.nd4j.linalg.dataset.api.preprocessor.VGG16ImagePreProcessor;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.indexing.NDArrayIndex;
 
 /**
  * My attempt to implement the neural style transfer algorithm in DL4J:
@@ -42,47 +45,63 @@ public class NeuralStyleTransfer {
 	 * @param b Another tensor
 	 * @return Sum of squared errors: scalar
 	 */
-	public static Number sumOfSquaredErrors(INDArray a, INDArray b) {
+	public static double sumOfSquaredErrors(INDArray a, INDArray b) {
 		INDArray diff = a.sub(b); // difference
 		INDArray squares = diff.mul(diff); // element-wise squaring
-		return squares.sumNumber();
+		return squares.sumNumber().doubleValue();
 	}
 	
-	public static double content_loss(ComputationGraph vgg16) {
-		// TODO
-		return Double.NaN;
+	public static double content_loss(Map<String, INDArray> activations) {
+		INDArray block2_conv2_features = activations.get("block2_conv2");
+		INDArray content_features = block2_conv2_features.get(NDArrayIndex.point(0), NDArrayIndex.all(), NDArrayIndex.all(), NDArrayIndex.all());
+		INDArray combination_features = block2_conv2_features.get(NDArrayIndex.point(2), NDArrayIndex.all(), NDArrayIndex.all(), NDArrayIndex.all());
+		
+		System.out.println(content_features.shapeInfoToString());
+		System.out.println(combination_features.shapeInfoToString());
+		
+		return content_weight * sumOfSquaredErrors(content_features, combination_features);
 	}
 	
 	public static void main(String[] args) throws IOException {
+		
+		final int HEIGHT = 224;
+		final int WIDTH = 224;
+		final int CHANNELS = 3;
+		
 		ZooModel zooModel = new VGG16();
 		
 		// Do I need separate copies for each image input?
 		ComputationGraph vgg16 = (ComputationGraph) zooModel.initPretrained(PretrainedType.IMAGENET);
 	
-		NativeImageLoader loader = new NativeImageLoader(224, 224, 3);
+		NativeImageLoader loader = new NativeImageLoader(HEIGHT, WIDTH, CHANNELS);
 		DataNormalization scaler = new VGG16ImagePreProcessor();
 		
 		String contentFile = "data/imagematch/cat.jpg";
 		INDArray content = loader.asMatrix(new File(contentFile));
 		scaler.transform(content);
 		
-//		String styleFile = "data/imagematch/supercreepypersonimage.jpg";
-//		INDArray style = loader.asMatrix(new File(styleFile));
-//		scaler.transform(style);
-//		
-//		System.out.println(vgg16.summary());
+		String styleFile = "data/imagematch/supercreepypersonimage.jpg";
+		INDArray style = loader.asMatrix(new File(styleFile));
+		scaler.transform(style);
+
+		INDArray combination = Nd4j.create(1, CHANNELS, HEIGHT, WIDTH);
 		
-		INDArray[] result = vgg16.output(content);
+		INDArray input = Nd4j.concat(0, content, style, combination);
 		
-		// Is calling activate again necessary? Can't the values just be retrieved?
-		// Does this mean an input presentation has to occur first?
-		Layer block1_conv2 = vgg16.getLayer("block1_conv2");
-		INDArray block1_conv2_features = block1_conv2.activate();
+		//System.out.println(input.shapeInfoToString());
+		//System.out.println(vgg16.summary());
 		
-//		INDArray block2_conv2_features = vgg16.getLayer("block2_conv2").activate();
-//		INDArray block3_conv3_features = vgg16.getLayer("block3_conv3").activate();
-//		INDArray block4_conv3_features = vgg16.getLayer("block4_conv3").activate();
-//		INDArray block5_conv3_features = vgg16.getLayer("block5_conv3").activate();
+		INDArray[] result = vgg16.output(input);
+		Map<String, INDArray> activations = vgg16.feedForward();
+		
+		System.out.println(content_loss(activations));
+		
+		
+//		INDArray block1_conv2_features = activations.get("block1_conv2");		
+//		INDArray block2_conv2_features = activations.get("block2_conv2");
+//		INDArray block3_conv3_features = activations.get("block3_conv3");
+//		INDArray block4_conv3_features = activations.get("block4_conv3");
+//		INDArray block5_conv3_features = activations.get("block5_conv3");
 				
 	}
 }
