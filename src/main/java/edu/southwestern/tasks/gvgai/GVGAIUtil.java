7f73b81384e9c07a369be3a94fb0da7d611b8865
Jacob Schrum <schrum2@southwestern.edu>
2017-11-14 22:14:16 -0600

    #20 Basic level generation with CPPN
    I have a method that can generate a zelda level from a CPPN, though it doesn't add enemies yet ... only walls and unique items.

--- src/main/java/edu/southwestern/tasks/gvgai/GVGAIUtil.java
+++ src/main/java/edu/southwestern/tasks/gvgai/GVGAIUtil.java
@@ -1,5 +1,12 @@
 package edu.southwestern.tasks.gvgai;
 
+import java.util.Arrays;
+
+import edu.southwestern.evolution.genotypes.TWEANNGenotype;
+import edu.southwestern.networks.Network;
+import edu.southwestern.networks.TWEANN;
+import edu.southwestern.parameters.Parameters;
+import edu.southwestern.util.graphics.GraphicsUtil;
 import gvgai.core.competition.CompetitionParameters;
 import gvgai.core.game.Game;
 import gvgai.core.player.AbstractPlayer;
@@ -7,7 +14,6 @@ import gvgai.core.player.Player;
 import gvgai.core.vgdl.VGDLFactory;
 import gvgai.core.vgdl.VGDLParser;
 import gvgai.core.vgdl.VGDLRegistry;
-import gvgai.tools.IO;
 import gvgai.tracks.ArcadeMachine;
 import gvgai.tracks.singlePlayer.tools.human.Agent;
 
@@ -20,6 +26,7 @@ import gvgai.tracks.singlePlayer.tools.human.Agent;
  */
 public class GVGAIUtil {
 
+	public static final double FIXED_ITEM_THRESHOLD = 0.5;
 
 	/**
 	 * Based on a more complicated method in Arcade Machine with the same name.
@@ -73,11 +80,91 @@ public class GVGAIUtil {
 		return toPlay.getFullResult();
 	}	
 	
+	public static String[] generateLevelFromCPPN(Network n, int levelWidth, int levelHeight, 
+			char defaultBackground, char border, char[] fixed, char[] unique, int maxScale) {
+		// Start with 2D char array to fill out level: The +2 is for the border wall.
+		char[][] level = new char[levelHeight+2][levelWidth+2];
+		// Background
+		for(int i = 0; i < level.length; i++) {
+			Arrays.fill(level[i], defaultBackground);
+		}
+		// Border wall
+		for(int y = 0; y < levelHeight+2; y++) { // Vertical walls
+			level[y][0] = border;
+			level[y][levelWidth+1] = border;
+		}		
+		for(int x = 1; x < levelWidth+1; x++) { // Horizontal walls
+			level[0][x] = border;
+			level[levelHeight+1][x] = border;
+		}
+		// Query CPPN
+		double[] uniqueScores = new double[unique.length];
+		// Location with highest score will have the unique item
+		Arrays.fill(uniqueScores, Double.NEGATIVE_INFINITY);
+		int[][] uniqueLocations = new int[unique.length][2];
+		// Query spots within the border
+		for(int y = 1; y < levelHeight+1; y++) {
+			for(int x = 1; x < levelWidth+1; x++) {
+				// Able to use a method from GraphicsUtil here. The -1 is time, which is ignored.
+				double[] inputs = GraphicsUtil.get2DObjectCPPNInputs(x, y, levelWidth, levelHeight, -1);
+				double[] outputs = n.process(inputs);
+				// Check for presence of each fixed item
+				for(int i = 0; i < fixed.length; i++) {
+					if(outputs[i] > FIXED_ITEM_THRESHOLD) {
+						level[y][x] = fixed[i]; // Place item in level
+					}
+				}
+				// Only place unique and scaled items on empty spaces
+				if(level[y][x] == defaultBackground) {
+					
+					
+					// Find maximal output for each unique item
+					for(int i = 0; i < unique.length; i++) {
+						// Store maximal location queried for each unique item
+						if(outputs[i+fixed.length] > uniqueScores[i] && unclaimed(x,y,uniqueLocations)) {
+							uniqueScores[i] = outputs[i+fixed.length];
+							uniqueLocations[i] = new int[]{x,y};
+						}
+					}				
+				}
+			}
+		}
+		// Place the unique items
+		for(int i = 0; i < unique.length; i++) {
+			level[uniqueLocations[i][1]][uniqueLocations[i][0]] = unique[i];
+		}		
+		// Convert to String array
+		String[] stringLevel = new String[levelHeight+2];
+		for(int i = 0; i < level.length; i++) {
+			stringLevel[i] = new String(level[i]);
+		}
+		return stringLevel;
+	}
+	
+	/**
+	 * Make sure that no unique item is currently claiming the spot (x,y)
+	 * @param x x-coordinate
+	 * @param y y-coordinate
+	 * @param uniqueLocations Current claimed unique item locations
+	 * @return Whether the location is free
+	 */
+	private static boolean unclaimed(int x, int y, int[][] uniqueLocations) {
+		for(int i = 0; i < uniqueLocations.length; i++) {
+			if(uniqueLocations[i][0] == x && uniqueLocations[i][1] == y) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 	/**
 	 * For testing and troubleshooting
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Parameters.initializeParameterCollections(new String[] {});
+		//MMNEAT.loadClasses();
+		
 		VGDLFactory.GetInstance().init();
 		VGDLRegistry.GetInstance().init();
 
@@ -103,26 +190,37 @@ public class GVGAIUtil {
 		
 		////////////////////////////////////////////////////////
 		// Allows for playing a Zelda level defined as a String array
+//		Game toPlay = new VGDLParser().parseGame(game_file); // Initialize the game
+//		String[] level = new String[] {
+//			"wwwwwwwwwwwwwwwwwwwww", 
+//			"w..3.ww............Aw", 
+//			"w....w......wwwwwwwww", 
+//			"w.w.....wwwwwwwwwwwww", 
+//			"w.w........1.......1w", 
+//			"w.wwwwwwwwwwwwwwwww.w", 
+//			"w.......w...wwwwwww.w", 
+//			"w...2w....wgww+....3w", 
+//			"wwwwwwwwwwwwwwwwwwwww"	
+//		};
+//
+//		Agent agent = new Agent();
+//		agent.setup(null, seed, true); // null = no log, true = human 
+//
+//		runOneGame(toPlay, level, true, agent, seed, playerID);
+		//////////////////////////////////////////////////////
 		
+		////////////////////////////////////////////////////////
+		// Allows for playing a Zelda level defined as a String array
 		Game toPlay = new VGDLParser().parseGame(game_file); // Initialize the game
-		String[] level = new String[] {
-			"wwwwwwwwwwwwwwwwwwwww", 
-			"w..3.ww............Aw", 
-			"w....w......wwwwwwwww", 
-			"w.w.....wwwwwwwwwwwww", 
-			"w.w........1.......1w", 
-			"w.wwwwwwwwwwwwwwwww.w", 
-			"w.......w...wwwwwww.w", 
-			"w...2w....wgww+....3w", 
-			"wwwwwwwwwwwwwwwwwwwww"	
-		};
+		TWEANNGenotype cppn = new TWEANNGenotype(4, 4, 0);
+		TWEANN net = cppn.getPhenotype();
+		String[] level = generateLevelFromCPPN(net, 20, 20, '.', 'w', new char[]{'w'}, new char[]{'g','+','A'}, 3);
 
 		Agent agent = new Agent();
 		agent.setup(null, seed, true); // null = no log, true = human 
 
 		runOneGame(toPlay, level, true, agent, seed, playerID);
 		//////////////////////////////////////////////////////
-		
 		
 		
 	}
