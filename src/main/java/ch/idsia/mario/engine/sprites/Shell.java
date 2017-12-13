96ca063af5657bd458cf57190e13cbae9a6fe174
Jacob Schrum <schrum2@southwestern.edu>
2017-12-12 21:28:54 -0600

    #48 Allow Luigi to interact with several other sprites
    Fire Flowers, Mushrooms, and Shells

--- src/main/java/ch/idsia/mario/engine/sprites/Shell.java
+++ src/main/java/ch/idsia/mario/engine/sprites/Shell.java
@@ -74,43 +74,51 @@ public class Shell extends Sprite
 
     public void collideCheck()
     {
-        if (carried || dead || deadTime>0) return;
+    	if (carried || dead || deadTime>0) return;
 
-        float xMarioD = world.mario.x - x;
-        float yMarioD = world.mario.y - y;
-        @SuppressWarnings("unused")
-		float w = 16;
-        if (xMarioD > -16 && xMarioD < 16)
-        {
-            if (yMarioD > -height && yMarioD < world.mario.height)
-            {
-                if (world.mario.ya > 0 && yMarioD <= 0 && (!world.mario.onGround || !world.mario.wasOnGround))
-                {
-                    world.mario.stomp(this);
-                    if (facing != 0)
-                    {
-                        xa = 0;
-                        facing = 0;
-                    }
-                    else
-                    {
-                        facing = world.mario.facing;
-                    }
-                }
-                else
-                {
-                    if (facing != 0)
-                    {
-                        world.mario.getHurt();
-                    }
-                    else
-                    {
-                        world.mario.kick(this);
-                        facing = world.mario.facing;
-                    }
-                }
-            }
-        }
+    	// Treat Mario and Luigi the same
+    	Mario[] marios = new Mario[LevelScene.twoPlayers ? 2 : 1];
+    	marios[0] = world.mario;
+    	if(LevelScene.twoPlayers) {
+    		marios[1] = world.luigi;
+    	}
+
+    	for(Mario mario: marios) {
+    		float xMarioD = mario.x - x;
+    		float yMarioD = mario.y - y;
+    		float w = 16;
+    		if (xMarioD > -w && xMarioD < w)
+    		{
+    			if (yMarioD > -height && yMarioD < mario.height)
+    			{
+    				if (mario.ya > 0 && yMarioD <= 0 && (!mario.onGround || !mario.wasOnGround))
+    				{
+    					mario.stomp(this);
+    					if (facing != 0)
+    					{
+    						xa = 0;
+    						facing = 0;
+    					}
+    					else
+    					{
+    						facing = mario.facing;
+    					}
+    				}
+    				else
+    				{
+    					if (facing != 0)
+    					{
+    						mario.getHurt();
+    					}
+    					else
+    					{
+    						mario.kick(this);
+    						facing = mario.facing;
+    					}
+    				}
+    			}
+    		}
+    	}
     }
 
     public void move()
