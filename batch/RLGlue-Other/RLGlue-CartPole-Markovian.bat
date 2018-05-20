c82b651a5ea3c0093118e66d7c9fb847df901d55
Jacob Schrum <schrum2@southwestern.edu>
2018-05-20 15:47:12 -0500

    Updated tutorial
    Also changed Cartpole experiment to only evolve for 10 generations, because that is all that is needed to create a perfect agent.

--- batch/RLGlue-Other/RLGlue-CartPole-Markovian.bat
+++ batch/RLGlue-Other/RLGlue-CartPole-Markovian.bat
@@ -1,3 +1,3 @@
 cd ..
 cd ..
-java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar runNumber:%1 randomSeed:%1 base:cartpole trials:1 maxGens:100 mu:50 io:true netio:true mating:true task:edu.southwestern.tasks.rlglue.cartpole.CartPoleTask cleanOldNetworks:false fs:false noisyTaskStat:edu.southwestern.util.stats.Average log:RL-Markovian saveTo:Markovian rlGlueEnvironment:org.rlcommunity.environments.cartpole.CartPole
+java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar runNumber:%1 randomSeed:%1 base:cartpole trials:1 maxGens:10 mu:50 io:true netio:true mating:true task:edu.southwestern.tasks.rlglue.cartpole.CartPoleTask cleanOldNetworks:false fs:false noisyTaskStat:edu.southwestern.util.stats.Average log:RL-Markovian saveTo:Markovian rlGlueEnvironment:org.rlcommunity.environments.cartpole.CartPole
