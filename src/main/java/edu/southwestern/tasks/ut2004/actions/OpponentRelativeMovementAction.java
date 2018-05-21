1e23a9414d23056f53e34fc4c0f18cecd7c66432
friedmaa <friedmaa@southwestern.edu>
2018-05-21 14:00:44 -0500

    commented OpponentRelativeMovementAction

--- src/main/java/edu/southwestern/tasks/ut2004/actions/OpponentRelativeMovementAction.java
+++ src/main/java/edu/southwestern/tasks/ut2004/actions/OpponentRelativeMovementAction.java
@@ -8,6 +8,7 @@ import edu.southwestern.tasks.ut2004.Util;
 import javax.vecmath.Vector3d;
 
 /**
+ *This class determines how the bot should move in relation to an identifies opponent
  *
  * @author Jacob Schrum
  */
@@ -17,32 +18,38 @@ public class OpponentRelativeMovementAction implements BotAction {
 	 * Distance to each movement target
 	 */
 	public static final double MOVEMENT_MAGNITUDE = 100;
-	private final Player player;
-	private final double towards;
-	private final double side;
+	private final Player opponent;
+	private final double distanceFromOpponent;
+	private final double strafeLeftRight;
 	private final boolean shoot;
 	private final boolean jump;
 
 	/**
+	 * gathers movement data about the opponent
 	 * 
-	 * @param player (player identified as opponent)
-	 * @param towards
-	 * @param side
-	 * @param shoot
-	 * @param jump
+	 * @param opponent (player identified as opponent)
+	 * @param distanceFromOpponent (distance between player and opponent)
+	 * @param strafeLeftRight	(strafing around opponent, negative = left, positive = right, magnitude = speed)
+	 * @param shoot (should the bot shoot)
+	 * @param jump (should the bot jump)
 	 */
-	public OpponentRelativeMovementAction(Player player, double towards, double side, boolean shoot, boolean jump) {
-		this.player = player;
-		this.towards = towards;
-		this.side = side;
+	public OpponentRelativeMovementAction(Player opponent, double distanceFromOpponent, double strafeLeftRight, boolean shoot, boolean jump) {
+		this.opponent = opponent;
+		this.distanceFromOpponent = distanceFromOpponent;
+		this.strafeLeftRight = strafeLeftRight;
 		this.shoot = shoot;
 		this.jump = jump;
 	}
-
+	/**
+	 *instructs bot on how to respond to opponent data
+	 *
+	 * @param bot (identifies which bot should execute the command)
+	 * @return (returns a list in the form of strings of what actions the bot executed)
+	 */
 	public String execute(UT2004BotModuleController bot) {
-		if (player != null) {
-			Location opponentLocation = player.getLocation();
-			UnrealId opponentId = player.getId();
+		if (opponent != null) {
+			Location opponentLocation = opponent.getLocation();
+			UnrealId opponentId = opponent.getId();
 			Location botLocation = bot.getInfo().getLocation();
 			if (opponentId != null && opponentLocation != null && botLocation != null) {
 				// vector-to-opponent
@@ -52,7 +59,7 @@ public class OpponentRelativeMovementAction implements BotAction {
 																					// differences
 				// Counter-clockwise angle from vector-to-opponent to
 				// vector-to-destination
-				double polarAngle = Math.atan2(side, towards);
+				double polarAngle = Math.atan2(strafeLeftRight, distanceFromOpponent);
 				// vector-to-destination
 				Vector3d botToDestination = Util.rotateVectorInPlane(botToOpponent.asVector3d(), polarAngle);
 				// Destination
