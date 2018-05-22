5f3262a94a02d4d5a4c109f0252bc7ac37665dcf
friedmaa <friedmaa@southwestern.edu>
2018-05-22 13:27:09 -0500

    commenting for issue #87

--- src/main/java/edu/southwestern/tasks/ut2004/fitness/ShockRifleAccuracyFitness.java
+++ src/main/java/edu/southwestern/tasks/ut2004/fitness/ShockRifleAccuracyFitness.java
@@ -1,18 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.southwestern.tasks.ut2004.fitness;
 
 import cz.cuni.amis.pogamut.ut2004.communication.messages.ItemType;
 import edu.southwestern.networks.Network;
 
 /**
- *
+ *Measures the bot's accuracy with the shock rifle
  * @author Jacob
  */
 public class ShockRifleAccuracyFitness<T extends Network> extends WeaponAccuracyFitness<T> {
 
+	/**
+	 * @return returns weapon accuracy with the schock rifle
+	 */
 	public ShockRifleAccuracyFitness() {
 		super(ItemType.SHOCK_RIFLE);
 	}
--- src/main/java/edu/southwestern/tasks/ut2004/fitness/WeaponAccuracyFitness.java
+++ src/main/java/edu/southwestern/tasks/ut2004/fitness/WeaponAccuracyFitness.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.southwestern.tasks.ut2004.fitness;
 
 import cz.cuni.amis.pogamut.ut2004.communication.messages.ItemType;
@@ -9,17 +5,25 @@ import edu.southwestern.evolution.Organism;
 import edu.southwestern.networks.Network;
 
 /**
- *
+ * Measures an organism's accuracy with weapons 
  * @author Jacob
  */
 public class WeaponAccuracyFitness<T extends Network> extends UT2004FitnessFunction<T> {
 
 	private final ItemType weapon;
 
+	/**
+	 * sets the weapon type
+	 * @param weapon (weapon to find accuracy for)
+	 */
 	public WeaponAccuracyFitness(ItemType weapon) {
 		this.weapon = weapon;
 	}
-
+	
+	/**
+	 * compares damage done to ammo used to determine accuracy
+	 * @return returns the accuracy with the weapon
+	 */
 	public double fitness(Organism<T> individual) {
 		double ammoUsed = game.ammoUsed(weapon);
 		if (ammoUsed == 0) {
