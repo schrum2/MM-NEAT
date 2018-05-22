719320e7230f498466d8714f6f65fd6b5a9aaed2
friedmaa <friedmaa@southwestern.edu>
2018-05-22 10:15:18 -0500

    Commented OpponentRelativeMovementOutputModel #87

--- src/main/java/edu/southwestern/tasks/ut2004/actuators/OpponentRelativeMovementOutputModel.java
+++ src/main/java/edu/southwestern/tasks/ut2004/actuators/OpponentRelativeMovementOutputModel.java
@@ -16,30 +16,42 @@ import edu.southwestern.tasks.ut2004.actions.OpponentRelativeMovementAction;
 public class OpponentRelativeMovementOutputModel implements UT2004OutputInterpretation {
 
 	public static int MEMORY_TIME = 30;
+	public static final int OUTPUT_INDEX_MOVEMENT = 0;
+	public static final int OUTPUT_INDEX_STRAFING = 1;
+	public static final int  OUTPUT_INDEX_SHOOT = 2;
+	public static final int OUTPUT_INDEX_JUMP = 3;
 
 	public String[] outputLabels() {
-		return new String[] { "Left/right impulse", "Towards/back impulse", "Shoot", "Jump" };
+		return new String[] { "Towards/back impulse", "Left/right impulse", "Shoot", "Jump" };
 	}
 
+	/**
+	 * Collects Data on the opponent and interprets it
+	 * 
+	 * @returns returns an array of outputs based on the opponent's movements
+	 */
 	public BotAction interpretOutputs(UT2004BotModuleController bot, double[] outputs) {
 		Player opponent = bot.getPlayers().getNearestEnemy(MEMORY_TIME);
-		double towards = outputs[0];
-		double side = outputs[1];
-		boolean shoot = outputs[2] > 0;
-		boolean jump = outputs[3] > 0;
-		return new OpponentRelativeMovementAction(opponent, towards, side, shoot, jump);
+		double moveForwardBackward = outputs[OUTPUT_INDEX_MOVEMENT];
+		double strafeLeftRight = outputs[OUTPUT_INDEX_STRAFING];
+		boolean shoot = outputs[OUTPUT_INDEX_SHOOT] > 0;
+		boolean jump = outputs[OUTPUT_INDEX_JUMP] > 0;
+		return new OpponentRelativeMovementAction(opponent, moveForwardBackward, strafeLeftRight, shoot, jump);
 	}
 
 	/**
 	 * - Opponent relative left/right movement impulse - Opponent relative
 	 * forward/back movement impulse - True/false shoot - True/false jump
 	 *
-	 * @return
+	 * @return returns whether or not the bot jumps
 	 */
 	public int numberOfOutputs() {
 		return 4;
 	}
 
+	/**
+	 * @return returns a copy of the movement model
+	 */
 	public UT2004OutputInterpretation copy() {
 		return new OpponentRelativeMovementOutputModel();
 	}
