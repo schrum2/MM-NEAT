8f9e9f17eaeaee4c44716c72b695a44aa37390d4
Will Price <Squillyprice01@users.noreply.github.com>
2018-05-29 16:55:21 -0500

    #143 DirectionalGhostComparator method compare supports po conditions

--- src/main/java/edu/southwestern/tasks/mspacman/ghosts/DirectionalGhostComparator.java
+++ src/main/java/edu/southwestern/tasks/mspacman/ghosts/DirectionalGhostComparator.java
@@ -24,7 +24,10 @@ public class DirectionalGhostComparator implements Comparator<Integer> {
 		this.proximityOnly = proximityOnly;
 		this.dir = dir;
 	}
-
+	
+	/**
+	 * Supports popacman
+	 */
 	public int compare(Integer o1, Integer o2) {
 		if (!proximityOnly && gs.isGhostEdible(o1) && !gs.isGhostEdible(o2)) {
 			return -1 * sign;
@@ -32,9 +35,24 @@ public class DirectionalGhostComparator implements Comparator<Integer> {
 			return 1 * sign;
 		} else {
 			// If getGhostCurrentNodeIndex is -1 then the ghost is not visible and the effective distance is infinity
-			// NOTE: Maybe improve this in the future with some kind of memory that makes a better guess, but for now saw infinity
-			int o1Dist = gs.getDirectionalPath(current, gs.getGhostCurrentNodeIndex(o1), dir).length;
-			int o2Dist = gs.getDirectionalPath(current, gs.getGhostCurrentNodeIndex(o2), dir).length;
+			// NOTE: Maybe improve this in the future with some kind of memory that makes a better guess, but for now say infinity
+			int o1Dist;
+			int o2Dist;
+			
+			//If you can't see ghost, it is infinitely far away. Else, get its actual distance.
+			if(gs.getGhostCurrentNodeIndex(o1) == -1) {
+				o1Dist = Integer.MAX_VALUE; 
+			} else {
+				o1Dist = gs.getDirectionalPath(current, gs.getGhostCurrentNodeIndex(o1), dir).length;	
+			}
+			
+			//If you can't see ghost, it is infinitely far away. Else, get its actual distance.
+			if(gs.getGhostCurrentNodeIndex(o2) == -1) {
+				o2Dist = Integer.MAX_VALUE; 
+			} else {
+				o2Dist = gs.getDirectionalPath(current, gs.getGhostCurrentNodeIndex(o2), dir).length;	
+			}
+			
 			if (o2Dist == 0 && o1Dist > 0) {
 				return -1;
 			} else if (o1Dist == 0 && o2Dist > 0) {
