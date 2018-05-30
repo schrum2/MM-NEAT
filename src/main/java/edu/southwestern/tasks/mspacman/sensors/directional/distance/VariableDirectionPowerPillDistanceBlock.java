4421b6bff16ce96198d21bf660549fb828a765f7
Will Price <Squillyprice01@users.noreply.github.com>
2018-05-30 12:50:34 -0500

    #142 Added MiscUtil.wait... to some classes' method's for debugging

--- src/main/java/edu/southwestern/tasks/mspacman/sensors/directional/distance/VariableDirectionPowerPillDistanceBlock.java
+++ src/main/java/edu/southwestern/tasks/mspacman/sensors/directional/distance/VariableDirectionPowerPillDistanceBlock.java
@@ -5,9 +5,10 @@
 package edu.southwestern.tasks.mspacman.sensors.directional.distance;
 
 import edu.southwestern.tasks.mspacman.facades.GameFacade;
+import edu.southwestern.util.MiscUtil;
 
 /**
- *
+ * tODO: what to do in PO conditions
  * @author Jacob Schrum
  */
 public class VariableDirectionPowerPillDistanceBlock extends VariableDirectionDistanceBlock {
@@ -23,6 +24,12 @@ public class VariableDirectionPowerPillDistanceBlock extends VariableDirectionDi
 
 	@Override
 	public int[] getTargets(GameFacade gf) {
+		//TODO: what should be returned if no targets are visible?
+		int[] intermediate = gf.getActivePowerPillsIndices();;
+		for(int i = 0; i < intermediate.length; i++) {
+			System.out.println(intermediate[i]);
+		}
+		MiscUtil.waitForReadStringAndEnterKeyPress();
 		return gf.getActivePowerPillsIndices();
 	}
 }
