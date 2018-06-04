7e6f380b796f60ee6e15abb4d228e07b9122fac7
JoeRoybal <joewroybal@gmail.com>
2018-06-04 13:46:22 -0500

    assemblyline is now supported plus the framework for all other intergrations
    added space for all other games and commented them out. assemblyline game can be launched evolved and played

--- src/main/java/edu/southwestern/tasks/interactive/gvgai/LevelBreederTask.java
+++ src/main/java/edu/southwestern/tasks/interactive/gvgai/LevelBreederTask.java
@@ -69,6 +69,170 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 			new char[]{'w'}, // Walls are fixed
 			new char[]{'a','a','d','d','i','o','A'}, // There are two angels, two demons, an input and output, and an Avatar
 			new char[]{'t','x'}}); // There are random sky boxes and a sky trunk
+		SPECIFIC_GAME_LEVEL_CHARS.put("assemblyline", new char[][] {
+			new char[]{'w'}, // Walls are fixed
+			new char[]{'l','r','u','d','g','p','v','A'}, // spawns a lcleft, lcright,lcup, lcdown, goal, portal, vortex, and an Avatar
+			new char[]{'1','2','3','4','5','6','7','8','9'}}); // Spawns random assemblies
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+//			new char[]{'w'}, // Walls are fixed
+//			new char[]{'a','a','d','d','i','o','A'}, // 
+//			new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+//			new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
+//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
+	//		new char[]{'w'}, // Walls are fixed
+	//		new char[]{'a','a','d','d','i','o','A'}, // 
+	//		new char[]{'t','x'}}); //
 
 	}
 	
@@ -217,7 +381,7 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 
 	public static void main(String[] args) {
 		try {
-			MMNEAT.main(new String[]{"runNumber:0","randomSeed:1","trials:1","mu:16","maxGens:500","gvgaiGame:angelsdemons","io:false","netio:false","mating:true","fs:false","task:edu.southwestern.tasks.interactive.gvgai.LevelBreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","simplifiedInteractiveInterface:false","recurrency:false","saveAllChampions:true","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageWidth:2000","imageHeight:2000","imageSize:200","includeFullSigmoidFunction:true","includeFullGaussFunction:true","includeCosineFunction:true","includeGaussFunction:false","includeIdFunction:true","includeTriangleWaveFunction:true","includeSquareWaveFunction:true","includeFullSawtoothFunction:true","includeSigmoidFunction:false","includeAbsValFunction:false","includeSawtoothFunction:false"});
+			MMNEAT.main(new String[]{"runNumber:0","randomSeed:1","trials:1","mu:16","maxGens:500","gvgaiGame:assemblyline","io:false","netio:false","mating:true","fs:false","task:edu.southwestern.tasks.interactive.gvgai.LevelBreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","simplifiedInteractiveInterface:false","recurrency:false","saveAllChampions:true","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageWidth:2000","imageHeight:2000","imageSize:200","includeFullSigmoidFunction:true","includeFullGaussFunction:true","includeCosineFunction:true","includeGaussFunction:false","includeIdFunction:true","includeTriangleWaveFunction:true","includeSquareWaveFunction:true","includeFullSawtoothFunction:true","includeSigmoidFunction:false","includeAbsValFunction:false","includeSawtoothFunction:false"});
 		} catch (FileNotFoundException | NoSuchMethodException e) {
 			e.printStackTrace();
 		}
