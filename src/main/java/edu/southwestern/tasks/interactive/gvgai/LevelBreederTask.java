f67d4fedc1b1686d902d9195e18f82c247c514cc
JoeRoybal <joewroybal@gmail.com>
2018-06-04 12:09:14 -0500

    Pacman is now supported
    The game can be launched and levels can be evolved. The game can not be played however.

--- src/main/java/edu/southwestern/tasks/interactive/gvgai/LevelBreederTask.java
+++ src/main/java/edu/southwestern/tasks/interactive/gvgai/LevelBreederTask.java
@@ -56,15 +56,15 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 			new char[]{'w'}, // Walls are fixed
 			new char[]{'-','r','g','b','y','1','2','3','4','e','A'}, // There is one locked gate, four keys, four doors, one escape floor and one avatar
 			new char[]{'~','m','x','f','i','d'}}); // There are random hazards dignified by ~(water), m(mud), x(fire) and random perks dignified by f (flippers), i(fireboots), d(crate)
-		//TODO: fix this
+		//TODO: fix aliens: everything spawns yet some levels are broken, force the spawn of the avatar and the aliens
 		SPECIFIC_GAME_LEVEL_CHARS.put("aliens", new char[][] {
 			new char[]{'.'}, // floor
 			new char[]{'1','2','A'}, // There is one slow portal, one fast portal, and one avatar
 			new char[]{'0'}}); // There are base blocks dignified by 0 
-//		SPECIFIC_GAME_LEVEL_CHARS.put("pacman", new char[][] {
-//			new char[]{'+','w'}, // Walls and floor are fixed
-//			new char[]{'1','2','3','4','A'}, // There are four ghosts and one avatar
-//			new char[]{'.','f','0'}}); // There are random pellets, fruits. and power pellets
+		SPECIFIC_GAME_LEVEL_CHARS.put("pacman", new char[][] {
+		new char[]{'w'}, // Walls are fixed
+		new char[]{'1','2','3','4','A'}, // There are four ghosts and one avatar
+		new char[]{'.','f','0','+'}}); // There are random pellets, fruits. and power pellets
 
 	}
 	
@@ -213,7 +213,7 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 
 	public static void main(String[] args) {
 		try {
-			MMNEAT.main(new String[]{"runNumber:0","randomSeed:1","trials:1","mu:16","maxGens:500","gvgaiGame:aliens","io:false","netio:false","mating:true","fs:false","task:edu.southwestern.tasks.interactive.gvgai.LevelBreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","simplifiedInteractiveInterface:false","recurrency:false","saveAllChampions:true","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageWidth:2000","imageHeight:2000","imageSize:200","includeFullSigmoidFunction:true","includeFullGaussFunction:true","includeCosineFunction:true","includeGaussFunction:false","includeIdFunction:true","includeTriangleWaveFunction:true","includeSquareWaveFunction:true","includeFullSawtoothFunction:true","includeSigmoidFunction:false","includeAbsValFunction:false","includeSawtoothFunction:false"});
+			MMNEAT.main(new String[]{"runNumber:0","randomSeed:1","trials:1","mu:16","maxGens:500","gvgaiGame:pacman","io:false","netio:false","mating:true","fs:false","task:edu.southwestern.tasks.interactive.gvgai.LevelBreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","simplifiedInteractiveInterface:false","recurrency:false","saveAllChampions:true","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageWidth:2000","imageHeight:2000","imageSize:200","includeFullSigmoidFunction:true","includeFullGaussFunction:true","includeCosineFunction:true","includeGaussFunction:false","includeIdFunction:true","includeTriangleWaveFunction:true","includeSquareWaveFunction:true","includeFullSawtoothFunction:true","includeSigmoidFunction:false","includeAbsValFunction:false","includeSawtoothFunction:false"});
 		} catch (FileNotFoundException | NoSuchMethodException e) {
 			e.printStackTrace();
 		}
