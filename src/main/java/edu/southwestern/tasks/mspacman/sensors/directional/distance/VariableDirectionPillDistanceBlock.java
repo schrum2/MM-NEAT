96914f8e49cc45ddcb5850eac063dbbe6f688907
Will Price <Squillyprice01@users.noreply.github.com>
2018-06-05 13:45:02 -0500

    #142 commented VariableDirectionPillDistanceBlock. Needs PillModel to work

--- src/main/java/edu/southwestern/tasks/mspacman/sensors/directional/distance/VariableDirectionPillDistanceBlock.java
+++ src/main/java/edu/southwestern/tasks/mspacman/sensors/directional/distance/VariableDirectionPillDistanceBlock.java
@@ -8,7 +8,7 @@ import edu.southwestern.tasks.mspacman.facades.GameFacade;
 import edu.southwestern.util.MiscUtil;
 
 /**
- * handles PO conditions (TODO: test)
+ * handles PO conditions (TODO: implement PillModel to finish this)
  * @author Jacob Schrum
  */
 public class VariableDirectionPillDistanceBlock extends VariableDirectionDistanceBlock {
@@ -23,6 +23,16 @@ public class VariableDirectionPillDistanceBlock extends VariableDirectionDistanc
 	}
 
 	@Override
+	/**
+	 * The targets for this sensor are the active pill indicies in the current maze. In non-PO
+	 * conditions, we always have access to this information.
+	 * 
+	 * In PO conditions, if we are using a pill model (see parameters, booleanOption usePillModel), then 
+	 * we get the active pill indices from the GameFacade's PillModel (TODO: get PillModel up and running).
+	 * 
+	 * If we are not using a pill model bu are in PO conditions, we recieve an array of pills that we can see. 
+	 * This array could be empty if we cannot see any pills.
+	 */
 	public int[] getTargets(GameFacade gf) {
 		return gf.getActivePillsIndices();
 	}
