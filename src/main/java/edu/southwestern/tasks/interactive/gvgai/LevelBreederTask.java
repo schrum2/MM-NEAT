51bcdc54a3fc8d5e4716bf4e58654a94358e2019
JoeRoybal <joewroybal@gmail.com>
2018-06-06 10:58:44 -0500

    thecitadel and thesnowman both are supported now
    #57

--- src/main/java/edu/southwestern/tasks/interactive/gvgai/LevelBreederTask.java
+++ src/main/java/edu/southwestern/tasks/interactive/gvgai/LevelBreederTask.java
@@ -575,18 +575,18 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 			new char[]{'.','+','-','g','B','A','L','*','@','$','t','u','r'}, // not quite sure what is spawning
 			new char[]{'.'},
 			new char[]{'w'}});
-//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
-	//		new char[]{'w'}, // Walls are fixed
-	//		new char[]{'a','a','d','d','i','o','A'}, // 
-	//		new char[]{'t','x'}, //
-//			new char[]{'.'},
-//			new char[]{'w'}});
-//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
-	//		new char[]{'w'}, // Walls are fixed
-	//		new char[]{'a','a','d','d','i','o','A'}, // 
-	//		new char[]{'t','x'}, //
-//			new char[]{'.'},
-//			new char[]{'w'}});
+		SPECIFIC_GAME_LEVEL_CHARS.put("thecitadel", new char[][] {
+			new char[]{'w'}, // Walls are fixed
+			new char[]{'g','A'}, // goal and avatar
+			new char[]{'0','1','b','c'}, // roundhole, squarehole, boulder, crate
+			new char[]{'.'},
+			new char[]{'w'}});
+		SPECIFIC_GAME_LEVEL_CHARS.put("thesnowman", new char[][] {
+			new char[]{'w'}, // Walls are fixed
+			new char[]{'s','b','c','h','k','l','A'}, // base, body, chest, head, key, lock
+			new char[]{' '}, //
+			new char[]{'.'},
+			new char[]{'w'}});
 //		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
 	//		new char[]{'w'}, // Walls are fixed
 	//		new char[]{'a','a','d','d','i','o','A'}, // 
@@ -766,7 +766,7 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 
 	public static void main(String[] args) {
 		try {
-			MMNEAT.main(new String[]{"runNumber:0","randomSeed:1","trials:1","mu:16","maxGens:500","gvgaiGame:tercio","io:false","netio:false","mating:true","fs:false","task:edu.southwestern.tasks.interactive.gvgai.LevelBreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","simplifiedInteractiveInterface:false","recurrency:false","saveAllChampions:true","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageWidth:2000","imageHeight:2000","imageSize:200","includeFullSigmoidFunction:true","includeFullGaussFunction:true","includeCosineFunction:true","includeGaussFunction:false","includeIdFunction:true","includeTriangleWaveFunction:true","includeSquareWaveFunction:true","includeFullSawtoothFunction:true","includeSigmoidFunction:false","includeAbsValFunction:false","includeSawtoothFunction:false"});
+			MMNEAT.main(new String[]{"runNumber:0","randomSeed:1","trials:1","mu:16","maxGens:500","gvgaiGame:thesnowman","io:false","netio:false","mating:true","fs:false","task:edu.southwestern.tasks.interactive.gvgai.LevelBreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","simplifiedInteractiveInterface:false","recurrency:false","saveAllChampions:true","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageWidth:2000","imageHeight:2000","imageSize:200","includeFullSigmoidFunction:true","includeFullGaussFunction:true","includeCosineFunction:true","includeGaussFunction:false","includeIdFunction:true","includeTriangleWaveFunction:true","includeSquareWaveFunction:true","includeFullSawtoothFunction:true","includeSigmoidFunction:false","includeAbsValFunction:false","includeSawtoothFunction:false"});
 		} catch (FileNotFoundException | NoSuchMethodException e) {
 			e.printStackTrace();
 		}
