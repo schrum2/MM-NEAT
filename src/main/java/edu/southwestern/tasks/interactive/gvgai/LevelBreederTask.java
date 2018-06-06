0ac754bfae4d6c7f974b68c61029054f7a3df56e
JoeRoybal <joewroybal@gmail.com>
2018-06-06 10:46:38 -0500

    suerman, surround, survivezombies, and tercio all have support
    #57

--- src/main/java/edu/southwestern/tasks/interactive/gvgai/LevelBreederTask.java
+++ src/main/java/edu/southwestern/tasks/interactive/gvgai/LevelBreederTask.java
@@ -544,36 +544,37 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 			new char[]{'o','1'}, // hole and box
 			new char[]{'.'},
 			new char[]{'w'}});
+		//BROKEN
 		SPECIFIC_GAME_LEVEL_CHARS.put("solarfox", new char[][] {
 			new char[]{'w'}, // Walls are fixed
 			new char[]{'A'}, // avatar
 			new char[]{'1','2','.','b','p'}, // enemy spawn, blips
 			new char[]{'+'},
 			new char[]{'w'}});
-//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
-	//		new char[]{'w'}, // Walls are fixed
-	//		new char[]{'a','a','d','d','i','o','A'}, // 
-	//		new char[]{'t','x'}, //
-//			new char[]{'.'},
-//			new char[]{'w'}});
-//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
-	//		new char[]{'w'}, // Walls are fixed
-	//		new char[]{'a','a','d','d','i','o','A'}, // 
-	//		new char[]{'t','x'}, //
-//			new char[]{'.'},
-//			new char[]{'w'}});
-//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
-	//		new char[]{'w'}, // Walls are fixed
-	//		new char[]{'a','a','d','d','i','o','A'}, // 
-	//		new char[]{'t','x'}, //
-//			new char[]{'.'},
-//			new char[]{'w'}});
-//		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
-	//		new char[]{'w'}, // Walls are fixed
-	//		new char[]{'a','a','d','d','i','o','A'}, // 
-	//		new char[]{'t','x'}, //
-//			new char[]{'.'},
-//			new char[]{'w'}});
+		SPECIFIC_GAME_LEVEL_CHARS.put("superman", new char[][] {
+			new char[]{'w'}, // Walls are fixed
+			new char[]{'j','A'}, // jail, avatar
+			new char[]{'b','s','l','r','c','p','q'}, // building, civilian, portals, sky
+			new char[]{'.'},
+			new char[]{'w'}});
+		SPECIFIC_GAME_LEVEL_CHARS.put("surround", new char[][] {
+			new char[]{'w'}, // Walls are fixed
+			new char[]{'A'}, // avatar
+			new char[]{'1'}, //mud
+			new char[]{'.'},
+			new char[]{'w'}});
+		SPECIFIC_GAME_LEVEL_CHARS.put("survivezombies", new char[][] {
+			new char[]{'w'}, // Walls are fixed
+			new char[]{'A'}, // avatar
+			new char[]{'0','1','2','.','_'}, // flower, zombie portals, honey, zombie
+			new char[]{'+'},
+			new char[]{'w'}});
+		SPECIFIC_GAME_LEVEL_CHARS.put("tercio", new char[][] {
+			new char[]{'w'}, // Walls are fixed
+			new char[]{' '}, // 
+			new char[]{'.','+','-','g','B','A','L','*','@','$','t','u','r'}, // not quite sure what is spawning
+			new char[]{'.'},
+			new char[]{'w'}});
 //		SPECIFIC_GAME_LEVEL_CHARS.put("", new char[][] {
 	//		new char[]{'w'}, // Walls are fixed
 	//		new char[]{'a','a','d','d','i','o','A'}, // 
@@ -765,7 +766,7 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 
 	public static void main(String[] args) {
 		try {
-			MMNEAT.main(new String[]{"runNumber:0","randomSeed:1","trials:1","mu:16","maxGens:500","gvgaiGame:solarfox","io:false","netio:false","mating:true","fs:false","task:edu.southwestern.tasks.interactive.gvgai.LevelBreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","simplifiedInteractiveInterface:false","recurrency:false","saveAllChampions:true","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageWidth:2000","imageHeight:2000","imageSize:200","includeFullSigmoidFunction:true","includeFullGaussFunction:true","includeCosineFunction:true","includeGaussFunction:false","includeIdFunction:true","includeTriangleWaveFunction:true","includeSquareWaveFunction:true","includeFullSawtoothFunction:true","includeSigmoidFunction:false","includeAbsValFunction:false","includeSawtoothFunction:false"});
+			MMNEAT.main(new String[]{"runNumber:0","randomSeed:1","trials:1","mu:16","maxGens:500","gvgaiGame:tercio","io:false","netio:false","mating:true","fs:false","task:edu.southwestern.tasks.interactive.gvgai.LevelBreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","simplifiedInteractiveInterface:false","recurrency:false","saveAllChampions:true","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageWidth:2000","imageHeight:2000","imageSize:200","includeFullSigmoidFunction:true","includeFullGaussFunction:true","includeCosineFunction:true","includeGaussFunction:false","includeIdFunction:true","includeTriangleWaveFunction:true","includeSquareWaveFunction:true","includeFullSawtoothFunction:true","includeSigmoidFunction:false","includeAbsValFunction:false","includeSawtoothFunction:false"});
 		} catch (FileNotFoundException | NoSuchMethodException e) {
 			e.printStackTrace();
 		}
