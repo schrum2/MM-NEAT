d3dac7faec7ebacdfc63c14203d78b39f2b5befd
JoeRoybal <joewroybal@gmail.com>
2018-06-07 14:59:32 -0500

    error occurs at the resetbuttons(true); line770
    #198

--- src/main/java/edu/southwestern/tasks/interactive/gvgai/LevelBreederTask.java
+++ src/main/java/edu/southwestern/tasks/interactive/gvgai/LevelBreederTask.java
@@ -754,7 +754,7 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 		
 		gameChoice = new JComboBox<String>(choices);
 		gameChoice.setSelectedIndex(choices.length - 1); 
-		gameChoice.setSize(80, 40);
+		gameChoice.setSize(90, 40);
 		gameChoice.addItemListener(new ItemListener() {
 			@SuppressWarnings({ "unchecked"})
 			@Override
@@ -764,9 +764,11 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 				gameFile = (String) source.getSelectedItem();
 				Parameters.parameters.setString("gvgaiGame", gameFile);
 				fullGameFile = GAMES_PATH + gameFile + ".txt";
+				System.out.println("fullGameFile");
 				gameCharData = SPECIFIC_GAME_LEVEL_CHARS.get(gameFile);
+				System.out.println("gameCharData");
 				
-				resetButtons(true);
+				//resetButtons(true);
 				
 			}
 
@@ -823,7 +825,7 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 			return;
 		}
 	}
-
+	
 	/**
 	 * Use a CPPN to create a level and wrap in a game bundle with a new game.
 	 * @param phenotype CPPN
@@ -862,6 +864,7 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 				public void run() {
 					// True is to watch the game being played
 					GVGAIUtil.runOneGame(bundle, true);
+					
 				}
 			}.start();
 			System.out.println("Launched");
@@ -897,7 +900,7 @@ public class LevelBreederTask<T extends Network> extends InteractiveEvolutionTas
 
 	public static void main(String[] args) {
 		try {
-			MMNEAT.main(new String[]{"runNumber:0","randomSeed:1","trials:1","mu:16","maxGens:500","gvgaiGame:wildgunman","io:false","netio:false","mating:true","fs:false","task:edu.southwestern.tasks.interactive.gvgai.LevelBreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","simplifiedInteractiveInterface:false","recurrency:false","saveAllChampions:true","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageWidth:2000","imageHeight:2000","imageSize:200","includeFullSigmoidFunction:true","includeFullGaussFunction:true","includeCosineFunction:true","includeGaussFunction:false","includeIdFunction:true","includeTriangleWaveFunction:true","includeSquareWaveFunction:true","includeFullSawtoothFunction:true","includeSigmoidFunction:false","includeAbsValFunction:false","includeSawtoothFunction:false"});
+			MMNEAT.main(new String[]{"runNumber:0","randomSeed:1","trials:1","mu:16","maxGens:500","gvgaiGame:pacman","io:false","netio:false","mating:true","fs:false","task:edu.southwestern.tasks.interactive.gvgai.LevelBreederTask","allowMultipleFunctions:true","ftype:0","watch:false","netChangeActivationRate:0.3","cleanFrequency:-1","simplifiedInteractiveInterface:false","recurrency:false","saveAllChampions:true","cleanOldNetworks:false","ea:edu.southwestern.evolution.selectiveBreeding.SelectiveBreedingEA","imageWidth:2000","imageHeight:2000","imageSize:200","includeFullSigmoidFunction:true","includeFullGaussFunction:true","includeCosineFunction:true","includeGaussFunction:false","includeIdFunction:true","includeTriangleWaveFunction:true","includeSquareWaveFunction:true","includeFullSawtoothFunction:true","includeSigmoidFunction:false","includeAbsValFunction:false","includeSawtoothFunction:false"});
 		} catch (FileNotFoundException | NoSuchMethodException e) {
 			e.printStackTrace();
 		}
