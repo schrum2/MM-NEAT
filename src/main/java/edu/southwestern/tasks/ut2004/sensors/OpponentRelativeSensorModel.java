16744ae6f5014fb519efd4e3613430d5ca5c0af3
friedmaa <friedmaa@southwestern.edu>
2018-06-11 11:20:07 -0500

    Commenting sensors package #210

--- src/main/java/edu/southwestern/tasks/ut2004/sensors/OpponentRelativeSensorModel.java
+++ src/main/java/edu/southwestern/tasks/ut2004/sensors/OpponentRelativeSensorModel.java
@@ -3,12 +3,15 @@ package edu.southwestern.tasks.ut2004.sensors;
 import edu.southwestern.tasks.ut2004.sensors.blocks.*;
 
 /**
- *
+ * Finds the the bot's position and velocity with respect to the opponent it's facing 
  * @author Jacob Schrum
  */
 public class OpponentRelativeSensorModel extends UT2004BlockLoadedSensorModel {
 
-	public OpponentRelativeSensorModel() {
+	/**
+	 * creates the block of sensors
+	 */
+	public OpponentRelativeSensorModel() { //adds the specific sensor blocks
 		blocks.add(new AutoRayTraceSensorBlock());
 		blocks.add(new PieSliceOpponentSensorBlock());
 		blocks.add(new NearestOpponentDistanceBlock());
@@ -16,6 +19,9 @@ public class OpponentRelativeSensorModel extends UT2004BlockLoadedSensorModel {
 		blocks.add(new SelfAwarenessBlock());
 	}
 
+	/**
+	 * creates a copy of the sensor model
+	 */
 	public UT2004SensorModel copy() {
 		return new OpponentRelativeSensorModel();
 	}
--- src/main/java/edu/southwestern/tasks/ut2004/sensors/UT2004BlockLoadedSensorModel.java
+++ src/main/java/edu/southwestern/tasks/ut2004/sensors/UT2004BlockLoadedSensorModel.java
@@ -5,7 +5,7 @@ import edu.southwestern.tasks.ut2004.sensors.blocks.UT2004SensorBlock;
 import java.util.ArrayList;
 
 /**
- *
+ * Creates a sensor model with the necesarry sensor blocks
  * @author Jacob Schrum
  */
 public abstract class UT2004BlockLoadedSensorModel implements UT2004SensorModel {
@@ -21,36 +21,47 @@ public abstract class UT2004BlockLoadedSensorModel implements UT2004SensorModel 
 	}
 
 	/**
-	 * 
+	 * prepares the blocks array
 	 */
 	public void prepareSensorModel(UT2004BotModuleController bot) {
 		for (int i = 0; i < blocks.size(); i++) {
-			blocks.get(i).prepareBlock(bot);
+			blocks.get(i).prepareBlock(bot); //sets the array up to host sensors
 		}
 	}
 
+	/**
+	 * reads the inputs from the sensors and interprets them
+	 * @return returns the data gathered in an array of inputs
+	 */
 	public double[] readSensors(UT2004BotModuleController bot) {
 		double[] inputs = new double[numberOfSensors()];
 		int in = 0;
-		for (int i = 0; i < blocks.size(); i++) {
+		for (int i = 0; i < blocks.size(); i++) { //collects the data from the sensors
 			in = blocks.get(i).incorporateSensors(bot, in, inputs);
 		}
 		return inputs;
 	}
 
+	/**
+	 * creates a string array that stores the labels for the sensors in corresponding addresses to the blocks array
+	 * @return returns the labels array
+	 */
 	public String[] sensorLabels() {
-		String[] labels = new String[numberOfSensors()];
+		String[] labels = new String[numberOfSensors()];//sets up the sensor array
 		int in = 0;
-		for (int i = 0; i < blocks.size(); i++) {
+		for (int i = 0; i < blocks.size(); i++) {//populates the array
 			in = blocks.get(i).incorporateLabels(in, labels);
 		}
 		return labels;
 	}
 
+	/**
+	 * @return returns the number of sensors in the block
+	 */
 	public int numberOfSensors() {
 		int count = 0;
 		for (int i = 0; i < blocks.size(); i++) {
-			count += blocks.get(i).numberOfSensors();
+			count += blocks.get(i).numberOfSensors(); //loops through the array to count the sensors
 		}
 		return count;
 	}
