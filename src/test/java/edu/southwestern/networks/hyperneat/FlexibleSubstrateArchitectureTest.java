de3e8118ca166d686a79020cbbcae83d0a041b62
DevonFulcher <fulcherd@southwestern.edu>
2018-06-12 09:51:13 -0500

    tests still don't work together #68

--- src/test/java/edu/southwestern/networks/hyperneat/FlexibleSubstrateArchitectureTest.java
+++ src/test/java/edu/southwestern/networks/hyperneat/FlexibleSubstrateArchitectureTest.java
@@ -1,23 +1,25 @@
 package edu.southwestern.networks.hyperneat;
 
 import static org.junit.Assert.*;
-
 import java.util.List;
 
 import org.junit.Test;
 
 import edu.southwestern.MMNEAT.MMNEAT;
+import edu.southwestern.evolution.EvolutionaryHistory;
 import edu.southwestern.parameters.Parameters;
-import edu.southwestern.util.MiscUtil;
 import edu.southwestern.util.datastructures.Triple;
 
 public class FlexibleSubstrateArchitectureTest {
 
-	//@Test
+//	@Test
 	public void testGetHiddenArchitectureD1W1CNN() {
 		MMNEAT.clearClasses();
+		EvolutionaryHistory.archetypes = null;
+		EvolutionaryHistory.setInnovation(0l);
 		Parameters.initializeParameterCollections(new String[] {"runNumber:1", "randomSeed:1", "trials:3", "maxGens:500", "mu:50", "io:false", "netio:false", "mating:true", "task:edu.southwestern.tasks.rlglue.tetris.HyperNEATTetrisTask",  "rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris", "rlGlueExtractor:edu.southwestern.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor", "tetrisTimeSteps:true", "tetrisBlocksOnScreen:false",  "rlGlueAgent:edu.southwestern.tasks.rlglue.tetris.TetrisAfterStateAgent", "splitRawTetrisInputs:true", "senseHolesDifferently:true", "hyperNEAT:true", "genotype:edu.southwestern.evolution.genotypes.HyperNEATCPPNGenotype", "allowMultipleFunctions:true", "ftype:1", "netChangeActivationRate:0.3", "substrateMapping:edu.southwestern.networks.hyperneat.BottomSubstrateMapping", "steps:500000", "perLinkMutateRate:0.05", "netLinkRate:0.4", "netSpliceRate:0.2", "crossoverRate:0.5", "extraHNLinks:true", "HNProcessDepth:1", "HNProcessWidth:1", "convolution:true", "tetrisAllowLine:false", "tetrisAllowSquare:false", "tetrisAllowTri:false", "tetrisAllowLShape:false", "tetrisAllowJShape:false", "senseTetrisHolesAsPositive:true"});
 		MMNEAT.loadClasses();
+		EvolutionaryHistory.initArchetype(0);
 		
 		HyperNEATTask tetris = (HyperNEATTask) MMNEAT.task;
 		List<Triple<Integer, Integer, Integer>> test = FlexibleSubstrateArchitecture.getHiddenArchitecture(tetris);
@@ -29,11 +31,14 @@ public class FlexibleSubstrateArchitectureTest {
 		MMNEAT.clearClasses();
 	}
 
-	//@Test
+//	@Test
 	public void testGetHiddenArchitectureD2W1CNN() {
 		MMNEAT.clearClasses();
+		EvolutionaryHistory.archetypes = null;
+		EvolutionaryHistory.setInnovation(0l);
 		Parameters.initializeParameterCollections(new String[] {"runNumber:1", "randomSeed:1", "trials:3", "maxGens:500", "mu:50", "io:false", "netio:false", "mating:true", "task:edu.southwestern.tasks.rlglue.tetris.HyperNEATTetrisTask",  "rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris", "rlGlueExtractor:edu.southwestern.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor", "tetrisTimeSteps:true", "tetrisBlocksOnScreen:false",  "rlGlueAgent:edu.southwestern.tasks.rlglue.tetris.TetrisAfterStateAgent", "splitRawTetrisInputs:true", "senseHolesDifferently:true", "hyperNEAT:true", "genotype:edu.southwestern.evolution.genotypes.HyperNEATCPPNGenotype", "allowMultipleFunctions:true", "ftype:1", "netChangeActivationRate:0.3", "substrateMapping:edu.southwestern.networks.hyperneat.BottomSubstrateMapping", "steps:500000", "perLinkMutateRate:0.05", "netLinkRate:0.4", "netSpliceRate:0.2", "crossoverRate:0.5", "extraHNLinks:true", "HNProcessDepth:2", "HNProcessWidth:1", "convolution:true", "tetrisAllowLine:false", "tetrisAllowSquare:false", "tetrisAllowTri:false", "tetrisAllowLShape:false", "tetrisAllowJShape:false", "senseTetrisHolesAsPositive:true"});
 		MMNEAT.loadClasses();
+		EvolutionaryHistory.initArchetype(0);
 		
 		HyperNEATTask tetris = (HyperNEATTask) MMNEAT.task;
 		List<Triple<Integer, Integer, Integer>> test = FlexibleSubstrateArchitecture.getHiddenArchitecture(tetris);
@@ -51,8 +56,11 @@ public class FlexibleSubstrateArchitectureTest {
 	//@Test
 	public void testGetHiddenArchitectureD1W2CNN() {
 		MMNEAT.clearClasses();
+		EvolutionaryHistory.archetypes = null;
+		EvolutionaryHistory.setInnovation(0l);
 		Parameters.initializeParameterCollections(new String[] {"runNumber:1", "randomSeed:1", "trials:3", "maxGens:500", "mu:50", "io:false", "netio:false", "mating:true", "task:edu.southwestern.tasks.rlglue.tetris.HyperNEATTetrisTask",  "rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris", "rlGlueExtractor:edu.southwestern.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor", "tetrisTimeSteps:true", "tetrisBlocksOnScreen:false",  "rlGlueAgent:edu.southwestern.tasks.rlglue.tetris.TetrisAfterStateAgent", "splitRawTetrisInputs:true", "senseHolesDifferently:true", "hyperNEAT:true", "genotype:edu.southwestern.evolution.genotypes.HyperNEATCPPNGenotype", "allowMultipleFunctions:true", "ftype:1", "netChangeActivationRate:0.3", "substrateMapping:edu.southwestern.networks.hyperneat.BottomSubstrateMapping", "steps:500000", "perLinkMutateRate:0.05", "netLinkRate:0.4", "netSpliceRate:0.2", "crossoverRate:0.5", "extraHNLinks:true", "HNProcessDepth:1", "HNProcessWidth:2", "convolution:true", "tetrisAllowLine:false", "tetrisAllowSquare:false", "tetrisAllowTri:false", "tetrisAllowLShape:false", "tetrisAllowJShape:false", "senseTetrisHolesAsPositive:true"});
 		MMNEAT.loadClasses();
+		EvolutionaryHistory.initArchetype(0);
 		
 		HyperNEATTask tetris = (HyperNEATTask) MMNEAT.task;
 		List<Triple<Integer, Integer, Integer>> test = FlexibleSubstrateArchitecture.getHiddenArchitecture(tetris);
@@ -65,11 +73,14 @@ public class FlexibleSubstrateArchitectureTest {
 		MMNEAT.clearClasses();
 	}
 	
-//	@Test
+	//@Test
 	public void testGetHiddenArchitectureD2W2CNN() {
 		MMNEAT.clearClasses();
+		EvolutionaryHistory.archetypes = null;
+		EvolutionaryHistory.setInnovation(0l);
 		Parameters.initializeParameterCollections(new String[] {"runNumber:1", "randomSeed:1", "trials:3", "maxGens:500", "mu:50", "io:false", "netio:false", "mating:true", "task:edu.southwestern.tasks.rlglue.tetris.HyperNEATTetrisTask",  "rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris", "rlGlueExtractor:edu.southwestern.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor", "tetrisTimeSteps:true", "tetrisBlocksOnScreen:false",  "rlGlueAgent:edu.southwestern.tasks.rlglue.tetris.TetrisAfterStateAgent", "splitRawTetrisInputs:true", "senseHolesDifferently:true", "hyperNEAT:true", "genotype:edu.southwestern.evolution.genotypes.HyperNEATCPPNGenotype", "allowMultipleFunctions:true", "ftype:1", "netChangeActivationRate:0.3", "substrateMapping:edu.southwestern.networks.hyperneat.BottomSubstrateMapping", "steps:500000", "perLinkMutateRate:0.05", "netLinkRate:0.4", "netSpliceRate:0.2", "crossoverRate:0.5", "extraHNLinks:true", "HNProcessDepth:2", "HNProcessWidth:2", "convolution:true", "tetrisAllowLine:false", "tetrisAllowSquare:false", "tetrisAllowTri:false", "tetrisAllowLShape:false", "tetrisAllowJShape:false", "senseTetrisHolesAsPositive:true"});
 		MMNEAT.loadClasses();
+		EvolutionaryHistory.initArchetype(0);
 		
 		HyperNEATTask tetris = (HyperNEATTask) MMNEAT.task;
 		List<Triple<Integer, Integer, Integer>> test = FlexibleSubstrateArchitecture.getHiddenArchitecture(tetris);
@@ -84,11 +95,14 @@ public class FlexibleSubstrateArchitectureTest {
 		MMNEAT.clearClasses();
 	}
 	
-//	@Test
+	//@Test
 	public void testGetHiddenArchitectureD1W1() {
 		MMNEAT.clearClasses();
+		EvolutionaryHistory.archetypes = null;
+		EvolutionaryHistory.setInnovation(0l);
 		Parameters.initializeParameterCollections(new String[] {"runNumber:1", "randomSeed:1", "trials:3", "maxGens:500", "mu:50", "io:false", "netio:false", "mating:true", "task:edu.southwestern.tasks.rlglue.tetris.HyperNEATTetrisTask",  "rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris", "rlGlueExtractor:edu.southwestern.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor", "tetrisTimeSteps:true", "tetrisBlocksOnScreen:false",  "rlGlueAgent:edu.southwestern.tasks.rlglue.tetris.TetrisAfterStateAgent", "splitRawTetrisInputs:true", "senseHolesDifferently:true", "hyperNEAT:true", "genotype:edu.southwestern.evolution.genotypes.HyperNEATCPPNGenotype", "allowMultipleFunctions:true", "ftype:1", "netChangeActivationRate:0.3", "substrateMapping:edu.southwestern.networks.hyperneat.BottomSubstrateMapping", "steps:500000", "perLinkMutateRate:0.05", "netLinkRate:0.4", "netSpliceRate:0.2", "crossoverRate:0.5", "extraHNLinks:true", "HNProcessDepth:1", "HNProcessWidth:1", "convolution:false", "tetrisAllowLine:false", "tetrisAllowSquare:false", "tetrisAllowTri:false", "tetrisAllowLShape:false", "tetrisAllowJShape:false", "senseTetrisHolesAsPositive:true"});
 		MMNEAT.loadClasses();
+		EvolutionaryHistory.initArchetype(0);
 		
 		HyperNEATTask tetris = (HyperNEATTask) MMNEAT.task;
 		List<Triple<Integer, Integer, Integer>> test = FlexibleSubstrateArchitecture.getHiddenArchitecture(tetris);
@@ -100,11 +114,14 @@ public class FlexibleSubstrateArchitectureTest {
 		MMNEAT.clearClasses();
 	}
 	
-//	@Test
+	//@Test
 	public void testGetHiddenArchitectureD2W2() {
 		MMNEAT.clearClasses();
+		EvolutionaryHistory.archetypes = null;
+		EvolutionaryHistory.setInnovation(0l);
 		Parameters.initializeParameterCollections(new String[] {"runNumber:1", "randomSeed:1", "trials:3", "maxGens:500", "mu:50", "io:false", "netio:false", "mating:true", "task:edu.southwestern.tasks.rlglue.tetris.HyperNEATTetrisTask",  "rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris", "rlGlueExtractor:edu.southwestern.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor", "tetrisTimeSteps:true", "tetrisBlocksOnScreen:false",  "rlGlueAgent:edu.southwestern.tasks.rlglue.tetris.TetrisAfterStateAgent", "splitRawTetrisInputs:true", "senseHolesDifferently:true", "hyperNEAT:true", "genotype:edu.southwestern.evolution.genotypes.HyperNEATCPPNGenotype", "allowMultipleFunctions:true", "ftype:1", "netChangeActivationRate:0.3", "substrateMapping:edu.southwestern.networks.hyperneat.BottomSubstrateMapping", "steps:500000", "perLinkMutateRate:0.05", "netLinkRate:0.4", "netSpliceRate:0.2", "crossoverRate:0.5", "extraHNLinks:true", "HNProcessDepth:2", "HNProcessWidth:2", "convolution:false", "tetrisAllowLine:false", "tetrisAllowSquare:false", "tetrisAllowTri:false", "tetrisAllowLShape:false", "tetrisAllowJShape:false", "senseTetrisHolesAsPositive:true"});
 		MMNEAT.loadClasses();
+		EvolutionaryHistory.initArchetype(0);
 		
 		HyperNEATTask tetris = (HyperNEATTask) MMNEAT.task;
 		List<Triple<Integer, Integer, Integer>> test = FlexibleSubstrateArchitecture.getHiddenArchitecture(tetris);
@@ -118,11 +135,14 @@ public class FlexibleSubstrateArchitectureTest {
 		MMNEAT.clearClasses();
 	}
 	
-//	@Test
+	//@Test
 	public void testGetHiddenArchitectureD4W4() {
 		MMNEAT.clearClasses();
+		EvolutionaryHistory.archetypes = null;
+		EvolutionaryHistory.setInnovation(0l);
 		Parameters.initializeParameterCollections(new String[] {"runNumber:1", "randomSeed:1", "trials:3", "maxGens:500", "mu:50", "io:false", "netio:false", "mating:true", "task:edu.southwestern.tasks.rlglue.tetris.HyperNEATTetrisTask",  "rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris", "rlGlueExtractor:edu.southwestern.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor", "tetrisTimeSteps:true", "tetrisBlocksOnScreen:false",  "rlGlueAgent:edu.southwestern.tasks.rlglue.tetris.TetrisAfterStateAgent", "splitRawTetrisInputs:true", "senseHolesDifferently:true", "hyperNEAT:true", "genotype:edu.southwestern.evolution.genotypes.HyperNEATCPPNGenotype", "allowMultipleFunctions:true", "ftype:1", "netChangeActivationRate:0.3", "substrateMapping:edu.southwestern.networks.hyperneat.BottomSubstrateMapping", "steps:500000", "perLinkMutateRate:0.05", "netLinkRate:0.4", "netSpliceRate:0.2", "crossoverRate:0.5", "extraHNLinks:true", "HNProcessDepth:4", "HNProcessWidth:4", "convolution:false", "tetrisAllowLine:false", "tetrisAllowSquare:false", "tetrisAllowTri:false", "tetrisAllowLShape:false", "tetrisAllowJShape:false", "senseTetrisHolesAsPositive:true"});
 		MMNEAT.loadClasses();
+		EvolutionaryHistory.initArchetype(0);
 		
 		HyperNEATTask tetris = (HyperNEATTask) MMNEAT.task;
 		List<Triple<Integer, Integer, Integer>> test = FlexibleSubstrateArchitecture.getHiddenArchitecture(tetris);
@@ -145,8 +165,11 @@ public class FlexibleSubstrateArchitectureTest {
 	//@Test
 	public void testGetHiddenArchitectureD2W4Rec5x5() {
 		MMNEAT.clearClasses();
+		EvolutionaryHistory.archetypes = null;
+		EvolutionaryHistory.setInnovation(0l);
 		Parameters.initializeParameterCollections(new String[] {"runNumber:1", "randomSeed:1", "trials:3", "maxGens:500", "mu:50", "io:false", "netio:false", "mating:true", "task:edu.southwestern.tasks.rlglue.tetris.HyperNEATTetrisTask",  "rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris", "rlGlueExtractor:edu.southwestern.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor", "tetrisTimeSteps:true", "tetrisBlocksOnScreen:false",  "rlGlueAgent:edu.southwestern.tasks.rlglue.tetris.TetrisAfterStateAgent", "splitRawTetrisInputs:true", "senseHolesDifferently:true", "hyperNEAT:true", "genotype:edu.southwestern.evolution.genotypes.HyperNEATCPPNGenotype", "allowMultipleFunctions:true", "ftype:1", "netChangeActivationRate:0.3", "substrateMapping:edu.southwestern.networks.hyperneat.BottomSubstrateMapping", "steps:500000", "perLinkMutateRate:0.05", "netLinkRate:0.4", "netSpliceRate:0.2", "crossoverRate:0.5", "extraHNLinks:true", "HNProcessDepth:2", "HNProcessWidth:4", "convolution:true", "tetrisAllowLine:false", "tetrisAllowSquare:false", "tetrisAllowTri:false", "tetrisAllowLShape:false", "tetrisAllowJShape:false", "senseTetrisHolesAsPositive:true", "receptiveFieldHeight:5", "receptiveFieldWidth:5"});
 		MMNEAT.loadClasses();
+		EvolutionaryHistory.initArchetype(0);
 		
 		HyperNEATTask tetris = (HyperNEATTask) MMNEAT.task;
 
@@ -162,11 +185,14 @@ public class FlexibleSubstrateArchitectureTest {
 		MMNEAT.clearClasses();
 	}
 	
-//	@Test
+	//@Test
 	public void testGetHiddenArchitectureCNND4W4() {
 		MMNEAT.clearClasses();
+		EvolutionaryHistory.archetypes = null;
+		EvolutionaryHistory.setInnovation(0l);
 		Parameters.initializeParameterCollections(new String[] {"runNumber:1", "randomSeed:1", "trials:3", "maxGens:500", "mu:50", "io:false", "netio:false", "mating:true", "task:edu.southwestern.tasks.rlglue.tetris.HyperNEATTetrisTask",  "rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris", "rlGlueExtractor:edu.southwestern.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor", "tetrisTimeSteps:true", "tetrisBlocksOnScreen:false",  "rlGlueAgent:edu.southwestern.tasks.rlglue.tetris.TetrisAfterStateAgent", "splitRawTetrisInputs:true", "senseHolesDifferently:true", "hyperNEAT:true", "genotype:edu.southwestern.evolution.genotypes.HyperNEATCPPNGenotype", "allowMultipleFunctions:true", "ftype:1", "netChangeActivationRate:0.3", "substrateMapping:edu.southwestern.networks.hyperneat.BottomSubstrateMapping", "steps:500000", "perLinkMutateRate:0.05", "netLinkRate:0.4", "netSpliceRate:0.2", "crossoverRate:0.5", "extraHNLinks:true", "HNProcessDepth:4", "HNProcessWidth:4", "convolution:true", "tetrisAllowLine:false", "tetrisAllowSquare:false", "tetrisAllowTri:false", "tetrisAllowLShape:false", "tetrisAllowJShape:false", "senseTetrisHolesAsPositive:true"});
 		MMNEAT.loadClasses();
+		EvolutionaryHistory.initArchetype(0);
 		
 		HyperNEATTask tetris = (HyperNEATTask) MMNEAT.task;
 		List<Triple<Integer, Integer, Integer>> test = FlexibleSubstrateArchitecture.getHiddenArchitecture(tetris);
