77dfdd6ca9ff91c8cd2151837cb870779137fbdc
DevonFulcher <fulcherd@southwestern.edu>
2018-06-12 17:01:09 -0500

    This fix allows to maven compile #212

--- src/test/java/edu/southwestern/networks/hyperneat/HyperNEATSeedTest.java
+++ src/test/java/edu/southwestern/networks/hyperneat/HyperNEATSeedTest.java
@@ -1,26 +1,21 @@
 package edu.southwestern.networks.hyperneat;
 
 import static org.junit.Assert.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
-
 import edu.southwestern.MMNEAT.MMNEAT;
 import edu.southwestern.evolution.EvolutionaryHistory;
 import edu.southwestern.evolution.genotypes.HyperNEATCPPNGenotype;
 import edu.southwestern.evolution.genotypes.TWEANNGenotype;
-import edu.southwestern.evolution.genotypes.TWEANNGenotype.LinkGene;
 import edu.southwestern.parameters.Parameters;
+import edu.southwestern.tasks.rlglue.tetris.HyperNEATTetrisTask;
 import edu.southwestern.util.ClassCreation;
-import edu.southwestern.util.datastructures.Triple;
 
 public class HyperNEATSeedTest {
 	
 	@Test
 	public void test() throws NoSuchMethodException {
 		MMNEAT.clearClasses();
+		HyperNEATTetrisTask.hardSubstrateReset();
 		EvolutionaryHistory.archetypes = null;
 		EvolutionaryHistory.setInnovation(0l);
 		Parameters.initializeParameterCollections(new String[] {"runNumber:1", "randomSeed:1", "trials:3", "maxGens:500", "mu:50", "io:false", "netio:false", "mating:true", "task:edu.southwestern.tasks.rlglue.tetris.TetrisTask", "rlGlueEnvironment:org.rlcommunity.environments.tetris.Tetris", "rlGlueExtractor:edu.southwestern.tasks.rlglue.featureextractors.tetris.RawTetrisStateExtractor", "tetrisTimeSteps:true", "tetrisBlocksOnScreen:false", "rlGlueAgent:edu.southwestern.tasks.rlglue.tetris.TetrisAfterStateAgent", "splitRawTetrisInputs:true", "senseHolesDifferently:true", "hyperNEATSeedTask:edu.southwestern.tasks.rlglue.tetris.HyperNEATTetrisTask", "substrateMapping:edu.southwestern.networks.hyperneat.BottomSubstrateMapping", "HNProcessDepth:1", "netLinkRate:0.0", "netSpliceRate:0.0", "linkExpressionThreshold:-1", "steps:500000", "extraHNLinks:true", "convolution:true"});
