fd4809cb6682292cf924fdd98e6dc6b8d9556cb9
Jacob Schrum <schrum2@southwestern.edu>
2018-06-14 14:30:03 -0500

    #205 Can successfully launch botprize server
    I think this issue may be complete, though we need to see if it works on
    another computer first. The code will now actually copy the version of
    GameBots (which contains BotPrize) directly to the UT2004\System
    directory, replacing whatever version is there. In addition, it copies a
    GameBots.ini file that disables all visualization settings, so that
    there is not cheating at the BotPrize game. Let's see if it works on
    other machines, and then close this issue @friedmaa

--- src/main/java/edu/southwestern/tasks/ut2004/BotPrizeServer.java
+++ src/main/java/edu/southwestern/tasks/ut2004/BotPrizeServer.java
@@ -1,5 +1,10 @@
 package edu.southwestern.tasks.ut2004;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import edu.southwestern.evolution.genotypes.Genotype;
 import edu.southwestern.networks.TWEANN;
@@ -7,23 +12,47 @@ import edu.southwestern.parameters.Parameters;
 import edu.southwestern.tasks.ut2004.controller.BotController;
 import edu.southwestern.tasks.ut2004.fitness.UT2004FitnessFunction;
 
+import static java.nio.file.StandardCopyOption.*;
+
 /**
- * Launches a server with the botprize modifier
+ * Launches a server with the botprize modifier and botprize agents:
+ * two UT^2 agents and two MirrorBot agents
  * @author Jacob Schrum
  */
 public class BotPrizeServer {
-	public static void main(String[] args) {
-		Parameters.initializeParameterCollections(new String[] {"runNumber:0", "io:false", "netio:false", "numUT2Bots:2", "numMirrorBots:2", "botprizeMod:true"});
+	public static void main(String[] args) throws IOException {
+		Parameters.initializeParameterCollections(new String[] {"runNumber:0", "io:false", "netio:false", "numUT2Bots:2", "numMirrorBots:2", "botprizeMod:true", "utEvalMinutes:15"});
+		
+		copyBotPrizeVersionOfGameBots(); // Make sure right version of GameBots is being used
 		
 		@SuppressWarnings("unchecked")
 		Genotype<TWEANN>[] individuals = new Genotype[0];
 		BotController[] controller = new BotController[0];
 		int[] nativeBotSkills = new int[0];
-		int evalMinutes = 2;
+		int evalMinutes = Parameters.parameters.integerParameter("utEvalMinutes");
 		int desiredSkill = 0;
 		ArrayList<UT2004FitnessFunction<TWEANN>> fitness = new ArrayList<>();
 		ArrayList<UT2004FitnessFunction<TWEANN>> others = new ArrayList<>();
 		//launches server
-		UT2004Task.evaluateMultipleGenotypes(individuals, "DM-Insidious", null, null, null, controller,	nativeBotSkills, evalMinutes, desiredSkill,	fitness, others);
+		UT2004Task.evaluateMultipleGenotypes(individuals, "DM-TrainingDay", null, null, null, controller,	nativeBotSkills, evalMinutes, desiredSkill,	fitness, others);
+	}
+	
+	/**
+	 * Copy the version of GameBots stored in MM-NEAT into the UT2004\System directory to make sure that
+	 * the botprize mode is available.
+	 * @throws IOException 
+	 */
+	public static void copyBotPrizeVersionOfGameBots() throws IOException {
+		String[] filesToCopy = new String[] {
+				"GameBots2004.u",	// 2012 version of BotPrize mod
+				"GameBots2004.ucl", // 2012 version of BotPrize mod
+				"GameBots2004.ini"	// Configuration that disables all GameBots visualization
+		};
+		String utSystemDir = Parameters.parameters.stringParameter("utDrive") + ":" + File.separator + Parameters.parameters.stringParameter("utPath") + File.separator + "System";
+		for(String file :  filesToCopy) {
+			Files.copy(Paths.get("data" + File.separatorChar + "unreal" + File.separatorChar + "BotPrizeGameBots"  + File.separatorChar + file), 
+					   Paths.get(utSystemDir + File.separatorChar + file), 
+					   REPLACE_EXISTING);
+		}
 	}
 }
