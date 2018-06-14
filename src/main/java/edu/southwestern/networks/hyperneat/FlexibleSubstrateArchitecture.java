5920fbde26b27e466218cc69d54fe86485b052a7
DevonFulcher <fulcherd@southwestern.edu>
2018-06-14 16:45:05 -0500

    batch files to test #157

--- src/main/java/edu/southwestern/networks/hyperneat/FlexibleSubstrateArchitecture.java
+++ src/main/java/edu/southwestern/networks/hyperneat/FlexibleSubstrateArchitecture.java
@@ -5,7 +5,6 @@ import java.util.Iterator;
 import java.util.List;
 
 import edu.southwestern.parameters.Parameters;
-import edu.southwestern.util.MiscUtil;
 import edu.southwestern.util.datastructures.Pair;
 import edu.southwestern.util.datastructures.Triple;
 
