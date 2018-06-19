0df33acae9de6d5add0afd93576c5be6d130f515
DevonFulcher <fulcherd@southwestern.edu>
2018-06-19 16:58:29 -0500

    Custom Architectures and tests #157

---
+++ src/main/java/edu/southwestern/networks/hyperneat/architecture/TinySubstrates.java
@@ -0,0 +1,36 @@
+package edu.southwestern.networks.hyperneat.architecture;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.southwestern.networks.hyperneat.HyperNEATTask;
+import edu.southwestern.networks.hyperneat.SubstrateConnectivity;
+import edu.southwestern.util.datastructures.Pair;
+import edu.southwestern.util.datastructures.Triple;
+
+/**
+ * Input connected to 1 first hidden substrate with 3x3 convolutional. First hidden connected to 20 1x1 second hidden with full. Last hidden connected to output with full 
+ * @author Devon Fulcher
+ */
+public class TinySubstrates implements SubstrateArchitectureDefinition{
+
+	@Override
+	public List<Triple<Integer, Integer, Integer>> getNetworkHiddenArchitecture() {
+		List<Triple<Integer, Integer, Integer>> networkHiddenArchitecture = new ArrayList<Triple<Integer, Integer, Integer>>();
+		networkHiddenArchitecture.add(new Triple<Integer, Integer, Integer>(1, 8, 18));
+		networkHiddenArchitecture.add(new Triple<Integer, Integer, Integer>(20, 1, 1));
+		return networkHiddenArchitecture;
+	}
+
+	@Override
+	public List<SubstrateConnectivity> getSubstrateConnectivity(HyperNEATTask hnt) {
+		List<SubstrateConnectivity> substrateConnectivity = new ArrayList<SubstrateConnectivity>();
+		Pair<List<String>, List<String>> io = FlexibleSubstrateArchitecture.getInputAndOutputNames(hnt);
+		List<Triple<Integer, Integer, Integer>> networkHiddenArchitecture = getNetworkHiddenArchitecture();
+		FlexibleSubstrateArchitecture.connectInputToFirstHidden(substrateConnectivity, io.t1, networkHiddenArchitecture, 3, 3);
+		FlexibleSubstrateArchitecture.connectAdjacentHiddenLayers(substrateConnectivity, networkHiddenArchitecture, SubstrateConnectivity.CTYPE_FULL);
+		FlexibleSubstrateArchitecture.connectLastHiddenToOutput(substrateConnectivity, io.t2, networkHiddenArchitecture, SubstrateConnectivity.CTYPE_FULL);
+		return substrateConnectivity;
+	}
+
+}
