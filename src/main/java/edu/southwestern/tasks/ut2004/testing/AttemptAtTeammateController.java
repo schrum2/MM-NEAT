574f29a74f258c45e40b197a3733f98f9a5e664c
friedmaa <31463347+friedmaa@users.noreply.github.com>
2018-06-20 16:55:47 -0500

    Bot now runs to get health when its HP frops below 20

--- src/main/java/edu/southwestern/tasks/ut2004/testing/AttemptAtTeammateController.java
+++ src/main/java/edu/southwestern/tasks/ut2004/testing/AttemptAtTeammateController.java
@@ -1,16 +1,21 @@
 package edu.southwestern.tasks.ut2004.testing;
 
+import cz.cuni.amis.pogamut.base3d.worldview.object.Location;
 import cz.cuni.amis.pogamut.ut2004.bot.impl.UT2004BotModuleController;
+import cz.cuni.amis.pogamut.ut2004.communication.messages.ItemType;
 import cz.cuni.amis.pogamut.ut2004.communication.messages.gbinfomessages.Player;
+import cz.cuni.amis.utils.collections.MyCollections;
 import edu.southwestern.tasks.ut2004.actions.BotAction;
 import edu.southwestern.tasks.ut2004.actions.EmptyAction;
 import edu.southwestern.tasks.ut2004.actions.FollowEnemy;
 import edu.southwestern.tasks.ut2004.actions.FollowTeammate;
+import edu.southwestern.tasks.ut2004.actions.NavigateToLocationAction;
 import edu.southwestern.tasks.ut2004.controller.BotController;
 import edu.southwestern.tasks.ut2004.controller.behaviors.AttackEnemyAloneModule;
 
 public class AttemptAtTeammateController implements BotController {
 	AttackEnemyAloneModule attack = new AttackEnemyAloneModule();
+	public static final int FULL_HEALTH = 100;
 	
 	/**
 	 * bot will follow closest teammate, if one is not there, it will stand in place
@@ -18,6 +23,13 @@ public class AttemptAtTeammateController implements BotController {
 	public BotAction control(@SuppressWarnings("rawtypes") UT2004BotModuleController bot) {
 		Player nearestFriend = bot.getPlayers().getNearestVisibleFriend();
 		Player nearestEnemy = bot.getPlayers().getNearestVisibleEnemy();
+		
+		//bot should look for health pickups if it drops below 20hp
+		if(bot.getBot().getSelf().getHealth() < (FULL_HEALTH/5)) {
+			//tell bot to abandon whatever it's doing and go find health
+			Location getHealth =  bot.getItems().getNearestVisibleItem(ItemType.Category.HEALTH).getLocation();
+			return new NavigateToLocationAction(getHealth);
+		}
 		
 		// null check first
 		if(nearestFriend != null) {
