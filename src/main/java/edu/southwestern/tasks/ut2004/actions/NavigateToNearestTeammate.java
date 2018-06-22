720d7036aef50b5a8ca1359c5bd834079015cc9b
friedmaa <31463347+friedmaa@users.noreply.github.com>
2018-06-22 14:01:38 -0500

    Trying to create a NavigateToTeammateAction

--- src/main/java/edu/southwestern/tasks/ut2004/actions/NavigateToNearestTeammate.java
+++ src/main/java/edu/southwestern/tasks/ut2004/actions/NavigateToNearestTeammate.java
@@ -1,3 +1,37 @@
+package edu.southwestern.tasks.ut2004.actions;
+
+import java.util.HashMap;
+
+import cz.cuni.amis.pogamut.base3d.worldview.object.Location;
+import cz.cuni.amis.pogamut.ut2004.bot.impl.UT2004BotModuleController;
+import cz.cuni.amis.pogamut.ut2004.communication.messages.gbinfomessages.Player;
+
+/**
+ * Tells the bot to navigate to the given teammate
+ * @author Adina Friedman
+ */
+public class NavigateToNearestTeammate extends NavigateToLocationAction {
+	public Player friend;
+	Location nearestFriend = null;
+	HashMap<String,Location> friendLocations;
+	public static final int MAX_DISTANCE = 1000;
+	
+	public void giveFriendLocations(HashMap<String,Location> friendLocations) {
+		this.friendLocations = friendLocations;
+	}
+	
+	public Location locationOfNearestTeammate(UT2004BotModuleController bot) {
+		Location botLocation = bot.getBot().getLocation();
+		double minDistance = MAX_DISTANCE;
+		for(String s: friendLocations.keySet()) {
+			Location friendLocation = friendLocations.get(s);
+			if(friendLocation == botLocation) {
+				continue;
+			}
+			double friendDistance = friendLocation.getDistance(botLocation);
+			if(friendDistance < minDistance) {
+				nearestFriend = friendLocation;
+			}			
 		}
 		return nearestFriend;
 	}
