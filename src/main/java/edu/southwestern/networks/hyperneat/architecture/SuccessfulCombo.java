4a1df41b0a20a68e41258a48862fae905316e5ff
DevonFulcher <fulcherd@southwestern.edu>
2018-06-22 10:32:39 -0500

    new batch files and custom architectures #157

---
+++ src/main/java/edu/southwestern/networks/hyperneat/architecture/SuccessfulCombo.java
@@ -0,0 +1,38 @@
+package edu.southwestern.networks.hyperneat.architecture;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.southwestern.networks.hyperneat.HyperNEATTask;
+import edu.southwestern.networks.hyperneat.SubstrateConnectivity;
+import edu.southwestern.util.datastructures.Pair;
+import edu.southwestern.util.datastructures.Triple;
+
+/**
+ * Inputs are connected to the first hidden layer by a 3x3 receptive field and the first hidden layer is fully connected to the second 1x10 hidden layer.
+ * @author Devon Fulcher
+ */
+public class SuccessfulCombo implements SubstrateArchitectureDefinition{
+
+	@Override
+	public List<Triple<Integer, Integer, Integer>> getNetworkHiddenArchitecture() {
+		List<Triple<Integer, Integer, Integer>> networkHiddenArchitecture = new ArrayList<Triple<Integer, Integer, Integer>>();
+		networkHiddenArchitecture.add(new Triple<Integer, Integer, Integer>(1, 8, 18));
+		networkHiddenArchitecture.add(new Triple<Integer, Integer, Integer>(1, 8, 18));
+		networkHiddenArchitecture.add(new Triple<Integer, Integer, Integer>(1, 10, 1));
+		return networkHiddenArchitecture;
+	}
+
+	@Override
+	public List<SubstrateConnectivity> getSubstrateConnectivity(HyperNEATTask hnt) {
+		List<SubstrateConnectivity> substrateConnectivity = new ArrayList<SubstrateConnectivity>();
+		Pair<List<String>, List<String>> io = FlexibleSubstrateArchitecture.getInputAndOutputNames(hnt);
+		List<Triple<Integer, Integer, Integer>> networkHiddenArchitecture = getNetworkHiddenArchitecture();
+		FlexibleSubstrateArchitecture.connectInputToFirstHidden(substrateConnectivity, io.t1, networkHiddenArchitecture, 3, 3);
+		FlexibleSubstrateArchitecture.connectHiddenToHidden(substrateConnectivity, networkHiddenArchitecture, 1, 1, 0, 1);
+		FlexibleSubstrateArchitecture.connectHiddenToHidden(substrateConnectivity, networkHiddenArchitecture, -1, -1, 1, 2);
+		FlexibleSubstrateArchitecture.connectLastHiddenToOutput(substrateConnectivity, io.t2, networkHiddenArchitecture, SubstrateConnectivity.CTYPE_FULL);
+		return substrateConnectivity;
+	}
+
+}
