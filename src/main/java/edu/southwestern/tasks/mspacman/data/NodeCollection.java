7310e6fa079352fe91dcb8a2371b9626d327956d
Will Price <Squillyprice01@users.noreply.github.com>
2018-06-27 09:22:30 -0500

    #218 made a potential fix to the null pointer error

--- src/main/java/edu/southwestern/tasks/mspacman/data/NodeCollection.java
+++ src/main/java/edu/southwestern/tasks/mspacman/data/NodeCollection.java
@@ -55,31 +55,33 @@ public abstract class NodeCollection {
 		assert activePills != null : "Why is active pills null?";
 		int nearestPill = gs.getClosestNodeIndexFromNodeIndex(current, activePills);
 		int farthestPill = gs.getFarthestNodeIndexFromNodeIndex(current, activePills);
-		assert nearestPill != -1 : "The pillModel should stop this from happening";
-		assert farthestPill != -1 : "The pillModel should stop this from happening";
-		int[] tempPath = gs.getShortestPath(nearestPill, farthestPill);
-		assert tempPath != null : "tempPath is null in NodeCollection.addExtraNodes";
-		int[] pillPath = new int[tempPath.length + 1];
-		System.arraycopy(tempPath, 0, pillPath, 0, tempPath.length);
-		pillPath[tempPath.length] = farthestPill;
-		if (ArrayUtil.subset(activePills, pillPath)) {
-			// Path will consume all pills
-			//assert gs.nodeInMaze(farthestPill) : "Farthest pill " + farthestPill + " not in maze";
-			addedNodes.add(farthestPill);
-		}
-
-		if (escapeToPowerPills) {
-			int[] activePowerPills = gs.getActivePowerPillsIndices();
-			for (Integer j : activePowerPills) {
-				if (j == current || j == lastNodeVisited) { // Can't escape to
-															// current location
-															// or previous
-															// escape node
-					continue;
-				}
-				assert gs.nodeInMaze(j) : "Power pill " + j + " not in maze";
-				addedNodes.add(j);
+		if( nearestPill != -1 && farthestPill != -1) {
+			assert nearestPill != -1 : "The pillModel should stop this from happening";
+			assert farthestPill != -1 : "The pillModel should stop this from happening";
+			int[] tempPath = gs.getShortestPath(nearestPill, farthestPill);
+			assert tempPath != null : "tempPath is null in NodeCollection.addExtraNodes";
+			int[] pillPath = new int[tempPath.length + 1];
+			System.arraycopy(tempPath, 0, pillPath, 0, tempPath.length);
+			pillPath[tempPath.length] = farthestPill;
+			if (ArrayUtil.subset(activePills, pillPath)) {
+				// Path will consume all pills
+				//assert gs.nodeInMaze(farthestPill) : "Farthest pill " + farthestPill + " not in maze";
+				addedNodes.add(farthestPill);
 			}
+
+			if (escapeToPowerPills) {
+				int[] activePowerPills = gs.getActivePowerPillsIndices();
+				for (Integer j : activePowerPills) {
+					if (j == current || j == lastNodeVisited) { // Can't escape to
+																// current location
+																// or previous
+																// escape node
+						continue;
+					}
+					assert gs.nodeInMaze(j) : "Power pill " + j + " not in maze";
+					addedNodes.add(j);
+				}
+			}	
 		}
 
 	}
