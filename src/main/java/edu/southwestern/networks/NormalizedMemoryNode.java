86613c2f1f708b7856967fa2c3054ec17f2f1cad
DevonFulcher <devonfulcher3@gmail.com>
2018-07-03 09:30:25 -0500

    normalized memory cleanup #162

--- src/main/java/edu/southwestern/networks/NormalizedMemoryNode.java
+++ src/main/java/edu/southwestern/networks/NormalizedMemoryNode.java
@@ -1,21 +1,16 @@
 package edu.southwestern.networks;
 
-import java.util.ArrayDeque;
 import edu.southwestern.networks.TWEANN.Link;
-import edu.southwestern.parameters.Parameters;
 
 public class NormalizedMemoryNode extends TWEANN.Node{
 	
 	//the last nodeNormMemoryLength values that this node has seen
-	public ArrayDeque<Double> normalizedMemory;
-	public double normalizedMemorySum;
-	public double normalizedMemoryMean;
-	public double normalizedMemorySumVarianceTerms;
-	public double normalizedMemoryVariance;
-	public static int normalizedMemoryMaxSize;
-	double gamma;
-	double beta;
-	double epsilon;
+	private int numActivationsSeenSoFar;
+	private double memoryMean; //change name
+	private double memoryVariance;
+	private double gamma;
+	private double beta;
+	private double epsilon;
 
 	public NormalizedMemoryNode(TWEANN tweann, int ftype, int ntype, long innovation) {
 		this(tweann, ftype, ntype, innovation, 0.0);
@@ -27,37 +22,29 @@ public class NormalizedMemoryNode extends TWEANN.Node{
 
 	public NormalizedMemoryNode(TWEANN tweann, int ftype, int ntype, long innovation, boolean frozen, double bias) {
 		tweann.super(ftype, ntype, innovation, frozen, bias);
-		this.normalizedMemorySum = 0;
-		this.normalizedMemoryMean = 0;
-		this.normalizedMemorySumVarianceTerms = 0;
-		this.normalizedMemoryVariance = 0;
+		this.numActivationsSeenSoFar = 0;
+		this.memoryMean = 0;
+		this.memoryVariance = 0;
 		//expensive to compute for every node. Should this be a common constant?
-		normalizedMemoryMaxSize = Parameters.parameters.integerParameter("nodeNormMemoryLength");
-		this.normalizedMemory  = new ArrayDeque<Double>(normalizedMemoryMaxSize);
 	}
 	
 	@Override
 	protected void activateAndTransmit() {
 		activation = ActivationFunctions.activation(ftype, sum);
-		if(normalizedMemory.size() == normalizedMemoryMaxSize) {
-			double removedValue = normalizedMemory.poll();
-			normalizedMemorySum -= removedValue;
-			normalizedMemorySumVarianceTerms -= (removedValue - normalizedMemoryMean) * (removedValue - normalizedMemoryMean);
-		}
-		normalizedMemory.add(activation);
-		//calculate mean
-		normalizedMemorySum += activation;
-		normalizedMemoryMean = normalizedMemorySum / normalizedMemory.size();
-		
-		//calculate variance
-		normalizedMemorySumVarianceTerms += (activation - normalizedMemoryMean) * (activation - normalizedMemoryMean);
-		normalizedMemoryVariance = normalizedMemorySumVarianceTerms / normalizedMemory.size();
+		numActivationsSeenSoFar++;
+		double oldMean = memoryMean;
+		memoryMean = oldMean + ((activation - memoryMean) / numActivationsSeenSoFar);
+		double oldVariance = memoryVariance;
+		memoryVariance = oldVariance + (activation - oldMean) * (activation - memoryMean);
 		
 		//normalize activation
-		activation = (activation - normalizedMemoryMean) / Math.sqrt(normalizedMemoryVariance + epsilon);
+		activation = (activation - memoryMean) / Math.sqrt(memoryVariance + epsilon);
 		
 		//scale and shift
 		activation = gamma * activation + beta;
+		
+		//should this be here instead
+		//normalizedMemory.add(activation);
 		
         // reset sum to original bias after activation 
 		sum = bias;
