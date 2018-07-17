9dcc140e08d35204a6cec94c84cc539c71864446
Jacob Schrum <schrum2@southwestern.edu>
2018-07-17 03:14:58 -0500

    Added Zelda levels from VGLC
    Also added basic code to convert the tile encoding over to GVG-AI, but I can already see that there will be massive problems. #282

--- src/main/java/edu/southwestern/tasks/gvgai/zelda/ZeldaVGLCUtil.java
+++ src/main/java/edu/southwestern/tasks/gvgai/zelda/ZeldaVGLCUtil.java
@@ -1,11 +1,38 @@
 package edu.southwestern.tasks.gvgai.zelda;
 
 import edu.southwestern.util.random.RandomNumbers;
+import gvgai.tools.IO;
 
 public class ZeldaVGLCUtil {
 
+	public static final String ZELDA_LEVEL_PATH = "data/VGLC/Zelda/Processed/";
+	
+	/**
+	 * Get converted VGLC -> GVG-AI level by specifying file name of source VGLC level
+	 * @param fileName
+	 * @return
+	 */
+	public static String[] convertZeldaLevelFileVGLCtoGVGAI(String fileName) {
+		String[] level = new IO().readFile(fileName);
+		return convertZeldaLevelVGLCtoGVGAI(level);
+	}
+	
+	/**
+	 * Take Zelda level as a String array (converted from VGLC file) and
+	 * return a version with tiles swapped to those used by GVG-AI.
+	 * @param level
+	 * @return
+	 */
 	public static String[] convertZeldaLevelVGLCtoGVGAI(String[] level) {
-		return null; //TODO
+		String[] result = new String[level.length];
+		for(int i = 0; i < level.length; i++) {
+			StringBuilder sb = new StringBuilder();
+			for(int j = 0; j < level[i].length(); j++) {
+				sb.append(convertZeldaTileVGLCtoGVGAI(level[i].charAt(j)));
+			}
+			result[i] = sb.toString();
+		}
+		return result;
 	}
 	
 	/**
@@ -62,6 +89,18 @@ public class ZeldaVGLCUtil {
 				return 'g';
 			default:
 				throw new IllegalArgumentException("Invalid Zelda tile from VGLV: " + tile);
+		}
+	}
+	
+	/**
+	 * For quick tests
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[] level = convertZeldaLevelFileVGLCtoGVGAI(ZELDA_LEVEL_PATH+"tloz1_1.txt");
+		
+		for(String line : level) {
+			System.out.println(line);
 		}
 	}
 }
