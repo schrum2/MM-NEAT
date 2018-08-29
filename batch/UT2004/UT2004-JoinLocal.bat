e49c38091bb497a2967d0b93506ca59b0d8c4227
Jacob Schrum <schrum2@southwestern.edu>
2018-08-29 12:45:30 -0500

    Launch script uses 64 bit version of game now
    Also dropped the belownormal priority setting

--- batch/UT2004/UT2004-JoinLocal.bat
+++ batch/UT2004/UT2004-JoinLocal.bat
@@ -3,4 +3,4 @@ cd ..
 set /p texte=< my_ut2004_path.txt  
 cd %texte%
 cd System
-C:\WINDOWS\system32\cmd.exe /c start "UT2004" /belownormal "UT2004.exe" "127.0.0.1"
+C:\WINDOWS\system32\cmd.exe /c start "UT2004 Game" "ut2004-win64.exe" "127.0.0.1"
