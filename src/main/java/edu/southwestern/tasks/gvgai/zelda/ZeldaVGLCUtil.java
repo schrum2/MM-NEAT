ec1ab2b594242a4e836d1f01a2dd08373d189b67
Jacob Schrum <schrum2@southwestern.edu>
2018-08-30 12:24:13 -0500

    #291 Added code to produce json training samples for a single Zelda dungeon
    Currently, the list is just a print of the standard toString, but I think this is compatible with json. It needs to be manually copied into files.

--- src/main/java/edu/southwestern/tasks/gvgai/zelda/ZeldaVGLCUtil.java
+++ src/main/java/edu/southwestern/tasks/gvgai/zelda/ZeldaVGLCUtil.java
@@ -1,6 +1,8 @@
 package edu.southwestern.tasks.gvgai.zelda;
 
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.southwestern.parameters.Parameters;
 import edu.southwestern.tasks.gvgai.GVGAIUtil;
@@ -15,6 +17,8 @@ import gvgai.tracks.singlePlayer.tools.human.Agent;
 public class ZeldaVGLCUtil {
 
 	public static final String ZELDA_LEVEL_PATH = "data/VGLC/Zelda/Processed/";
+	public static final int ZELDA_ROOM_COLUMNS = 11; // This is actually the room height from the original game, since VGLC rotates rooms
+	public static final int ZELDA_ROOM_ROWS = 16; // Equivalent to width in original game
 	
 	/**
 	 * Get converted VGLC -> GVG-AI level by specifying file name of source VGLC level
@@ -25,6 +29,50 @@ public class ZeldaVGLCUtil {
 	public static String[] convertZeldaLevelFileVGLCtoGVGAI(String fileName, Point startLocation) {
 		String[] level = new IO().readFile(fileName);
 		return convertZeldaLevelVGLCtoGVGAI(level, startLocation);
+	}
+	
+	/**
+	 * Takes VGLC text file for a single dungeon, identifies the individual rooms of the dungeon,
+	 * converts them to lists of lists of number codes, and combines the results into a list of
+	 * rooms (where each room is a list of lists).
+	 * @param fileName
+	 * @return
+	 */
+	public static List<List<List<Integer>>> convertZeldaLevelFileVGLCtoListOfRooms(String fileName) {
+		String[] level = new IO().readFile(fileName);
+		List<List<List<Integer>>> rooms = new ArrayList<>();
+		// Parse out rooms
+		for(int i = 0; i < level.length; i += ZELDA_ROOM_ROWS) { // Move down by one room "height"
+			for(int j = 0; j < level[i].length(); j+= ZELDA_ROOM_COLUMNS) { // Move right by one room "width"
+				if(level[i].charAt(j) != '-') { // If the upper-right corner is not "void" then there is a room
+					// Separate the room from the rest of the level
+					String[] room = new String[ZELDA_ROOM_ROWS];
+					for(int r = 0; r < ZELDA_ROOM_ROWS; r++) { // Each row of the room
+						room[r] = level[i+r].substring(j, j+ZELDA_ROOM_COLUMNS); // Only the columns for that room
+					}
+					// Convert individual room to list of codes and add to rooms list
+					rooms.add(convertZeldaLevelVGLCtoRoomAsList(room));
+				}
+			}
+		}		
+		return rooms;
+	}
+	
+	/**
+	 * Takes VGLC room and converts it to a list of lists of numeric codes
+	 * @param room String array representing one dungeon room
+	 * @return List of lists representing one room
+	 */
+	public static List<List<Integer>> convertZeldaLevelVGLCtoRoomAsList(String[] room) {
+		List<List<Integer>> result = new ArrayList<>(ZELDA_ROOM_ROWS);
+		for(int i = 0; i < room.length; i++) {
+			result.add(new ArrayList<Integer>(ZELDA_ROOM_COLUMNS)); // Next row of room
+			for(int j = 0; j < room[i].length(); j++) {
+				int code = convertZeldaTileVGLCtoNumberCode(room[i].charAt(j));
+				result.get(i).add(code);
+			}
+		}
+		return result;
 	}
 	
 	/**
@@ -72,16 +120,17 @@ public class ZeldaVGLCUtil {
 	 * . = floor
 	 * 
 	 * The original game is much more complex. This is an attempt at a simple mapping.
-	 * F -> .
-	 * B -> w (in Zelda, blocks are sometimes movable, though the corpus does not indicate which ones can move)
-	 * M -> Random choice between 1, 2, and 3
-	 * P -> w (in Zelda, projectiles can move over these tiles)
-	 * O -> . (not fully clear what this represents, but I think it is a passable tile)
-	 * I -> w (not clear what this is either)
-	 * D -> g (This is probably not a semantically appropriate choice for GVG-AI, but it should look good)
-	 * S -> g (Also questionable, though a staircase probably is a way of exiting an area)
-	 * W -> w
-	 * - -> .
+	 * The mapping also assigns arbitrary number codes for GAN learning.
+	 * F -> 0:.
+	 * B -> 1:w (in Zelda, blocks are sometimes movable, though the corpus does not indicate which ones can move)
+	 * M -> 2:Random choice between 1, 2, and 3
+	 * P -> 3:w (in Zelda, projectiles can move over these tiles)
+	 * O -> 0:. (not fully clear what this represents, but I think it is a passable tile)
+	 * I -> 3:w (not clear what this is either)
+	 * D -> 4:g (This is probably not a semantically appropriate choice for GVG-AI, but it should look good)
+	 * S -> 4:g (Also questionable, though a staircase probably is a way of exiting an area)
+	 * W -> 3:w
+	 * - -> 0:.
 	 * 
 	 * @param tile From VGLC
 	 * @return Corresponding tile for GVG-AI version of Zelda
@@ -106,6 +155,34 @@ public class ZeldaVGLCUtil {
 				throw new IllegalArgumentException("Invalid Zelda tile from VGLV: " + tile);
 		}
 	}
+
+	/**
+	 * Shares the mapping above, but returns the arbitrary number code.
+	 * Used for GAN learning.
+	 * @param tile A VGLC tile
+	 * @return Number code associated with different classes of tile
+	 */
+	public static int convertZeldaTileVGLCtoNumberCode(char tile) {
+		switch(tile) {
+		case 'F':
+		case 'O':
+		case '-':
+			return 0;	// Passable
+		case 'B': 
+		case 'P':
+		case 'I':
+		case 'W':
+			return 3;	// Impassable
+		case 'M':
+			return 2;	// Monster
+		case 'D':
+		case 'S':
+			return 4;	// Door
+		default:
+			throw new IllegalArgumentException("Invalid Zelda tile from VGLV: " + tile);
+		}
+	}
+
 	
 	/**
 	 * For quick tests
@@ -129,6 +206,10 @@ public class ZeldaVGLCUtil {
 		for(String line : level) {
 			System.out.println(line);
 		}
+		
+		List<List<List<Integer>>> roomList = convertZeldaLevelFileVGLCtoListOfRooms(ZELDA_LEVEL_PATH+"tloz1_1.txt");
+		
+		System.out.println(roomList);
 		
 		Agent agent = new Agent();
 		agent.setup(null, 0, true); // null = no log, true = human 
