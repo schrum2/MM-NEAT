ceabb32f2018ae68d1f1e0877c76d8f71d0b962c
Jacob Schrum <schrum2@southwestern.edu>
2018-09-12 12:33:39 -0500

    Auto-join works now
    Had to specify the port number.
    
    There are also some improvements to the study batch file, but currently it launches both servers at once, which causes problems.

--- batch/Experiments-2018-UT2004/UT2004-Study.bat
+++ batch/Experiments-2018-UT2004/UT2004-Study.bat
@@ -1,12 +1,41 @@
+set /a id=%1
 
+set /a divnumber=%id%/2
+set /a sum=%divnumber%*2
+
+if %id% NEQ %sum% goto odds
+if %id% EQU %sum% goto evens
+
+:odds
 START UT2004-Ethan.bat %1
 CALL UT2004-JoinLocal.bat
 
 REM Survey 1
-start "" https://goo.gl/forms/1xproe3C3HTpPqaG2
+CALL "" https://goo.gl/forms/1xproe3C3HTpPqaG2
 
+REM get back to starting directory
+cd %0\..\
 START UT2004-Jude.bat %1
 CALL UT2004-JoinLocal.bat
 
 REM Survey 2
-start "" https://goo.gl/forms/8gQ68KJnMbQk2Ppz2
+CALL "" https://goo.gl/forms/8gQ68KJnMbQk2Ppz2
+
+goto finished
+
+:evens
+START UT2004-Jude.bat %1
+CALL UT2004-JoinLocal.bat
+
+REM Survey 1
+CALL "" https://goo.gl/forms/1xproe3C3HTpPqaG2
+
+REM get back to starting directory
+cd %0\..\
+START UT2004-Ethan.bat %1
+CALL UT2004-JoinLocal.bat
+
+REM Survey 2
+CALL "" https://goo.gl/forms/8gQ68KJnMbQk2Ppz2
+
+:finished
