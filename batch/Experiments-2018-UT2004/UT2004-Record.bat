7a7b2ebb35f71fe702623b9a2fea5a2d3ca257c1
Jacob Schrum <schrum2@southwestern.edu>
2018-09-13 13:36:40 -0500

    Trying to solve recording problem
    Recording full screen with VLC doesn't really work. Tried ffmpeg, and had the same problem. What do other screen recording programs do differently?

--- batch/Experiments-2018-UT2004/UT2004-Record.bat
+++ batch/Experiments-2018-UT2004/UT2004-Record.bat
@@ -1,1 +1,5 @@
+REM same full screen game problem
+REM ffmpeg -f dshow -i video="screen-capture-recorder" %1.mp4
+
+REM vlc cannot handle full screen game recording well
 vlc screen:// -I rc --screen-follow-mouse --screen-fps 24 :sout=#transcode{vcodec=h264,vb=1800,scale=1}:std{access=file,dst=%1.mp4}
