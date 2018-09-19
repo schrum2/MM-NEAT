696cf151f39f057308770033d75ff6563f75c576
Jacob Schrum <schrum2@southwestern.edu>
2018-09-18 22:52:51 -0500

    Changed the recording script
    Seems to work at home ... need to test it on the study machine

--- batch/Experiments-2018-UT2004/UT2004-Record.bat
+++ batch/Experiments-2018-UT2004/UT2004-Record.bat
@@ -1,5 +1,8 @@
 REM same full screen game problem
 REM ffmpeg -f dshow -i video="screen-capture-recorder" %1.mp4
 
+REM May need to adjust resolution for particular system
+ffmpeg -f gdigrab -framerate 30 -offset_x 0 -offset_y 0 -video_size 1280x768 -show_region 1 -i desktop %1.mkv
+
 REM vlc cannot handle full screen game recording well
-vlc screen:// -I rc --screen-follow-mouse --screen-fps 24 :sout=#transcode{vcodec=h264,vb=1800,scale=1}:std{access=file,dst=%1.mp4}
+REM vlc screen:// -I rc --screen-follow-mouse --screen-fps 24 :sout=#transcode{vcodec=h264,vb=1800,scale=1}:std{access=file,dst=%1.mp4}
