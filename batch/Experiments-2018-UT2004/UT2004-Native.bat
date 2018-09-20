f5b8fd2a5ce119eb5e88cc883f3a9175032c86b2
Jacob Schrum <schrum2@southwestern.edu>
2018-09-20 16:51:42 -0500

    Native bot server is ok ... but
    I'm finally able to reliably launch the specific native bots (named individuals with certain attributes) but they are not as clingy as we want them to me. We may need to fix "Jude" to do the studty after all.

--- batch/Experiments-2018-UT2004/UT2004-Native.bat
+++ batch/Experiments-2018-UT2004/UT2004-Native.bat
@@ -1,8 +1,11 @@
-REM Just launch the server instead? The problem with bots not loading seems to be because of the GameBots game type, but that is not needed for the native bots
-REM C:\TEMP\UT2004\System\ucc.exe server DM-Flux2?game=XGame.XTeamGame?fraglimit=0?GoalScore=0?TimeLimit=10?DoUplink=False?UplinkToGamespy=False?SendStats=False?bAllowPrivateChat=False?bAllowTaunts=False?bEnableVoiceChat=False?bAllowLocalBroadcast=False?BotServerPort=3000?ControlServerPort=3001?ObservingServerPort=3002?BlueTeam=XGame.TeamBlueConfigured?RedTeam=XGame.TeamRedConfigured?NumBots=3?MinPlayers=4?QuickStart=True -port=3003
+cd ..
+cd ..
+set /p texte=< my_ut2004_path.txt  
+cd %texte%
+cd System
+ucc.exe server DM-Flux2?game=XGame.XTeamGame?fraglimit=0?GoalScore=0?TimeLimit=10?DoUplink=False?UplinkToGamespy=False?SendStats=False?bAllowPrivateChat=False?bAllowTaunts=False?bEnableVoiceChat=False?bAllowLocalBroadcast=False?BotServerPort=3000?ControlServerPort=3001?ObservingServerPort=3002?RedTeam=XGame.TeamRedConfigured?BlueTeam=XGame.TeamBlueConfigured?NumBots=3?MinPlayers=4?QuickStart=False -port=3003
 
-
-REM Code for INI file (which one?)
+REM The approach above assumes the following configurations are in the appropriate INI file
 
 REM [UnrealGame.TeamGame]
 REM bBalanceTeams=True
@@ -12,12 +15,9 @@ REM [XGame.TeamRedConfigured]
 REM Characters=Zarina
 
 REM [XGame.TeamBlueConfigured]
-REM Characters=Zarina
+REM Characters=Greith
 REM Characters=Kragoth
 
-REM Problem: Same person cannot be on both team (problem with Zarina)
-
-cd ..
-cd ..
-java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar utStudyTeammate:native runNumber:%1 randomSeed:%1 base:ut2004 io:true netio:false log:UT2004-Native saveTo:Native numUT2Bots:0 numMirrorBots:0 utNumNativeBots:2 botprizeMod:false utEvalMinutes:10 utNumOpponents:1 utGameType:botTeamGame utMap:DM-Flux2 utBotLogOutput:true utBotKilledAtEnd:false
+REM Launch MM-NEAT to launch game with native bots and GameBots mod
+REM java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar utStudyTeammate:native runNumber:%1 randomSeed:%1 base:ut2004 io:true netio:false log:UT2004-Native saveTo:Native numUT2Bots:0 numMirrorBots:0 utNumNativeBots:2 botprizeMod:false utEvalMinutes:10 utNumOpponents:1 utGameType:botTeamGame utMap:DM-Flux2 utBotLogOutput:true utBotKilledAtEnd:false
 exit
