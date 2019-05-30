c63e97483975b28b7ee1919853e66cc23a148eab
Jacob <schrum2@southwestern.edu>
2019-05-30 15:59:22 -0500

    Prototype R File for Averages With Error Bars
    Currently, the experiment directory is hard coded to be cartpole. However, the script automatically figures out how to sort the different experimental conditions, counts how many there are to figure out the right degrees of freedom for each type, and produces a plot.
    
    Another issue: currently only handles first plotted column (or rather, the max value of score one). Need to generalize a bit more

---
+++ AveragesWithErrorBars.R
@@ -0,0 +1,50 @@
+library(ggplot2)
+library(tidyr)
+library(plyr)
+library(dplyr)
+
+setwd("C:\\Users\\schrum2\\eclipse-workspace\\MM-NEAT\\cartpole")
+rm(list = ls())
+# Determine the different experimental conditions
+types <- unique(sub("\\d+","",list.files(".",pattern="[a-zA-Z]+\\d+")))
+# Initialize empty data
+evolutionData <- data.frame(generation = integer(), score = double())
+# Exach experimental condition
+for(t in types) {
+  # Get each directory starting with the type name, followed by digits
+  directories <- list.files(".",pattern=paste("^",t,"\\d*", sep = ""))
+  for(d in directories) {
+    # Read each individual file
+    temp <- read.table(file = paste(d,"/RL-",d,"_parents_log.txt", sep = ""), sep = '\t', header = FALSE)
+    evolutionData <- rbind(evolutionData, data.frame(generation = temp$V1, 
+                                       type = paste(t,sep=""),
+                                       run = substring(d,nchar(t)+1), # Get the number following the type
+                                       score = temp$V4))
+  }
+}
+
+# Extract states: mean, lower confidence bound, upper confidence bound
+evolutionStats <- evolutionData %>%
+  group_by(type, generation) %>%
+  summarize(n = length(run), avgScore = mean(score), stdevScore = sd(score)) %>%
+  mutate(stderrScore = qt(0.975, df = n - 1)*stdevScore/sqrt(n)) %>%
+  mutate(lowScore = avgScore - stderrScore, highScore = avgScore + stderrScore)
+
+png("cartpole.png", width=2000, height=1000)
+v <- ggplot(evolutionStats, aes(x = generation, y = avgScore, color = type)) +
+  geom_ribbon(aes(ymin = lowScore, ymax = highScore, fill = type, alpha = 0.05)) +
+  geom_line(size = 1.5) + # + facet_wrap(~type)
+  ggtitle("Evolution in Cartpole") +
+  ylab("Average Score") +
+  theme(
+    plot.title = element_text(size=25, face="bold"),
+    axis.title.x = element_text(size=25, face="bold"),
+    axis.text.x = element_text(size=25, face="bold"),
+    axis.title.y = element_text(size=25, face="bold"),
+    axis.text.y = element_text(size=25, face="bold"),
+    legend.title = element_text(size=25, face="bold"),
+    legend.text = element_text(size=25, face="bold"),
+    legend.position = c(0.8, 0.2)
+  )
+print(v)
+dev.off()
