19d5bf6f03005a3f6d459ffa9da2b2ee2b4efd79
Jacob <schrum2@southwestern.edu>
2019-05-30 16:36:21 -0500

    Launch from command line with result dir
    #321 R script can now be launched from command line with the result dir as an argument.

--- AveragesWithErrorBars.R
+++ AveragesWithErrorBars.R
@@ -1,10 +1,17 @@
+#!/usr/bin/env Rscript
 library(ggplot2)
 library(tidyr)
 library(plyr)
 library(dplyr)
 
-setwd("C:\\Users\\schrum2\\eclipse-workspace\\MM-NEAT\\cartpole")
-rm(list = ls())
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)==0) {
+  stop("Specify name of result directory as command line parameter.", call.=FALSE)
+} 
+# Set working directory and move into it
+resultDir <- args[1]
+setwd(paste("./",resultDir,sep=""))
 # Determine the different experimental conditions
 types <- unique(sub("\\d+","",list.files(".",pattern="[a-zA-Z]+\\d+")))
 # Initialize empty data
@@ -30,12 +37,14 @@ evolutionStats <- evolutionData %>%
   mutate(stderrScore = qt(0.975, df = n - 1)*stdevScore/sqrt(n)) %>%
   mutate(lowScore = avgScore - stderrScore, highScore = avgScore + stderrScore)
 
-png("cartpole.png", width=2000, height=1000)
+png(paste(resultDir,".png",sep=""), width=2000, height=1000)
 v <- ggplot(evolutionStats, aes(x = generation, y = avgScore, color = type)) +
   geom_ribbon(aes(ymin = lowScore, ymax = highScore, fill = type, alpha = 0.05)) +
-  geom_line(size = 1.5) + # + facet_wrap(~type)
-  ggtitle("Evolution in Cartpole") +
+  geom_line(size = 1.5) + 
+  #facet_wrap(~type) + # For separate plots
+  #ggtitle("INSERT COOL TITLE HERE") +
   ylab("Average Score") +
+  xlab("Generation") +
   theme(
     plot.title = element_text(size=25, face="bold"),
     axis.title.x = element_text(size=25, face="bold"),
@@ -48,3 +57,6 @@ v <- ggplot(evolutionStats, aes(x = generation, y = avgScore, color = type)) +
   )
 print(v)
 dev.off()
+
+print("Success!")
+print(paste("File saved in ",getwd(),"/",resultDir,".png"), sep="")
