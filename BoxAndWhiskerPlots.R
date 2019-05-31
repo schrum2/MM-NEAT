1242722bf3b1ef09c2b93db5210040adea4787c0
Jacob <schrum2@southwestern.edu>
2019-05-31 16:24:09 -0500

    Beginning work on boxplots
    This doesn't really do what I want yet, but is a step in the right direction #321

--- BoxAndWhiskerPlots.R
+++ BoxAndWhiskerPlots.R
@@ -8,7 +8,7 @@ args = commandArgs(trailingOnly=TRUE)
 
 if (length(args) < 3) {
   print("Specify name of result directory, a log prefix, and a score index as command line parameters.")
-  print("Example: Rscript.exe AveragesWithErrorBars.R tetris Tetris 0")
+  print("Example: Rscript.exe BoxAndWhiskerPlots.R tetris Tetris 0")
   stop()
 } 
 # Set working directory and move into it
@@ -50,18 +50,21 @@ for(t in types) {
 # Extract states: mean, lower confidence bound, upper confidence bound
 evolutionStats <- evolutionData %>%
   group_by(type, generation) %>%
-  summarize(n = length(run), avgScore = mean(score), stdevScore = sd(score)) %>%
-  mutate(stderrScore = qt(0.975, df = n - 1)*stdevScore/sqrt(n)) %>%
-  mutate(lowScore = avgScore - stderrScore, highScore = avgScore + stderrScore)
-
-saveFile <- paste("AVG-",resultDir,args[3],".png",sep="")
+  summarize(n = length(run), 
+            medianScore = median(score), 
+            minScore = min(score),
+            maxScore = max(score),
+            firstQuartile = quantile(score, 0.25),
+            thirdQuartile = quantile(score, 0.75))
+  
+saveFile <- paste("BW-",resultDir,args[3],".png",sep="")
 png(saveFile, width=2000, height=1000)
-v <- ggplot(evolutionStats, aes(x = generation, y = avgScore, color = type)) +
-  geom_ribbon(aes(ymin = lowScore, ymax = highScore, fill = type, alpha = 0.05)) +
+v <- ggplot(evolutionStats, aes(x = generation, y = medianScore, color = type)) +
+  geom_ribbon(aes(ymin = minScore, ymax = maxScore, fill = type, alpha = 0.05)) +
   geom_line(size = 1.5) + 
   #facet_wrap(~type) + # For separate plots
   #ggtitle("INSERT COOL TITLE HERE") +
-  ylab("Average Score") +
+  ylab("Score") +
   xlab("Generation") +
   theme(
     plot.title = element_text(size=25, face="bold"),
@@ -78,3 +81,14 @@ dev.off()
 
 print("Success!")
 print(paste("File saved in ",getwd(),"/",saveFile,sep=""))
+
+# Ignoring most of the above, this is pretty close to what I want, but not there yet.
+evolutionData %>% 
+  filter(generation %% 10 == 0) %>%
+  mutate(genF = as.factor(generation)) %>%
+  select(-generation) %>%
+  ggplot(aes(x = genF, y = score, fill = type)) +
+  geom_line(aes(group = type)) +
+  geom_boxplot() 
+
+
