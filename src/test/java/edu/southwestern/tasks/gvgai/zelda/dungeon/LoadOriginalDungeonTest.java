fea5bfd463ca72f98d8feac17a0f78ffbe7be00e
Jake Gutierrez <jakegut0108@gmail.com>
2019-07-11 11:11:23 -0500

    Add comment

--- src/test/java/edu/southwestern/tasks/gvgai/zelda/dungeon/LoadOriginalDungeonTest.java
+++ src/test/java/edu/southwestern/tasks/gvgai/zelda/dungeon/LoadOriginalDungeonTest.java
@@ -21,6 +21,7 @@ public class LoadOriginalDungeonTest {
 	public void setUp() {
 		String format = "tloz%s_1_flip";
 		int numberOfDungeons = 8;
+		// Dungeon 9: Takes a long time for A* to run but it has never been finished before
 		toTest = new String[numberOfDungeons];
 		
 		for(int i = 1; i <= numberOfDungeons; i++) {
