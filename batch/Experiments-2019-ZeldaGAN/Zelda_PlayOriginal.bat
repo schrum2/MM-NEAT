21f7f552e8d1d98ca442718f7e703a65299df741
Jacob Schrum <schrum2@southwestern.edu>
2019-08-31 17:02:44 -0500

    kills ffmpeg too early
    The taskkill command terminates ffmpeg, but we lose the last bit of the recording.
    
    The SLEEP is supposed to delay it, but no matter how large I make it, there doesn't seem to be a difference in the timing.

--- batch/Experiments-2019-ZeldaGAN/Zelda_PlayOriginal.bat
+++ batch/Experiments-2019-ZeldaGAN/Zelda_PlayOriginal.bat
@@ -2,4 +2,6 @@ START Record.bat Subject-%1\Subject-%1-OriginalLevel
 cd ..
 cd ..
 java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar zeldaType:original randomSeed:%1 > batch/Experiments-2019-ZeldaGAN/Subject-%1/Subject-%1-OriginalDungeon.txt
+SLEEP 1000
+taskkill /F /IM ffmpeg.exe /T
 exit
