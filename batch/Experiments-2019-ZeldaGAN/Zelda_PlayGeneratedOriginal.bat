5c911e1a6af2b6740a52e5a89b01d015602bc033
Jacob Schrum <schrum2@southwestern.edu>
2019-08-31 17:02:51 -0500

    Revert "kills ffmpeg too early"
    This reverts commit 21f7f552e8d1d98ca442718f7e703a65299df741.

--- batch/Experiments-2019-ZeldaGAN/Zelda_PlayGeneratedOriginal.bat
+++ batch/Experiments-2019-ZeldaGAN/Zelda_PlayGeneratedOriginal.bat
@@ -2,6 +2,4 @@ START Record.bat Subject-%1\Subject-%1-GraphOriginalRooms
 cd ..
 cd ..
 java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar zeldaType:generated randomSeed:%1 zeldaLevelLoader:edu.southwestern.tasks.gvgai.zelda.level.OriginalLoader > batch/Experiments-2019-ZeldaGAN/Subject-%1/Subject-%1-GeneratedOriginal.txt
-SLEEP 1000
-taskkill /F /IM ffmpeg.exe /T
 exit
