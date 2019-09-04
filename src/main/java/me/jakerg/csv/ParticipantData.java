37d94f6d48103e1b11ad860fc8830f1730b25e7e
Jake Gutierrez <jakegut0108@gmail.com>
2019-09-03 19:24:37 -0500

    Make simple CSV class and datafor logging #408

---
+++ src/main/java/me/jakerg/csv/ParticipantData.java
@@ -0,0 +1,50 @@
+package me.jakerg.csv;
+
+import java.io.File;
+
+import edu.southwestern.parameters.Parameters;
+
+public class ParticipantData {
+	@CSVField
+	public int participantID = Parameters.parameters.integerParameter("randomSeed");
+	
+	@CSVField
+	public int actionsPerformed = 0;
+	
+	@CSVField
+	public int keysCollected = 0;
+	
+	@CSVField
+	public int enemiesKilled = 0;
+	
+	@CSVField
+	public int damageReceived = 0;
+	
+	@CSVField
+	public int heartsCollected = 0;
+	
+	@CSVField
+	public int bombsCollected = 0;
+	
+	@CSVField
+	public int bombsUsed = 0;
+	
+	@CSVField
+	public int deaths = 0;
+	
+	@CSVField
+	public int distintRoomsVisited = 0;
+	
+	public static void main(String[] args) {
+		Parameters.initializeParameterCollections(new String[] {"randomSeed:1111111"});
+		ParticipantData pd = new ParticipantData();
+		pd.deaths = 5;
+		SimpleCSV<ParticipantData> csv = new SimpleCSV<>(pd);
+		try {
+			csv.saveToCSV(true, new File("data/test.csv"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
