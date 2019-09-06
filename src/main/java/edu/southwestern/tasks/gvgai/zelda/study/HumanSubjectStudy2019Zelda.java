fa7143919e9db1aceb1fdcf9144053c1628ed2bf
Jacob Schrum <schrum2@southwestern.edu>
2019-09-05 20:42:47 -0500

    Troubleshooting inconsistent levels
    Different levels are produced by batch file with jar and from running in Eclipse. When a random seed of 4 is used, this is a problem, because there is an unsolvable block puzzle we're trying to fix ... however, the level only appears when launched from a jar file and not from Eclipse. Why not? #410

--- src/main/java/edu/southwestern/tasks/gvgai/zelda/study/HumanSubjectStudy2019Zelda.java
+++ src/main/java/edu/southwestern/tasks/gvgai/zelda/study/HumanSubjectStudy2019Zelda.java
@@ -107,7 +107,7 @@ public class HumanSubjectStudy2019Zelda {
 		//                   edu.southwestern.tasks.gvgai.zelda.level.OriginalLoader
 		
 		
-		MMNEAT.main("zeldaType:generated randomSeed:28 zeldaLevelLoader:edu.southwestern.tasks.gvgai.zelda.level.GANLoader".split(" "));
+		MMNEAT.main("zeldaType:generated randomSeed:4 zeldaLevelLoader:edu.southwestern.tasks.gvgai.zelda.level.OriginalLoader".split(" "));
 //		MMNEAT.main("zeldaType:original randomSeed:2".split(" "));
 	}
 
