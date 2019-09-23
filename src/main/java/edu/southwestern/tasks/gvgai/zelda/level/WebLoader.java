ee8ec4480e1656644631ba2cfadf9a31f8ddd718
Jake Gutierrez <jakegut0108@gmail.com>
2019-09-23 10:26:35 -0500

    Add loader to fetch levels from a REST API

---
+++ src/main/java/edu/southwestern/tasks/gvgai/zelda/level/WebLoader.java
@@ -0,0 +1,64 @@
+package edu.southwestern.tasks.gvgai.zelda.level;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.southwestern.parameters.Parameters;
+import edu.southwestern.tasks.mario.gan.reader.JsonReader;
+
+public class WebLoader implements LevelLoader{
+	
+	
+	URL url;
+	HttpURLConnection con;
+	public WebLoader() {
+		Parameters.parameters.setBoolean("zeldaGANUsesOriginalEncoding", false);
+		try {
+			url = new URL("https://zelda-level-api.herokuapp.com/api/get-level");
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public ArrayList<ArrayList<ArrayList<Integer>>> getLevels() {
+
+		try {
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		con.setRequestProperty("Content-Type", "application/json");
+		String inputLine;
+		StringBuffer content = new StringBuffer();
+		try {
+			BufferedReader in = new BufferedReader(
+			  new InputStreamReader(con.getInputStream()));
+			while ((inputLine = in.readLine()) != null) {
+			    content.append(inputLine);
+			}
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(content.toString());
+		List<List<List<Integer>>> roomInList = JsonReader.JsonToInt("[" + content.toString() + "]");
+		List<List<Integer>> r = roomInList.get(0);
+		ArrayList<ArrayList<ArrayList<Integer>>> ret = new ArrayList<>();
+		ret.add(ZeldaLevelUtil.listToArrayList(r));
+		return ret;
+	}
+
+}
