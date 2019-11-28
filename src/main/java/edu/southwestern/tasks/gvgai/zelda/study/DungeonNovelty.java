bbb000d87efdf19e67a69197b3e9166947658d77
Jacob Schrum <schrum2@southwestern.edu>
2019-11-28 12:25:48 -0600

    Removed "neighbor" concept from Novelty
    #393 The calculation was getting neighboring rooms in terms of dungeon location, but the novelty concept is based on neighbors in characterization space.
    
    However, to make things simple, I now just compute the novelty with respect to the whole list of other rooms.

--- src/main/java/edu/southwestern/tasks/gvgai/zelda/study/DungeonNovelty.java
+++ src/main/java/edu/southwestern/tasks/gvgai/zelda/study/DungeonNovelty.java
@@ -18,80 +18,80 @@ public class DungeonNovelty {
 	static final Point START = new Point(2, 2); // Starting point
 	
 	/**
-	 * Find the novelty of a room (given by focus) in a dungeon by comparing them with the other rooms (for numNeighbors) of the dungeon
+	 * Find the novelty of a room (given by focus) with respect to all the other rooms of the list.
+	 * This is essentially the average distance of the room from all other rooms.
+	 * 
 	 * @param rooms List of rooms to compare with
-	 * @param focus Number representing the room to compare the other rooms with
-	 * @param numNeighbors The amount of rooms to compare the focus room with
+	 * @param focus Index in rooms representing the room to compare the other rooms with
 	 * @return Real number between 0 and 1, 0 being non-novel and 1 being completely novel
 	 */
-	public static double roomNovelty (List<Dungeon.Node> rooms, int focus, int numNeighbors) {
+	public static double roomNovelty(List<Dungeon.Node> rooms, int focus) {
 		double novelty = 0;
 		
-		for(int i = 0; i < numNeighbors; i++) { // For each neighbor we have
-			int neighbor = Math.floorMod(focus + calcNeighbor(i), rooms.size()); // Calculate distance from focus
-			novelty += oneRoomNovelty(rooms.get(focus), rooms.get(neighbor)); // Get the novelty of focus room and calculated neighbor room
+		for(int i = 0; i < rooms.size(); i++) { // For each other room
+			if(i != focus) { // don't compare with self
+				novelty += roomDistance(rooms.get(focus), rooms.get(i)); 
+			}
 		}
 		
-		return novelty / numNeighbors; // Average novelty for each room and neighbors
+		return novelty / rooms.size(); // Novelty is average distance from other rooms
 	}
 
 	/**
-	 * Get the novelty of the a room (node) and another node
+	 * Measure the distance between two rooms, which is like a Hamming distance
+	 * according to different tiles at each coordinate position. The distance is
+	 * then normalized based on the number of tiles.
+	 * 
 	 * @param node "Room" of a dungeon
 	 * @param node2 Another "room" of a dungeon
-	 * @return Real number between 0 and 1, 0 being non-novel and 1 being completely novel
+	 * @return Real number between 0 and 1, 0 being identical and 1 being completely different
 	 */
-	private static double oneRoomNovelty(Node node, Node node2) {
-		double novelty = 0;
+	private static double roomDistance(Node node, Node node2) {
+		double distance = 0;
 		ArrayList<ArrayList<Integer>> room1 = node.level.intLevel;
 		ArrayList<ArrayList<Integer>> room2 = node2.level.intLevel;
 		
 		for(int y = START.y; y < ROWS; y++) {
 			for(int x = START.x; x < COLUMNS; x++) {
 				if(room1.get(y).get(x) != room2.get(y).get(x)) // If the blocks at the same position are not the same, increment novelty
-					novelty++;
+					distance++;
 			}
 		}
-		return novelty / (ROWS * COLUMNS);
+		return distance / (ROWS * COLUMNS);
 	}
 	
 	/**
-	 * Get the novelty of a dungeon by looking through each room and comparing it with its' neighbors
+	 * Get the novelty of a dungeon by computing average novelty of all rooms in the dungeon
+	 * with respect to each other.
+	 * 
 	 * @param dungeon Dungeon to check the novelty of
-	 * @param numNeighbors The amount of rooms to compare each room
 	 * @return Real number between 0 and 1, 0 being non-novel and 1 being completely novel
 	 */
-	public static double dungeonNovelty(Dungeon dungeon, int numNeighbors) {
-		double novelty = 0;
+	public static double averageDungeonNovelty(Dungeon dungeon) {
 		List<Node> rooms = dungeon.getNodes();
-		for(int i = 0; i < rooms.size(); i++) { // For each node in the dungeon
-			novelty += roomNovelty(rooms, i, numNeighbors); // Calculate novelty of room and each of its neighbors
-		}
-		
-		return novelty / rooms.size(); // Get average novelty of each room
+		return averageRoomNovelty(rooms);
 	}
 
 	/**
-	 * Function to get the distance of a neighbor based off of i
-	 * 0  1  2  3  4
-	 * 0  -1 1  -2 2
-	 * @param i Neighbor to check
-	 * @return Distance from focus
+	 * Average novelty across a list of rooms.
+	 * 
+	 * @param rooms List of rooms
+	 * @return Real number between 0 and 1, 0 being non-novel and 1 being completely novel
 	 */
-	private static int calcNeighbor(int i) {
-		int n = (i + 2) / 2;
-		
-		n = (i % 2 == 0) ? n : -n;
-		
-		return n;
+	public static double averageRoomNovelty(List<Node> rooms) {
+		double novelty = 0;
+		for(int i = 0; i < rooms.size(); i++) { // For each node in the dungeon
+			novelty += roomNovelty(rooms, i); // Calculate novelty of room and each of its neighbors
+		}
+		return novelty / rooms.size(); // Get average novelty of each room
 	}
+
 	
 	public static void main(String[] args) throws Exception {
-		int neighbors = 5;
 		Parameters.initializeParameterCollections(args);
 		Dungeon dungeon = LoadOriginalDungeon.loadOriginalDungeon("tloz4_1_flip");
 		
-		System.out.println(dungeonNovelty(dungeon, neighbors));
+		System.out.println(averageDungeonNovelty(dungeon));
 	}
 	
 }
