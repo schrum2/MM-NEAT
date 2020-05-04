8aa0a3df3f442bf12998d87646b1dca33a76ac20
Jacob Schrum <schrum2@southwestern.edu>
2020-05-04 00:57:44 -0500

    Fixed directory for Zelda experiments

--- batch/Experiments-2020-CEC-ZeldaGAN/Zelda_PlayGeneratedGAN.bat
+++ batch/Experiments-2020-CEC-ZeldaGAN/Zelda_PlayGeneratedGAN.bat
@@ -1,5 +1,5 @@
 START "Recording" Record.bat Subject-%1\Subject-%1-GraphGAN
 cd ..
 cd ..
-java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar zeldaType:generated randomSeed:%1 zeldaLevelLoader:edu.southwestern.tasks.gvgai.zelda.level.GANLoader > batch/Experiments-2019-ZeldaGAN/Subject-%1/Subject-%1-GeneratedGAN.txt
+java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar zeldaType:generated randomSeed:%1 zeldaLevelLoader:edu.southwestern.tasks.gvgai.zelda.level.GANLoader > batch/Experiments-2020-CEC-ZeldaGAN/Subject-%1/Subject-%1-GeneratedGAN.txt
 exit
