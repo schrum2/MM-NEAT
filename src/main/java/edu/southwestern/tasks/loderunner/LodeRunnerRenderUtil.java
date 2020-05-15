3777c923ad5b7582456a125cff7f27d7ed2db0a0
Kirby <kdsteckel1@gmail.com>
2020-05-15 16:07:27 -0500

    Continued work on Renderer for Lode Runner
    #470
    Wrote a method that displays the level after all the tile images are placed in the right place.
    Started to write the method that places the tiles in the correct place.

--- src/main/java/edu/southwestern/tasks/loderunner/LodeRunnerRenderUtil.java
+++ src/main/java/edu/southwestern/tasks/loderunner/LodeRunnerRenderUtil.java
@@ -1,7 +1,15 @@
 package edu.southwestern.tasks.loderunner;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 public class LodeRunnerRenderUtil {
@@ -17,9 +25,38 @@ public class LodeRunnerRenderUtil {
 
 	
 	public static BufferedImage getBufferedImage(List<List<Integer>> list) {
-		BufferedImage image = new BufferedImage(LODE_RUNNER_TILE_X*LODE_RUNNER_COLUMNS, LODE_RUNNER_TILE_Y*LODE_RUNNER_ROWS, BufferedImage.TYPE_INT_ARGB);
-		
+		int width = LODE_RUNNER_TILE_X*LODE_RUNNER_COLUMNS;
+		int height = LODE_RUNNER_TILE_Y*LODE_RUNNER_ROWS;
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		setTilesForBufferedImage(image, list, width, height);
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel(new ImageIcon(image.getScaledInstance(width/2, height/2, Image.SCALE_FAST)));
+		panel.add(label);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
 		return image;
 	}
+
+
+	private static BufferedImage setTilesForBufferedImage(BufferedImage image, List<List<Integer>> list, int width, int height) {
+		Graphics2D load = (Graphics2D) image.getGraphics();
+		load.setRenderingHint(
+			    RenderingHints.KEY_ANTIALIASING,
+			    RenderingHints.VALUE_ANTIALIAS_ON);
+		load.setRenderingHint(
+			    RenderingHints.KEY_TEXT_ANTIALIASING,
+			    RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		image =  load.getDeviceConfiguration().createCompatibleImage(width, height);
+		for(int x = 0; x < LODE_RUNNER_TILE_X; x++) {
+			for(int y = 0; y < LODE_RUNNER_TILE_Y; y++) {
+				
+			}
+		}
+		return image;
+	}
+	
+	
 	
 }
