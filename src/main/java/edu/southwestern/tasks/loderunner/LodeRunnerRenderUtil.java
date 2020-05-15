09d74a69ff6d984c48f4b82135d39896dbb014d6
Kirby <kdsteckel1@gmail.com>
2020-05-15 16:53:05 -0500

    Added main method to get a Level and print a bufferedImage
    #470
    Added in main method that should work as expected once the bufferedImage is returned correclty from the getBufferedImage method.
    Commented out a chunk of the code, left it for now becuase I may end up needing it, to try to do it a simpler way than I was initally trying

--- src/main/java/edu/southwestern/tasks/loderunner/LodeRunnerRenderUtil.java
+++ src/main/java/edu/southwestern/tasks/loderunner/LodeRunnerRenderUtil.java
@@ -20,15 +20,16 @@ public class LodeRunnerRenderUtil {
 	
 	
 	public static void main(String[] args) {
-		
+		List<List<Integer>> list = LodeRunnerVGLCUtil.convertLodeRunnerLevelFileVGLCtoListOfLevel("C:/Users/kdste/Documents/GitHub/MM-NEAT/data/VGLC/Lode Runner/Processed/Level 1.txt");
+		BufferedImage image = getBufferedImage(list);
+		System.out.println(image);
 	}
 
 	
 	public static BufferedImage getBufferedImage(List<List<Integer>> list) {
 		int width = LODE_RUNNER_TILE_X*LODE_RUNNER_COLUMNS;
 		int height = LODE_RUNNER_TILE_Y*LODE_RUNNER_ROWS;
-		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		setTilesForBufferedImage(image, list, width, height);
+		BufferedImage image = setTilesForBufferedImage(list, width, height);
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		JLabel label = new JLabel(new ImageIcon(image.getScaledInstance(width/2, height/2, Image.SCALE_FAST)));
@@ -39,21 +40,24 @@ public class LodeRunnerRenderUtil {
 		return image;
 	}
 
-
-	private static BufferedImage setTilesForBufferedImage(BufferedImage image, List<List<Integer>> list, int width, int height) {
-		Graphics2D load = (Graphics2D) image.getGraphics();
-		load.setRenderingHint(
-			    RenderingHints.KEY_ANTIALIASING,
-			    RenderingHints.VALUE_ANTIALIAS_ON);
-		load.setRenderingHint(
-			    RenderingHints.KEY_TEXT_ANTIALIASING,
-			    RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-		image =  load.getDeviceConfiguration().createCompatibleImage(width, height);
+	;
+	private static BufferedImage setTilesForBufferedImage( List<List<Integer>> list, int width, int height) {
+		//do I need this?
+//		Graphics2D load = (Graphics2D) image.getGraphics();
+//		load.setRenderingHint(
+//			    RenderingHints.KEY_ANTIALIASING,
+//			    RenderingHints.VALUE_ANTIALIAS_ON);
+//		load.setRenderingHint(
+//			    RenderingHints.KEY_TEXT_ANTIALIASING,
+//			    RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+//		image =  load.getDeviceConfiguration().createCompatibleImage(width, height);
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		for(int x = 0; x < LODE_RUNNER_TILE_X; x++) {
 			for(int y = 0; y < LODE_RUNNER_TILE_Y; y++) {
 				
 			}
 		}
+		
 		return image;
 	}
 	
