9b77085fefb4031a06796ab33f782aa6aec0da71
Kirby <kdsteckel1@gmail.com>
2020-05-15 14:54:55 -0500

    Updated JSON file with new mappings
    #394
    Updated JSON file with full key mapping.
    Also added a cosntant variables for width and height of the level and tiles in the LodeRunnerRenderUtil.java class #470

--- src/main/java/edu/southwestern/tasks/loderunner/LodeRunnerRenderUtil.java
+++ src/main/java/edu/southwestern/tasks/loderunner/LodeRunnerRenderUtil.java
@@ -1,5 +1,25 @@
 package edu.southwestern.tasks.loderunner;
 
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+
 public class LodeRunnerRenderUtil {
+	public static final int LODE_RUNNER_TILE_X = 8; // x length of an individual tile 
+	public static final int LODE_RUNNER_TILE_Y = 7; // y length of an individual tile 
+	public static final int LODE_RUNNER_COLUMNS = 32; // This is actually the room height from the original game, since VGLC rotates rooms
+	public static final int LODE_RUNNER_ROWS = 22; // Equivalent to width in original game
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+	
+	public static BufferedImage getBufferedImage(List<List<Integer>> list) {
+		BufferedImage image = new BufferedImage(LODE_RUNNER_TILE_X*LODE_RUNNER_COLUMNS, LODE_RUNNER_TILE_Y*LODE_RUNNER_ROWS, BufferedImage.TYPE_INT_ARGB);
+		
+		return image;
+	}
 	
 }
