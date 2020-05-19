178d80f935edff8088db310a912e61d7205bc100
Benjamin Capps <benjamin.capps16@gmail.com>
2020-05-19 15:31:34 -0500

    #457 update

--- src/main/java/edu/southwestern/tasks/zelda/ZeldaDungeonDirectEncodingTask.java
+++ src/main/java/edu/southwestern/tasks/zelda/ZeldaDungeonDirectEncodingTask.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 
 import edu.southwestern.MMNEAT.MMNEAT;
 import edu.southwestern.evolution.genotypes.Genotype;
-import edu.southwestern.parameters.Parameters;
 import edu.southwestern.scores.Score;
 import edu.southwestern.tasks.gvgai.zelda.ZeldaVGLCUtil;
 import edu.southwestern.tasks.gvgai.zelda.dungeon.Dungeon;
@@ -14,7 +13,6 @@ import edu.southwestern.tasks.gvgai.zelda.dungeon.DungeonUtil;
 import edu.southwestern.tasks.gvgai.zelda.dungeon.ZeldaDungeon;
 import edu.southwestern.tasks.gvgai.zelda.dungeon.Dungeon.Node;
 import edu.southwestern.tasks.gvgai.zelda.dungeon.ZeldaDungeon.Level;
-import edu.southwestern.tasks.gvgai.zelda.level.ZeldaLevelUtil;
 import edu.southwestern.util.random.RandomNumbers;
 import me.jakerg.rougelike.Tile;
 
@@ -47,12 +45,12 @@ public class ZeldaDungeonDirectEncodingTask extends ZeldaDungeonTask<List<List<I
 						uuidLabels[y][x] = UUID.nameUUIDFromBytes(RandomNumbers.randomByteArray(16)).toString();
 					}
 					String name = uuidLabels[y][x];
-					Node newNode = dungeonInstance.newNode(name, dungeon[y][x]);
-					
-					ZeldaDungeon.addAdjacencyIfAvailable(dungeonInstance, dungeon, uuidLabels, newNode, x + 1, y, "RIGHT");
-					ZeldaDungeon.addAdjacencyIfAvailable(dungeonInstance, dungeon, uuidLabels, newNode, x, y - 1, "UP");
-					ZeldaDungeon.addAdjacencyIfAvailable(dungeonInstance, dungeon, uuidLabels, newNode, x - 1, y, "LEFT");
-					ZeldaDungeon.addAdjacencyIfAvailable(dungeonInstance, dungeon, uuidLabels, newNode, x, y + 1, "DOWN");
+//					Node newNode = dungeonInstance.newNode(name, dungeon[y][x]);
+					dungeonInstance.newNode(name, dungeon[y][x]);
+//					ZeldaDungeon.addAdjacencyIfAvailable(dungeonInstance, dungeon, uuidLabels, newNode, x + 1, y, "RIGHT");
+//					ZeldaDungeon.addAdjacencyIfAvailable(dungeonInstance, dungeon, uuidLabels, newNode, x, y - 1, "UP");
+//					ZeldaDungeon.addAdjacencyIfAvailable(dungeonInstance, dungeon, uuidLabels, newNode, x - 1, y, "LEFT");
+//					ZeldaDungeon.addAdjacencyIfAvailable(dungeonInstance, dungeon, uuidLabels, newNode, x, y + 1, "DOWN");
 				}	
 			}
 		}
@@ -73,31 +71,48 @@ public class ZeldaDungeonDirectEncodingTask extends ZeldaDungeonTask<List<List<I
 		
 		@SuppressWarnings("unchecked")
 		List<List<Integer>>[][] levelAsListsGrid = new List[2][2];
+		makeEmptyRoom(levelAsListsGrid, 0, 0);
+		makeEmptyRoom(levelAsListsGrid, 1, 0);
+		makeEmptyRoom(levelAsListsGrid, 0, 1);
+		makeEmptyRoom(levelAsListsGrid, 1, 1);
+		int y=ZeldaVGLCUtil.ZELDA_ROOM_ROWS-1;
 		
-		levelAsListsGrid[0][0] = new ArrayList<List<Integer>>();
-		// Make totally empty room
-		for(int y = 0; y < ZeldaVGLCUtil.ZELDA_ROOM_ROWS; y++) {
-			ArrayList<Integer> row = new ArrayList<>();
-			for(int x = 0; x < ZeldaVGLCUtil.ZELDA_ROOM_COLUMNS; x++) {
-				row.add(Tile.FLOOR.getNum());
-			}
-			levelAsListsGrid[0][0].add(row);
-		}
-		// Set left/right walls
-		for(int y = 0; y < ZeldaVGLCUtil.ZELDA_ROOM_ROWS; y++) {
-			for(int x = 0; x <= 1; x++) {
-				levelAsListsGrid[0][0].get(y).set(x,Tile.WALL.getNum());
-				levelAsListsGrid[0][0].get(y).set(levelAsListsGrid[0][0].get(y).size() - 1 - x,Tile.WALL.getNum());
-			}
-		}
-		// Set top/bottom walls
-		for(int y = 0; y <= 1; y++) {
-			for(int x = 0; x < ZeldaVGLCUtil.ZELDA_ROOM_COLUMNS; x++) {
-				levelAsListsGrid[0][0].get(y).set(x,Tile.WALL.getNum());
-				levelAsListsGrid[0][0].get(y).set(levelAsListsGrid[0][0].get(y).size() - 1 - x,Tile.WALL.getNum());
-			}
-		}
+		levelAsListsGrid[0][0].get(4).set(14,Tile.DOOR.getNum());
+		levelAsListsGrid[0][0].get(5).set(14,Tile.DOOR.getNum());
+		levelAsListsGrid[0][0].get(6).set(14,Tile.DOOR.getNum());
+		levelAsListsGrid[0][1].get(4).set(1,Tile.DOOR.getNum());
+		levelAsListsGrid[0][1].get(5).set(1,Tile.DOOR.getNum());
+		levelAsListsGrid[0][1].get(6).set(1,Tile.DOOR.getNum());
 		
+		
+		levelAsListsGrid[0][0].get(y-1).set(8,Tile.LOCKED_DOOR.getNum());
+		levelAsListsGrid[0][0].get(y-1).set(7,Tile.LOCKED_DOOR.getNum());
+		levelAsListsGrid[0][0].get(y-1).set(9,Tile.LOCKED_DOOR.getNum());
+		levelAsListsGrid[1][0].get(1).set(8,Tile.LOCKED_DOOR.getNum());
+		levelAsListsGrid[1][0].get(1).set(7,Tile.LOCKED_DOOR.getNum());
+		levelAsListsGrid[1][0].get(1).set(9,Tile.LOCKED_DOOR.getNum());
+		levelAsListsGrid[1][0].get(4).set(14,Tile.DOOR.getNum());
+		levelAsListsGrid[1][0].get(5).set(14,Tile.DOOR.getNum());
+		levelAsListsGrid[1][0].get(6).set(14,Tile.DOOR.getNum());
+		levelAsListsGrid[1][1].get(4).set(1,Tile.DOOR.getNum());
+		levelAsListsGrid[1][1].get(5).set(1,Tile.DOOR.getNum());
+		levelAsListsGrid[1][1].get(6).set(1,Tile.DOOR.getNum());
+		levelAsListsGrid[1][1].get(1).set(8,Tile.DOOR.getNum());
+		levelAsListsGrid[1][1].get(1).set(7,Tile.DOOR.getNum());
+		levelAsListsGrid[1][1].get(1).set(9,Tile.DOOR.getNum());
+		levelAsListsGrid[0][1].get(y-1).set(8,Tile.DOOR.getNum());
+		levelAsListsGrid[0][1].get(y-1).set(7,Tile.DOOR.getNum());
+		levelAsListsGrid[0][1].get(y-1).set(9,Tile.DOOR.getNum());
+		for(int x = 0; x<ZeldaVGLCUtil.ZELDA_ROOM_COLUMNS;x++) {
+			if(y>=0) {
+			levelAsListsGrid[0][1].get(y).set(x,Tile.WALL.getNum());
+			y--;
+			}
+		}
+		levelAsListsGrid[0][1].get(5).set(3, Tile.KEY.getNum());
+		levelAsListsGrid[0][1].get(6).set(8, Tile.TRIFORCE.getNum());
+		
+
 		// Look at dungeon structure
 		Dungeon dungeon = task.makeDungeon(levelAsListsGrid);
 		DungeonUtil.viewDungeon(dungeon);
@@ -145,5 +160,30 @@ public class ZeldaDungeonDirectEncodingTask extends ZeldaDungeonTask<List<List<I
 		});
 		
 		System.out.println(s);
+	}
+	private static void makeEmptyRoom(List<List<Integer>>[][] levelAsListsGrid, int x1, int y1) {
+		levelAsListsGrid[x1][y1] = new ArrayList<List<Integer>>();
+		// Make totally empty room
+		for(int y = 0; y < ZeldaVGLCUtil.ZELDA_ROOM_ROWS; y++) {
+			ArrayList<Integer> row = new ArrayList<>();
+			for(int x = 0; x < ZeldaVGLCUtil.ZELDA_ROOM_COLUMNS; x++) {
+				row.add(Tile.FLOOR.getNum());
+			}
+			levelAsListsGrid[x1][y1].add(row);
+		}
+		// Set left/right walls
+		for(int y = 0; y < ZeldaVGLCUtil.ZELDA_ROOM_ROWS; y++) {
+			for(int x = 0; x <= 1; x++) {
+				levelAsListsGrid[x1][y1].get(y).set(x,Tile.WALL.getNum());
+				levelAsListsGrid[x1][y1].get(y).set(levelAsListsGrid[x1][y1].get(y).size() - 1 - x,Tile.WALL.getNum());
+			}
+		}
+		// Set top/bottom walls
+		for(int y = 0; y <= 1; y++) {
+			for(int x = 0; x < ZeldaVGLCUtil.ZELDA_ROOM_COLUMNS; x++) {
+				levelAsListsGrid[x1][y1].get(y).set(x,Tile.WALL.getNum());
+				levelAsListsGrid[x1][y1].get(ZeldaVGLCUtil.ZELDA_ROOM_ROWS-y-1).set(levelAsListsGrid[x1][y1].get(y).size() - 1 - x,Tile.WALL.getNum());
+			}
+		}
 	}
 }
