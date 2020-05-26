089e18f8a4fe17d1f070cc145c3bdb241f1a7200
Benjamin Capps <benjamin.capps16@gmail.com>
2020-05-26 10:57:14 -0500

    #487 now works both ways
    Called MMNEAT method as well as the reset method to generate a new population of the correct size.

--- src/main/java/edu/southwestern/tasks/interactive/gvgai/ZeldaCPPNtoGANLevelBreederTask.java
+++ src/main/java/edu/southwestern/tasks/interactive/gvgai/ZeldaCPPNtoGANLevelBreederTask.java
@@ -7,8 +7,6 @@ import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -19,10 +17,8 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.Random;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -205,13 +201,10 @@ public class ZeldaCPPNtoGANLevelBreederTask extends InteractiveEvolutionTask<TWE
 			public void actionPerformed(ActionEvent e) {
 				int continueOption = JOptionPane.showConfirmDialog(null,"Warning! Changing this setting will reset the evolved population. Continue anyway?");
 				if(continueOption == 0) { //0 means user pressed yes
-					
 					Parameters.parameters.changeBoolean("zeldaCPPNtoGANAllowsRaft");
+					MMNEAT.setNNInputParameters(ZeldaCPPNtoGANLevelBreederTask.SENSOR_LABELS.length, GANProcess.latentVectorLength()+ZeldaCPPNtoGANLevelBreederTask.numberOfNonLatentVariables());
 					reset();
-					//resetPopulation()
 					resetButtons(true);
-					
-
 				}else { //user pressed something other than yes
 					boolean changeTo = true;
 					if(allowRaft.isSelected()) changeTo=false;
