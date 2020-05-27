ed87edd1c6bb57e0e8cfa2702013e99a72f2fc85
Kirby <kdsteckel1@gmail.com>
2020-05-27 16:48:12 -0500

    Lode Runner A* Agent Parameter
    #474
    Added parameter to create a A* agent to play LodeRunner.
    Created agent in the main method of LodeRunnerLevelTask

--- src/main/java/edu/southwestern/tasks/loderunner/astar/LodeRunnerLevelTask.java
+++ src/main/java/edu/southwestern/tasks/loderunner/astar/LodeRunnerLevelTask.java
@@ -1,13 +1,20 @@
 package edu.southwestern.tasks.loderunner.astar;
 
+import ch.idsia.ai.agents.Agent;
 import edu.southwestern.evolution.genotypes.Genotype;
 import edu.southwestern.tasks.NoisyLonerTask;
+import edu.southwestern.util.ClassCreation;
 import edu.southwestern.util.datastructures.Pair;
 
 public class LodeRunnerLevelTask<T> extends NoisyLonerTask<T> {
+	private Agent agent;
 	
 	public LodeRunnerLevelTask() {
-		
+		try {//tries to create an agent from the command line parameter
+			agent = (Agent) ClassCreation.createObject("lodeRunnerLevelAgent");
+		} catch (NoSuchMethodException e) { //if it can't then it prints the stack trace
+			e.printStackTrace();
+		}
 	}
 
 	@Override
