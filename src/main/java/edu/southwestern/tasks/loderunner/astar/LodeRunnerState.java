179d2c9d3698739e029b5fc83ec6b00617b3b51d
Kirby <kdsteckel1@gmail.com>
2020-05-27 16:13:19 -0500

    LodeRunnerState and LodeRunnerAction
    #474
    Created the LodeRunnerState class that allows the A* algorithm to play the level by defining the moves you can make at any given time, and priovdes a way to evaulate winning a level.
    Inside this class, there is a sub class called LodeRunnerAction that defines all of the actions that a player can do as they play a level of LodeRunner.

--- src/main/java/edu/southwestern/tasks/loderunner/astar/LodeRunnerState.java
+++ src/main/java/edu/southwestern/tasks/loderunner/astar/LodeRunnerState.java
@@ -1,52 +1,217 @@
 package edu.southwestern.tasks.loderunner.astar;
 
+import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 import edu.southwestern.util.search.Action;
 import edu.southwestern.util.search.Heuristic;
 import edu.southwestern.util.search.State;
 
 public class LodeRunnerState extends State<LodeRunnerState.LodeRunnerAction>{
-	public LodeRunnerState() {
-		
+	private List<List<Integer>> level;
+	private HashSet<Point> goldLeft; //set containing the points with gold 
+	public int currentX; 
+	public int currentY;
+	
+	//may add a way to track the enemies in the future, but we are using a simple version right now 
+	public LodeRunnerState(List<List<Integer>> level, HashSet<Point> goldLeft, int currentX, int currentY) {
+		this.level = level;
+		this.goldLeft = goldLeft;
+		this.currentX = currentX;
+		this.currentY = currentY;
 	}
 	
+	/**
+	 * Defines the Actions that can be used by a player for Lode Runner 
+	 * @author kdste
+	 *
+	 */
 	public static class LodeRunnerAction implements Action{
+		public enum MOVE {RIGHT,LEFT,UP,DOWN,DIG_LEFT,DIG_RIGHT,NOTHING}
+		private MOVE movement;
 		
+		public LodeRunnerAction(MOVE m) {
+			this.movement = m;
+		}
+		
+		/**
+		 * Gets the current action 
+		 * @return The current action 
+		 */
+		public MOVE getMove() {
+			return movement;
+		}
+		
+		/**
+		 * Checks if the current action is equal to the parameter
+		 * @return True if they are equal, false otherwise 
+		 */
+		public boolean equals(Object other) {
+			if(other instanceof LodeRunnerAction) {
+				return ((LodeRunnerAction) other).movement.equals(this.movement); 
+			}
+			return false;
+		}
+		
+		/**
+		 * @return String representation of the action
+		 */
+		public String toString() {
+			return movement.toString();
+		}
 	}
 	
-	public static Heuristic<LodeRunnerAction,LodeRunnerState> collectGold = new Heuristic<LodeRunnerAction,LodeRunnerState>(){
+	public static Heuristic<LodeRunnerAction,LodeRunnerState> manhattanToFarthestGold = new Heuristic<LodeRunnerAction,LodeRunnerState>(){
 
+		/**
+		 * Calculates the Manhattan distance from the player to the farthest gold coin
+		 * @return Manhattan distance from play to farthest coin 
+		 */
 		@Override
 		public double h(LodeRunnerState s) {
-			// TODO Auto-generated method stub
-			return 0;
+			int maxDistance = 0;
+			HashSet<Point> goldLeft = s.goldLeft;
+			for(Point p: goldLeft) {
+				int xDistance = Math.abs(s.currentX - p.x);
+				int yDistance = Math.abs(s.currentY - p.y);
+				Math.max(maxDistance, (xDistance+yDistance));
+			}
+			return maxDistance;
 		}
 		
 	};
 
+	/**
+	 * Gets the next state from the current state
+	 * @return The next state, if null the state was not legal at that time 
+	 */
 	@Override
 	public State<LodeRunnerAction> getSuccessor(LodeRunnerAction a) {
-		// TODO Auto-generated method stub
-		return null;
+		int newX = currentX;
+		int newY = currentY;
+		
+		if(a.getMove().equals(LodeRunnerAction.MOVE.RIGHT)) {
+			if(passable(newX+1, newY)) {
+				newX++;
+				if(tileAtPosition(newX, newY)==1) { //if the tile at the new position is gold, then it removes it from the set 
+					Point gold = new Point(newX, newY);
+					goldLeft.remove(gold);
+				}
+			} else return null;
+		}
+		if(a.getMove().equals(LodeRunnerAction.MOVE.LEFT)) {
+			if(passable(newX-1,newY)) {
+				newX--;
+				if(tileAtPosition(newX, newY)==1) {//if the tile at the new position is gold, then it removes it from the set 
+					Point gold = new Point(newX, newY);
+					goldLeft.remove(gold);
+				}
+			}else return null; 
+		}
+		if(a.getMove().equals(LodeRunnerAction.MOVE.NOTHING)) {
+			//if the tile at the new position is gold, then it removes it from the set
+			//you can still collect gold while in free fall 
+			if(tileAtPosition(newX, newY)==1) { 
+				Point gold = new Point(newX, newY);
+				goldLeft.remove(gold);
+			}
+			//no new action, if the action is nothing you are most likely in a free fall and have to wait until you hit the ground to make a new action
+			return null;  
+		}
+		if(a.getMove().equals(LodeRunnerAction.MOVE.UP)) {
+			if(passable(newX, newY+1)) {
+				newY++;
+				if(tileAtPosition(newX, newY)==1) {//if the tile at the new position is gold, then it removes it from the set 
+					Point gold = new Point(newX, newY);
+					goldLeft.remove(gold);
+				}
+			}else return null; 
+		}
+		if(a.getMove().equals(LodeRunnerAction.MOVE.DOWN)) {
+			if(passable(newX, newY-1)) {
+				newY--;
+				if(tileAtPosition(newX, newY)==1) {//if the tile at the new position is gold, then it removes it from the set
+					Point gold = new Point(newX, newY);
+					goldLeft.remove(gold);
+				}
+			}else return null;
+		}
+		if(a.getMove().equals(LodeRunnerAction.MOVE.DIG_LEFT)) {
+			int tile = tileAtPosition(newX-1,newY-1); //tile down and to the left 
+			//if the tile is diggable ground than it becomes an empty space 
+			if(tile == 3) {
+				tile = 0;
+			} else return null; 
+		}
+		if(a.getMove().equals(LodeRunnerAction.MOVE.DIG_RIGHT)) {
+			int tile = tileAtPosition(newX+1,newY-1);//tile down and to the right
+			//if the tile is diggable ground than it becomes an empty space 
+			if(tile == 3) {
+				tile = 0;
+			} else return null;
+		}
+		return new LodeRunnerState(level, goldLeft, newX, newY);
 	}
 
+	/**
+	 * Gets a list of all of the lode runner actions
+	 * @return List of valid lode runner actions
+	 */
 	@Override
 	public ArrayList<LodeRunnerAction> getLegalActions(State<LodeRunnerAction> s) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<LodeRunnerAction> vaildActions = new ArrayList<>();
+		for(LodeRunnerAction.MOVE move: LodeRunnerAction.MOVE.values()) {
+			if(s.getSuccessor(new LodeRunnerAction(move)) != null) {
+				vaildActions.add(new LodeRunnerAction(move));
+			}
+		}
+		return vaildActions;
+	}
+	
+	/**
+	 * Determines if a tile is passable or not 
+	 * @param x Current X coordinate
+	 * @param y Current Y coordinate
+	 * @return True if you can pass that tile, false otherwise 
+	 */
+	public boolean passable(int x, int y) {
+		int tile = tileAtPosition(x,y);
+		//0 is empty, 1 is gold, 2 is an enemy(simplified version that ignores enemies), 4 is a ladder, 5 is a rope 
+		if(tile == 0 || tile ==1 || tile == 2 || tile == 4 || tile==5)
+			return true;
+		return false; 
+	}
+	
+	/**
+	 * Easy access to the given tile integer at given (x,y) coordinates.
+	 * 
+	 * @param x horizontal position 
+	 * @param y vertical position 
+	 * @return tile int at those coordinates
+	 */
+	private int tileAtPosition(int x, int y) {
+		return level.get(y).get(x);
 	}
 
+	/**
+	 * Determines if you have won the level
+	 * @return True if there is no gold left, false otherwise
+	 */
 	@Override
 	public boolean isGoal() {
-		// TODO Auto-generated method stub
-		return false;
+		//when the hash set is empty 
+		return goldLeft.isEmpty();
 	}
 
+	/**
+	 * It moves on square at a time 
+	 * @return Number of tiles for every action 
+	 */
 	@Override
 	public double stepCost(State<LodeRunnerAction> s, LodeRunnerAction a) {
-		// TODO Auto-generated method stub
-		return 0;
+		return 1;
 	}
 	
 }
