230e7cfb4573d75c8e6cc53a2d7e04866b3c3862
Benjamin Capps <benjamin.capps16@gmail.com>
2020-06-02 13:58:11 -0500

    #473 Able to convert first level mostly well
    Includes:
    spike
    ladder
    block
    megaman
    
    Problem outlined in issue thread

--- src/main/java/megaManMaker/MegaManVGLCUtil.java
+++ src/main/java/megaManMaker/MegaManVGLCUtil.java
@@ -2,11 +2,15 @@ package megaManMaker;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.io.File;
+//import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
 import gvgai.tools.IO;
 
 public class MegaManVGLCUtil {
 	public static final String MEGAMAN_LEVEL_PATH = "data/VGLC/MegaMan/Processed/";
+
 	
 	public static void main(String[] args) {
 		List<List<Integer>> level = convertMegamanVGLCtoListOfLists(MEGAMAN_LEVEL_PATH+"megaman_1_1.txt");
@@ -17,6 +21,164 @@ public class MegaManVGLCUtil {
 			}
 			System.out.println();
 		}
+		convertMegaManLevelToMMLV(level);
+	}
+	private static void convertMegaManLevelToMMLV(List<List<Integer>> level) {
+		// TODO Auto-generated method stub
+		int xcoord = 0;
+		int ycoord = 0;
+		int levelNumber = 0;
+		try {
+		File levelFile = new File("MegaManLevel"+levelNumber+".mmlv");
+		
+		if(!levelFile.exists()) {
+			levelFile.createNewFile();
+			
+		}
+		PrintWriter p = new PrintWriter(levelFile);
+		p.println("[Level]");
+		for(List<Integer> k : level) {
+			//int l=0;
+			for(Integer m : k) {
+				if(m==1) { //solid ground
+					p.println("k"+xcoord+","+ycoord+"=\"71.000000\"");
+					p.println("j"+xcoord+","+ycoord+"=\"71.000000\"");
+					p.println("i"+xcoord+","+ycoord+"=\"1.000000\"");
+					p.println("e"+xcoord+","+ycoord+"=\"3.000000\"");
+					p.println("a"+xcoord+","+ycoord+"=\"1.000000\"");
+				//	l=1;
+				}else if(m==2) { //ladders
+					/*
+					 * i0,0="3.000000"e0,0="98.000000"a0,0="1.000000"
+					 */
+					p.println("i"+xcoord+","+ycoord+"=\"3.000000\"");
+					p.println("e"+xcoord+","+ycoord+"=\"98.000000\"");
+					p.println("a"+xcoord+","+ycoord+"=\"1.000000\"");
+				//	l=2;
+
+				}else if(m==3) { //spikes
+					p.println("l"+xcoord+","+ycoord+"=\"4.000000\"");
+					p.println("i"+xcoord+","+ycoord+"=\"2.000000\"");
+					p.println("e"+xcoord+","+ycoord+"=\"7.000000\"");
+					p.println("a"+xcoord+","+ycoord+"=\"1.000000\"");
+				//	l=3;
+				}
+				else if (m==11) {
+					p.println("1t=\"0.000000\"");
+					p.println("o"+xcoord+","+ycoord+"=\"9999.000000\"");
+					p.println("d"+xcoord+","+ycoord+"=\"4.000000\"");
+					p.println("a"+xcoord+","+ycoord+"=\"1.000000\"");
+					//l=11;
+				}
+//				else if (m==17) {
+//					l=17;
+//				}
+				if(xcoord%256==0/*&&m!=17*/) {
+					//add 2a clause
+					p.println("2a"+xcoord+","+ycoord+"=\"1.000000\"");
+					p.println("2c"+xcoord+","+ycoord+"=\"1.000000\"");
+
+				}
+				xcoord+=16;
+			}
+			xcoord = 0;
+			ycoord+=16;
+			if(ycoord%256==0/*&&l!=17*/) {
+				//add 2a clause
+				p.println("2a"+xcoord+","+ycoord+"=\"1.000000\"");
+				p.println("2c"+xcoord+","+ycoord+"=\"1.000000\"");
+
+			}
+			
+		}
+		//NEED 2a for enabling squares
+		p.println("2b"+0+","+896+"=\"0.000000\"");
+		p.println("2b"+0+","+896+"=\"0.000000\"");
+
+		p.println("2b"+0+","+672+"=\"0.000000\"");
+//				2b0,448="0.000000"
+		p.println("2b"+0+","+448+"=\"0.000000\"");
+
+//				2b0,224="0.000000"
+		p.println("2b"+0+","+224+"=\"0.000000\"");
+
+//				2b0,0="0.000000"
+		p.println("2b"+0+","+0+"=\"0.000000\"");
+
+//				2a0,0="1.000000"
+		p.println("2a"+0+","+0+"=\"1.000000\"");
+
+//				1s="240.000000"
+		p.println("1s=\"1335.000000\"");
+
+//				1r="0.000000"
+		p.println("1r=\"0.000000\"");
+
+//				1q="272.000000"
+		p.println("1q=\""+12800+"\""); //CHANGE TO POS INFINITY
+
+//				1p="0.000000"
+		p.println("1p=\"0.000000\"");
+
+//				1m="9.000000"
+		p.println("1m=\"9.000000\"");
+
+//				1l="11.000000"
+		p.println("1l=\"11.000000\"");
+
+//				1k0="0.000000"
+		p.println("1k0=\"0.000000\"");
+
+//				1bc="0.000000"
+		p.println("1bc=\"0.000000\"");
+
+//				1f="-1.000000"
+		p.println("1f=\"-1.000000\"");
+
+//				1e="29.000000"
+		p.println("1e=\"29.000000\"");
+
+//				1d="6.000000"
+		p.println("1d=\"6.000000\"");
+
+//				1bb="0.000000"
+		p.println("1bb=\"0.000000\"");
+
+//				1ca="0.000000"
+		p.println("1ca=\"0.000000\"");
+
+//				1ba="0.000000"
+		p.println("1ba=\"0.000000\"");
+
+//				1c="1.000000"
+		p.println("1c=\"1.000000\"");
+
+//				1b="1.000000"
+		p.println("1b=\"1.000000\"");
+
+//				4b="64.000000"
+		p.println("4b=\"64.000000\"");
+
+//				4a="dakuchen"
+		p.println("4a=\"dakuchen\""); //your user name
+
+//				1a="Super Simple"
+		//"MegaManLevel"+levelNumber+".txt"
+		p.println("1a=\"MegaManLevel"+levelNumber+".mmlv\"");
+
+//				0v="1.6.3"
+		p.println("0v=\"1.6.3\"");
+
+//				0a="408382.000000"
+		p.println("0a=\"408382.000000\"");
+		p.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
 	}
 	private static List<List<Integer>> convertMegamanVGLCtoListOfLists(String fileName) {
 		String[] level = new IO().readFile(fileName);
@@ -60,7 +222,7 @@ public class MegaManVGLCUtil {
 			return 9;
 		case 'M': //Moving platform
 			return 10;
-		case 'P': //Moving platform
+		case 'P': //Player
 			return 11;
 		case 'C': //Cannon/shooter
 			return 12; 
