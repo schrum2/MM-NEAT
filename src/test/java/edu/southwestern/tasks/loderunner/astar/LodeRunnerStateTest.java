238e4b49544ac3632206c7ce127fc3b343f9f7f8
schrum2 <schrum2@southwestern.edu>
2020-06-04 10:22:25 -0500

    #501 Starter file for this issue
    @kdsteckel

---
+++ src/test/java/edu/southwestern/tasks/loderunner/astar/LodeRunnerStateTest.java
@@ -0,0 +1,24 @@
+package edu.southwestern.tasks.loderunner.astar;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LodeRunnerStateTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testLodeRunnerAStarPath() {
+		fail("Not yet implemented");
+	}
+
+}
