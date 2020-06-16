376b252c4da125e999b8780f8aea65ab527c0bcb
Benjamin Capps <benjamin.capps16@gmail.com>
2020-06-16 12:49:12 -0500

    #523 fixed magic #'s

--- src/main/java/megaManMaker/MegaManGANUtil.java
+++ src/main/java/megaManMaker/MegaManGANUtil.java
@@ -17,7 +17,8 @@ public class MegaManGANUtil {
 	public static final int LATENT_VECTOR_SIZE = 5;//latent vector dimension, 20 improved the model 
 	public static final int MEGA_MAN_ALL_TERRAIN = 7; //number of tiles in MegaMan
 	public static final int MEGA_MAN_FIRST_LEVEL_ALL_TILES = 21; //number of tiles in MegaMan
-	
+	public static final int MEGA_MAN_LEVEL_WIDTH = 16;
+	public static final int MEGA_MAN_LEVEL_HEIGHT = 14;
 	
 
 	/**
@@ -143,8 +144,8 @@ public class MegaManGANUtil {
 		List<List<Integer>> oneLevel = levelInList.get(0); // gets first level in the set 
 		Random rand = new Random(Double.doubleToLongBits(latentVector[0]));
 		List<Integer> nullLine = new ArrayList<Integer>(16);
-		for(int i=0;i<16;i++) { // <--- TODO from Schrum: Don't like these magic numbers!
-			nullLine.add(9); 	// <--- TODO from Schrum: Don't like these magic numbers!
+		for(int i=0;i<MEGA_MAN_LEVEL_WIDTH;i++) { // <--- TODO from Schrum: Don't like these magic numbers!
+			nullLine.add(MegaManState.MEGA_MAN_TILE_NULL); 	// <--- TODO from Schrum: Don't like these magic numbers!
 		}
 		for(int level = 1;level<numberOfChunks;level++) {
 			right = rand.nextBoolean();
@@ -154,17 +155,17 @@ public class MegaManGANUtil {
 					oneLevel.get(i).addAll(nullLine);
 				}
 				//take the information from the previous run to replace null with a level in the appropriate spots
-				for(int x = (int) previousMove.getX()+16;x<(int) previousMove.getX()+32;x++) {
-					for(int y = (int) previousMove.getY();y<(int) previousMove.getY()+14;y++) {
+				for(int x = (int) previousMove.getX()+MEGA_MAN_LEVEL_WIDTH;x<(int) previousMove.getX()+2*MEGA_MAN_LEVEL_WIDTH;x++) {
+					for(int y = (int) previousMove.getY();y<(int) previousMove.getY()+MEGA_MAN_LEVEL_HEIGHT;y++) {
 //						System.out.println(x+", "+y);
 //						System.out.println(previousMove.getX()+", "+previousMove.getY());
 
-						oneLevel.get(y).set(x, levelInList.get(level).get((int) (y - previousMove.getY())).get((int) (x-16-previousMove.getX())));
+						oneLevel.get(y).set(x, levelInList.get(level).get((int) (y - previousMove.getY())).get((int) (x-MEGA_MAN_LEVEL_WIDTH-previousMove.getX())));
 					}
 				}
 				//wasRight = true;
 				d = Direction.RIGHT;
-				previousMove=new Point((int) previousMove.getX()+16,(int) previousMove.getY());
+				previousMove=new Point((int) previousMove.getX()+MEGA_MAN_LEVEL_WIDTH,(int) previousMove.getY());
 
 			}else {
 				up = rand.nextBoolean();
@@ -182,17 +183,17 @@ public class MegaManGANUtil {
 				}
 				if(d.equals(Direction.UP)) { //add null lines all on top
 					List<List<Integer>> nullScreen = new ArrayList<List<Integer>>();
-					for(int i = 0;i<14;i++) {
+					for(int i = 0;i<MEGA_MAN_LEVEL_HEIGHT;i++) {
 						List<Integer> nullLines = new ArrayList<Integer>();
 						for(int j = 0;j<oneLevel.get(0).size();j++) {
-							nullLines.add(9);
+							nullLines.add(MegaManState.MEGA_MAN_TILE_NULL);
 						}
 						nullScreen.add(nullLines);
 					}
 					oneLevel.addAll(0, nullScreen);
 					//now at the previous point, add in a new version of levelInList.get(level)
-					for(int y = (int) previousMove.getY();y<previousMove.getY()+14;y++) {
-						for(int x = (int) previousMove.getX();x<previousMove.getX()+16;x++) {
+					for(int y = (int) previousMove.getY();y<previousMove.getY()+MEGA_MAN_LEVEL_HEIGHT;y++) {
+						for(int x = (int) previousMove.getX();x<previousMove.getX()+MEGA_MAN_LEVEL_WIDTH;x++) {
 							oneLevel.get(y).set(x, levelInList.get(level).get((int) (y - (int)previousMove.getY())).get((int) (x-(int)previousMove.getX())));
 						}
 					}
@@ -200,10 +201,10 @@ public class MegaManGANUtil {
 					int y1 = (int) previousMove.getY();
 					if(y1+15>=oneLevel.size()) {
 						List<List<Integer>> nullScreen = new ArrayList<List<Integer>>();
-						for(int i = 0;i<14;i++) {
+						for(int i = 0;i<MEGA_MAN_LEVEL_HEIGHT;i++) {
 							List<Integer> nullLines = new ArrayList<Integer>();
 							for(int j = 0;j<oneLevel.get(0).size();j++) {
-								nullLines.add(9);
+								nullLines.add(MegaManState.MEGA_MAN_TILE_NULL);
 							}
 							nullScreen.add(nullLines);
 						}
@@ -211,12 +212,12 @@ public class MegaManGANUtil {
 					}
 					
 					
-					for(int y = (int) previousMove.getY()+14;y<previousMove.getY()+28;y++) {
-						for(int x = (int) previousMove.getX();x<previousMove.getX()+16;x++) {
-							oneLevel.get(y).set(x, levelInList.get(level).get((int) (y -14- previousMove.getY())).get((int) (x-previousMove.getX())));
+					for(int y = (int) previousMove.getY()+MEGA_MAN_LEVEL_HEIGHT;y<previousMove.getY()+2*MEGA_MAN_LEVEL_HEIGHT;y++) {
+						for(int x = (int) previousMove.getX();x<previousMove.getX()+MEGA_MAN_LEVEL_WIDTH;x++) {
+							oneLevel.get(y).set(x, levelInList.get(level).get((int) (y -MEGA_MAN_LEVEL_HEIGHT- previousMove.getY())).get((int) (x-previousMove.getX())));
 						}
 					}
-					previousMove=new Point((int) previousMove.getX(),(int) previousMove.getY()+14);
+					previousMove=new Point((int) previousMove.getX(),(int) previousMove.getY()+MEGA_MAN_LEVEL_HEIGHT);
 
 					
 					
