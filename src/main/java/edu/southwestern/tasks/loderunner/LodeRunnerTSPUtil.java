00d8bf31c5d2141a1f5f1fdd7c3535963720a9ea
Kirby Steckel <steckelk@southwestern.edu>
2020-06-23 16:15:09 -0500

    Wrote a method to find the order in which to collect the gold in the level
    #528
    Refactored this class into method instead of having everything in the main method.
    An order to collect the gold in the level is now created, but is not optimal because it was written with a greedy technique.

--- src/main/java/edu/southwestern/tasks/loderunner/LodeRunnerTSPUtil.java
+++ src/main/java/edu/southwestern/tasks/loderunner/LodeRunnerTSPUtil.java
@@ -3,29 +3,75 @@ package edu.southwestern.tasks.loderunner;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 import edu.southwestern.parameters.Parameters;
 import edu.southwestern.tasks.loderunner.astar.LodeRunnerState;
 import edu.southwestern.tasks.loderunner.astar.LodeRunnerState.LodeRunnerAction;
 import edu.southwestern.util.datastructures.Graph;
-import edu.southwestern.util.datastructures.Graph.Node;
 import edu.southwestern.util.datastructures.ListUtil;
 import edu.southwestern.util.datastructures.Pair;
 import edu.southwestern.util.datastructures.Triple;
 
 public class LodeRunnerTSPUtil {
 
-	//we need to accumulate the size of the visited states 
-
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
 		Parameters.initializeParameterCollections(args);
 		//int visitedSize = 0;
-		List<List<Integer>> level = LodeRunnerVGLCUtil.convertLodeRunnerLevelFileVGLCtoListOfLevelForLodeRunnerState(LodeRunnerVGLCUtil.LODE_RUNNER_LEVEL_PATH + "Level 4.txt");
+		List<List<Integer>> level = LodeRunnerVGLCUtil.convertLodeRunnerLevelFileVGLCtoListOfLevelForLodeRunnerState(LodeRunnerVGLCUtil.LODE_RUNNER_LEVEL_PATH + "Level 150.txt");
+		Graph<Point> tsp = getTSPGraph(level);
+		//		//calculates number of states visited from the spawn to every other gold
+		//		for(Point p : gold) {
+		//			List<List<Integer>> levelCopy = ListUtil.deepCopyListOfLists(level);
+		//			levelCopy.get(spawn.y).set(spawn.x, LodeRunnerState.LODE_RUNNER_TILE_SPAWN);
+		//			levelCopy.get(p.y).set(p.x, LodeRunnerState.LODE_RUNNER_TILE_GOLD);
+		//			Triple<HashSet<LodeRunnerState>, ArrayList<LodeRunnerAction>, LodeRunnerState> aStarInfo =LodeRunnerLevelAnalysisUtil.performAStarSearch(levelCopy, Double.NaN);
+		//			visitedSize+=aStarInfo.t1.size();//keeps track of how many visited states for every run of A*
+		//		}
+		//		System.out.println(tsp.root());
+		//		System.out.println(tsp.root().adjacencies());
+		//		System.out.println(tsp.getNodes());
+		//		System.out.println("Number of states visited: "+visitedSize);
+
+		List<Pair<Graph<Point>.Node, Double>> solutionPath = getTSPGreedyPath(tsp);
+		System.out.println(solutionPath);
+	}
+
+	/**
+	 * Creates a sequence to collect the gold for in the level 
+	 * by moving to the node that has the least weight on its edge and removing 
+	 * the current node
+	 * It will always give a solution, but not an optimal one
+	 * @param tsp A digraph holding points of gold and weights
+	 * @return The order to collect gold
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static List<Pair<Graph<Point>.Node, Double>> getTSPGreedyPath(Graph<Point> tsp) {
+		//solving the TSP problem from the graph 
+		List<Pair<Graph<Point>.Node, Double>> solutionPath = new ArrayList<>(); //set of points
+		HashSet<Pair<Graph<Point>.Node, Double>> nodes = (HashSet<Pair<Graph<Point>.Node, Double>>) tsp.root().adjacencies();
+		//Pair<Graph<Point>.Node, Double> min = new Pair<Graph<Point>.Node, Double>(tsp.root(), Double.MAX_VALUE);
+		//loops through all adjacent nodes, adds the node with the lowest weight 
+		while(!tsp.root().adjacencies().isEmpty()) {
+			Iterator itr = tsp.root().adjacencies().iterator();
+			Pair<Graph<Point>.Node, Double> min = new Pair<Graph<Point>.Node, Double>(tsp.root(), Double.MAX_VALUE);
+			while(itr.hasNext()) {
+				Pair<Graph<Point>.Node, Double> node = (Pair<Graph<Point>.Node, Double>) itr.next();
+				if(node.t2 < min.t2) {
+					min = node;
+				}
+			}
+			solutionPath.add(min);
+			//System.out.println(solutionPath);
+			tsp.root().adjacencies().remove(min);
+		}
+		return solutionPath;
+	}
+
+	private static Graph<Point> getTSPGraph(List<List<Integer>> level) {
 		//clears level of gold and spawn but maintains a reference in this set 
 		HashSet<Point> gold = LodeRunnerState.fillGold(level);
-		//System.out.println(gold.toString());
 		Point spawn = findSpawnAndRemove(level);
 		Graph<Point> tsp = new Graph<Point>();
 		tsp.addNode(spawn);
@@ -46,28 +92,7 @@ public class LodeRunnerTSPUtil {
 				}
 			}
 		}
-//		//calculates number of states visited from the spawn to every other gold
-//		for(Point p : gold) {
-//			List<List<Integer>> levelCopy = ListUtil.deepCopyListOfLists(level);
-//			levelCopy.get(spawn.y).set(spawn.x, LodeRunnerState.LODE_RUNNER_TILE_SPAWN);
-//			levelCopy.get(p.y).set(p.x, LodeRunnerState.LODE_RUNNER_TILE_GOLD);
-//			Triple<HashSet<LodeRunnerState>, ArrayList<LodeRunnerAction>, LodeRunnerState> aStarInfo =LodeRunnerLevelAnalysisUtil.performAStarSearch(levelCopy, Double.NaN);
-//			visitedSize+=aStarInfo.t1.size();//keeps track of how many visited states for every run of A*
-//		}
-//		System.out.println(tsp.root());
-//		System.out.println(tsp.root().adjacencies());
-//		System.out.println(tsp.getNodes());
-//		System.out.println("Number of states visited: "+visitedSize);
-		
-		//solving the TSP problem from the graph 
-		System.out.println(tsp.root().adjacencies());
-		List<Pair<Graph<Point>.Node, Double>> solutionPath = new ArrayList<>(); //set of points
-		//loops through all adjacent nodes, adds the node with the lowest weight 
-		for(Pair<Graph<Point>.Node, Double> n: tsp.root().adjacencies()) {
-			for(Pair<Graph<Point>.Node, Double> p: tsp.root().adjacencies()) {
-				
-			}
-		}
+		return tsp;
 	}
 
 	/**
