19a451951bb1031cee253784b6e213c13fbd38a5
schrum2 <schrum2@southwestern.edu>
2020-06-24 14:40:35 -0500

    modified forward method of discriminator
    However, just realized that because we are not using modules, the weights may not initialize and save correctly, which is a problem.

--- src/main/python/GAN/models/cdcgan.py
+++ src/main/python/GAN/models/cdcgan.py
@@ -5,7 +5,14 @@ import torch.nn.parallel
 class DCGAN_D(nn.Module):
     def __init__(self, isize, nz, nc, ndf, ngpu, n_extra_layers=0):
         super(DCGAN_D, self).__init__()
+
+        self.isize = isize
+        self.nz = nz
+        self.nc = nc
+        self.ndf = ndf
         self.ngpu = ngpu
+        self.n_extra_layers = n_extra_layers
+
         assert isize % 16 == 0, "isize has to be a multiple of 16"
 
         main = dict()
@@ -35,11 +42,32 @@ class DCGAN_D(nn.Module):
 
 
     def forward(self, input):
-        if self.ngpu > 1 and isinstance(input.data, torch.cuda.FloatTensor):
-            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
-        else: 
-            output = self.main(input)
-            
+        isize = self.isize
+        nc = self.nc
+        ndf = self.ndf
+        n_extra_layers = self.n_extra_layers
+
+        x = self.main['initial:conv:{0}-{1}'.format(nc, ndf)](input)
+        x = self.main['initial:relu:{0}'.format(ndf)](x)
+        csize, cndf = isize / 2, ndf
+
+        # Extra layers
+        for t in range(n_extra_layers):
+            x = self.main['extra-layers-{0}:{1}:conv'.format(t, cndf)](x)
+            x = self.main['extra-layers-{0}:{1}:batchnorm'.format(t, cndf)](x)
+            x = self.main['extra-layers-{0}:{1}:relu'.format(t, cndf)](x)
+
+        while csize > 4:
+            in_feat = cndf
+            out_feat = cndf * 2
+            x = self.main['pyramid:{0}-{1}:conv'.format(in_feat, out_feat)](x)
+            x = self.main['pyramid:{0}:batchnorm'.format(out_feat)](x)
+            x = self.main['pyramid:{0}:relu'.format(out_feat)](x)
+            cndf = cndf * 2
+            csize = csize / 2
+
+        # state size. K x 4 x 4
+        output = self.main['final:{0}-{1}:conv'.format(cndf, 1)](x)            
         output = output.mean(0)
         return output.view(1)
 
@@ -79,10 +107,10 @@ class DCGAN_G(nn.Module):
         self.main = main
 
     def forward(self, input):
-        if self.ngpu > 1 and isinstance(input.data, torch.cuda.FloatTensor):
-            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
-        else: 
-            output = self.main(input)
+
+        # TODO: Modify this the same way the discriminator's forward method was modified
+
+        output = self.main(input)
 
         #print (output[0,:,0,0])
         #exit()
