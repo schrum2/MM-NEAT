478b1f30798670ba0b850a30163080986429f453
cappsb <63438571+cappsb@users.noreply.github.com>
2020-06-30 09:44:31 -0500

    Revert "#555 Progress"
    This reverts commit 7d618b093613cb375be1a4b426075ea52e3ebd60.

--- src/main/java/edu/southwestern/tasks/megaman/astar/MegaManState.java
+++ src/main/java/edu/southwestern/tasks/megaman/astar/MegaManState.java
@@ -44,7 +44,6 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 	public int currentX; 
 	public int currentY;
 	private int jumpVelocity;
-	private int fallHorizontalModInt;
 	//private boolean climbing;
 	//the distance to the level orb
 	public static Heuristic<MegaManAction,MegaManState> manhattanToOrb = new Heuristic<MegaManAction,MegaManState>(){
@@ -143,22 +142,17 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 	 * @param start The spawn point 
 	 */
 	public MegaManState(List<List<Integer>> level, Point start) {
-		this(level, getJumpVelocity(), getOrb(level), start.x, start.y, getFallHorizontalModInt());
-	}
-	private static int getFallHorizontalModInt() {
-		// TODO Auto-generated method stub
-		return 0;
+		this(level, getJumpVelocity(), getOrb(level), start.x, start.y);
 	}
 	private static int getJumpVelocity() {
 		return 0;
 	}
-	public MegaManState(List<List<Integer>> level, int jumpVelocity, Point orb, int currentX, int currentY, int fallHorizontalModInt) {
+	public MegaManState(List<List<Integer>> level, int jumpVelocity, Point orb, int currentX, int currentY) {
 		this.level = level;
 		this.orb = orb;
 		this.jumpVelocity =jumpVelocity;
 		this.currentX = currentX;
 		this.currentY = currentY;
-		this.fallHorizontalModInt = fallHorizontalModInt;
 	}
 
 	@Override
@@ -166,8 +160,6 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 		int newJumpVelocity = jumpVelocity;
 		int newX = currentX;
 		int newY = currentY;
-		int newFallHorizontalModInt = fallHorizontalModInt;
-		boolean falling = false;
 		assert inBounds(newX,newY): "x is:" + newX + "\ty is:"+newY + "\t" + inBounds(newX,newY);
 		// Falling off bottom of screen (into a gap). No successor (death)
 		//System.out.print("("+newX+", "+newY+")");
@@ -185,27 +177,22 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 				newJumpVelocity = 0; // Can't jump if blocked above
 			}
 		}
-		
+
 		// Potentially deal with JUMP action
 		if(newJumpVelocity == 0) { // Not mid-Jump
 			//int beneath = tileAtPosition(newX,newY+1);
 			if(passable(newX,newY+1)&& tileAtPosition(newX, newY+1)!=MEGA_MAN_TILE_LADDER) { // Falling
 				newY++; // Fall down
-				newFallHorizontalModInt++;
-				falling = true;
 			} else if(a.getMove().equals(MegaManAction.MOVE.JUMP)&& tileAtPosition(newX, newY)!=MEGA_MAN_TILE_LADDER) { // Start jump
 				newJumpVelocity = Parameters.parameters.integerParameter("megaManAStarJumpHeight"); // Accelerate up
-			} else {
-				falling = false;
-			}
+			} 
 		} else if(a.getMove().equals(MegaManAction.MOVE.JUMP)) {
 			return null; // Can't jump mid-jump. Reduces search space.
 		}
 
 		// Right movement
 		if(a.getMove().equals(MegaManAction.MOVE.RIGHT)) {
-			if(falling&&passable(newX+1,newY)&&newFallHorizontalModInt%2==0) newX++;
-			else if(!falling&&passable(newX+1,newY)) {
+			if(passable(newX+1,newY)) {
 				newX++;
 			} else if(currentY == newY) { // vertical position did not change
 				// This action does not change the state. Neither jumping up nor falling down, and could not move right, so there is no NEW state to go to
@@ -215,12 +202,9 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 
 		// Left movement
 		if(a.getMove().equals(MegaManAction.MOVE.LEFT)) {
-			if(falling&&passable(newX-1,newY)&&newFallHorizontalModInt%2==0) {
+			if(passable(newX-1,newY)) {
 				newX--;
-			}else if (!falling&&passable(newX-1, newY)) {
-				newX--;
-			}
-			else if(currentY == newY) { // vertical position did not change
+			} else if(currentY == newY) { // vertical position did not change
 				// This action does not change the state. Neither jumping up nor falling down, and could not move left, so there is no NEW state to go to
 				return null;
 			}
@@ -241,7 +225,7 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 		if(!inBounds(newX, newY)){
 			return null;
 		}
-		MegaManState result = new MegaManState(level, newJumpVelocity, orb, newX, newY, newFallHorizontalModInt);
+		MegaManState result = new MegaManState(level, newJumpVelocity, orb, newX, newY);
 		//renderLevelAndPause((MegaManState) result);
 		//System.out.println(newX+", "+newY);
 		return result;
@@ -353,7 +337,6 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 		result = prime * result + jumpVelocity;
 		result = prime * result + currentX;
 		result = prime * result + currentX;
-		result = prime * result + fallHorizontalModInt;
 		return result;
 	}
 	@Override
@@ -382,8 +365,6 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 		if (currentY != other.currentY)
 			return false;
 		if (jumpVelocity != other.jumpVelocity)
-			return false;
-		if(fallHorizontalModInt != other.fallHorizontalModInt)
 			return false;
 		if (orb == null) {
 			if (other.orb != null)
@@ -464,7 +445,7 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 	 */
 	public static void main(String args[]) {
 		//converts Level in VGLC to hold all 8 tiles so we can get the real spawn point from the level 
-		List<List<Integer>> level = MegaManVGLCUtil.convertMegamanVGLCtoListOfLists(MegaManVGLCUtil.MEGAMAN_LEVEL_PATH+"megaman_1_"+1+".txt"); //converts to JSON
+		List<List<Integer>> level = MegaManVGLCUtil.convertMegamanVGLCtoListOfLists(MegaManVGLCUtil.MEGAMAN_LEVEL_PATH+"megaman_1_"+6+".txt"); //converts to JSON
 		Parameters.initializeParameterCollections(new String[] { "io:false", "netio:false", "recurrency:false"
 				, "megaManAStarJumpHeight:3" });
 		MegaManVGLCUtil.printLevel(level);
@@ -484,13 +465,9 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 		mostRecentVisited = ((AStarSearch<MegaManAction, MegaManState>) search).getVisited();
 		System.out.println(mostRecentVisited.toString());
 		System.out.println("actionSequence: " + actionSequence);
-		
 		try {
 			//visualizes the points visited with red and whit x's
 			vizualizePath(level,mostRecentVisited,actionSequence,start);
-			
-			BufferedImage[] images = MegaManRenderUtil.loadImagesForASTAR(MegaManRenderUtil.MEGA_MAN_TILE_PATH);
-			MegaManRenderUtil.getBufferedImageWithRelativeRendering(level, images);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
