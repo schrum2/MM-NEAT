3e92fbe27dcdbc7a9f4072880a92dc761ffd29c4
cappsb <63438571+cappsb@users.noreply.github.com>
2020-07-28 23:45:55 -0500

    #570 Small fixes

--- src/main/java/edu/southwestern/tasks/megaman/astar/MegaManState.java
+++ src/main/java/edu/southwestern/tasks/megaman/astar/MegaManState.java
@@ -238,7 +238,7 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 		if(a.getMove().equals(MegaManAction.MOVE.RIGHT)) {
 			if((!jumping&& //If you're not jumping, then you're either falling or on solid ground
 					((falling&&passable(newX+1,newY)&&passable(newX+1, newY-1)&&newFallHorizontalModInt%FALL_STEPS_PER_SIDEWAYS_MOVE==0)|| //If you're falling, then make sure that there is headspace for MegaMan and make falling happen faster
-					(!falling&&passable(newX+1,newY)/*&&(!passable(newX, newY+1)||tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_LADDER||tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_MOVING_PLATFORM)*/)))|| //otherwise, you're on the ground, so you can slide
+					(!falling&&passable(newX+1,newY)&&(!passable(newX, newY+1)||tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_LADDER||tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_MOVING_PLATFORM))))|| //otherwise, you're on the ground, so you can slide
 					(jumping&&passable(newX+1, newY)&&((passable(newX+1, newY-1)&&passable(newX, newY-1))||passable(newX+1, newY+1)&&passable(newX, newY+1)))) newX++; //If you're jumping, then make sure that there is headspace for MegaMan
 			else if(currentY == newY) { // vertical position did not change
 				// This action does not change the state. Neither jumping up nor falling down, and could not move right, so there is no NEW state to go to
@@ -250,7 +250,7 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 		if(a.getMove().equals(MegaManAction.MOVE.LEFT)) {
 			if((!jumping&& //If you're not jumping, then you're either falling or on solid ground
 					(falling&&passable(newX-1,newY)&&passable(newX-1, newY-1)&&newFallHorizontalModInt%FALL_STEPS_PER_SIDEWAYS_MOVE==0)|| //If you're falling, then make sure that there is head space for MegaMan and make falling happen faster
-					(!falling&&passable(newX-1, newY)/*&&(!passable(newX, newY+1)||tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_LADDER||tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_MOVING_PLATFORM)*/))|| //otherwise, you're on the ground, so you can slide
+					(!falling&&passable(newX-1, newY)&&(!passable(newX, newY+1)||tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_LADDER||tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_MOVING_PLATFORM)))|| //otherwise, you're on the ground, so you can slide
 					(jumping&&passable(newX-1, newY)&&((passable(newX-1, newY-1)&&passable(newX, newY-1))||passable(newX-1, newY+1)&&passable(newX, newY+1)))) {  //If you're jumping, then make sure that there is head space for MegaMan
 				newX--;
 			}
@@ -267,7 +267,7 @@ public class MegaManState extends State<MegaManState.MegaManAction>{
 		}
 		//down movement(on ladder)
 		if(a.getMove().equals(MegaManAction.MOVE.DOWN)) {
-			if(inBounds(newX, newY+1)&&(tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_LADDER||tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_MOVING_PLATFORM)) 
+			if((!sliding&&inBounds(newX, newY+1)&&(tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_LADDER||tileAtPosition(newX, newY+1)==MEGA_MAN_TILE_MOVING_PLATFORM))) 
 				newY++;
 			else return null;
 		}
