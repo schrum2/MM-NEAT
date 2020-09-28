607e911157c5bf419e9f63272dbc2e949a561b6c
Jacob Schrum <schrum2@southwestern.edu>
2020-09-28 08:18:20 -0500

    Removed showVizDoomInputs #589
    Was crashing post eval batch files

--- postBestObjectiveWatchGeneration.bat
+++ postBestObjectiveWatchGeneration.bat
@@ -1,4 +1,4 @@
 REM NOTE: Can only be used with saveAllChampions set to true
 REM Usage:   postBestObjectiveWatchGeneration.bat <experiment directory> <log prefix> <run type> <run number> <generation> <number of trials per team>
 REM Example: postBestObjectiveWatchGeneration.bat toruspred TorusPred CoOpMultiCCQ 0 3 10
-java -jar "target/MM-NEAT-0.0.1-SNAPSHOT.jar" runNumber:%4 parallelEvaluations:false base:%1 log:%2-%3 saveTo:%3 trials:%5 watch:true showNetworks:true io:false netio:false onlyWatchPareto:true printFitness:true animateNetwork:false monitorInputs:true experiment:edu.southwestern.experiment.post.ObjectiveBestTeamsExperiment logLock:true watchLastBestOfTeams:true lastSavedGeneration:%5 showVizDoomInputs:true showCPPN:true
+java -jar "target/MM-NEAT-0.0.1-SNAPSHOT.jar" runNumber:%4 parallelEvaluations:false base:%1 log:%2-%3 saveTo:%3 trials:%5 watch:true showNetworks:true io:false netio:false onlyWatchPareto:true printFitness:true animateNetwork:false monitorInputs:true experiment:edu.southwestern.experiment.post.ObjectiveBestTeamsExperiment logLock:true watchLastBestOfTeams:true lastSavedGeneration:%5 showCPPN:true
