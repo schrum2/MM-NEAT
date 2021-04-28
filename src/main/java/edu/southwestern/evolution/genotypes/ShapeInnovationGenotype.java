37506493a3ade55c357e96811258137004df80bc
Jacob Schrum <schrum2@southwestern.edu>
2021-04-27 22:14:21 -0500

    comments for shape innovation genotype

--- src/main/java/edu/southwestern/evolution/genotypes/ShapeInnovationGenotype.java
+++ src/main/java/edu/southwestern/evolution/genotypes/ShapeInnovationGenotype.java
@@ -4,8 +4,21 @@ import java.util.ArrayList;
 
 import edu.southwestern.networks.TWEANN;
 
+/**
+ * Used to evolve 3D shapes with an innovation engine. Two genotypes are combined.
+ * The TWEANN is actually a CPPN, that generates a shape in the manner of the
+ * ThreeDimensionalObjectBreeder. The second part of the genome is just a
+ * vector that specifies some details of how the shape is displayed within
+ * a 2D picture. 
+ * 
+ * @author Jacob Schrum
+ *
+ */
 public class ShapeInnovationGenotype extends CombinedGenotype<TWEANN, ArrayList<Double>>{
 
+	/**
+	 * Default constructor
+	 */
 	public ShapeInnovationGenotype() {
 		super(new TWEANNGenotype(), // Generates the shape 
 			  new BoundedRealValuedGenotype(new double[]{0,0,0,0,0}, new double[]{1,1,1,1,1})); // Background color (first three) and pitch, heading
