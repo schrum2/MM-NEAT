8b8ea086fa17b01d0276c9d7c678425ef06f8e4a
Anna <wickera@southwestern.edu>
2021-05-13 10:30:59 -0500

    Update BoundedIntegerValuedGenotype.java

--- src/main/java/edu/southwestern/evolution/genotypes/BoundedIntegerValuedGenotype.java
+++ src/main/java/edu/southwestern/evolution/genotypes/BoundedIntegerValuedGenotype.java
@@ -14,37 +14,73 @@ import java.util.List;
  */
 public class BoundedIntegerValuedGenotype extends NumericArrayGenotype<Integer> {
 
+	/**
+	 * Makes a new array with random integers bounded by discreteCeilings.
+	 */
 	public BoundedIntegerValuedGenotype() {
 		super(RandomNumbers.randomIntArray(MMNEAT.discreteCeilings));
 	}
 
+	/**
+	 * Wraps the arrayList genes in a genotype.
+	 * 
+	 * @param genes an arrayList of integers
+	 */
 	public BoundedIntegerValuedGenotype(ArrayList<Integer> genes) {
 		super(genes);
 	}
 
+	/**
+	 * Creates a copy of the whole genotype that
+	 * contains a copy of the arrayList
+	 * 
+	 * @return returns the new genotype
+	 */
 	public Genotype<ArrayList<Integer>> copy() {
 		return new BoundedIntegerValuedGenotype(genes);
 	}
 
+	/**
+	 * Sets a value in the arrayList genes.
+	 *  
+	 * @param pos the position to set the new value at
+	 * @param value the new value
+	 */
 	public void setValue(int pos, int value) {
 		genes.set(pos, value);
 	}
 
+	/**
+	 * Creates a new genotype from an instance.
+	 * 
+	 * @return returns the new genotype
+	 */
 	public Genotype<ArrayList<Integer>> newInstance() {
 		return new BoundedIntegerValuedGenotype();
 	}
 
+	/**
+	 * Mutates the arrayList genes.
+	 */
 	public void mutate() {
 		new ReplaceMutation().mutate(this);
 	}
 
 	transient List<Long> parents = new LinkedList<Long>();
 	
+	/**
+	 * Adds a parent.
+	 * 
+	 * @param id number of parent genotype
+	 */
 	@Override
 	public void addParent(long id) {
 		parents.add(id);
 	}
 
+	/**
+	 * Gets parent IDs.
+	 */
 	@Override
 	public List<Long> getParentIDs() {
 		return parents;
