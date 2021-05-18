043831ef10aaf4af8194075edb485717ae489128
Maxx Batterton <battertm@southwestern.edu>
2021-05-18 14:26:55 -0500

    Emitter setup and cleanup #622

--- src/main/java/edu/southwestern/evolution/mapelites/CMAME.java
+++ src/main/java/edu/southwestern/evolution/mapelites/CMAME.java
@@ -2,33 +2,22 @@ package edu.southwestern.evolution.mapelites;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import edu.southwestern.MMNEAT.MMNEAT;
-import edu.southwestern.evolution.EvolutionaryHistory;
 import edu.southwestern.evolution.genotypes.Genotype;
 import edu.southwestern.parameters.Parameters;
 import edu.southwestern.scores.Score;
 import edu.southwestern.util.PopulationUtil;
-import edu.southwestern.util.random.RandomNumbers;
 import fr.inria.optimization.cmaes.CMAEvolutionStrategy;
 
 public class CMAME extends MAPElites<ArrayList<Double>> {
 	
-	public CMAEvolutionStrategy optEmitter = new CMAEvolutionStrategy();
+	private Emitter[] emitters;
 	
 	public void initialize(Genotype<ArrayList<Double>> example) {
 		super.initialize(example);
-		optEmitter.setDimension(example.getPhenotype().size());
-		optEmitter.setInitialX(0.05); //TODO test 0.0
-		optEmitter.setInitialStandardDeviation(0.2); // unsure if should be hardcoded or not
-		optEmitter.options.stopFitness = 1e-14; // unsure about value here
-		int mu = Parameters.parameters.integerParameter("mu");
-		optEmitter.parameters.setMu(mu);
-		optEmitter.parameters.setPopulationSize(mu); 
-		optEmitter.init();
-		optEmitter.writeToDefaultFilesHeaders(0); // Overwrite existing CMA-ES files
+		int dimension = example.getPhenotype().size();
+		emitters = new Emitter[] {newEmitter(dimension, "Emitter 1")};
 	}
 	
 	/**
@@ -38,8 +27,10 @@ public class CMAME extends MAPElites<ArrayList<Double>> {
 	 * afterwards
 	 */
 	public void newIndividual() {
-		double[][] pop = optEmitter.samplePopulation(); // need to check feasibility?
-		double[] deltaI = new double[pop.length]; // create delta array
+		//Arrays.sort(emitters); // will need for multiple emitters
+		Emitter thisEmitter = emitters[0];
+		double[][] pop = thisEmitter.samplePopulation(); // need to check feasibility?
+		double[] deltaI = new double[pop.length]; // create array for deltas
 		ArrayList<Genotype<ArrayList<Double>>> popGenotypes = PopulationUtil.genotypeArrayListFromDoubles(pop); // convert population to genotypes
 		assert task.evaluate(popGenotypes.get(0)).usesMAPElitesBinSpecification() : "Cannot use a traditional behavior vector with CMA-ME";
 		for (int i = 0; i < popGenotypes.size(); i++) { // for each individual
@@ -54,22 +45,59 @@ public class CMAME extends MAPElites<ArrayList<Double>> {
 				System.out.println("Current bin ("+currentBinScore+") was already better than or equal to new bin ("+individualBinScore+").");
 			} else if (currentBinScore == Double.NEGATIVE_INFINITY) { // if bin was empty
 				System.out.println("Added new bin ("+individualBinScore+").");
+				thisEmitter.solutionCount++;
 				deltaI[i] = -individualBinScore;
 			} else { // if bin existed, but was worse than the new one
 				System.out.println("Improved current bin ("+currentBinScore+"), replaced with new bin ("+individualBinScore+").");
 				deltaI[i] = -(individualBinScore - currentBinScore);
+				thisEmitter.solutionCount++;
 			}
-			
+			System.out.println("Emitter: \""+thisEmitter.emitterTag+"\"\tSolutions: "+thisEmitter.solutionCount);
 			fileUpdates(replacedBool); // log failure or success
 		}
-		optEmitter.updateDistribution(deltaI); // update distribution once population is done evaluating
+		thisEmitter.updateDistribution(deltaI); // update distribution once population is done evaluating
+	}
+	
+	private class Emitter implements Comparable<Emitter> {
+		int solutionCount = 0;
+		String emitterTag;
+		CMAEvolutionStrategy CMAESInstance;
+		
+		public Emitter(CMAEvolutionStrategy CMAESInstance, String tag) {
+			this.CMAESInstance = CMAESInstance;
+			this.emitterTag = tag;
+		}
+		
+		public double[][] samplePopulation() {
+			return CMAESInstance.samplePopulation();
+		}
+		
+		public void updateDistribution(double[] functionValues) {
+			CMAESInstance.updateDistribution(functionValues);
+		}
+
+		@Override
+		public int compareTo(Emitter other) {
+			return this.solutionCount - other.solutionCount;
+		}
+	}
+	
+	private Emitter newEmitter(int dimension, String tag) {
+		CMAEvolutionStrategy optEmitter = new CMAEvolutionStrategy();
+		optEmitter.setDimension(dimension);
+		optEmitter.setInitialX(0.0); //TODO test 0.0
+		optEmitter.setInitialStandardDeviation(0.2); // unsure if should be hardcoded or not
+		int mu = Parameters.parameters.integerParameter("mu");
+		optEmitter.parameters.setMu(mu);
+		optEmitter.parameters.setPopulationSize(mu); 
+		optEmitter.init();
+		optEmitter.writeToDefaultFilesHeaders(0); // Overwrite existing CMA-ES files
+		return new Emitter(optEmitter, tag);
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException, NoSuchMethodException {
 		System.out.println("Testing CMA-ME");
-		int runNum = 1;
+		int runNum = 13;
 		MMNEAT.main(("runNumber:"+runNum+" randomSeed:"+runNum+" marioGANLevelChunks:10 marioSimpleAStarDistance:true ea:edu.southwestern.evolution.mapelites.CMAME base:mariogan marioGANModel:GECCO2018GAN_World1-1_32_Epoch5000.pth GANInputSize:32 log:MarioGAN-CMAMETest saveTo:CMAMETest trials:1 printFitness:true mu:50 maxGens:500 io:true netio:true genotype:edu.southwestern.evolution.genotypes.BoundedRealValuedGenotype mating:true fs:false task:edu.southwestern.tasks.mario.MarioGANLevelTask saveAllChampions:false cleanOldNetworks:true logTWEANNData:false logMutationAndLineage:false marioLevelLength:120 marioStuckTimeout:20 watch:false steadyStateIndividualsPerGeneration:100 aStarSearchBudget:100000 mapElitesBinLabels:edu.southwestern.tasks.mario.MarioMAPElitesDistinctChunksNSAndDecorationBinLabels experiment:edu.southwestern.experiment.evolution.SteadyStateExperiment").split(" "));
-		//MMNEAT.main("runNumber:0 randomSeed:0 marioSimpleAStarDistance:true ea:edu.southwestern.evolution.mapelites.CMAME base:mariogan marioGANModel:GECCO2018GAN_World1-1_32_Epoch5000.pth GANInputSize:32 log:MarioGAN-CMAMETest saveTo:CMAMETest trials:1 printFitness:true mu:50 maxGens:500 io:false netio:false genotype:edu.southwestern.evolution.genotypes.BoundedRealValuedGenotype mating:true fs:false task:edu.southwestern.tasks.mario.MarioGANLevelTask saveAllChampions:false cleanOldNetworks:true logTWEANNData:false logMutationAndLineage:false marioLevelLength:120 marioStuckTimeout:20 watch:false steadyStateIndividualsPerGeneration:100 aStarSearchBudget:100000 mapElitesBinLabels:edu.southwestern.tasks.mario.MarioMAPElitesDistinctChunksNSAndDecorationBinLabels experiment:edu.southwestern.experiment.evolution.SteadyStateExperiment".split(" "));
-		
 	}
 }
