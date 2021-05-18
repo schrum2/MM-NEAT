c8a0ebdaa795dc339f03193a69912d99fc3c0905
Anna <wickera@southwestern.edu>
2021-05-18 16:39:24 -0500

    #635 Created a new autoencoder package and class
    Created a package for the autoencoder and created the corresponding class.  This code was taken from https://github.com/deeplearning4j/deeplearning4j-examples/blob/master/dl4j-examples/src/main/java/org/deeplearning4j/examples/quickstart/modeling/feedforward/unsupervised/MNISTAutoencoder.java.

---
+++ src/main/java/edu/southwestern/tasks/innovationengines/PictureTargetFitnessTroubleshoot.java
@@ -0,0 +1,10 @@
+package edu.southwestern.tasks.innovationengines;
+
+public class PictureTargetFitnessTroubleshoot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
