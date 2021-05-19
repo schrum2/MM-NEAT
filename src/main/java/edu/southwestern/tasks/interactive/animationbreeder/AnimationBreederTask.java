a83f53e4e4726707033c456e538f146143a83081
Jacob <schrum2@southwestern.edu>
2021-05-19 11:30:56 -0500

    Removed unnecessary imports

--- src/main/java/edu/southwestern/tasks/interactive/animationbreeder/AnimationBreederTask.java
+++ src/main/java/edu/southwestern/tasks/interactive/animationbreeder/AnimationBreederTask.java
@@ -2,7 +2,6 @@ package edu.southwestern.tasks.interactive.animationbreeder;
 
 import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -30,7 +29,6 @@ import edu.southwestern.parameters.Parameters;
 import edu.southwestern.scores.Score;
 import edu.southwestern.tasks.interactive.InteractiveEvolutionTask;
 import edu.southwestern.util.graphics.AnimationUtil;
-import edu.southwestern.util.graphics.GraphicsUtil;
 
 /**
  * Interface that interactively evolves originally generated animations
