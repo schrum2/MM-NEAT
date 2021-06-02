4cde98d75e8a0ff52b4b4e7210ba4d977c3c50bb
Anna <wickera@southwestern.edu>
2021-06-02 14:45:09 -0500

    #655 Autoencoder generator
    This would work, except that the console input has a hard length limit! A 28*28 length list is too long. Need alternate solution

--- src/main/python/AutoEncoder/autoencoderInputGenerator.py
+++ src/main/python/AutoEncoder/autoencoderInputGenerator.py
@@ -9,27 +9,21 @@ from model import autoencoder
 if __name__ == '__main__':
     
     modelToLoad = 'sim_autoencoder.pth'
-    nz = int(sys.argv[2])
-    z_dims = int(sys.argv[3])
-    out_width = int(sys.argv[4])
-    out_height = int(sys.argv[5])
-    fixedModel = OrderedDict() ## Use torch.load
-
-    imageSize = 32
-    ngf = 64
-    ngpu = 1
-    n_extra_layers = 0
-    batchSize = 1
+    fixedModel = torch.load(modelToLoad, map_location=lambda storage, loc: storage)
 
     model = autoencoder().cuda()
     model.load_state_dict(fixedModel)
+
+    print("READY") # Java loops until it sees this special signal
+    sys.stdout.flush() # Make sure Java can sense this output before Python blocks waiting for input
 
     while True:
         line = sys.stdin.readline()
 
         lv = numpy.array(json.loads(line))
-        latent_vector = torch.FloatTensor( lv ).view(batchSize, nz, 1, 1) 
-        levels = model(Variable(latent_vector, volatile=True))
+        # Input is already a flat 1D array, so no new view needed
+        input = torch.FloatTensor( lv ) 
+        output = model(Variable(input, volatile=True))
 
-print(json.dumps(level[0].tolist()))
-sys.stdout.flush() # Make Java sense output before blocking on next input
+        print(json.dumps(output.tolist()))
+        sys.stdout.flush() # Make Java sense output before blocking on next input
