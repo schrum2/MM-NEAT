82449b5b992994963526d6a532872d8cbe9e1af8
Anna <wickera@southwestern.edu>
2021-06-03 10:37:25 -0500

    #655 Autoencoder can now send an input image and receive the output image.
    Sends the python script a flat array of the image, and receives a flat array of the output, and converts the output array into an image.

--- src/main/java/autoencoder/python/AutoEncoderProcess.java
+++ src/main/java/autoencoder/python/AutoEncoderProcess.java
@@ -1,14 +1,23 @@
 package autoencoder.python;
 
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.lang.ProcessBuilder.Redirect;
+import java.util.Arrays;
 
+import javax.imageio.ImageIO;
+
+import edu.southwestern.parameters.Parameters;
 import edu.southwestern.tasks.mario.gan.Comm;
 import edu.southwestern.util.PythonUtil;
+import edu.southwestern.util.graphics.DrawingPanel;
+import edu.southwestern.util.graphics.GraphicsUtil;
 
 public class AutoEncoderProcess extends Comm {
 	
@@ -18,6 +27,7 @@ public class AutoEncoderProcess extends Comm {
 	
 	public static final String SAVED_AUTOENCODER = PYTHON_BASE_PATH + "sim_autoencoder.pth";
 		
+	public static final int SIDE_LENGTH = 28;
 	
 	public AutoEncoderProcess() {
 		//TODO: Anything needed here?
@@ -69,19 +79,55 @@ public class AutoEncoderProcess extends Comm {
 	}
 	
 	public static void main (String[] args) throws IOException {
+		Parameters.initializeParameterCollections(new String[] {"blackAndWhitePicbreeder:true"});
 		PythonUtil.PYTHON_EXECUTABLE = "C:\\ProgramData\\Anaconda3\\python.exe";
 		
 		AutoEncoderProcess p = new AutoEncoderProcess();
 		p.start();
 		
+		//BufferedImage img = ImageIO.read(new File("parentDir" + File.separator + "PicbreederTargetTrainingSet" + File.separator + "0.71288Neurons[35]links[63]1788009.jpg"));
+		//BufferedImage img = ImageIO.read(new File("parentDir" + File.separator + "PicbreederTargetTrainingSet" + File.separator + "0.52723Neurons[15]links[58]143861.jpg"));
+		//BufferedImage img = ImageIO.read(new File("parentDir" + File.separator + "PicbreederTargetTrainingSet" + File.separator + "0.68266Neurons[14]links[62]1105464.jpg"));
+		BufferedImage img = ImageIO.read(new File("data" + File.separator + "imagematch" + File.separator + "skull64.png"));
+		Image scaled = img.getScaledInstance(28, 28, BufferedImage.SCALE_DEFAULT);
+		img = GraphicsUtil.convertToBufferedImage(scaled);
+		DrawingPanel picture = GraphicsUtil.drawImage(img, "Before", img.getWidth(), img.getHeight());
+		
+		double[] imageInput =  GraphicsUtil.flatFeatureArrayFromBufferedImage(img);
+		
 		String s = p.commRecv();
 		System.out.println(s);
+		System.out.println("Before:"+Arrays.toString(imageInput));
 		
-		for(int i = 0; i < 28 * 28; i++) {
-			p.commSend(Math.random() + "");
+		for(int i = 0; i < imageInput.length; i++) {
+			p.commSend(imageInput[i] + "");
 		}
+		
 		String output = p.commRecv();
-		System.out.println(output);
+		System.out.println("After:"+output);
+		
+		// Remove [ and ]
+		output = output.substring(1,output.length()-1);
+		String[] arrOutput = output.split(",");
+		double[] numOut = new double[arrOutput.length];
+		for(int i = 0; i < numOut.length; i++) {
+			numOut[i] = Double.parseDouble(arrOutput[i]);
+		}
+		
+		BufferedImage pythonOutput = new BufferedImage(SIDE_LENGTH, SIDE_LENGTH, BufferedImage.TYPE_INT_RGB);
+		int i = 0;
+		for(int y = 0; y < SIDE_LENGTH; y++) {
+			for(int x = 0; x < SIDE_LENGTH; x++) {
+				float gray = (float) numOut[i++];
+				gray = Math.max(gray, 0); // Why is negative possible!?
+				Color c = new Color(gray,gray,gray);
+				pythonOutput.setRGB(x, y, c.getRGB());	
+			}
+		}
+
+		DrawingPanel outputImage = GraphicsUtil.drawImage(pythonOutput, "After", img.getWidth(), img.getHeight());
+		outputImage.getFrame().setLocation(100, 0);
+		
 		System.out.println("done");
 	}
 
