b63fa82fe9af9aabb24288925807fc9e0c6f6deb
MBatt1 <53229958+MBatt1@users.noreply.github.com>
2021-06-03 09:46:28 -0500

    Failed Mario GAN all training, changed to --nz 30 #648

--- src/main/python/GAN/train_mario_all.bat
+++ src/main/python/GAN/train_mario_all.bat
@@ -1,1 +1,1 @@
-python main.py --niter 5000 --nz 10 --json Mario-all.json --experiment Mario1_All_10_Epoch5000 --tiles 13 --cuda
+python main.py --niter 5000 --nz 30 --json Mario-all.json --experiment Mario1_All_30_Epoch5000 --tiles 13 --cuda
