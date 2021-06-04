590ebfaa1f1b7b40c735304df2795321fe0aa161
Anna <wickera@southwestern.edu>
2021-06-04 16:43:17 -0500

    #676 Added the rotate method to GraphicsUtil.

--- src/main/java/edu/southwestern/util/graphics/GraphicsUtil.java
+++ src/main/java/edu/southwestern/util/graphics/GraphicsUtil.java
@@ -6,7 +6,9 @@ import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
@@ -832,5 +834,40 @@ public class GraphicsUtil {
 	    bGr.drawImage(img, 0, 0, null);
 	    bGr.dispose();
 		return bimage;
+	}
+	
+	/**
+	 * Rotates the input image.
+	 * Source code taken from the following link:
+	 * https://stackoverflow.com/questions/37758061/rotate-a-buffered-image-in-java
+	 * 
+	 * @param img Image to be rotated
+	 * @param angle	Angle to be rotated by
+	 * @return Return the rotatted image
+	 */
+	public BufferedImage rotateImageByDegrees(BufferedImage img, double angle) {
+	    double rads = Math.toRadians(angle);
+	    double sin = Math.abs(Math.sin(rads)), cos = Math.abs(Math.cos(rads));
+	    int w = img.getWidth();
+	    int h = img.getHeight();
+	    int newWidth = (int) Math.floor(w * cos + h * sin);
+	    int newHeight = (int) Math.floor(h * cos + w * sin);
+
+	    BufferedImage rotated = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
+	    Graphics2D g2d = rotated.createGraphics();
+	    AffineTransform at = new AffineTransform();
+	    at.translate((newWidth - w) / 2, (newHeight - h) / 2);
+
+	    int x = w / 2;
+	    int y = h / 2;
+
+	    at.rotate(rads, x, y);
+	    g2d.setTransform(at);
+	    g2d.drawImage(img, 0, 0, (ImageObserver) this);
+	    g2d.setColor(Color.RED);
+	    g2d.drawRect(0, 0, newWidth - 1, newHeight - 1);
+	    g2d.dispose();
+
+	    return rotated;
 	}
 }
