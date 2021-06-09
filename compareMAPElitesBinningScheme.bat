67c054e40205cdba7d88c0277cda6c2cecfed571
Jacob Schrum <schrum2@southwestern.edu>
2021-06-09 00:46:30 -0500

    #669 Added logLock
    This prevents the original parameters file from being modified

--- compareMAPElitesBinningScheme.bat
+++ compareMAPElitesBinningScheme.bat
@@ -1,3 +1,3 @@
 REM Usage:   <experiment directory> <log prefix> <run type> <run number> <number of trials per individual> <Binning Labels Package Path>
 REM Example: mariolevelsdecoratensleniency MarioLevelsDecorateNSLeniency CPPNThenDirect2GAN 0 1 edu.southwestern.tasks.mario.MarioMAPElitesDistinctChunksNSAndDecorationBinLabels
-java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar runNumber:%4 parallelEvaluations:false base:%1 log:%2-%3 saveTo:%3 trials:%5 experiment:edu.southwestern.experiment.post.CompareMAPElitesBinningSchemeExperiment mapElitesBinLabels:%6
+java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar runNumber:%4 parallelEvaluations:false base:%1 log:%2-%3 saveTo:%3 trials:%5 experiment:edu.southwestern.experiment.post.CompareMAPElitesBinningSchemeExperiment mapElitesBinLabels:%6 logLock:true
