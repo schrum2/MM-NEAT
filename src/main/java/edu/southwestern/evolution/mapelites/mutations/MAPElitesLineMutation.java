f7ebe7e3562d8913e52e6011a3e71e8cae2e96a8
MBatt1 <53229958+MBatt1@users.noreply.github.com>
2021-06-09 09:14:52 -0500

    comments to ME-Line #678

--- src/main/java/edu/southwestern/evolution/mapelites/mutations/MAPElitesLineMutation.java
+++ src/main/java/edu/southwestern/evolution/mapelites/mutations/MAPElitesLineMutation.java
@@ -1,5 +1,8 @@
 package edu.southwestern.evolution.mapelites.mutations;
 
+import java.util.ArrayList;
+
+import edu.southwestern.evolution.genotypes.Genotype;
 import edu.southwestern.evolution.genotypes.RealValuedGenotype;
 import edu.southwestern.evolution.mutation.real.RealMutation;
 import edu.southwestern.parameters.Parameters;
@@ -9,12 +12,21 @@ public class MAPElitesLineMutation extends RealMutation {
 	
 	protected final double lineRate; 
 	
+	
 	public MAPElitesLineMutation() {
 		this.lineRate = Parameters.parameters.doubleParameter("meLineMutationRate");
 	}
 	
 	@Override
-	public void mutateIndex(RealValuedGenotype genotype, int i) {
+	public void mutate(Genotype<ArrayList<Double>> genotype) {
+		// Randomly select y from archive, and save in global variable
+		for (int i = 0; i < genotype.getPhenotype().size(); i++) {
+			mutateIndex((RealValuedGenotype) genotype, i);
+		}
+	}
+	
+	@Override
+	public void mutateIndex(RealValuedGenotype genotypeX, int index) {
 		// TODO Auto-generated method stub
 		
 	}
