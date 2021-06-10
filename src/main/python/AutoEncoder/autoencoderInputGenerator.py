fc83366fbe3dc18546f67fe3b9e732e963a9a467
Anna <wickera@southwestern.edu>
2021-06-10 09:58:22 -0500

    #674 Prints the loss or image.

--- src/main/python/AutoEncoder/autoencoderInputGenerator.py
+++ src/main/python/AutoEncoder/autoencoderInputGenerator.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 from torch.autograd import Variable
 
 import sys
@@ -9,10 +10,17 @@ from model import autoencoder
 if __name__ == '__main__':
     
     modelToLoad = sys.argv[1] #'sim_autoencoder.pth'
+    mode = sys.argv[2] # loss | image
+    # loss : code prints MSELoss to console
+    # image: code prints vector representation of image to console
+    if mode != "loss" and mode != "image":
+        print('mode must be either "loss" or "image"')
+        quit()
     fixedModel = torch.load(modelToLoad)
 
     model = autoencoder().cuda()
     model.load_state_dict(fixedModel)
+    criterion = nn.MSELoss()
 
     print("READY") # Java loops until it sees this special signal
     sys.stdout.flush() # Make sure Java can sense this output before Python blocks waiting for input
@@ -34,6 +42,10 @@ if __name__ == '__main__':
         # Input is already a flat 1D array, so no new view needed
         input = torch.FloatTensor( lv ).cuda()
         output = model(Variable(input))
+        loss = criterion(output, input)
 
-        print(json.dumps(output.tolist()))
+        if mode == "image":
+            print(json.dumps(output.tolist()))
+        else: # should be "loss"
+            print(loss.item())
         sys.stdout.flush() # Make Java sense output before blocking on next input
