7f0b7934f49a2c8856928c23f3b15a0ed972cd31
annazwicker <82399865+annazwicker@users.noreply.github.com>
2021-06-23 14:28:55 -0500

    #704 The autoencoder runs, needs changes.
    The images produced appear to be grayscale/black and white rather than color.  Also needs to run for longer to see if that changes anything produced.

--- src/main/python/ColorAutoEncoder/ColorConvolutionalAutoencoderWithCUDA.py
+++ src/main/python/ColorAutoEncoder/ColorConvolutionalAutoencoderWithCUDA.py
@@ -10,18 +10,66 @@ from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch.nn.functional as F
+import myColorData
+
+#if __name__ == '__main__':
+#    trainingImagesDirectory = sys.argv[1]
+#
+#    batch_size = 128
+
+#training_data = datasets.FashionMNIST(
+#    root="data",
+#    train=True,
+#    download=True,
+#    transform=ToTensor()
+#)
+
+#test_data = datasets.FashionMNIST(
+#    root="data",
+#    train=False,
+#    download=True,
+#    transform=ToTensor()
+#)
+
+
+
+# Display image and label.
+#train_features, train_labels = next(iter(train_dataloader))
+#print(f"Feature batch shape: {train_features.size()}")
+#print(f"Labels batch shape: {train_labels.size()}")
+#img = train_features[0].squeeze()
+#label = train_labels[0]
+#plt.imshow(img, cmap="gray")
+#plt.show()
+#print(f"Label: {label}")
 
 #Converting data to torch.FloatTensor
 transform = transforms.ToTensor()
+batch_size = 20
 
 # Download the training and test datasets
-train_data = datasets.CIFAR10(root='data', train=True, download=True, transform=transform)
+#train_data = datasets.CIFAR10(root='data', train=True, download=True, transform=transform)
 
-test_data = datasets.CIFAR10(root='data', train=False, download=True, transform=transform)
+#test_data = datasets.CIFAR10(root='data', train=False, download=True, transform=transform)
+
+train_data = myColorData.CustomImageDataSet("C:\\Users\\wickera\\GitHub\\MM-NEAT\\src\\main\\python\\ColorAutoEncoder\\ColorTrainingSet", transform)
+
+# Download the Picbreeder color images training set
+#dataset = mydata.DatasetLoader(trainingImagesDirectory)
+
+#dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+#train_dataloader = DataLoader(training_data, batch_size=128, shuffle=True)
+
+# Why does it want us to drop_last?
+#train_dataloader = DataLoader(train_data , batch_size=batch_size, shuffle=False, num_workers=4, drop_last=True)
+train_dataloader = DataLoader(train_data , batch_size=batch_size, shuffle=False, num_workers=0)
+
+#test_dataloader = DataLoader(test_data, batch_size=128, shuffle=True)
 
 #Prepare data loaders
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, num_workers=0)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, num_workers=0)
+#train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, num_workers=0)
+#test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, num_workers=0)
 
 #Utility functions to un-normalize and display an image
 def imshow(img):
@@ -30,11 +78,11 @@ def imshow(img):
 
  
 #Define the image classes
-classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+#classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 #Obtain one batch of training images
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
+dataiter = iter(train_dataloader)
+images = dataiter.next()
 images = images.numpy() # convert images to numpy for display
 
 #Plot the images
@@ -43,7 +91,7 @@ fig = plt.figure(figsize=(8, 8))
 for idx in np.arange(9):
     ax = fig.add_subplot(3, 3, idx+1, xticks=[], yticks=[])
     imshow(images[idx])
-    ax.set_title(classes[labels[idx]])
+    #ax.set_title(classes[labels[idx]])
 
 #Define the Convolutional Autoencoder
 class ConvAutoencoder(nn.Module):
@@ -100,8 +148,8 @@ for epoch in range(1, n_epochs+1):
     train_loss = 0.0
 
     #Training
-    for data in train_loader:
-        images, _ = data
+    for data in train_dataloader:
+        images = data
         images = images.to(device)
         optimizer.zero_grad()
         outputs = model(images)
@@ -110,20 +158,21 @@ for epoch in range(1, n_epochs+1):
         optimizer.step()
         train_loss += loss.item()*images.size(0)
           
-    train_loss = train_loss/len(train_loader)
+    train_loss = train_loss/len(train_dataloader)
     print('Epoch: {} \tTraining Loss: {:.6f}'.format(epoch, train_loss))
+
+# For now: Test on Training Data
+test_loader = train_dataloader
 
 #Batch of test images
 dataiter = iter(test_loader)
-images, labels = dataiter.next()
+images = dataiter.next()
 
 #Sample outputs
 output = model(images.to(device))
 images = images.numpy()
 
-batch_size = 32
-
-output = output.view(batch_size, 3, 32, 32)
+output = output.view(batch_size, 3, 28, 28)
 output = output.detach().cpu().numpy()
 
 #Original Images
@@ -132,7 +181,7 @@ fig, axes = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True, figsize=(12
 for idx in np.arange(5):
     ax = fig.add_subplot(1, 5, idx+1, xticks=[], yticks=[])
     imshow(images[idx])
-    ax.set_title(classes[labels[idx]])
+    #ax.set_title(classes[labels[idx]])
 plt.show()
 
 #Reconstructed Images
@@ -141,5 +190,5 @@ fig, axes = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True, figsize=(12
 for idx in np.arange(5):
     ax = fig.add_subplot(1, 5, idx+1, xticks=[], yticks=[])
     imshow(output[idx])
-    ax.set_title(classes[labels[idx]])
+    #ax.set_title(classes[labels[idx]])
 plt.show() 
