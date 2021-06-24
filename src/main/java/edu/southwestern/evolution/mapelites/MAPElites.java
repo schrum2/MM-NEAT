77a01eb54508dd66b4f03f17a41b7957d7a06884
annazwicker <82399865+annazwicker@users.noreply.github.com>
2021-06-24 14:16:49 -0500

    #715 Logging has been added and is successful.

--- src/main/java/edu/southwestern/evolution/mapelites/MAPElites.java
+++ src/main/java/edu/southwestern/evolution/mapelites/MAPElites.java
@@ -122,6 +122,7 @@ public class MAPElites<T> implements SteadyStateEA<T> {
 		String fillPercentagePrefix = experimentPrefix + "_" + "FillPercentage";
 		String qdPrefix = experimentPrefix + "_" + "QD";
 		String maxPrefix = experimentPrefix + "_" + "Maximum";
+		String lossPrefix = experimentPrefix + "_" + "ReconstructionLoss";
 		String directory = FileUtilities.getSaveDirectory();// retrieves file directory
 		directory += (directory.equals("") ? "" : "/");
 		String fullPDFName = directory + prefix + "_pdf_log.plt";
@@ -131,6 +132,7 @@ public class MAPElites<T> implements SteadyStateEA<T> {
 		String fullFillPercentageName = directory + fillPercentagePrefix + "_log.plt";
 		String fullQDName = directory + qdPrefix + "_log.plt";
 		String maxFitnessName = directory + maxPrefix + "_log.plt";
+		String reconstructionLossName = directory + lossPrefix + "_log.plt";
 		File pdfPlot = new File(fullPDFName);
 		File plot = new File(fullName); // for archive log plot file
 		File fillPlot = new File(fullFillName);
@@ -203,6 +205,13 @@ public class MAPElites<T> implements SteadyStateEA<T> {
 			ps.println("set title \"" + experimentPrefix + " Maximum individual fitness score");
 			ps.println("set output \"" + maxFitnessName.substring(maxFitnessName.lastIndexOf('/')+1, maxFitnessName.lastIndexOf('.')) + ".pdf\"");
 			ps.println("plot \"" + name + ".txt\" u 1:4 w linespoints t \"Maximum fitness Score\"");
+			
+			if(Parameters.parameters.booleanParameter("dynamicAutoencoderIntervals")) {
+				ps.println("set title \"" + experimentPrefix + " Reconstruction Loss Range");
+				ps.println("set output \"" + reconstructionLossName.substring(reconstructionLossName.lastIndexOf('/')+1, reconstructionLossName.lastIndexOf('.')) + ".pdf\"");
+				ps.println("plot \"" + name + ".txt\" u 1:2 w linespoints t \"Min Loss\"");
+				ps.println("plot \"" + name + ".txt\" u 1:3 w linespoints t \"Max Loss\"");
+			}
 			
 			ps.close();
 			
