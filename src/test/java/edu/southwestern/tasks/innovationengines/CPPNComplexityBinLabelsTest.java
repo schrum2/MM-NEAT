1359529a71862f668ad66c661b72bf97f33cc1b6
Jacob Schrum <schrum2@southwestern.edu>
2021-06-24 19:27:23 -0500

    Fixed test to account for new MIN_NEURON value of 7

--- src/test/java/edu/southwestern/tasks/innovationengines/CPPNComplexityBinLabelsTest.java
+++ src/test/java/edu/southwestern/tasks/innovationengines/CPPNComplexityBinLabelsTest.java
@@ -25,15 +25,13 @@ public class CPPNComplexityBinLabelsTest {
 	@Test
 	public void testOneDimensionalIndex() {
 
-		int[] allNeuronValues = new int[] {5,32,34,53,97,41,44,62,56,99,24,6,15,74,42,53,57,29,81,20,92,47,74,18,34,72,88,78,67};
-		int[] allLinkValues = new int[] {10,35,123,68,64,194,199,65,38,190,150,35,74,91,53,46,141,23,93,58,184,127,8,4,6,12,9,45,5};
+		int[] allNeuronValues = new int[] {7 ,34,36 ,55,99,43 ,46 ,64,58,100,26 ,8 ,15,76,44,55,59,31,83,22,94,49,76,20,36,74,90,80,69};
+		int[] allLinkValues = new int[]   {10,35,123,68,64,194,199,65,38,190,150,35,74,91,53,46,141,23,93,58,184,127,8,4,6,12,9,45,5};
 		
 		assert allNeuronValues.length == allLinkValues.length: "array lengths need to match";
 		
 		for(int i = 0; i < allNeuronValues.length; i++) {
-			for(int j = 0; j < allLinkValues.length; j++) {
-				assertEquals("Neurons" + allNeuronValues[i] + "links" + allLinkValues[j], labels.binLabels().get(labels.oneDimensionalIndex(new int[] {allNeuronValues[i],allLinkValues[j]})));
-			}
+			assertEquals("Neurons" + allNeuronValues[i] + "links" + allLinkValues[i], labels.binLabels().get(labels.oneDimensionalIndex(new int[] {allNeuronValues[i],allLinkValues[i]})));
 		}
 	
 	}
