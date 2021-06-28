c5a43da108f0173608064d6a9499f407849b034e
MBatt1 <53229958+MBatt1@users.noreply.github.com>
2021-06-28 15:17:15 -0500

    Decor and Leniency bonning scheme with bin amount parameter #717

--- src/main/java/edu/southwestern/tasks/mario/MarioMAPElitesDecorAndLeniencyBinLabels.java
+++ src/main/java/edu/southwestern/tasks/mario/MarioMAPElitesDecorAndLeniencyBinLabels.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import cern.colt.Arrays;
 import edu.southwestern.MMNEAT.MMNEAT;
 import edu.southwestern.evolution.mapelites.BaseBinLabels;
 import edu.southwestern.parameters.Parameters;
@@ -17,13 +18,13 @@ public class MarioMAPElitesDecorAndLeniencyBinLabels extends BaseBinLabels {
 		@Override
 		public List<String> binLabels() {
 			if(labels == null) { // Create once and re-use, but wait until after Parameters are loaded	
-				binsPerDimension = Parameters.parameters.integerParameter("marioGANLevelChunks");
+				binsPerDimension = Parameters.parameters.integerParameter("marioStatBasedMEBinIntervals");
 				
 				int size = binsPerDimension*binsPerDimension;
 				labels = new ArrayList<String>(size);
 				for(int i = 0; i < binsPerDimension; i++) { // Decoration
 					for(int r = -(binsPerDimension/2); r < binsPerDimension/2; r++) { // Leniency allows negative range
-						labels.add("Decoration"+i+"0-"+(i+1)+"0Leniency"+r+"0-"+(r+1)+"0");
+						labels.add("Decoration"+i+"-"+(i+1)+"Leniency"+r+"-"+(r+1));
 					}
 				}
 			}
@@ -47,23 +48,22 @@ public class MarioMAPElitesDecorAndLeniencyBinLabels extends BaseBinLabels {
 		}
 
 		public static void main(String[] args) throws FileNotFoundException, NoSuchMethodException {
-			MMNEAT.main("runNumber:1 randomSeed:1 base:mariolevelsdecorateleniency log:MarioLevelsDecorateLeniency-ME saveTo:ME marioGANLevelChunks:10 marioGANUsesOriginalEncoding:false marioGANModel:Mario1_Overworld_5_Epoch5000.pth GANInputSize:5 trials:1 mu:100 maxGens:100000 io:true netio:true genotype:edu.southwestern.evolution.genotypes.BoundedRealValuedGenotype mating:true fs:false task:edu.southwestern.tasks.mario.MarioGANLevelTask cleanFrequency:-1 saveAllChampions:true cleanOldNetworks:false logTWEANNData:false logMutationAndLineage:false marioStuckTimeout:20 watch:false marioProgressPlusJumpsFitness:false marioRandomFitness:false marioSimpleAStarDistance:true ea:edu.southwestern.evolution.mapelites.MAPElites experiment:edu.southwestern.experiment.evolution.SteadyStateExperiment mapElitesBinLabels:edu.southwestern.tasks.mario.MarioMAPElitesDecorAndLeniencyBinLabels steadyStateIndividualsPerGeneration:100 aStarSearchBudget:100000".split(" "));
+			MMNEAT.main("runNumber:701 randomSeed:0 base:mariolevelsdecorateleniency log:MarioLevelsDecorateLeniency-CMAME5Improvement saveTo:CMAME5Improvement marioGANLevelChunks:1 marioGANUsesOriginalEncoding:false marioGANModel:Mario1_Overworld_5_Epoch5000.pth GANInputSize:5 trials:1 mu:37 lambda:100 maxGens:100000 io:true netio:true genotype:edu.southwestern.evolution.genotypes.BoundedRealValuedGenotype mating:true fs:false task:edu.southwestern.tasks.mario.MarioGANLevelTask cleanFrequency:-1 saveAllChampions:true cleanOldNetworks:false logTWEANNData:false logMutationAndLineage:false marioStuckTimeout:20 watch:false marioProgressPlusJumpsFitness:false marioRandomFitness:false marioSimpleAStarDistance:true ea:edu.southwestern.evolution.mapelites.CMAME experiment:edu.southwestern.experiment.evolution.SteadyStateExperiment mapElitesBinLabels:edu.southwestern.tasks.mario.MarioMAPElitesDecorAndLeniencyBinLabels steadyStateIndividualsPerGeneration:100 aStarSearchBudget:100000 numImprovementEmitters:5 numOptimizingEmitters:0 CMAMESigma:0.5 marioStatBasedMEBinIntervals:50".split(" "));
 		}
 		
 		@Override
 		public int[] multiDimensionalIndices(HashMap<String, Object> keys) {
 			@SuppressWarnings("unchecked")
-			ArrayList<double[]> lastLevelStats = (ArrayList<double[]>) keys.get("Level Stats");		
+			ArrayList<double[]> lastLevelStats = (ArrayList<double[]>) keys.get("Level Stats");	
 			
-			double leniencySum = MarioLevelTask.sumStatScore(lastLevelStats, MarioLevelTask.LENIENCY_STAT_INDEX);
-			double decorationSum = MarioLevelTask.sumStatScore(lastLevelStats, MarioLevelTask.DECORATION_FREQUENCY_STAT_INDEX);
-
-			double DECORATION_SCALE = 3;
-			// Scale scores so that we are less likely to overstep the bounds of the bins
-			final int BINS_PER_DIMENSION = Parameters.parameters.integerParameter("marioGANLevelChunks");
+			double ganChunks = Parameters.parameters.integerParameter("marioGANLevelChunks");
 			
-			int leniencySumIndex = Math.min(Math.max((int)((leniencySum*(BINS_PER_DIMENSION/2)+0.5)*BINS_PER_DIMENSION),0), BINS_PER_DIMENSION-1); //LEANIENCY BIN INDEX
-			int decorationBinIndex =  Math.min((int)(decorationSum*DECORATION_SCALE*BINS_PER_DIMENSION), BINS_PER_DIMENSION-1); //decorationBinIndex
+			double leniencyPercentage = (MarioLevelTask.sumStatScore(lastLevelStats, MarioLevelTask.LENIENCY_STAT_INDEX) / ganChunks)*8.0 + 0.5;
+			double decorationPercentage = (MarioLevelTask.sumStatScore(lastLevelStats, MarioLevelTask.DECORATION_FREQUENCY_STAT_INDEX) / ganChunks);
+			decorationPercentage *= 8.0;
+			
+			int leniencySumIndex = Math.min((int) (leniencyPercentage*binsPerDimension), binsPerDimension-1); //LENIENCY BIN INDEX
+			int decorationBinIndex =  Math.min((int) (decorationPercentage*binsPerDimension), binsPerDimension-1); //decorationBinIndex
 			
 			return new int[] {decorationBinIndex, leniencySumIndex};
 		}
