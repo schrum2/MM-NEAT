ba80ac2f4195be67b648869e4c21353d75740f7c
MBatt1 <53229958+MBatt1@users.noreply.github.com>
2021-07-02 16:11:50 -0500

    Visualize compared bins plotting

--- batch/Experiments-2021-CompareBinning/VisualizeComparedBinningSchemes.py
+++ batch/Experiments-2021-CompareBinning/VisualizeComparedBinningSchemes.py
@@ -3,14 +3,8 @@ python VisualizeComparedBinningSchemes.py mariocomparebins DistinctNSDecorate
 """
 import sys
 import os
-
-import os.path
-from os import path
-
-#test_path = u"\\\\?\\G:\\.shortcut-targets-by-id\\0B0N6MBJZ1slKRFpSRjMyY3luZFE\SCOPE Artifacts\\SCOPE 2021 MAP-Elites Comparison\\mariocomparebins\\LineKLDivergence222and620\\MarioCompareBins-LineKLDivergence222and620_comparedTo_MarioMAPElitesDistinctChunksNSAndLeniencyBinLabels_log.txt"
-#print(test_path)
-#print(path.exists(test_path))
-#quit()
+import numpy
+import matplotlib.pyplot as plt
 
 def file_contents_to_dictionary(original_scheme, data_array):
     data_dict = {}
@@ -21,8 +15,24 @@ def file_contents_to_dictionary(original_scheme, data_array):
     data_dict["original_bins"] = int(data_array[3][(data_array[3].rfind("/")+1):(len(data_array[3])-1)]) # Surviving num
     data_dict["qd"] = float(data_array[4].replace("QD Score: ", ""))
     data_dict["max_fitness"] = float(data_array[5].replace("Maximum Fitness: ", ""))
-    
     return data_dict
+
+
+def data_mean_error(input_data):
+    organized_data = {}
+    organized_data["val"] = numpy.mean(input_data)
+    organized_data["standard_deviation"] = numpy.std(input_data)
+    return organized_data
+
+
+def convert_data_to_averages(data_array):
+    processed = {}
+    processed["occupied_bins"] = data_mean_error([v["occupied_bins"] for v in data_array])
+    processed["overall_bins"] = data_mean_error([((v["occupied_bins"]/v["overall_bins"])*100) for v in data_array])
+    processed["original_bins"] = data_mean_error([((v["occupied_bins"]/v["original_bins"])*100) for v in data_array])
+    processed["qd"] = data_mean_error([v["qd"] for v in data_array])
+    processed["max_fitness"] = data_mean_error([v["max_fitness"] for v in data_array])
+    return processed
     
 
 try: # Get the file path from arguments
@@ -44,12 +54,11 @@ except:
 directories = os.listdir(folder_path)
 targetted_directories = []
 for dir in directories:
-    if base_bins in dir and "." not in dir:
+    if base_bins in dir and "." not in dir and "Line" not in dir: # Remove "Line" from directories since results are not important
         targetted_directories.append(dir)
 
 targetted_directories.sort()
 
-#print(targetted_directories)
 
 converted_data = []
 for target in targetted_directories:
@@ -58,7 +67,6 @@ for target in targetted_directories:
     for fil in files:
         if "_comparedTo_" in fil and "_MAPElites_" not in fil:
             valid_files.append(fil)
-    #print(valid_files)
     
     file_data = []
     for valid in valid_files:
@@ -71,27 +79,64 @@ for target in targetted_directories:
             file_data.append(temp_data)
     
     for i in file_data:
-        #print(file_contents_to_dictionary(target, i))
         converted_data.append(file_contents_to_dictionary(target, i))
-    
-    
-    #quit()    
 
+# Sort data by the type of the original binning scheme
 segmented_data = {}
 for i in range(len(converted_data)):
-    #print(converted_data[i]["original_scheme"][:-1])
     if converted_data[i]["original_scheme"][:-1] not in segmented_data:
         segmented_data[converted_data[i]["original_scheme"][:-1]] = []
     segmented_data[converted_data[i]["original_scheme"][:-1]].append(converted_data[i])
 
-for i in segmented_data:
-    for j in segmented_data[i]:
-        print(j)
-    quit()
+# Sort the data by the type of bin that is being compared to
+scheme_sorted_data = {}
+for scheme in segmented_data:
+    sorted_scheme = sorted(segmented_data[scheme], key=lambda k: k["name"]) 
+    scheme_sorted_data[scheme] = sorted_scheme
+
+# Separate things into arrays
+final_sorted_data = {}
+for i in scheme_sorted_data:
+    final_sorted_data[i] = {}
+    for j in scheme_sorted_data[i]:
+        #print(j)
+        if j["name"] not in final_sorted_data[i]:
+            final_sorted_data[i][j["name"]] = []
+        final_sorted_data[i][j["name"]].append(j)
+
+# Process data into averages with standard deviations
+averaged_data = {}
+for i in final_sorted_data:
+    averaged_data[i] = {}
+    for j in final_sorted_data[i]:
+        averaged_data[i][j] = convert_data_to_averages(final_sorted_data[i][j])
 
 
-
-
-
-
-
+for i in averaged_data:
+    titles = []
+    vals = []
+    errs = []
+    colors = []
+    for j in averaged_data[i]:
+        current = averaged_data[i][j]
+        left_label_bound = j.rfind("MAPElites")+9
+        if left_label_bound == 8:
+            titles.append(j[:j.rfind("BinLabels")])
+        else:
+            titles.append(j[left_label_bound:j.rfind("BinLabels")]) # Cut out "MAPElites" part of 
+        vals.append(current["original_bins"]["val"])
+        errs.append(current["original_bins"]["standard_deviation"])
+        colors.append("red")
+    fig, ax = plt.subplots()
+    ax.bar(
+        titles, 
+        vals, 
+        yerr=errs,
+        color=colors
+        )
+    plt.ylim(0, 100) 
+    plt.xticks(rotation=25)
+    plt.tight_layout()
+    ax.set(title=(folder_path[:folder_path.rfind("comparebins")].title()+" "+i))
+    plt.savefig(folder_path+"/"+i+".png") # Save file
+    #plt.show() # Show the plot
