ce2dc9f220d281951245c05eab2cbd6b946ff0be
MBatt1 <53229958+MBatt1@users.noreply.github.com>
2021-07-06 16:48:22 -0500

    Batch file for training MegaMan 7 GAN without level 9 #681

---
+++ src/main/python/GAN/MegaManSevenGANNoWater9.bat
@@ -0,0 +1,13 @@
+python main.py --niter 5000 --nz 5 --json MegaManSevenGANDownNoWater9.json --experiment MegaManSevenGANDownNoWater9_5_Epoch5000 --tiles 12 --cuda
+
+python main.py --niter 5000 --nz 5 --json MegaManSevenGANUpNoWater9.json --experiment MegaManSevenGANUpNoWater9_5_Epoch5000 --tiles 12 --cuda
+
+python main.py --niter 5000 --nz 5 --json MegaManSevenGANHorizontalNoWater9.json --experiment MegaManSevenGANHorizontalNoWater9_5_Epoch5000 --tiles 12 --cuda
+
+python main.py --niter 5000 --nz 5 --json MegaManSevenGANLowerLeftCornerNoWater9.json --experiment MegaManSevenGANLowerLeftCornerNoWater9_5_Epoch5000 --tiles 12 --cuda
+
+python main.py --niter 5000 --nz 5 --json MegaManSevenGANLowerRightCornerNoWater9.json --experiment MegaManSevenGANLowerRightCornerNoWater9_5_Epoch5000 --tiles 12 --cuda
+
+python main.py --niter 5000 --nz 5 --json MegaManSevenGANUpperLeftCornerNoWater9.json --experiment MegaManSevenGANUpperLeftCornerNoWater9_5_Epoch5000 --tiles 12 --cuda
+
+python main.py --niter 5000 --nz 5 --json MegaManSevenGANUpperRightCornerNoWater9.json --experiment MegaManSevenGANUpperRightCornerNoWater9_5_Epoch5000 --tiles 12 --cuda
