e97d8fa75fc0e1f0554cdbe7146fc13a18b6babe
MBatt1 <53229958+MBatt1@users.noreply.github.com>
2021-07-08 10:45:43 -0500

    comments and small fix #721

--- src/main/python/Pyribs/MarioLatentPartition2SlicesTEST.py
+++ src/main/python/Pyribs/MarioLatentPartition2SlicesTEST.py
@@ -29,32 +29,26 @@ from ribs.visualize import grid_archive_heatmap
 
 ### FUNCTIONS
 def sum_halves(sol):
-    dim = sol.shape[1]
-
-    # Normalize the objective to the range [0, 100] where 100 is optimal.
-#    best_obj = 0.0
-#    worst_obj = (-5.12 - sphere_shift)**2 * dim
-    #print("SOLUTION=")
-    #print(sol)
-#    objs = (raw_obj - worst_obj) / (best_obj - worst_obj) * 100
+    """
+        Calculates the behavior characterization by summing like normal
+    """
+    dim = sol.shape[1] # sol typically has more than one solution, so checks second dim of solution 2D array
 
     # Calculate BCs.
     copied = sol.copy()
-    bcs = np.concatenate(
+    bcs = np.concatenate( # numpy functions work on 2D arrays, this is just to keep parity with summing despite the fact we only have 1 in the "batch"
         (
             np.sum(copied[:, :dim // 2], axis=1, keepdims=True),
             np.sum(copied[:, dim // 2:], axis=1, keepdims=True),
         ),
         axis=1,
     )
-    #print("BC=")
-    #print(bcs)
     return bcs
 
 
 def create_optimizer(algorithm, dim, seed):
-    """Creates an optimizer based on the algorithm name.
-
+    """Creates an optimizer based on the algorithm name. (THIS TEXT FRM PYRIBS)
+    
     Args:
         algorithm (str): Name of the algorithm passed into sphere_main.
         dim (int): Dimensionality of the sphere function.
@@ -62,15 +56,15 @@ def create_optimizer(algorithm, dim, seed):
     Returns:
         Optimizer: A ribs Optimizer for running the algorithm.
     """
-    max_bound = dim / 2
+    max_bound = dim / 2 # max bound for dim size of 10 is -5 to 5 in each dimension
     bounds = [(-max_bound, max_bound), (-max_bound, max_bound)]
-    initial_sol = np.zeros(dim)
+    initial_sol = np.zeros(dim) # Inital solution of zeros
     
-    emitter_bounds = []
+    emitter_bounds = [] 
     for i in range(dim):
-        emitter_bounds.append((-1, 1))
-    batch_size = 1
-    num_emitters = 1
+        emitter_bounds.append((-1, 1)) # Bound each value in solution between -1 and 1
+    batch_size = 1 # Since we do one at a time there is only 1 in a "batch"
+    num_emitters = 1 
     # https://docs.pyribs.org/en/stable/api/ribs.archives.GridArchive.html#ribs.archives.GridArchive
     archive = GridArchive((100, 100), bounds, seed=seed)
 
@@ -110,24 +104,22 @@ def create_optimizer(algorithm, dim, seed):
 
 # Using the JAR, get the bins and evaluated score from a provided level
 def get_data_from_level(string_level):
-    #print(str(string_level[0].tolist()))
-    jar.stdin.write(str(string_level[0].tolist())+ "\n")
+    jar.stdin.write(str(string_level[0].tolist())+ "\n") # Send vector to jar
     jar.stdin.flush()
     coords = ""
     while "[" not in coords:
         coords = jar.stdout.readline().strip()
     data_dict = {}
-    exec("data_dict[\"Bin Coordinates\"] = "+coords)
+    exec("data_dict[\"Bin Coordinates\"] = "+coords) # Bin coords are first, and do not have a "____ = " prefix
     s = jar.stdout.readline().strip()
-    while s != "MAP DONE":
-        #print("<From JAR> " + s)
+    while s != "MAP DONE": # Run until MAP DONE
+        #print("<From JAR> " + s) # DEBUG
         try:
-            exec("data_dict[\""+s.split(" = ")[0]+"\"] = "+s.split(" = ")[1])
-            #data_dict[s.split(" = ")[0]] = float(s.split(" = ")[1])
+            exec("data_dict[\""+s.split(" = ")[0]+"\"] = "+s.split(" = ")[1]) # Attempt to parse as given, either int, float, or array
         except:
-            data_dict[s.split(" = ")[0]] = s.split(" = ")[1]
-        s = jar.stdout.readline().strip()
-    data_dict["stats"] = [data_dict["stats[0]"], data_dict["stats[1]"]]
+            data_dict[s.split(" = ")[0]] = s.split(" = ")[1] # If unable, just send as string
+        s = jar.stdout.readline().strip() # Read next line
+    data_dict["stats"] = [data_dict["stats[0]"], data_dict["stats[1]"]] # Put stats into array instead of separated
     del data_dict["stats[0]"]
     del data_dict["stats[1]"]
     return data_dict
@@ -141,7 +133,7 @@ def save_heatmap(archive, heatmap_path, min_max):
         heatmap_path: Image path for the heatmap.
     """
     plt.figure(figsize=(8, 6))
-    grid_archive_heatmap(archive, vmin=min_max[0], vmax=min_max[1])
+    grid_archive_heatmap(archive, vmin=min_max[0], vmax=min_max[1]) # This is a pyribs thing it just works
     plt.tight_layout()
     plt.savefig(heatmap_path)
 
@@ -149,20 +141,20 @@ def save_heatmap(archive, heatmap_path, min_max):
 ### MAIN
 
 def pyribs_main():
-    algorithm = "map_elites"
-    dim=10
-    iterations = 5000
-    outdir="mariolatentpartition2slices_pyribs"
-    log_freq=100
-    name = f"{algorithm}_{dim}"
+    algorithm = "map_elites" # Algorithm
+    dim=10 # Length of solution vector to be expected
+    iterations = 5000 # Total number of iterations
+    outdir="mariolatentpartition2slices_pyribs" # Output directory
+    log_freq=100 # Logging frequency
+    name = f"{algorithm}_{dim}" # Name for output images and data
     
     outdir = Path(outdir)
     if not outdir.is_dir():
         outdir.mkdir()
     
-    optimizer = create_optimizer(algorithm, dim, None)
-    archive = optimizer.archive
-    metrics = {
+    optimizer = create_optimizer(algorithm, dim, None) # Make optimizer with contained archive
+    archive = optimizer.archive # Get archive
+    metrics = { # Set up starter metrics, will be populated later
         "QD Score": {
             "x": [0],
             "y": [0.0],
@@ -180,18 +172,23 @@ def pyribs_main():
         for itr in range(1, iterations + 1):
             itr_start = time.time()
             sols = optimizer.ask()
-            #print(sols)
-            data_out = get_data_from_level(sols)
-            #print(data_out)
-            bcs = sum_halves(sols)
-            #print("binScore= "+str([data_out["binScore"]]), "bc= "+str(bcs))
-            optimizer.tell([data_out["binScore"]], bcs)
+            data_out = get_data_from_level(sols) # Get score (and other data) from jar
+            bcs = sum_halves(sols) # Get bcs
+            """
+                optimizer.tell() takes in two array-likes, one for scores 
+                and one for the behavior characterizations. Since out batch
+                size is only one, this function must be passed a list that
+                only contains a single value, either a score or a bc based
+                on which it is. I messed up the lode runner one because I
+                hadn't embedded either within another array so just make sure!
+            """
+            optimizer.tell([data_out["binScore"]], bcs) # Send optimizer (and archive) array of scores and bcs, however batch size of one means only one of each
             non_logging_time += time.time() - itr_start
-            progress()
+            progress() # Progress bar stuff
 
             # Logging and output.
             final_itr = itr == iterations
-            if itr % log_freq == 0 or final_itr:
+            if itr % log_freq == 0 or final_itr: # Stuff to do on logging frequency steps
                 data = archive.as_pandas(include_solutions=final_itr)
                 if final_itr:
                     data.to_csv(str(outdir / f"{name}_archive.csv"))
@@ -201,12 +198,12 @@ def pyribs_main():
                 metrics["QD Score"]["x"].append(itr)
                 metrics["QD Score"]["y"].append(data['objective'].sum())
                 metrics["Archive Coverage"]["x"].append(itr)
-                metrics["Archive Coverage"]["y"].append(len(data)) #/ total_cells * 100) # Maxx: this can be added to chart percentage filled instead of actual filled
+                metrics["Archive Coverage"]["y"].append(len(data))
                 print(f"Iteration {itr}\t| Archive Coverage: "
                       f"{metrics['Archive Coverage']['y'][-1]/100:.3f}% "
-                      f"QD Score: {metrics['QD Score']['y'][-1]:.3f}")
+                      f"QD Score: {metrics['QD Score']['y'][-1]:.3f}") # Console output
                 
-                save_heatmap(archive, str(outdir / f"{name}_heatmap_{itr:05d}.png"), [0, 500])
+                save_heatmap(archive, str(outdir / f"{name}_heatmap_{itr:05d}.png"), [0, 400])
 
 
 if __name__ == '__main__':
@@ -220,7 +217,7 @@ if __name__ == '__main__':
         s = jar.stdout.readline().strip()
         #print("<From JAR> " + s) # DEBUG
         
-    fire.Fire(pyribs_main)
+    fire.Fire(pyribs_main) # Run with progress bar
     
     # Exit JAR and close stdout
     jar.stdin.write("exit")
