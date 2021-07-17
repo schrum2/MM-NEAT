038b099c7f5296ca073bbbca5c83f3475857f4a7
Jacob Schrum <schrum2@southwestern.edu>
2021-07-16 22:33:31 -0500

    Batch file to train Lode Runner GANs
    I believe this batch file reproduces all of the commands for training the GANs used in the 2021 GECCO poster paper on Lode Runner.

---
+++ src/main/python/GAN/LodeRunnerGECCOModels.bat
@@ -0,0 +1,6 @@
+python main.py --niter 20000 --nz 10 --json LodeRunnerFirstFiveAllTilesNoSpawn.json    --experiment LodeRunnerAllGround5LevelsEpoch20000_10_7.pth   --tiles 7
+python main.py --niter 20000 --nz 10 --json LodeRunnerFirstTwentyAllTilesNoSpawn.json  --experiment LodeRunnerAllGround20LevelsEpoch20000_10_7.pth  --tiles 7
+python main.py --niter 20000 --nz 10 --json LodeRunnerFirstFiftyAllTilesNoSpawn.json   --experiment LodeRunnerAllGround50LevelsEpoch20000_10_7.pth  --tiles 7
+python main.py --niter 20000 --nz 10 --json LodeRunnerFirstHundredAllTilesNoSpawn.json --experiment LodeRunnerAllGround100LevelsEpoch20000_10_7.pth --tiles 7
+python main.py --niter 20000 --nz 10 --json LodeRunnerAllTiles150Levels.json           --experiment LodeRunnerAllGround150LevelsEpoch20000_10_7.pth --tiles 7
+python main.py --niter 20000 --nz 10 --json LodeRunnerAllTilesWordsPresent.json        --experiment LodeRunnerAllGroundWordsPresent20000_10_7.pth   --tiles 7
