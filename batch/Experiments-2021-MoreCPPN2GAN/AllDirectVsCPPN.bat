6439a78ef8ee2c9f6cc148c38876c75e10cc098f
Jacob Schrum <schrum2@southwestern.edu>
2021-09-27 00:20:42 -0500

    More results scripts for CPPNThenDirect2GAN
    plot avg num CPPN vs Direct genotypes

---
+++ batch/Experiments-2021-MoreCPPN2GAN/AllDirectVsCPPN.bat
@@ -0,0 +1,4 @@
+Rscript AverageDirectVsCPPN.R MarioLevelsDecorateNSLeniency 0.8 0.6
+Rscript AverageDirectVsCPPN.R MarioLevelsDistinctNSDecorate 0.8 0.3
+Rscript AverageDirectVsCPPN.R ZeldaDungeonsDistinctBTRooms 0.8 0.5
+Rscript AverageDirectVsCPPN.R ZeldaDungeonsWallWaterRooms 0.8 0.6
