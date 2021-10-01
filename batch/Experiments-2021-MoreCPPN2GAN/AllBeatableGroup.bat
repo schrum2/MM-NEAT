a7aafd30e8cea9b1ea39501fb90b3aa1749e070d
Jacob Benoid Schrum <schrum2@southwestern.edu>
2021-10-01 17:09:31 -0500

    More results processing

--- batch/Experiments-2021-MoreCPPN2GAN/AllBeatableGroup.bat
+++ batch/Experiments-2021-MoreCPPN2GAN/AllBeatableGroup.bat
@@ -2,3 +2,8 @@ CALL AllBeatable.bat 29 mariolevelsdecoratensleniency MarioLevelsDecorateNSLenie
 CALL AllBeatable.bat 29 mariolevelsdistinctnsdecorate MarioLevelsDistinctNSDecorate
 CALL AllBeatable.bat 29 zeldadungeonsdistinctbtrooms ZeldaDungeonsDistinctBTRooms
 CALL AllBeatable.bat 29 zeldadungeonswallwaterrooms ZeldaDungeonsWallWaterRooms
+
+Rscript.exe AverageBeatable.R MarioLevelsDecorateNSLeniency
+Rscript.exe AverageBeatable.R MarioLevelsDistinctNSDecorate
+Rscript.exe AverageBeatable.R ZeldaDungeonsDistinctBTRooms
+Rscript.exe AverageBeatable.R ZeldaDungeonsWallWaterRooms
