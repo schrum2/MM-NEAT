a776f9434476290c6baeb37a687cb81851249a89
Jacob Schrum <schrum2@southwestern.edu>
2022-01-09 01:31:11 -0600

    Made class to replace Easy from wox
    #348
    This class seems to successfully replace the wox Easy class for any class that implements Serializable. Next step is to actually switch from using Easy to using Serialization throughout the code.

--- src/test/java/edu/southwestern/evolution/genotypes/TWEANNGenotypeSerializationTest.java
+++ src/test/java/edu/southwestern/evolution/genotypes/TWEANNGenotypeSerializationTest.java
@@ -16,10 +16,11 @@ import org.junit.Test;
 import edu.southwestern.MMNEAT.MMNEAT;
 import edu.southwestern.evolution.EvolutionaryHistory;
 import edu.southwestern.parameters.Parameters;
+import edu.southwestern.util.file.Serialization;
 
 public class TWEANNGenotypeSerializationTest {
 
-	private static final String TWEANN_GENOTYPE_TXT = "TWEANNGenotype.txt";
+	private static final String TWEANN_GENOTYPE = "TWEANNGenotypeTest";
 
 	@Before
 	public void setup() {
@@ -42,20 +43,39 @@ public class TWEANNGenotypeSerializationTest {
 		TWEANNGenotype tgBefore = new TWEANNGenotype(5, 2, 0);
 		TWEANNGenotype tgAfter = null;
 		try {
-			FileOutputStream fileOutputStream = new FileOutputStream(TWEANN_GENOTYPE_TXT);
+			FileOutputStream fileOutputStream = new FileOutputStream(TWEANN_GENOTYPE+"1");
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 			objectOutputStream.writeObject(tgBefore);
 			objectOutputStream.flush();
 			objectOutputStream.close();
 
-			FileInputStream fileInputStream = new FileInputStream(TWEANN_GENOTYPE_TXT);
+			FileInputStream fileInputStream = new FileInputStream(TWEANN_GENOTYPE+"1");
 			ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 			tgAfter = (TWEANNGenotype) objectInputStream.readObject();
 			objectInputStream.close(); 
 		} finally {
-			File txt = new File(TWEANN_GENOTYPE_TXT);
-			if(txt.exists()) {
-				//txt.delete();
+			File ser = new File(TWEANN_GENOTYPE);
+			if(ser.exists()) {
+				ser.delete();
+			}
+		}
+	    assertEquals(tgBefore, tgAfter);
+	    
+	    
+	}
+	
+	@Test 
+	public void test_serialization2() throws IOException, ClassNotFoundException {
+		
+		TWEANNGenotype tgBefore = new TWEANNGenotype(5, 2, 0);
+		TWEANNGenotype tgAfter = null;
+		try {
+			Serialization.save(tgBefore, TWEANN_GENOTYPE+"2");
+			tgAfter = (TWEANNGenotype) Serialization.load(TWEANN_GENOTYPE+"2");
+		} finally {
+			File ser = new File(TWEANN_GENOTYPE+".ser");
+			if(ser.exists()) {
+				ser.delete();
 			}
 		}
 	    assertEquals(tgBefore, tgAfter);
