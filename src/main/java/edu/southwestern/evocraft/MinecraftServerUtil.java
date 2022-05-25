f93716ebe1d81cf59bbc11735e85d04f53bb093c
Jacob Schrum <schrum2@southwestern.edu>
2022-05-25 12:01:33 -0500

    Beginnings of Java code to interface with Minecraft server

---
+++ src/main/java/edu/southwestern/evocraft/MinecraftServerUtil.java
@@ -0,0 +1,12 @@
+package edu.southwestern.evocraft;
+
+import java.io.File;
+
+public class MinecraftServerUtil {
+
+	public static final String PYTHON_BASE_PATH = "." + File.separator + "src" + File.separator + "main" + File.separator + "python" + File.separator + "EvoCraft" + File.separator;
+	// Python script to interact with a Minecraft server on the localhost
+	public static final String WASSERSTEIN_PATH = PYTHON_BASE_PATH + "ServerSendReceive.py";
+	
+	
+}
