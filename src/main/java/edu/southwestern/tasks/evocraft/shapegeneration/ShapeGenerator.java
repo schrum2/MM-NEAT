c58d4755c8ec5345adb0ad9dc92616be9479fcff
richeyme <richey2@southwestern.edu>
2022-05-31 10:35:04 -0500

    #761 Refactored some of the code to use for snakes
    Part of the code that was used in ThreeDimensionalVolumeGenerator was extracted to make a method that can also be used for snake evolution. So far the snakes are not generating but the world is generating other structures correctly instead of spawning a player into a non-flat world

--- src/main/java/edu/southwestern/tasks/evocraft/shapegeneration/ShapeGenerator.java
+++ src/main/java/edu/southwestern/tasks/evocraft/shapegeneration/ShapeGenerator.java
@@ -1,11 +1,16 @@
 package edu.southwestern.tasks.evocraft.shapegeneration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.southwestern.evolution.genotypes.Genotype;
+import edu.southwestern.networks.Network;
 import edu.southwestern.tasks.evocraft.MinecraftClient.Block;
 import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
+import edu.southwestern.tasks.evocraft.MinecraftClient.Orientation;
 import edu.southwestern.tasks.evocraft.blocks.BlockSet;
+import edu.southwestern.util.graphics.ThreeDimensionalUtil;
+import edu.southwestern.util.stats.StatisticsUtilities;
 
 /**
  * Defines an approach to generating a list of Blocks to place in Minecraft
@@ -31,4 +36,38 @@ public interface ShapeGenerator<T> {
 	 */
 	public List<Block> generateShape(Genotype<T> genome, MinecraftCoordinates corner, BlockSet blockSet);
 	
+	/**
+	 * 
+	 * @param corner
+	 * @param blockSet
+	 * @param blocks List of blocks to add each generated block to
+	 * @param net CPPN that is queried to generate blocks
+	 * @param ranges
+	 * @param distanceInEachPlane Whether CPPN takes additional center distance inputs for each plane
+	 * @param xi
+	 * @param yi
+	 * @param zi
+	 * @return Coordinates that indicate the direction to move in next for snake generation, where a null result indicates
+	 * 			that shape generation should stop. This is not used for standard 3D volume generation.
+	 */
+	public static MinecraftCoordinates generateBlock(MinecraftCoordinates corner, BlockSet blockSet, List<Block> blocks, Network net,
+			MinecraftCoordinates ranges, boolean distanceInEachPlane, int xi, int yi, int zi) {
+		double[] inputs = ThreeDimensionalUtil.get3DObjectCPPNInputs(xi, yi, zi, ranges.x(), ranges.y(), ranges.z(), -1, distanceInEachPlane);
+		double[] outputs = net.process(inputs);
+		int numBlockTypes = blockSet.getPossibleBlocks().length;
+		if(outputs[OUTPUT_INDEX_PRESENCE] > VOXEL_EXPRESSION_THRESHOLD) {
+			ArrayList<Double> blockPreferences = new ArrayList<Double>(numBlockTypes);
+			for(int i = 1; i <= numBlockTypes; i++) {
+				blockPreferences.add(outputs[i]);
+			}
+			int typeIndex = StatisticsUtilities.argmax(blockPreferences);
+			// TODO: Add way to evolve orientation
+			Orientation blockOrientation = Orientation.NORTH;
+			Block b = new Block(corner.add(new MinecraftCoordinates(xi,yi,zi)), blockSet.getPossibleBlocks()[typeIndex], blockOrientation);
+			blocks.add(b);
+		}
+		
+		return null; // null result means to stop generation when used to evolve snakes
+	}
+
 }
--- src/main/java/edu/southwestern/tasks/evocraft/shapegeneration/SnakeGeneration.java
+++ src/main/java/edu/southwestern/tasks/evocraft/shapegeneration/SnakeGeneration.java
@@ -8,30 +8,48 @@ import edu.southwestern.networks.Network;
 import edu.southwestern.parameters.Parameters;
 import edu.southwestern.tasks.evocraft.MinecraftClient.Block;
 import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
-import edu.southwestern.tasks.evocraft.MinecraftClient.Orientation;
 import edu.southwestern.tasks.evocraft.blocks.BlockSet;
 import edu.southwestern.util.graphics.ThreeDimensionalUtil;
-import edu.southwestern.util.stats.StatisticsUtilities;
 
-public class ThreeDimensionalVolumeGenerator<T extends Network> implements ShapeGenerator<T> {
+public class SnakeGeneration<T extends Network> implements ShapeGenerator<T> {
 
 	@Override
 	public List<Block> generateShape(Genotype<T> genome, MinecraftCoordinates corner, BlockSet blockSet) {
-		List<Block> blocks = new ArrayList<>();
+		// Create CPPN out of genome
 		Network net = genome.getPhenotype();
+		
+		// List of blocks that make up snake
+		List<Block> snake = new ArrayList<>();
+		
+		// Ranges for the x, y, and z direction
 		MinecraftCoordinates ranges = new MinecraftCoordinates(
 				Parameters.parameters.integerParameter("minecraftXRange"),
 				Parameters.parameters.integerParameter("minecraftYRange"),
 				Parameters.parameters.integerParameter("minecraftZRange"));
+		
+		boolean done = false;
+		int numberOfIterations = 0;
+		
 		boolean distanceInEachPlane = Parameters.parameters.booleanParameter("objectBreederDistanceInEachPlane");
-		int numBlockTypes = blockSet.getPossibleBlocks().length;
-		for(int xi = 0; xi < ranges.x(); xi++) {
-			for(int yi = 0; yi < ranges.y(); yi++) {
-				for(int zi = 0; zi < ranges.z(); zi++) {
-					ShapeGenerator.generateBlock(corner, blockSet, blocks, net, ranges, distanceInEachPlane, xi, yi, zi);
-				}
+		
+		int xi = ranges.x()/2;
+		int yi = ranges.y()/2;
+		int zi = ranges.z()/2;
+		
+		while(!done) {
+			numberOfIterations++;
+			
+			double[] inputs = ThreeDimensionalUtil.get3DObjectCPPNInputs(xi, yi, zi, ranges.x(), ranges.y(), ranges.z(), -1, distanceInEachPlane);
+			double[] outputs = net.process(inputs);
+			
+			//List<Integer> direction = nextDirection();
+			if(numberOfIterations == 100) { // the 100 should be a command line parameter (maxSnakeLength)
+				done = true;
+			} else {
+				
 			}
 		}
-		return blocks;
+		return snake;
 	}
+
 }
