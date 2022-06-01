f90ef77ece6c9b3ddc89900fcb49935104574006
Jacob Schrum <schrum2@southwestern.edu>
2022-06-01 14:25:34 -0500

    #767 Making a LonerTask version of the Minecraft task
    Unfortunately, MAP Elites is steady state, and thus needs a LonerTask. Specifically, it only generates and evaluates one item at a time. This class is a start on the loner version of the task, but a lot of work is left to do

---
+++ src/main/java/edu/southwestern/tasks/evocraft/MinecraftLonerShapeTask.java
@@ -0,0 +1,50 @@
+package edu.southwestern.tasks.evocraft;
+
+import edu.southwestern.networks.NetworkTask;
+import edu.southwestern.tasks.NoisyLonerTask;
+
+/**
+ * MAPElites only works with LonerTasks because it is a steady-state algorithm.
+ * MinecraftShapeTask is parallelized for evolving populations, but this
+ * version of the task cannot take advantage of that (though there may be a work-around
+ * using multiple emitters). However, some of the code from MinecraftShapeTask is
+ * re-used.
+ * 
+ * @author schrum2
+ *
+ * @param <T>
+ */
+public class MinecraftLonerShapeTask<T> extends NoisyLonerTask<T> implements NetworkTask {
+
+	private MinecraftShapeTask<T> internalMinecraftShapeTask;
+	
+	public MinecraftLonerShapeTask() {
+		internalMinecraftShapeTask = new MinecraftShapeTask<T>();
+	}
+	
+	@Override
+	public int numObjectives() {
+		return internalMinecraftShapeTask.numObjectives();
+	}
+
+	@Override
+	public double getTimeStamp() {
+		return 0;
+	}
+
+	@Override
+	public void postConstructionInitialization() {
+		internalMinecraftShapeTask.postConstructionInitialization();
+	}
+
+	@Override
+	public String[] sensorLabels() {
+		return internalMinecraftShapeTask.sensorLabels();
+	}
+
+	@Override
+	public String[] outputLabels() {
+		return internalMinecraftShapeTask.outputLabels();
+	}
+
+}
