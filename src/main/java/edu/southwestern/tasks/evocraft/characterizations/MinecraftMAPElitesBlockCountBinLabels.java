da0104a14213344765dece8dcce925b09230d4a7
Alejandro Medina <medina4@southwestern.edu>
2022-06-06 15:42:36 -0500

    #769 New bin labels based on number of non-AIR blocks

--- src/main/java/edu/southwestern/tasks/evocraft/characterizations/MinecraftMAPElitesBlockCountBinLabels.java
+++ src/main/java/edu/southwestern/tasks/evocraft/characterizations/MinecraftMAPElitesBlockCountBinLabels.java
@@ -1,41 +1,50 @@
 package edu.southwestern.tasks.evocraft.characterizations;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import edu.southwestern.tasks.evocraft.fitness.DepthFitness;
-import edu.southwestern.tasks.evocraft.fitness.HeightFitness;
+import edu.southwestern.parameters.Parameters;
 import edu.southwestern.tasks.evocraft.fitness.MinecraftFitnessFunction;
-import edu.southwestern.tasks.evocraft.fitness.WidthFitness;
-
+import edu.southwestern.tasks.evocraft.fitness.OccupiedCountFitness;
+/**
+ * Minecraft shapes are categorized based on non-AIR blocks.
+ *
+ */
 public class MinecraftMAPElitesBlockCountBinLabels extends MinecraftMAPElitesBinLabels {
+	
 	private List<String> labels = null;
-	// TODO: find out if this should stay the same as from the other class file:
-	// private List<MinecraftFitnessFunction> properties = Arrays.asList(new WidthFitness(), new HeightFitness(), new DepthFitness());
+	private List<MinecraftFitnessFunction> properties = Arrays.asList(new OccupiedCountFitness());
 	
 	@Override
 	public List<String> binLabels() {
-		// TODO Auto-generated method stub
-		return null;
+		if(labels == null) { // Create once and re-use, but wait until after Parameters are loaded	
+			int xDim = Parameters.parameters.integerParameter("minecraftXRange")+1;
+			int yDim = Parameters.parameters.integerParameter("minecraftYRange")+1;
+			int zDim = Parameters.parameters.integerParameter("minecraftZRange")+1;
+			
+			int size = xDim*yDim*zDim;
+			
+			labels = new ArrayList<String>(size);
+			for(int i = 0; i < size + 1; i++) labels.add(i + " Blocks");
+		}
+		return labels;
 	}
 
 	@Override
 	public int oneDimensionalIndex(int[] multi) {
-		// TODO Auto-generated method stub
-		return 0;
+		int binIndex = multi[0];
+		return binIndex;
 	}
 
 	@Override
 	public int[] dimensionSizes() {
-		// TODO Auto-generated method stub
-		return null;
+		return new int[] {Parameters.parameters.integerParameter("minecraftXRange")+1, Parameters.parameters.integerParameter("minecraftYRange")+1, Parameters.parameters.integerParameter("minecraftZRange")+1};
 	}
 
 	@Override
 	public List<MinecraftFitnessFunction> properties() {
-		// TODO: uncomment this out once I find out the right properties.
-		// return properties;
-		return null;
+		return properties;
 	}
 
 }
