a07f8778696bda7c5f4a3836857d0f6c74ffc49e
Mark Mueller <muellerm@southwestern.edu>
2022-06-09 15:41:48 -0500

    #780 Created the class for 2D
    created the file MinecraftMAPElitesBlockCountEmptyPercentBinLabels, starting to work within it

---
+++ src/main/java/edu/southwestern/tasks/evocraft/characterizations/MinecraftMAPElitesBlockCountEmptyPercentBinLabels.java
@@ -0,0 +1,33 @@
+package edu.southwestern.tasks.evocraft.characterizations;
+
+import java.util.List;
+
+import edu.southwestern.tasks.evocraft.fitness.MinecraftFitnessFunction;
+
+public class MinecraftMAPElitesBlockCountEmptyPercentBinLabels extends MinecraftMAPElitesBinLabels {
+
+	@Override
+	public List<String> binLabels() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int oneDimensionalIndex(int[] multi) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] dimensionSizes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MinecraftFitnessFunction> properties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
