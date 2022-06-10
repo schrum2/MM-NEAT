3b871670e8e4453a4f92c8510aa9d85d592c3c79
Mark Mueller <muellerm@southwestern.edu>
2022-06-10 10:51:52 -0500

    #780 reworking min and maxes
    committing in the middle to show how its being refactored, just in case

--- src/main/java/edu/southwestern/tasks/evocraft/fitness/NegativeSpaceCountFitness.java
+++ src/main/java/edu/southwestern/tasks/evocraft/fitness/NegativeSpaceCountFitness.java
@@ -1,52 +1,77 @@
 package edu.southwestern.tasks.evocraft.fitness;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import edu.southwestern.parameters.Parameters;
 import edu.southwestern.tasks.evocraft.MinecraftClient.Block;
 import edu.southwestern.tasks.evocraft.MinecraftClient.BlockType;
 import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
+import edu.southwestern.util.stats.StatisticsUtilities;
 
 public class NegativeSpaceCountFitness extends CheckBlocksInSpaceFitness {
 
 	@Override
 	public double fitnessFromBlocks(MinecraftCoordinates corner, List<Block> blocks) {
-		// Initializes all variables to 0, need min and max coordinates to calculate the space the shape takes up
-		int total=0, maxX=0, maxY=0, maxZ=0, 
-				minX=1000, minY=1000, minZ=1000;
-		for(Block b : blocks) {
-			if(b.type() != BlockType.AIR.ordinal()) {
-				// If any new min or max, change to it. Air blocks cannot be included in these coordinates (There is definitely a better way to do this)
-				if(b.x()>maxX) maxX=(b.x()-corner.x())+1;
-				if(b.x()<minX) minX=(b.x()-corner.x());
-				if(b.y()>maxY) maxY=(b.y()-corner.y())+1;
-				if(b.y()<minY) minY=(b.y()-corner.y());
-				if(b.z()>maxZ) {
-					
-					maxZ=(b.z()-corner.z())+1;
-					System.out.println("MAX=========================="+b.z());
-					System.out.println("MAX=========================="+corner.z());
-				}
-				if(b.z()<minZ) {
-					
-					minZ=(b.z()-corner.z());
-					System.out.println("MIN=========================="+minZ);
-				}
-			
-				// Total keeps track of all of the blocks that are not air within the shape
-				total++;
-			}
-			
-			
-		}
-		// Computes size of the shape based on the coordinates, then subtracts the non-air blocks to get negative space
-		System.out.println("X:"+(maxX-minX)+" Y:"+(maxY-minY)+" Z:"+(maxZ-minZ));
-		int sizeOfShape = (maxX-minX)*(maxY-minY)*(maxZ-minZ);
-		int negativeBlocks = sizeOfShape-total;
-		System.out.println("Size of shape = "+sizeOfShape+" Total = "+total);
-		System.out.println("Negative Blocks:"+negativeBlocks);
-		if(negativeBlocks<0) System.exit(1);
-		return negativeBlocks;
+		System.out.println("first");
+		// Needs refactoring
+		Stream<Block> blocklessX = blocks.parallelStream().filter(b -> b.type() != BlockType.AIR.ordinal());
+		double[] blockArrX = blocklessX.mapToDouble(b -> b.x()).toArray();
+		Stream<Block> blocklessY = blocks.parallelStream().filter(b -> b.type() != BlockType.AIR.ordinal());
+		double[] blockArrY = blocklessY.mapToDouble(b -> b.y()).toArray();
+		Stream<Block> blocklessZ = blocks.parallelStream().filter(b -> b.type() != BlockType.AIR.ordinal());
+		double[] blockArrZ = blocklessZ.mapToDouble(b -> b.z()).toArray();
+		
+		
+		int minX = (int) StatisticsUtilities.minimum(blockArrX);
+		int minY = (int) StatisticsUtilities.minimum(blockArrY);
+		int minZ = (int) StatisticsUtilities.minimum(blockArrZ);
+		System.out.println("X:"+minX+"Y:"+minY+"Z:"+minZ);
+		
+		Stream<Block> blockless4 = blocks.parallelStream().filter(b -> b.type() != BlockType.AIR.ordinal());
+		int maxX = (int) StatisticsUtilities.maximum(blockless4.mapToDouble(b -> b.x()).toArray());
+		Stream<Block> blockless5 = blocks.parallelStream().filter(b -> b.type() != BlockType.AIR.ordinal());
+		int maxY = (int) StatisticsUtilities.maximum(blockless5.mapToDouble(b -> b.y()).toArray());
+		Stream<Block> blockless6 = blocks.parallelStream().filter(b -> b.type() != BlockType.AIR.ordinal());
+		int maxZ = (int) StatisticsUtilities.maximum(blockless6.mapToDouble(b -> b.z()).toArray());
+		System.out.println("X:"+maxX+"Y:"+maxY+"Z:"+maxZ);
+		
+//		// Initializes all variables to 0, need min and max coordinates to calculate the space the shape takes up
+//		int total=0, maxX=0, maxY=0, maxZ=0, 
+//				minZ=1000;
+//		for(Block b : blocks) {
+//			if(b.type() != BlockType.AIR.ordinal()) {
+//				// If any new min or max, change to it. Air blocks cannot be included in these coordinates (There is definitely a better way to do this)
+//				if(b.x()>maxX) maxX=(b.x()-corner.x())+1;
+//				if(b.x()<minX) minX=(b.x()-corner.x());
+//				if(b.y()>maxY) maxY=(b.y()-corner.y())+1;
+//				if(b.y()<minY) minY=(b.y()-corner.y());
+//				if(b.z()>maxZ) {
+//					
+//					maxZ=(b.z()-corner.z())+1;
+//					System.out.println("MAX=========================="+b.z());
+//					System.out.println("MAX=========================="+corner.z());
+//				}
+//				if(b.z()<minZ) {
+//					
+//					minZ=(b.z()-corner.z());
+//					System.out.println("MIN=========================="+minZ);
+//				}
+//			
+//				// Total keeps track of all of the blocks that are not air within the shape
+//				total++;
+//			}
+//			
+//			
+//		}
+//		// Computes size of the shape based on the coordinates, then subtracts the non-air blocks to get negative space
+//		System.out.println("X:"+(maxX-minX)+" Y:"+(maxY-minY)+" Z:"+(maxZ-minZ));
+//		int sizeOfShape = (maxX-minX)*(maxY-minY)*(maxZ-minZ);
+//		int negativeBlocks = sizeOfShape-total;
+//		System.out.println("Size of shape = "+sizeOfShape+" Total = "+total);
+//		System.out.println("Negative Blocks:"+negativeBlocks);
+//		if(negativeBlocks<0) System.exit(1);
+		return 0;
 	}
 
 	@Override
