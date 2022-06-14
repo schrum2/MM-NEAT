6024cf60f04e4c6ea2d15e439e33cf01aae4b856
Jacob Schrum <schrum2@southwestern.edu>
2022-06-13 19:48:58 -0500

    #786 Fixed several errors here
    @alejmedinajr I actually found and fixed several errors here, and slightly refactored for style. Could still use comments though.

--- src/main/java/edu/southwestern/tasks/evocraft/shapegeneration/VectorToVolumeGenerator.java
+++ src/main/java/edu/southwestern/tasks/evocraft/shapegeneration/VectorToVolumeGenerator.java
@@ -8,7 +8,6 @@ import edu.southwestern.MMNEAT.MMNEAT;
 import edu.southwestern.evolution.genotypes.Genotype;
 import edu.southwestern.parameters.Parameters;
 import edu.southwestern.tasks.evocraft.MinecraftClient.Block;
-import edu.southwestern.tasks.evocraft.MinecraftClient.BlockType;
 import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
 import edu.southwestern.tasks.evocraft.MinecraftClient.Orientation;
 import edu.southwestern.tasks.evocraft.MinecraftUtilClass;
@@ -19,11 +18,10 @@ public class VectorToVolumeGenerator implements ShapeGenerator<ArrayList<Double>
 	
 	private static double[] upper = null;
 	private static double[] lower = null;
-	private static MinecraftCoordinates ranges = null;
 	private static int numBlocks = 0;
 	
 	public VectorToVolumeGenerator() {
-		ranges = MinecraftUtilClass.getRanges();
+		MinecraftCoordinates ranges = MinecraftUtilClass.getRanges();
 		
 		if(Parameters.parameters.booleanParameter("vectorPresenceThresholdForEachBlock")) numBlocks = 2*(ranges.x() * ranges.y() * ranges.z());
 		else numBlocks = ranges.x() * ranges.y() * ranges.z();
@@ -35,18 +33,13 @@ public class VectorToVolumeGenerator implements ShapeGenerator<ArrayList<Double>
 	@Override
 	public List<Block> generateShape(Genotype<ArrayList<Double>> genome, MinecraftCoordinates corner,
 			BlockSet blockSet) {
+		MinecraftCoordinates ranges = MinecraftUtilClass.getRanges();
 		
-		List<Block> blocks = new ArrayList<>();
-		List<Double> doubles = new ArrayList<>();
-		
-		for(double d = 0.0; d < blockSet.getPossibleBlocks().length; d++) {
-			doubles.add(d); // add the possible double values from the block list (0 to block list length)
-		}
+		List<Block> blocks = new ArrayList<Block>();
 		ArrayList<Double> phenotype = genome.getPhenotype();	
 		Orientation blockOrientation = Orientation.NORTH; // all blocks will have orientation of north for now
 		int counter= 0; // used to count the number of blocks added
-		int blockTypeIndex = 0; // blockType is index used to determine blocktype					
-		
+		int numBlockTypes = blockSet.getPossibleBlocks().length;
 		for(int xi = 0; xi < ranges.x(); xi++) {
 			for(int yi = 0; yi < ranges.y(); yi++) {
 				for(int zi = 0; zi < ranges.z(); zi++) {
@@ -58,20 +51,20 @@ public class VectorToVolumeGenerator implements ShapeGenerator<ArrayList<Double>
 						final int PRESENCE_INDEX = counter;
 						final int TYPE_INDEX = counter+1;
 						if(phenotype.get(PRESENCE_INDEX) >= Parameters.parameters.doubleParameter("voxelExpressionThreshold")) {
-							blockTypeIndex = (int)(phenotype.get(TYPE_INDEX)*(blockSet.getPossibleBlocks().length)); // length because there are no AIR blocks in list for this case (presence takes care of this)		
+							int blockTypeIndex = (int)(phenotype.get(TYPE_INDEX)*numBlockTypes); // length because there are no AIR blocks in list for this case (presence takes care of this)		
+							if(blockTypeIndex == numBlockTypes) blockTypeIndex--; // Rare case
+							b = new Block(corner.add(new MinecraftCoordinates(xi,yi,zi)), blockSet.getPossibleBlocks()[blockTypeIndex], blockOrientation);
 						} // else do not place any block
 						counter+=2; // two numbers per block
-						b = new Block(corner.add(new MinecraftCoordinates(xi,yi,zi)), blockSet.getPossibleBlocks()[blockTypeIndex], blockOrientation);
-
 					} else { 
-						blockTypeIndex = (int)(phenotype.get(counter)*(blockSet.getPossibleBlocks().length+1)); // length+1 because there could be airblocks
-						if(blockTypeIndex == blockSet.getPossibleBlocks().length) b = new Block(corner.add(new MinecraftCoordinates(xi,yi,zi)), BlockType.AIR, blockOrientation);
-						else b = new Block(corner.add(new MinecraftCoordinates(xi,yi,zi)), blockSet.getPossibleBlocks()[blockTypeIndex], blockOrientation);
+						int blockTypeIndex = (int)(phenotype.get(counter)*(numBlockTypes+1)); // length+1 because there could be airblocks
+						if(blockTypeIndex != numBlockTypes) { // Corresponds to empty/AIR block
+							b = new Block(corner.add(new MinecraftCoordinates(xi,yi,zi)), blockSet.getPossibleBlocks()[blockTypeIndex], blockOrientation);
+						}
 						counter++; // one number per block
 					}
-					blocks.add(b);
-					//System.out.println(Arrays.toString(blockSet.getPossibleBlocks()));
-					//System.out.println(b);
+					// Will be null if empty/AIR
+					if(b!=null) blocks.add(b);
 				}
 			}
 		}
