970a2d7eae97e7ef7db2b07ba5c662aa813b9a47
Mark Mueller <muellerm@southwestern.edu>
2022-06-20 10:39:18 -0500

    #803 redstone fitness function

---
+++ src/main/java/edu/southwestern/tasks/evocraft/characterizations/numRedstoneFitness.java
@@ -0,0 +1,32 @@
+package edu.southwestern.tasks.evocraft.characterizations;
+
+import java.util.List;
+
+import edu.southwestern.parameters.Parameters;
+import edu.southwestern.tasks.evocraft.MinecraftClient.Block;
+import edu.southwestern.tasks.evocraft.MinecraftClient.BlockType;
+import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
+import edu.southwestern.tasks.evocraft.fitness.CheckBlocksInSpaceFitness;
+
+public class numRedstoneFitness extends CheckBlocksInSpaceFitness{
+
+	@Override
+	public double fitnessFromBlocks(MinecraftCoordinates corner, List<Block> blocks) {
+		int redstoneCount = 0;
+		// increases count only if redstone block
+		for(Block b : blocks) {
+			if(b.type() == BlockType.REDSTONE_BLOCK.ordinal()) {
+				redstoneCount++;
+			}
+		}
+		return redstoneCount;
+	}
+
+	@Override
+	public double maxFitness() {
+		// TODO Auto-generated method stub
+		return Parameters.parameters.integerParameter("minecraftXRange") * Parameters.parameters.integerParameter("minecraftYRange") * Parameters.parameters.integerParameter("minecraftZRange");
+	}
+	
+
+}
