54466b13fd11ae44a469f8e599faa5bbf5ac20e2
Mark Mueller <muellerm@southwestern.edu>
2022-06-21 10:11:48 -0500

    #803 refactored fitness functions to use blockcountfitness
    uses block count fitness functions now

--- src/main/java/edu/southwestern/tasks/evocraft/characterizations/MinecraftMAPElitesRedstoneVSPistonNinLabels.java
+++ src/main/java/edu/southwestern/tasks/evocraft/characterizations/MinecraftMAPElitesRedstoneVSPistonNinLabels.java
@@ -1,5 +1,33 @@
 package edu.southwestern.tasks.evocraft.characterizations;
 
-public class MinecraftMAPElitesRedstoneVSPistonNinLabels {
+import java.util.List;
+
+import edu.southwestern.tasks.evocraft.fitness.MinecraftFitnessFunction;
+
+public class MinecraftMAPElitesRedstoneVSPistonNinLabels extends MinecraftMAPElitesBinLabels{
+
+	@Override
+	public List<String> binLabels() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int oneDimensionalIndex(int[] multi) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] dimensionSizes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MinecraftFitnessFunction> properties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
