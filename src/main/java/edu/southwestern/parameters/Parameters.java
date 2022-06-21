618ef4adcda83b928ecbe89da860dad4568b2d57
richeyme <richey2@southwestern.edu>
2022-06-21 09:54:59 -0500

    #808 Added command line parameter

--- src/main/java/edu/southwestern/parameters/Parameters.java
+++ src/main/java/edu/southwestern/parameters/Parameters.java
@@ -368,6 +368,8 @@ public class Parameters {
 		integerOptions.add("minecraftAmountOfBlocksToEvolve", 5, "The ammount of possible blocks that can be evolved by CPPN");
 		integerOptions.add("interactiveSleepTimer", 1000, "The ammount of time the interactive thread sleeps for");
 		integerOptions.add("extraSpaceBetweenMinecraftShapes", 1000, "The extra space that will be in between the shapes being evaluated");
+		integerOptions.add("leftoverMinecraftBlocksAllowed", 0, "The amount of blocks that will be allowed to be left behind by the flying machine and still get max fitness for the change in center of mass fitness function");
+
 		
 		// Long parameters
 		longOptions.add("lastGenotypeId", 0l, "Highest genotype id used so far");
