706c5802cb41ca0f9473c3bbbb8f8adaed2d17a2
Mark Mueller <muellerm@southwestern.edu>
2022-06-21 11:00:23 -0500

    #803 may be working, but not displaying right
    may need to make unit tests

--- src/main/java/edu/southwestern/tasks/evocraft/characterizations/MinecraftMAPElitesRedstoneVSPistonNinLabels.java
+++ src/main/java/edu/southwestern/tasks/evocraft/characterizations/MinecraftMAPElitesRedstoneVSPistonNinLabels.java
@@ -7,13 +7,13 @@ import java.util.List;
 
 import edu.southwestern.parameters.Parameters;
 import edu.southwestern.tasks.evocraft.fitness.MinecraftFitnessFunction;
-import edu.southwestern.tasks.evocraft.fitness.NegativeSpaceCountFitness;
-import edu.southwestern.tasks.evocraft.fitness.OccupiedCountFitness;
+import edu.southwestern.tasks.evocraft.fitness.NumPistonsFitness;
+import edu.southwestern.tasks.evocraft.fitness.NumRedstoneFitness;
 
 public class MinecraftMAPElitesRedstoneVSPistonNinLabels extends MinecraftMAPElitesBinLabels{
 
 	private List<String> labels = null;
-	private List<MinecraftFitnessFunction> properties = Arrays.asList(new OccupiedCountFitness(), new NegativeSpaceCountFitness());
+	private List<MinecraftFitnessFunction> properties = Arrays.asList(new NumRedstoneFitness(), new NumPistonsFitness());
 	
 	@Override
 	public List<String> binLabels() {
@@ -22,7 +22,7 @@ public class MinecraftMAPElitesRedstoneVSPistonNinLabels extends MinecraftMAPEli
 			int yDim = Parameters.parameters.integerParameter("minecraftYRange");
 			int zDim = Parameters.parameters.integerParameter("minecraftZRange");
 			
-			int sizeBlockCount = xDim*yDim*zDim; 
+			int sizeBlockCount = xDim*yDim*zDim+1; 
 			labels = new ArrayList<String>(sizeBlockCount*sizeBlockCount);
 			
 			// go through all possible bins+1 since both 0 and 1000 blocks are both possibilities , -1 for negative space(j < size would just give a range of 0-999)
@@ -36,7 +36,7 @@ public class MinecraftMAPElitesRedstoneVSPistonNinLabels extends MinecraftMAPEli
 
 	@Override
 	public int oneDimensionalIndex(int[] multi) {
-		multi[1]++; // Needs to be done so no negative indexes
+		//multi[1]++; // Needs to be done so no negative indexes
 		int binIndex = (multi[0])*dimensionSizes()[1] + multi[1];
 		return binIndex;
 	}
@@ -45,14 +45,14 @@ public class MinecraftMAPElitesRedstoneVSPistonNinLabels extends MinecraftMAPEli
 	public int[] multiDimensionalIndices(HashMap<String, Object> keys) {
 		int[] result = super.multiDimensionalIndices(keys);
 		// Actual block count could be 0, but such shapes are discarded, only for block Count
-		result[0]--;
+		//result[0]--;
 		return result;
 	}
 	
 	@Override
 	public int[] dimensionSizes() {
 		int xtimesYtimez = Parameters.parameters.integerParameter("minecraftXRange")*Parameters.parameters.integerParameter("minecraftYRange")*Parameters.parameters.integerParameter("minecraftZRange");
-		return new int[] {xtimesYtimez,xtimesYtimez}; // Makes sure no empty fences are placed
+		return new int[] {xtimesYtimez+1,xtimesYtimez+1}; // Makes sure no empty fences are placed
 	}
 
 	@Override
@@ -60,8 +60,8 @@ public class MinecraftMAPElitesRedstoneVSPistonNinLabels extends MinecraftMAPEli
 		return properties;
 	}
 
-	@Override
-	public boolean discard(HashMap<String, Object> behaviorMap) {
-		return ((Double) behaviorMap.get("OccupiedCountFitness")).doubleValue() == 0; // IF empty, discards it (mostly first row of blockCount)
-	}
+//	@Override
+//	public boolean discard(HashMap<String, Object> behaviorMap) {
+//		return ((Double) behaviorMap.get("OccupiedCountFitness")).doubleValue() == 0; // IF empty, discards it (mostly first row of blockCount)
+//	}
 }
