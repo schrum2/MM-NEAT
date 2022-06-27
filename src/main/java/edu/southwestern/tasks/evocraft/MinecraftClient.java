401a39ba78b656aa5acd9713c315e30278c8db5a
Mark Mueller <muellerm@southwestern.edu>
2022-06-27 14:59:13 -0500

    #788 Fixed the server crashing issue
    needs debugging though, I deleted the entire floor...

--- src/main/java/edu/southwestern/tasks/evocraft/MinecraftClient.java
+++ src/main/java/edu/southwestern/tasks/evocraft/MinecraftClient.java
@@ -672,7 +672,15 @@ public class MinecraftClient extends Comm {
 		//System.out.println("Starts:"+groundStart);
 		MinecraftCoordinates end = new MinecraftCoordinates(start.x() + numShapes*(ranges.x() + Parameters.parameters.integerParameter("spaceBetweenMinecraftShapes")) + buffer, start.y() + ranges.y() + buffer, start.z() + (int)(ranges.z()*Math.sqrt(numShapes)) + buffer);
 		//System.out.println("ENDS:"+end);
-		fillCube(groundStart, end, BlockType.AIR); // Calls clear cube, which checks coordinates
+		for(int x=0;x<=end.x();x+=100) {
+			for(int z=0;z<=end.z();z+=100) {
+				for(int y=5;y<=end.y();y+=100) {
+					fillCube(x,y,z,x+100,y+100,z+100, BlockType.AIR);
+				}
+			}
+		}
+		
+		//fillCube(groundStart, end, BlockType.AIR); // Calls clear cube, which checks coordinates
 	}
 	
 	/**
