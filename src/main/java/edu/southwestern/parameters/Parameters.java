03f47d3f41fc7ec130fcfbb69070289098db7a7f
Mark Mueller <muellerm@southwestern.edu>
2022-06-27 09:28:53 -0500

    #813 added command line params

--- src/main/java/edu/southwestern/parameters/Parameters.java
+++ src/main/java/edu/southwestern/parameters/Parameters.java
@@ -369,7 +369,10 @@ public class Parameters {
 		integerOptions.add("interactiveSleepTimer", 1000, "The ammount of time the interactive thread sleeps for");
 		integerOptions.add("extraSpaceBetweenMinecraftShapes", 50, "The extra space that will be in between the shapes being evaluated");
 		integerOptions.add("leftoverMinecraftBlocksAllowed", 0, "The amount of blocks that will be allowed to be left behind by the flying machine and still get max fitness for the change in center of mass fitness function");
-
+		integerOptions.add("startX", 0, "The X coordinate the archive is built on");
+		integerOptions.add("startY", 5, "The Y coordinate the archive is built on");
+		integerOptions.add("startZ", 0, "The Z coordinate the archive is built on");
+		
 		
 		// Long parameters
 		longOptions.add("lastGenotypeId", 0l, "Highest genotype id used so far");
