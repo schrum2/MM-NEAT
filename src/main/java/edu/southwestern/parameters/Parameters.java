db06d4f4acf28e76b8a1b941a85996e752a93a79
Mark Mueller <muellerm@southwestern.edu>
2022-06-28 13:11:31 -0500

    changed command line parameter to have minecraft in the name

--- src/main/java/edu/southwestern/parameters/Parameters.java
+++ src/main/java/edu/southwestern/parameters/Parameters.java
@@ -372,7 +372,7 @@ public class Parameters {
 		integerOptions.add("startX", 0, "The X coordinate the shapes are generated on");
 		integerOptions.add("startY", 5, "The Y coordinate the shapes are generated on");
 		integerOptions.add("startZ", 0, "The Z coordinate the shapes are generated on");
-		integerOptions.add("clearDimension", 100, "The coordinates to clear the space for the archive");
+		integerOptions.add("minecraftClearDimension", 100, "The coordinates to clear the space for the archive");
 		
 		
 		// Long parameters
