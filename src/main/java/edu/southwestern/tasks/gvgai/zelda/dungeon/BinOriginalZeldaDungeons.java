5086be710c10fa76a76e8509210fab7e2b374728
Jacob Schrum <schrum2@southwestern.edu>
2022-06-28 15:09:06 -0500

    #819 More command line parameters uncovered
    Also, levels from the second quest won't work.

--- src/main/java/edu/southwestern/tasks/gvgai/zelda/dungeon/BinOriginalZeldaDungeons.java
+++ src/main/java/edu/southwestern/tasks/gvgai/zelda/dungeon/BinOriginalZeldaDungeons.java
@@ -17,6 +17,16 @@ import edu.southwestern.tasks.zelda.ZeldaDungeonTask;
 public class BinOriginalZeldaDungeons {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws FileNotFoundException, Exception {
+		
+		// Load parameters here first, since some needs to be set for the loading to work
+		Parameters.initializeParameterCollections(new String[] {
+				"randomSeed:0", "rougeEnemyHealth:2",
+				"io:false","netio:false","watch:false",
+				"zeldaDungeonBackTrackRoomFitness:true","zeldaDungeonDistinctRoomFitness:true",
+				"zeldaDungeonDistanceFitness:false","zeldaDungeonFewRoomFitness:false",
+				"zeldaDungeonTraversedRoomFitness:true","zeldaPercentDungeonTraversedRoomFitness:true",
+				"zeldaDungeonRandomFitness:false"});
+		
 		String[] binningSchemes = new String[] {
 				"ZeldaMAPElitesWallWaterRoomsBinLabels", // WWR in Journal paper, also in original CPPN2GAN paper
 				"ZeldaMAPElitesNoveltyAndBackTrackRoomBinLabels", // Never used in publication
@@ -25,27 +35,28 @@ public class BinOriginalZeldaDungeons {
 		// Load all dungeons
 		String[] dungeonList = new String[] {
 				"tloz1_1_flip",
-				"tloz1_2_flip",
+				//"tloz1_2_flip",
 				"tloz2_1_flip",
-				"tloz2_2_flip",
+				//"tloz2_2_flip",
 				"tloz3_1_flip",
-				"tloz3_2_flip",
+				//"tloz3_2_flip",
 				"tloz4_1_flip",
-				"tloz4_2_flip",
+				//"tloz4_2_flip",
 				"tloz5_1_flip",
-				"tloz5_2_flip",
+				//"tloz5_2_flip",
 				"tloz6_1_flip",
-				"tloz6_2_flip",
+				//"tloz6_2_flip",
 				"tloz7_1_flip",
-				"tloz7_2_flip",
+				//"tloz7_2_flip",
 				"tloz8_1_flip",
-				"tloz8_2_flip",
-				"tloz9_1_flip",
-				"tloz9_2_flip"
+				//"tloz8_2_flip",
+				"tloz9_1_flip" //,
+				//"tloz9_2_flip"
 		};
 		
 		List<Dungeon> dungeonCollection = new ArrayList<>();
 		for(String name : dungeonList) {
+			System.out.println("Loading "+name);
 			dungeonCollection.add(LoadOriginalDungeon.loadOriginalDungeon(name, false));
 		}
 
@@ -60,6 +71,8 @@ public class BinOriginalZeldaDungeons {
 					"zeldaDungeonBackTrackRoomFitness:true","zeldaDungeonDistinctRoomFitness:true",
 					"zeldaDungeonDistanceFitness:false","zeldaDungeonFewRoomFitness:false",
 					"zeldaDungeonTraversedRoomFitness:true","zeldaPercentDungeonTraversedRoomFitness:true",
+					"noveltyBinAmount:10",
+					"zeldaGANLevelWidthChunks:8","zeldaGANLevelHeightChunks:8", // Largest Zelda levels is 8 by 8
 					"zeldaDungeonRandomFitness:false",
 					"ea:edu.southwestern.evolution.mapelites.MAPElites", 
 					"experiment:edu.southwestern.experiment.evolution.SteadyStateExperiment",
