fedff01e0991857798a7369f00c4b90ccaf7423e
Mark Mueller <muellerm@southwestern.edu>
2022-06-29 13:53:41 -0500

    #788 Added new command line param for sleep timer
    can control sleep timer from command line

--- src/main/java/edu/southwestern/parameters/Parameters.java
+++ src/main/java/edu/southwestern/parameters/Parameters.java
@@ -373,6 +373,7 @@ public class Parameters {
 		integerOptions.add("startY", 5, "The Y coordinate the shapes are generated on");
 		integerOptions.add("startZ", 0, "The Z coordinate the shapes are generated on");
 		integerOptions.add("minecraftClearDimension", 100, "The coordinates to clear the space for the archive");
+		integerOptions.add("minecraftClearSleepTimer", 100, "How long the thread should sleep between sending a clear command to python");
 		
 		
 		// Long parameters
