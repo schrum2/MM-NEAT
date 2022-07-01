555452c2c5d4cf6db315d9ff96a42e3b2bd920c9
Jacob Schrum <schrum2@southwestern.edu>
2022-07-01 11:47:41 -0500

    #827 For testing
    There are leftover shapes near the ones with eval problems, but I'm not sure that is the source of the problem yet.

--- src/main/java/edu/southwestern/tasks/evocraft/shapegeneration/VectorToVolumeGenerator.java
+++ src/main/java/edu/southwestern/tasks/evocraft/shapegeneration/VectorToVolumeGenerator.java
@@ -66,7 +66,7 @@ public class VectorToVolumeGenerator implements ShapeGenerator<ArrayList<Double>
 						if(phenotype.get(PRESENCE_INDEX) >= Parameters.parameters.doubleParameter("voxelExpressionThreshold")) {
 							assert phenotype.get(TYPE_INDEX) >= 0 : "index:"+TYPE_INDEX+":"+phenotype;
 							int blockTypeIndex = (int)(phenotype.get(TYPE_INDEX)*numBlockTypes); // length because there are no AIR blocks in list for this case (presence takes care of this)		
-							System.out.println("Block type index: " + blockTypeIndex);
+							//System.out.println("Block type index: " + blockTypeIndex);
 							assert blockTypeIndex >= 0 : "Index "+TYPE_INDEX+ " of " + phenotype + " multiplied by " + numBlockTypes;
 
 							if(blockTypeIndex == numBlockTypes) blockTypeIndex--; // Rare case
