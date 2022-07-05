bde1443eb2a7c8e95db7a93afedffa30af4eacc3
Alejandro Medina <alex.m@alexmedina.com>
2022-07-05 17:02:02 -0500

    #832 New experiment class for loading shape from file
    The class has been created, but the methods have not been filled out yet.

---
+++ src/main/java/edu/southwestern/experiment/post/MinecraftBlockRenderExperiment.java
@@ -0,0 +1,30 @@
+package edu.southwestern.experiment.post;
+
+import edu.southwestern.experiment.Experiment;
+
+/**
+ * 
+ * @author Alejandro Medina
+ *
+ */
+public class MinecraftBlockRenderExperiment implements Experiment {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean shouldStop() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
