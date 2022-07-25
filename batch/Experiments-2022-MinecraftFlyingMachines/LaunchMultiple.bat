c9fa88dcce88bfa0216fbc59c186e4b5a25ff633
Jacob Schrum <schrum2@southwestern.edu>
2022-07-25 09:48:03 -0500

    Switch back to the working dir

--- batch/Experiments-2022-MinecraftFlyingMachines/LaunchMultiple.bat
+++ batch/Experiments-2022-MinecraftFlyingMachines/LaunchMultiple.bat
@@ -2,5 +2,6 @@ REM Usage  : LaunchMultiple.bat <batch file> <starting run> <ending run>
 REM example: launchMultiple.bat MinecraftAccumulate-ME-Observer-VectorCountNegative.bat 0 2
 FOR /L %%A IN (%2,1,%3) DO (
   call Launch.bat %1 %%A
+  cd %~dp0
 )
 ECHO "All done!"
