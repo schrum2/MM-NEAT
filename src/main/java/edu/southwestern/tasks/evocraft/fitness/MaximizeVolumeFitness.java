74eb25b9cf9c5a7cf204542bc93fce7a32aae485
TjRaffert <111467145+TjRaffert@users.noreply.github.com>
2023-05-24 14:52:41 -0500

    #871 fitness function for volume
    Created fitness function for rewarding Volume created MaximizeVolumeFitness. I also added maxCoordinates in the MinecraftUtilClass to be used on MaximizeVolumeFitness.

---
+++ src/main/java/edu/southwestern/tasks/evocraft/fitness/MaximizeVolumeFitness.java
@@ -0,0 +1,43 @@
+package edu.southwestern.tasks.evocraft.fitness;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.clearspring.analytics.util.Pair;
+
+import edu.southwestern.tasks.evocraft.MinecraftClient.Block;
+import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
+import edu.southwestern.tasks.evocraft.MinecraftUtilClass;
+
+/**
+ * fitness function that rewards shapes based on their total volume. Keeps track of max and min coordinates to calculate volume.
+ * @author raffertyt
+ *
+ */
+public class MaximizeVolumeFitness extends TimedEvaluationMinecraftFitnessFunction {
+
+	
+	//Keeps track of max and min coordinates to eventually get the volume
+	@Override
+	public double calculateFinalScore(ArrayList<Pair<Long, List<Block>>> history, MinecraftCoordinates corner,
+			List<Block> originalBlocks) {
+
+		MinecraftCoordinates maxResultCoordinates = new MinecraftCoordinates(0, 0, 0);
+		MinecraftCoordinates minResultCoordinates = new MinecraftCoordinates(0, 0, 0);
+		List<Block> currentShapeBlockList;
+		for (int i = 0; i < history.size(); i++) {
+			currentShapeBlockList = history.get(i).right;
+			minResultCoordinates = MinecraftUtilClass.minCoordinates(currentShapeBlockList);
+			maxResultCoordinates = MinecraftUtilClass.maxCoordinates(currentShapeBlockList);
+		}
+	
+		return MinecraftUtilClass.volume(minResultCoordinates, maxResultCoordinates);
+	}
+	//maximum number of blocks that could be of the desired type (if the whole evaluation area was filled)
+	@Override
+	public double maxFitness() {
+		return MinecraftUtilClass.reservedSpace().x()*MinecraftUtilClass.reservedSpace().y();
+	}
+
+	
+}
