4e84b220548fe39d64e93b6ab00b8865290cd951
JoannaBlatt <110856717+JoannaBlatt@users.noreply.github.com>
2023-05-24 14:50:34 -0500

    #863 Created batch files and extended fitness function for water and lava
    started creating batch files, created WaterLavaSecondaryCreationFitness class, cleaned comments and code some

--- src/main/java/edu/southwestern/tasks/evocraft/fitness/SecondaryBlockCreationFitness.java
+++ src/main/java/edu/southwestern/tasks/evocraft/fitness/SecondaryBlockCreationFitness.java
@@ -12,22 +12,22 @@ import edu.southwestern.tasks.evocraft.MinecraftClient.BlockType;
 import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
 import edu.southwestern.tasks.evocraft.MinecraftUtilClass;
 
+/**
+ * This evaluates the number of blocks created after the shape is spawned
+ * currently overrides calculateFinalScore and maxFitness
+ * 
+ * @author lewisj
+ *
+ */
 public abstract class SecondaryBlockCreationFitness extends TimedEvaluationMinecraftFitnessFunction{
-	//currently making for water and lava
+
+	//the type of blocks you are looking for
+	private BlockType[] acceptedBlockTypes; //= {BlockType.COBBLESTONE, BlockType.OBSIDIAN};
 	
-	//take an array to specify blocks
-	//take a blockset we create with
-	
-	//check for secondary blocks in a timed evaluation minecraft fitness function
-		//-figure out time wait for this, maybe just a parameter setting? minecraftMandatoryWaitTime
-	
-	//reward for secondary block creation at end of evaluation time
-		// count blocks of desired type
-	
-	
-	//private blocktypes array - set to parameter list of block types
-	//TODO: undo hardcoding of block types
-	private BlockType[] acceptedBlockTypes = {BlockType.COBBLESTONE, BlockType.OBSIDIAN};
+	//constructor to create the accepted block type list
+	public SecondaryBlockCreationFitness(BlockType[] acceptedBlockTypes) {
+		this.acceptedBlockTypes = acceptedBlockTypes;
+	}
 
 	public BlockType[] getAcceptedBlockTypes() {
 		return acceptedBlockTypes;
@@ -39,18 +39,22 @@ public abstract class SecondaryBlockCreationFitness extends TimedEvaluationMinec
 	
 	@Override
 	public double calculateFinalScore (ArrayList<Pair<Long,List<Block>>> history, MinecraftCoordinates corner, List<Block> originalBlocks) {
-		//check the original block list with the history, last read
-		//create variable to last entry in history using history.size
+		
+		//initialized to the last 
 		List<Block> finalBlocksList = history.get(history.size()-1).right;
 		//check that list against originalBlocks to get a list of only the desired blocks
 		finalBlocksList = MinecraftUtilClass.getDesiredBlocks(finalBlocksList, acceptedBlockTypes);
 		
-		//add all the blocks.... which should just be the length of the list
+		//the number of blocks of the desired type(s) equals the length of the list of blocks
 		return finalBlocksList.size();
 		
 	}
-	//using the history list check if there are new blocks at each read
 	
-	//use get desired blocks
 
+	//maximum number of blocks that could be of the desired type (if the whole evaluation area was filled)
+	@Override
+	public double maxFitness() {
+		return MinecraftUtilClass.reservedSpace().x()*MinecraftUtilClass.reservedSpace().y();
+	}
+	
 }
