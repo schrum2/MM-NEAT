56f25445092e163021123f774e0a7ecdeb204de3
Jacob <schrum2@southwestern.edu>
2023-05-26 10:05:26 -0500

    #874 Test file for Water/Lava
    This is the file you need to add code to for the unit test @JoannaBlatt

---
+++ src/test/java/edu/southwestern/tasks/evocraft/fitness/WaterLavaSecondaryCreationFitnessTest.java
@@ -0,0 +1,14 @@
+package edu.southwestern.tasks.evocraft.fitness;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WaterLavaSecondaryCreationFitnessTest {
+
+	@Test
+	public void test() {
+		// TODO: Tests go here!
+	}
+
+}
