6b4e9e3ce6522cffc811b5e876947428cc40f832
TjRaffert <111467145+TjRaffert@users.noreply.github.com>
2023-05-26 16:19:48 -0500

    #865 Missile Fitness Almost Done
    Some fixes to the calculation and creation of batch file for experiments.

--- src/main/java/edu/southwestern/tasks/evocraft/fitness/MissileFitness.java
+++ src/main/java/edu/southwestern/tasks/evocraft/fitness/MissileFitness.java
@@ -10,11 +10,11 @@ import edu.southwestern.tasks.evocraft.MinecraftClient;
 import edu.southwestern.tasks.evocraft.MinecraftClient.Block;
 import edu.southwestern.tasks.evocraft.MinecraftClient.BlockType;
 import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
-import edu.southwestern.tasks.evocraft.MinecraftClient.Orientation;
 import edu.southwestern.tasks.evocraft.MinecraftUtilClass;
 
 
 /**
+ * TODO
  * 
  * @author raffertyt
  *
@@ -22,61 +22,66 @@ import edu.southwestern.tasks.evocraft.MinecraftUtilClass;
 public class MissileFitness extends TimedEvaluationMinecraftFitnessFunction {
 
 	private MinecraftCoordinates targetCornerOffset;
-	public BlockType targetBlockType;
+	private BlockType targetBlockType;
+	private BlockType[] acceptedBlownUpBlocks;
+	//constructor to create the accepted block type list
+
 	public MissileFitness() {
 		int xOffset = Parameters.parameters.integerParameter("minecraftTargetDistancefromShapeY");
 		int yOffset = Parameters.parameters.integerParameter("minecraftTargetDistancefromShapeY");
 		int zOffset = Parameters.parameters.integerParameter("minecraftTargetDistancefromShapeY");
 		targetCornerOffset = new MinecraftCoordinates(xOffset, yOffset, zOffset);
-				
+
 		targetBlockType = BlockType.values()[Parameters.parameters.integerParameter("minecraftMissleTargetBlockType")];
-		
+		acceptedBlownUpBlocks = new BlockType[] {targetBlockType};
+	}
+	
+	@Override
+	public double minFitness() {
+		MinecraftCoordinates ranges = MinecraftUtilClass.getRanges();
+		int min = ranges.x() * ranges.z() * ranges.y();
+		return -min;
 	}
 
 	//shape dimensions
 	@Override
 	public double maxFitness() {
-		MinecraftCoordinates ranges = MinecraftUtilClass.getRanges();
-		double max = ranges.x() * ranges.z() * ranges.y();
-		return -max;
+		return 0;
 	}
 
 	@Override
 	public double fitnessScore(MinecraftCoordinates corner, List<Block> originalBlocks) {
-		
-//		// TODO: Create target structure here
-//		List<Block> targetBlockList = new ArrayList<>();
-//		MinecraftCoordinates ranges = MinecraftUtilClass.getRanges();
-//		for(int i = 0; i < ranges.x(); i++) {
-//			for(int j = 0; j < ranges.x(); j++) {
-//				for(int k = 0; k < ranges.x(); k++) {
-//					MinecraftCoordinates loopCoordinates = new MinecraftCoordinates(i, j, k); 
-//					MinecraftCoordinates nextBlockCoordinates = new MinecraftCoordinates(targetCornerOffset.add(loopCoordinates));
-//					targetBlockList.add(new Block(nextBlockCoordinates.x(), nextBlockCoordinates.y(), nextBlockCoordinates.z(), BlockType.SLIME, Orientation.WEST));
-//				}
-//			}
-//		}
-//		MinecraftClient.getMinecraftClient().spawnBlocks(targetBlockList);
 
-		
-		// TODO: Replace SLIME with block from command line parameter
+		//		// TODO: Create target structure here
+		//		List<Block> targetBlockList = new ArrayList<>();
+		//		MinecraftCoordinates ranges = MinecraftUtilClass.getRanges();
+		//		for(int i = 0; i < ranges.x(); i++) {
+		//			for(int j = 0; j < ranges.x(); j++) {
+		//				for(int k = 0; k < ranges.x(); k++) {
+		//					MinecraftCoordinates loopCoordinates = new MinecraftCoordinates(i, j, k); 
+		//					MinecraftCoordinates nextBlockCoordinates = new MinecraftCoordinates(targetCornerOffset.add(loopCoordinates));
+		//					targetBlockList.add(new Block(nextBlockCoordinates.x(), nextBlockCoordinates.y(), nextBlockCoordinates.z(), BlockType.SLIME, Orientation.WEST));
+		//				}
+		//			}
+		//		}
+		//		MinecraftClient.getMinecraftClient().spawnBlocks(targetBlockList);
+
+
+		// Create structure to be blown up
 		MinecraftClient.getMinecraftClient().fillCube(corner.add(targetCornerOffset), corner.add(targetCornerOffset).add(MinecraftUtilClass.getRanges()), targetBlockType);
+		// Evaluate as normal
 		return super.fitnessScore(corner, originalBlocks);
 	}
-	
-	
+
+
 	@Override
 	public double calculateFinalScore(ArrayList<Pair<Long, List<Block>>> history, MinecraftCoordinates corner,
 			List<Block> originalBlocks) {
-		// TODO Auto-generated method stub
-		
-		
+
 		List<Block> leftOverBlocksFromTarget = MinecraftClient.getMinecraftClient().readCube(corner.add(targetCornerOffset), corner.add(targetCornerOffset).add(MinecraftUtilClass.getRanges()));
-		//new BlockType[] targetBlockArray;
-		
-	//	MinecraftUtilClass.getDesiredBlocks(leftOverBlocksFromTarget, targetBlockArray);
-		return 0;
+		List<Block> leftOverOfTargetBlocks = MinecraftUtilClass.getDesiredBlocks(leftOverBlocksFromTarget, acceptedBlownUpBlocks);
+		return -leftOverOfTargetBlocks.size();
 	}
 
-	
+
 }
