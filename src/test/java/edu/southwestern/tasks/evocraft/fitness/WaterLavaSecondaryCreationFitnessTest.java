45941a2efda308b0d3f6c202d9bf798391eed13b
Jacob Schrum <schrum2@southwestern.edu>
2023-05-26 21:18:15 -0500

    Trying to fix by tweaking parameters

--- src/test/java/edu/southwestern/tasks/evocraft/fitness/WaterLavaSecondaryCreationFitnessTest.java
+++ src/test/java/edu/southwestern/tasks/evocraft/fitness/WaterLavaSecondaryCreationFitnessTest.java
@@ -29,7 +29,7 @@ public class WaterLavaSecondaryCreationFitnessTest {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		CommonConstants.netio = false;
-		Parameters.initializeParameterCollections(new String[] {"watch:true","minecraftXRange:10","minecraftYRange:10","minecraftZRange:10","spaceBetweenMinecraftShapes:6","minecraftEndEvalNoMovement:true","shortTimeBetweenMinecraftReads:" + 150L,"minecraftMandatoryWaitTime:" + 10000L,"minecraftBlockSet:edu.southwestern.tasks.evocraft.blocks.WaterAndLavaBlockSet"});
+		Parameters.initializeParameterCollections(new String[] {"watch:true","minecraftClearWithGlass:false","minecraftXRange:10","minecraftYRange:10","minecraftZRange:10","spaceBetweenMinecraftShapes:6","minecraftEndEvalNoMovement:true","shortTimeBetweenMinecraftReads:" + 150L,"minecraftMandatoryWaitTime:" + 10000L,"minecraftBlockSet:edu.southwestern.tasks.evocraft.blocks.WaterAndLavaBlockSet","minecraftAccumulateChangeInCenterOfMass:false"});
 		MinecraftServer.launchServer();
 		MinecraftClient.getMinecraftClient();
 		CommonConstants.watch = true; // Displays debugging info
