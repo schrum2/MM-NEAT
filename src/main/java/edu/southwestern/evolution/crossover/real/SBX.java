a81e57b484f750b293f477a84e6819463e5a9b26
Jacob Schrum <schrum2@southwestern.edu>
2023-06-01 23:41:55 -0500

    #887 Fixed (?) SBX
    Couldn't make sense of my old implementation of SBX, so I looked up another one.

--- src/main/java/edu/southwestern/evolution/crossover/real/SBX.java
+++ src/main/java/edu/southwestern/evolution/crossover/real/SBX.java
@@ -11,12 +11,17 @@ import edu.southwestern.util.datastructures.Pair;
 
 /**
  * Simulated Binary Crossover of an array of doubles. Based
- * on a research paper by Kalyanmoy Deb.
+ * on a research paper by Kalyanmoy Deb. However, my original attempt
+ * at implementing had problems, so I borrowed code from here:
+ * http://www.java2s.com/example/java-src/pkg/es/udc/gii/common/eaf/algorithm/operator/reproduction/crossover/sbxcrossover-985d5.html
+ * 
  * @author Jacob Schrum
  */
 public class SBX extends ArrayCrossover<Double> {
 
+	// Other sources refer to this value as ETA
 	public static final double DEB_DI = 20; // 0.5; // mysterious variable from Deb's code
+	
 	private final double[] upperBounds;
 	private final double[] lowerBounds;
 
@@ -35,56 +40,110 @@ public class SBX extends ArrayCrossover<Double> {
 	}
 
 	public Pair<Double, Double> newIndexContents(Double par1, Double par2, Double upper, Double lower) {
-		double rnd = RandomNumbers.randomGenerator.nextDouble();
+		double yl = lower;
+		double yu = upper;
+		double rand = RandomNumbers.randomGenerator.nextDouble();
 		/* Check whether variable is selected or not */
-		if (rnd <= 0.5) {
+		if (rand <= 0.5) {
 			double y1, y2, betaq;
 			if (Math.abs(par1 - par2) > 0.000001) {
 				y2 = Math.max(par2, par1);
 				y1 = Math.min(par2, par1);
 
+				// Original code (source?)
 				/* Find beta value */
-				double beta;
-				if ((y1 - lower) > (upper - y2)) {
-					beta = 1 + (2 * (upper - y2) / (y2 - y1));
-				} else {
-					beta = 1 + (2 * (y1 - lower) / (y2 - y1));
-				}
+//				double beta;
+//				if ((y1 - lower) > (upper - y2)) {
+//					beta = 1 + (2 * (upper - y2) / (y2 - y1));
+//				} else {
+//					beta = 1 + (2 * (y1 - lower) / (y2 - y1));
+//				}
+//
+//				/* Find alpha */
+//				double expp = DEB_DI + 1.0;
+//
+//				beta = 1.0 / beta;
+//				double alpha = 2.0 - Math.pow(beta, expp);
 
-				/* Find alpha */
-				double expp = DEB_DI + 1.0;
-
-				beta = 1.0 / beta;
-				double alpha = 2.0 - Math.pow(beta, expp);
-
+				// Replacement code: http://www.java2s.com/example/java-src/pkg/es/udc/gii/common/eaf/algorithm/operator/reproduction/crossover/sbxcrossover-985d5.html
+				double crossOverIndex = DEB_DI; // Don't understand this name
+				double beta = 1.0 + (2.0 * (y1 - yl) / (y2 - y1));
+                double alpha = 2.0 - Math.pow(beta, -(crossOverIndex + 1.0));
+				
+                // Original code, but I'm not sure why this is an error condition
 				if (alpha < 0.0) {
 					System.out.println("ERRRROR:1: " + alpha + " " + par1 + " " + par2);
-					System.exit(-1);
+					throw new IllegalStateException("Why is this an error?: ERRRROR:1: " + alpha + " " + par1 + " " + par2);
 				}
 
-				rnd = RandomNumbers.randomGenerator.nextDouble();
-				if (rnd <= 1.0 / alpha) {
-					alpha = alpha * rnd;
-					expp = 1.0 / (DEB_DI + 1.0);
-					betaq = Math.pow(alpha, expp);
-				} else {
-					alpha = alpha * rnd;
-					alpha = 1.0 / (2.0 - alpha);
-					expp = 1.0 / (DEB_DI + 1.0);
-					if (alpha < 0.0) {
-						System.out.printf("ERRRORRR:2: " + alpha + " " + par1 + " " + par2);
-						System.exit(-1);
-					}
-					betaq = Math.pow(alpha, expp);
-				}
-			} else {
-				betaq = 1.0;
-				y1 = par1;
-				y2 = par2;
-			}
+				rand = RandomNumbers.randomGenerator.nextDouble();
+				// Original code: why ERRRORRR:2?
+//				if (rnd <= 1.0 / alpha) {
+//					alpha = alpha * rnd;
+//					expp = 1.0 / (DEB_DI + 1.0);
+//					betaq = Math.pow(alpha, expp);
+//				} else {
+//					alpha = alpha * rnd;
+//					alpha = 1.0 / (2.0 - alpha);
+//					expp = 1.0 / (DEB_DI + 1.0);
+//					if (alpha < 0.0) {
+//						System.out.printf("ERRRORRR:2: " + alpha + " " + par1 + " " + par2);
+//						System.exit(-1);
+//					}
+//					betaq = Math.pow(alpha, expp);
+//				}
+				
+				// Replacement code: http://www.java2s.com/example/java-src/pkg/es/udc/gii/common/eaf/algorithm/operator/reproduction/crossover/sbxcrossover-985d5.html
+				if (rand <= (1.0 / alpha)) {
+                    betaq = Math.pow((rand * alpha), (1.0 / (crossOverIndex + 1.0)));
+                } else {
+                    betaq = Math.pow((1.0 / (2.0 - rand * alpha)), (1.0 / (crossOverIndex + 1.0)));
+                }
+
+                double c1 = 0.5 * ((y1 + y2) - betaq * (y2 - y1));
+                beta = 1.0 + (2.0 * (yu - y2) / (y2 - y1));
+                alpha = 2.0 - Math.pow(beta, -(crossOverIndex + 1.0));
+                if (rand <= (1.0 / alpha)) {
+                    betaq = Math.pow((rand * alpha), (1.0 / (crossOverIndex + 1.0)));
+                } else {
+                    betaq = Math.pow((1.0 / (2.0 - rand * alpha)), (1.0 / (crossOverIndex + 1.0)));
+                }
+
+                double c2 = 0.5 * ((y1 + y2) + betaq * (y2 - y1));
+                if (c1 < yl) {
+                    c1 = yl;
+                }
+                if (c2 < yl) {
+                    c1 = yl;
+                }
+                if (c1 > yu) {
+                    c1 = yu;
+                }
+                if (c2 > yu) {
+                    c2 = yu;
+                }
+				
+                // My code to adapt the source to my framework
+                rand = RandomNumbers.randomGenerator.nextDouble();
+                // I don't bother bounding the values here because mutation happens after crossover,
+                // and the values get bound after mutation.
+                if(rand <= 0.5) {
+                    return new Pair<Double, Double>(c2,c1);                	
+                } else {
+                    return new Pair<Double, Double>(c1,c2);                	
+                }
+			} 
+			
+			// Original code: why an else case?
+//			else {
+//				betaq = 1.0;
+//				y1 = par1;
+//				y2 = par2;
+//			}
 			/* Generation two children */
-			return new Pair<Double, Double>(0.5 * ((y1 + y2) - betaq * (y2 - y1)),
-					0.5 * ((y1 + y2) + betaq * (y2 - y1)));
+			// Original code (similar calculations above)
+//			return new Pair<Double, Double>(0.5 * ((y1 + y2) - betaq * (y2 - y1)),
+//					0.5 * ((y1 + y2) + betaq * (y2 - y1)));
 		}
 		return new Pair<Double, Double>(par1, par2);
 	}
