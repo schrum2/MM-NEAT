1793ea19595e9e848b4fb3d9e05e2fdb3aca7a47
JoannaBlatt <110856717+JoannaBlatt@users.noreply.github.com>
2023-06-05 16:50:49 -0500

    #871 working on unit test
    working on unit test

--- src/test/java/edu/southwestern/tasks/evocraft/fitness/MaximizeVolumeFitnessTest.java
+++ src/test/java/edu/southwestern/tasks/evocraft/fitness/MaximizeVolumeFitnessTest.java
@@ -2,17 +2,21 @@ package edu.southwestern.tasks.evocraft.fitness;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import edu.southwestern.experiment.post.MinecraftBlockCompareExperiment;
 import edu.southwestern.parameters.CommonConstants;
 import edu.southwestern.parameters.Parameters;
 import edu.southwestern.tasks.evocraft.MinecraftClient;
 import edu.southwestern.tasks.evocraft.MinecraftServer;
+import edu.southwestern.tasks.evocraft.MinecraftUtilClass;
 import edu.southwestern.tasks.evocraft.MinecraftClient.Block;
 import edu.southwestern.tasks.evocraft.MinecraftClient.BlockType;
 import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
@@ -57,6 +61,29 @@ public class MaximizeVolumeFitnessTest {
 	
 	@Test
 	public void testNoInteraction() {	
+		Parameters.initializeParameterCollections(new String[] {"minecraftBlockListTextFile", "minecraftClearWithGlass:false","watch:false","minecraftXRange:10","minecraftYRange:10","minecraftZRange:10","spaceBetweenMinecraftShapes:6","minecraftEndEvalNoMovement:true","shortTimeBetweenMinecraftReads:" + 150L,"minecraftMandatoryWaitTime:" + 10000L,"minecraftBlockSet:edu.southwestern.tasks.evocraft.blocks.MachineBlockSet"});
+		
+		String shapeOneFileName = Parameters.parameters.stringParameter("minecraftBlockListTextFile");
+		File shapeOneTextFile = new File(shapeOneFileName);
+		
+		MinecraftCoordinates testCorner = new MinecraftCoordinates(-26,7,-35);
+		MinecraftClient.getMinecraftClient().clearSpaceForShapes(testCorner, ranges, 1, 50); // Larger buffer is important, but too large and it crashes!
+
+		ArrayList<Block> testBlockSet = new ArrayList<>();
+		//add blocks using file here
+		//List<Block> List1 = MinecraftBlockCompareExperiment.shiftBlocks(shapeOneTextFile,  MinecraftClient.POST_EVALUATION_SHAPE_CORNER);
+		List<Block> listOne = MinecraftUtilClass.loadMAPElitesOutputFile(shapeOneTextFile);
+		//MinecraftUtilClass.shiftBlocksBetweenCorners
+		//move blocks over
+		//add again
+		//combine lists
+		//
+	
+		assertEquals(0.0,testInstance.fitnessScore(testCorner,testBlockSet),0.0);
+	}
+	
+	@Test
+	public void testWorkingFlyingMachine() {	
 		Parameters.initializeParameterCollections(new String[] {"minecraftClearWithGlass:false","watch:false","minecraftXRange:10","minecraftYRange:10","minecraftZRange:10","spaceBetweenMinecraftShapes:6","minecraftEndEvalNoMovement:true","shortTimeBetweenMinecraftReads:" + 150L,"minecraftMandatoryWaitTime:" + 10000L,"minecraftBlockSet:edu.southwestern.tasks.evocraft.blocks.MachineBlockSet"});
 		
 		MinecraftCoordinates testCorner = new MinecraftCoordinates(-26,7,-35);
