4d2b9319b51dec8d257bff90e6d2a3121e9dc16f
TjRaffert <raffertyt@southwestern.edu>
2023-06-08 13:57:54 -0500

    #899 RemoveBlockMutation
    Extends MinecraftShapeMutation. Removes a block at a random coordinate in the genotype based on the rate set in the parameter

---
+++ src/main/java/edu/southwestern/tasks/evocraft/mutation/RemoveBlockMutation.java
@@ -0,0 +1,30 @@
+package edu.southwestern.tasks.evocraft.mutation;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import edu.southwestern.evolution.genotypes.Genotype;
+import edu.southwestern.tasks.evocraft.MinecraftClient.Block;
+import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
+import edu.southwestern.tasks.evocraft.genotype.MinecraftShapeGenotype;
+import edu.southwestern.util.datastructures.Pair;
+import edu.southwestern.tasks.evocraft.MinecraftUtilClass;
+/**
+ * Removes a block at a random coordinate in the genotype based on the rate set in the parameter.
+ * @author raffertyt
+ *
+ */
+public class RemoveBlockMutation extends MinecraftShapeMutation {
+
+	public RemoveBlockMutation() {
+		super("minecraftRemoveBlockMutationRate");
+	}
+
+	@Override
+	public void mutate(Genotype<Pair<HashMap<MinecraftCoordinates, Block>, HashSet<MinecraftCoordinates>>> genotype) {
+		MinecraftShapeGenotype shapeGenotype = (MinecraftShapeGenotype) genotype;
+		MinecraftCoordinates randomCoordinates = MinecraftUtilClass.randomCoordinatesInShapeRange();
+		shapeGenotype.removeBlock(randomCoordinates);
+	}
+	
+}
