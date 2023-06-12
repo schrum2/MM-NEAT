b35784f540a46358a9a5efae863e93176ad384bb
Jacob <schrum2@southwestern.edu>
2023-06-12 09:55:35 -0500

    Unused imports

--- src/test/java/edu/southwestern/tasks/evocraft/fitness/ChangeCenterOfMassFitnessTest.java
+++ src/test/java/edu/southwestern/tasks/evocraft/fitness/ChangeCenterOfMassFitnessTest.java
@@ -3,8 +3,6 @@ package edu.southwestern.tasks.evocraft.fitness;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
