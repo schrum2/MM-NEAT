ff4bf6163c5ae2b292c306aafe627d80f4df4b67
JoannaBlatt <110856717+JoannaBlatt@users.noreply.github.com>
2023-06-13 11:00:40 -0500

    #891 added more variables dealing with new individual creation
    needed to add variables related to mating. Made a subsection for them.

--- src/main/java/edu/southwestern/evolution/mome/MOME.java
+++ src/main/java/edu/southwestern/evolution/mome/MOME.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 import org.jenetics.internal.math.random;
 
 import edu.southwestern.MMNEAT.MMNEAT;
+import edu.southwestern.evolution.EvolutionaryHistory;
 import edu.southwestern.evolution.SteadyStateEA;
 import edu.southwestern.evolution.genotypes.BoundedRealValuedGenotype;
 import edu.southwestern.evolution.genotypes.Genotype;
@@ -20,12 +21,22 @@ import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
 import edu.southwestern.util.PopulationUtil;
 import edu.southwestern.util.random.RandomNumbers;
 
+/**
+ * yay! I made this!
+ * @author lewisj
+ *
+ * @param <T>
+ */
 public class MOME<T> implements SteadyStateEA<T>{
 
 	protected MOMEArchive<T> archive;
 	protected int iterations;	//might want to rename? what is it, just the number of individuals created so far?
 	protected LonerTask<T> task; ///seems to be for cleanup, not sure what else
+	
+	//below deals with new individual creation
 	private static final int NUM_CODE_EMPTY = -1;	//initialization number for a non existent parent
+	private boolean mating;	//determines if using mating functionality
+	private double crossoverRate;
 
 	@Override
 	public void initialize(Genotype<T> example) {
@@ -65,6 +76,9 @@ public class MOME<T> implements SteadyStateEA<T>{
 			archive.add(s); // Fill the archive with random starting individuals, only when this flag is true
 		});
 		
+		//initializing a variable
+		this.mating = Parameters.parameters.booleanParameter("mating");
+
 	}
 
 	@Override
@@ -74,9 +88,37 @@ public class MOME<T> implements SteadyStateEA<T>{
 		//get random individual for parent
 		Genotype<T> parentGenotype1 = archive.getRandomIndividaul().individual;
 		long parentId1 = parentGenotype1.getId();
-		long parentId2 = NUM_CODE_EMPTY;
+		long parentId2 = NUM_CODE_EMPTY;	//initialize second parent in case it's needed
+		
+		//creating the first child
 		Genotype<T> childGenotype1 = parentGenotype1.copy(); // Copy with different Id (will be further modified below)
 		childGenotype1.addParent(parentId1);
+
+		// Potentially mate with second individual
+		if (mating && RandomNumbers.randomGenerator.nextDouble() < crossoverRate) {
+		}
+		/**
+		 * // Potentially mate with second individual
+		if (mating && RandomNumbers.randomGenerator.nextDouble() < crossoverRate) {
+			int otherIndex = archive.randomOccupiedBinIndex(); // From a different bin
+			Genotype<T> parent2 = archive.getElite(otherIndex).individual;
+			parentId2 = parent2.getId(); // Parent Id comes from original genome
+			Genotype<T> child2 = parent2.copy(); // Copy with different Id (further modified below)
+			
+			// Replace child2 with a crossover result, and modify child1 in the process (two new children)
+			child2 = child1.crossover(child2);
+			child2.mutate(); // Probabilistic mutation of child
+			child2.addParent(parent2.getId());
+			child2.addParent(parent1.getId());
+			child1.addParent(parent2.getId());
+			EvolutionaryHistory.logLineageData(parentId1,parentId2,child2);
+			// Evaluate and add child to archive
+			Score<T> s2 = task.evaluate(child2);
+			// Indicate whether elite was added
+			boolean child2WasElite = archive.add(s2);
+			fileUpdates(child2WasElite); // Log for each individual produced
+		}
+		 */
 	}
 
 	@Override
