4bc61864a004a5c858a0b10903841fa82c00897c
TjRaffert <111467145+TjRaffert@users.noreply.github.com>
2023-06-20 15:46:16 -0500

    Update postSpawnMinecraftEvaluateBlocksCenterOfMass.bat

--- postSpawnMinecraftEvaluateBlocksCenterOfMass.bat
+++ postSpawnMinecraftEvaluateBlocksCenterOfMass.bat
@@ -1,3 +1,3 @@
 REM Usage:   postSpawnMinecraftEvaluateBlocks.bat <filename>
 REM Example: postSpawnMinecraftEvaluateBlocks.bat minecraftaccumulate/VectorPistonOrientationCount19/archive/NS0EW2UD8_0.07125_7270.txt
-java -ea -jar "target/MM-NEAT-0.0.1-SNAPSHOT.jar" minecraftEvaluate minecraftBlockListTextFile:%1 netio:false spaceBetweenMinecraftShapes:10 minecraftChangeCenterOfMassFitness:true minecraftAccumulateChangeInCenterOfMass:true watch:true
+java -ea -jar "target/MM-NEAT-0.0.1-SNAPSHOT.jar" minecraftEvaluate minecraftBlockListTextFile:%1 netio:false spaceBetweenMinecraftShapes:10 minecraftChangeCenterOfMassFitness:true minecraftAccumulateChangeInCenterOfMass:true watch:true minecraftClearSleepTimer:400
