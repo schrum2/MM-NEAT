92c00c28764416355806d271ceffea2ca2a4875d
JoannaBlatt <110856717+JoannaBlatt@users.noreply.github.com>
2023-06-23 14:08:19 -0500

    #917 hypervolume methods are done
    finished the class methods and needs to be tested

--- src/main/java/edu/southwestern/util/MultiobjectiveUtil.java
+++ src/main/java/edu/southwestern/util/MultiobjectiveUtil.java
@@ -1,7 +1,11 @@
 package edu.southwestern.util;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
+import edu.southwestern.evolution.nsga2.NSGA2;
+import edu.southwestern.evolution.nsga2.NSGA2Score;
 import edu.southwestern.scores.Score;
 import jmetal.qualityIndicator.Hypervolume;
 
@@ -14,34 +18,41 @@ import jmetal.qualityIndicator.Hypervolume;
 public class MultiobjectiveUtil {
 
 	/**
-	 * 
-	 * @param scores
-	 * @return
+	 * Calculates the hypervolume from a list of scores. Assumes that the list is the current pareto front.
+	 * Turns the list of scores into scoresArrayForHypervolume[number of points][number of objectives].
+	 * calles calculateHypervolume for hypervolume calculations
+	 * Assumes all scores have the same number of objective scores.
+	 * @param scores the list of scores for a pareto front being evaluated
+	 * @return the hypervolume of the pareto front.
 	 */
 	public static <T> double hypervolumeFromParetoFront(List<Score<T>> scores) {
-		// use calculate hypervolume
-		//numberOfObjectives = scores[i].scores.size()
 		int numberOfObjectives = scores.get(0).scores.length;
-		double[][] hypervolumeScoresArray = new double[scores.size()][numberOfObjectives];
-		for (Score<T> individualScore : scores) {
-			hypervolumeScoresArray[number of scores][number of objectives]
+		int numberOfPoints = scores.size();
+		double[][] scoresArrayForHypervolume = new double[numberOfPoints][numberOfObjectives];
+		int i = 0;
+		for (Score<T> individualScore : scores) {		//adds the scores to the double array of scores
+			scoresArrayForHypervolume[i] = individualScore.scores;
+			i++;
 		}
-		double result = Hypervolume; //how do I call the hypervolume function?
-		//get a double[numberInPop][numberOfObjectives] then calculate
-		return 0;
+		Hypervolume hv = new Hypervolume();	//instantiates an instance of the class for access to the calculateHypervolume method
+		return hv.calculateHypervolume(scoresArrayForHypervolume, numberOfPoints, numberOfObjectives);
+		//doubleScores[numberOfIndividualScoresInList][numberOfObjectives]
 	}
 	
-//population may not be pareto front
 	/**
-	 * does not assume that the scores have been properly sorted into pareto fronts
-	 * @param scores
-	 * @return
+	 * Calculates the hypervolume of a List of Scores. Does not assume that it has
+	 * been passed a pareto front. Creates a new pareto front first and then passes that list
+	 * to calculate the hypervolume.
+	 * @param scores a list of scores to be evaluated 
+	 * @return the hypervolume of the pareto front of the list of score
 	 */
 	public static <T> double hypervolumeFromPopulation(List<Score<T>> scores) {
-		//calculate pareto front NSGA2.paretofront
-		//then convert to double[][]
-		//calculate hypervolume
-		return 0;
+		List<NSGA2Score<T>> paretoFront = NSGA2.getParetoFront(NSGA2.staticNSGA2Scores(scores)); //get pareto front
+		List<Score<T>> scoresParetoList = new ArrayList<Score<T>>(scores.size());
+		for (NSGA2Score<T> score : paretoFront) {		//turn NSGA2Score pareto front into List of Scores
+			scoresParetoList.add(score);
+		}
+		return hypervolumeFromParetoFront(scoresParetoList);
 	}
 
 }
