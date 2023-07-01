49040ae4203279463eb1037746fdd1168ab2a2b5
Jacob Schrum <schrum2@southwestern.edu>
2023-07-01 14:31:42 -0500

    #897 Changed save condition to be a bit higher
    a multiple of 2 resulted in several non flying machines being saved. Make bigger, and turned into named constant. Might be a Parameter later.

--- src/main/java/edu/southwestern/tasks/evocraft/fitness/AccumulateNewBlockPositionsFitness.java
+++ src/main/java/edu/southwestern/tasks/evocraft/fitness/AccumulateNewBlockPositionsFitness.java
@@ -6,27 +6,34 @@ import java.util.List;
 
 import edu.southwestern.tasks.evocraft.MinecraftClient.Block;
 import edu.southwestern.tasks.evocraft.MinecraftClient.MinecraftCoordinates;
-import edu.southwestern.parameters.Parameters;
 import edu.southwestern.tasks.evocraft.MinecraftUtilClass;
 import edu.southwestern.util.datastructures.Pair;
 
-
+/**
+ * Fitness based on the number of distinct positions that a block of the shape
+ * occupies over the history of evaluation.
+ * 
+ * @author Travis Rafferty
+ *
+ */
 public class AccumulateNewBlockPositionsFitness extends TimedEvaluationMinecraftFitnessFunction {
-	
+
+	// Shape needs to visit this times as many blocks at in the shape positions in order to be saved
+	private static final double SAVE_MULTIPLE = 2.5;
 	
 	@Override
 	public Double earlyEvaluationTerminationResult(MinecraftCoordinates corner, List<Block> originalBlocks,
 			ArrayList<Pair<Long, List<Block>>> history, List<Block> newShapeBlockList) {
-		
+
 		int numReadings = history.size();
 		if(history.get(numReadings - 1).t2.equals(history.get(numReadings - 2).t2)) {
 			// Shape is not changing, so just compute the final score already
 			return this.calculateFinalScore(history, corner, originalBlocks);
 		}
-		
+
 		return null; // Do not end early
 	}
-	
+
 	@Override
 	public double calculateFinalScore(ArrayList<Pair<Long, List<Block>>> history, MinecraftCoordinates corner,
 			List<Block> originalBlocks) {
@@ -35,21 +42,21 @@ public class AccumulateNewBlockPositionsFitness extends TimedEvaluationMinecraft
 		for(int i = 0; i < history.size(); i++) {
 			List<Block> shape = history.get(i).t2;
 			for(int j = 0; j < shape.size(); j++) {
-				 fitness.add(shape.get(j).blockPosition());
+				fitness.add(shape.get(j).blockPosition());
 			}
 		}
-		
+
 		return fitness.size();
 	}
 	@Override
 	public boolean shapeIsWorthSaving(double fitnessScore, ArrayList<Pair<Long, List<Block>>> history, MinecraftCoordinates shapeCorner, List<Block> originalBlocks) {
-		
+
 		//System.out.println("target * PERCENT_NEEDED_TO_SAVE" + -(target * PERCENT_NEEDED_TO_SAVE) + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-		if(fitnessScore > (originalBlocks.size() * 2)) { 
+		if(fitnessScore > (originalBlocks.size() * SAVE_MULTIPLE)) { 
 			//System.out.println("target * PERCENT_NEEDED_TO_SAVE" + -(target * PERCENT_NEEDED_TO_SAVE) + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 			return true;
 		}
-			return false;
+		return false;
 	}
 	@Override
 	public double maxFitness() {
@@ -58,5 +65,5 @@ public class AccumulateNewBlockPositionsFitness extends TimedEvaluationMinecraft
 		// Assumes all blocks move to new positions on every reading, which is unreasonably fast
 		return ((minNumberOfShapeReadings()+1) * volume);
 	}
-	
+
 }
