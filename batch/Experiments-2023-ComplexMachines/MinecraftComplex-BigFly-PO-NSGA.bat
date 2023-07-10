40b92a10abe3b5b5b301ecba7d9c684e89712c21
Jacob <schrum2@southwestern.edu>
2023-07-10 11:27:19 -0500

    More and less batch files
    Realized that NSGA2 and WSPF don't need variants for different bin labels since they don't use any bins (though in the future, I could do a different pseudo archive with each one).
    
    However, I did add the different bin label options for MOME and MEWS, namely block count (C) and negative space plus block count (NC)

--- batch/Experiments-2023-ComplexMachines/MinecraftComplex-BigFly-PO-NSGA.bat
+++
@@ -1,7 +0,0 @@
-cd..
-cd..
-java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar runNumber:%1 randomSeed:%1 minecraftXRange:5 minecraftYRange:5 minecraftZRange:5 mapElitesQDBaseOffset:1 minecraftShapeGenerator:edu.southwestern.tasks.evocraft.shapegeneration.DirectRepresentationShapeGenerator trials:1 minecraftMandatoryWaitTime:10000 minecraftContainsWholeMAPElitesArchive:false forceLinearArchiveLayoutInMinecraft:false minecraftBlockSet:edu.southwestern.tasks.evocraft.blocks.MachineBlockSet io:true netio:true interactWithMapElitesInWorld:false mating:true spaceBetweenMinecraftShapes:22 launchMinecraftServerFromJava:false task:edu.southwestern.tasks.evocraft.MinecraftLonerShapeTask watch:false saveAllChampions:true genotype:edu.southwestern.tasks.evocraft.genotype.MinecraftShapeGenotype vectorPresenceThresholdForEachBlock:true parallelEvaluations:true threads:10 parallelMAPElitesInitialize:true minecraftClearWithGlass:true minecraftClearSleepTimer:400 minecraftSkipInitialClear:true rememberParentScores:true extraSpaceBetweenMinecraftShapes:100 mu:25 maxGens:4004 minecraftAccumulateChangeInCenterOfMass:true maximumMOMESubPopulationSize:10 minecraftChangeCenterOfMassFitness:true minecraftAccumulateNewBlockPositionsFitness:true base:minecraftcomplex log:MinecraftComplex-POBigFlyNSGA saveTo:POBigFlyNSGA
-
-
-
-
--- batch/Experiments-2023-ComplexMachines/MinecraftComplex-Missile-PO-WSPF.bat
+++
@@ -1,7 +0,0 @@
-cd..
-cd..
-java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar runNumber:%1 randomSeed:%1 minecraftXRange:3 minecraftYRange:3 minecraftZRange:3 minecraftEmptySpaceBufferX:27 mapElitesQDBaseOffset:1 minecraftEmptySpaceBufferZ:27 minecraftEmptySpaceBufferY:18 minecraftShapeGenerator:edu.southwestern.tasks.evocraft.shapegeneration.DirectRepresentationShapeGenerator trials:1 minecraftMandatoryWaitTime:15000 minecraftContainsWholeMAPElitesArchive:false forceLinearArchiveLayoutInMinecraft:false minecraftBlockSet:edu.southwestern.tasks.evocraft.blocks.ExplosiveBlockSet io:true netio:true interactWithMapElitesInWorld:false mating:true spaceBetweenMinecraftShapes:30 launchMinecraftServerFromJava:false task:edu.southwestern.tasks.evocraft.MinecraftLonerShapeTask watch:false saveAllChampions:true genotype:edu.southwestern.tasks.evocraft.genotype.MinecraftShapeGenotype vectorPresenceThresholdForEachBlock:true parallelEvaluations:true threads:10 mu:25 maxGens:4004 parallelMAPElitesInitialize:true minecraftClearWithGlass:true minecraftClearSleepTimer:400 minecraftSkipInitialClear:true rememberParentScores:true extraSpaceBetweenMinecraftShapes:100  minecraftAccumulateChangeInCenterOfMass:true maximumMOMESubPopulationSize:10 minecraftCompassMissileTargets:true minecraftTargetDistancefromShapeX:30 minecraftTargetDistancefromShapeY:0 minecraftTargetDistancefromShapeZ:0 minecraftWeightedSumsMissileAndChangeCenterOfMassFitness:true base:minecraftcomplex log:MinecraftComplex-POMissileWSPF saveTo:POMissileWSPF
-
-
-
-
