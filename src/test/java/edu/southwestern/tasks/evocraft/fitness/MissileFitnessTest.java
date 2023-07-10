5125ef9ef68e1f1ef5f2057b3677bce32c6da086
JoannaBlatt <110856717+JoannaBlatt@users.noreply.github.com>
2023-07-10 15:39:08 -0500

    Made more uniform jUnit test messages for readability
    Also trying to get all unit tests to work so maven install will work properly again.

--- src/test/java/edu/southwestern/tasks/evocraft/fitness/MissileFitnessTest.java
+++ src/test/java/edu/southwestern/tasks/evocraft/fitness/MissileFitnessTest.java
@@ -52,11 +52,11 @@ public class MissileFitnessTest {
 		CommonConstants.watch = false; // Displays debugging info
 	}
 	@Test
-	public void testNoTNTFitnessScore() {	
+	public void testNoTNTFitnessScore() {	//TODO: freezing
 		Parameters.initializeParameterCollections(new String[] {"minecraftClearWithGlass:false","watch:true","minecraftXRange:5","minecraftYRange:5","minecraftZRange:5","spaceBetweenMinecraftShapes:6","minecraftEndEvalNoMovement:true","shortTimeBetweenMinecraftReads:" + 150L,"minecraftMandatoryWaitTime:" + 10000L,"minecraftBlockSet:edu.southwestern.tasks.evocraft.blocks.CannonBlockSet", "minecraftTargetDistancefromShapeY:0", "minecraftTargetDistancefromShapeX:-10", "minecraftTargetDistancefromShapeZ:0"});
 		MissileFitness testInstance = new MissileFitness();
-		
-		System.out.println("Unit test: testNoTNTFitnessScore");
+		System.out.println("\n TEST: MissileFitnessTest: testNoTNTFitnessScore");
+
 		System.out.println("GO");
 		System.out.println("X:" + Parameters.parameters.integerParameter("minecraftTargetDistancefromShapeX") + " Y:" + Parameters.parameters.integerParameter("minecraftTargetDistancefromShapeY") + " Z:" + Parameters.parameters.integerParameter("minecraftTargetDistancefromShapeZ"));
 		System.out.println("1");
@@ -71,13 +71,14 @@ public class MissileFitnessTest {
 		int min = (ranges.x()) * (ranges.z()) * (ranges.y());	
 		//System.out.println("This is Ranges.x:" + ranges.x());
 		assertEquals(-min ,testInstance.fitnessScore(testCorner,testBlockSet),0.0);
+		System.out.println("\n");
 	}
 	@Test
 	public void testdropTNTFitnessScore() {	
 		Parameters.initializeParameterCollections(new String[] {"minecraftClearWithGlass:false","watch:false","minecraftXRange:5","minecraftYRange:5","minecraftZRange:5","spaceBetweenMinecraftShapes:6","minecraftEndEvalNoMovement:true","shortTimeBetweenMinecraftReads:" + 150L,"minecraftMandatoryWaitTime:" + 10000L,"minecraftBlockSet:edu.southwestern.tasks.evocraft.blocks.CannonBlockSet", "minecraftTargetDistancefromShapeY:0", "minecraftTargetDistancefromShapeX:0", "minecraftTargetDistancefromShapeZ:0"});
 		MissileFitness testInstance = new MissileFitness();
 		
-		System.out.println("Unit test: testNoTNTFitnessScore");
+		System.out.println("\n TEST: MissileFitnessTest: testNoTNTFitnessScore");
 
 		MinecraftCoordinates ranges = MinecraftUtilClass.getRanges();
 		MinecraftCoordinates testCorner = new MinecraftCoordinates(-26,30,-35);
@@ -89,6 +90,7 @@ public class MissileFitnessTest {
 		int min = (ranges.x()) * (ranges.z()) * (ranges.y());	
 		//System.out.println("This is Ranges.x:" + ranges.x());
 		assertTrue(-min < testInstance.fitnessScore(testCorner, testBlockSet));
+		System.out.println("\n");
 	}
 //	@Test
 //	public void testshootTNTFitnessScore() {	
