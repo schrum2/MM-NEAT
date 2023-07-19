77abfc90d796e0a4019d98f089caf49da5ace3ad
schrum2 <schrum2@southwestern.edu>
2023-07-19 17:23:13 -0500

    Deleted innovation engine and GVG batch files
    The code associated with these is gone now, so they would not work. Might need to delete some RL Glue Tetris stuff too

--- batch/GVGAILevels/GVGAILevel-CPPN-Basic.bat
+++
@@ -1,3 +0,0 @@
-cd ..
-cd ..
-java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar runNumber:%1 randomSeed:%1 base:gvgaicppn trials:1 mu:16 maxGens:500 io:true netio:true mating:true fs:false task:edu.southwestern.tasks.interactive.gvgai.LevelBreederTask log:GVGAICPPN-Basic saveTo:Basic allowMultipleFunctions:true ftype:0 netChangeActivationRate:0.3 cleanFrequency:50 recurrency:false saveInteractiveSelections:false simplifiedInteractiveInterface:false saveAllChampions:true cleanOldNetworks:true logTWEANNData:false logMutationAndLineage:false watch:false
