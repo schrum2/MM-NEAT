2859041c8855568c27ffd3b51a2dc65d7151efd4
schrum2 <schrum2@southwestern.edu>
2023-07-19 05:37:31 -0500

    Simple example in the complex group
    This is a simple batch file that models an older GECCO one. Just evolved small flying machines with the direct encoding. Need to have some thorough tests that verify that this direct representation is really better. Probably need to add more batch files for small flying machines to recreate old results with the new encoding and settings.

---
+++ batch/Experiments-2023-ComplexMachines/MinecraftComplex-SmallFly-PO-ME.bat
@@ -0,0 +1,23 @@
+cd..
+cd..
+java -jar target/MM-NEAT-0.0.1-SNAPSHOT.jar runNumber:%1 randomSeed:%1 ^
+minecraftShapeGenerator:edu.southwestern.tasks.evocraft.shapegeneration.DirectRepresentationShapeGenerator ^
+genotype:edu.southwestern.tasks.evocraft.genotype.MinecraftShapeGenotype vectorPresenceThresholdForEachBlock:true ^
+trials:1 io:true netio:true mating:true rememberParentScores:true ^
+watch:false saveAllChampions:true ^
+parallelEvaluations:true threads:10 parallelMAPElitesInitialize:true ^
+launchMinecraftServerFromJava:false minecraftClearSleepTimer:400 minecraftSkipInitialClear:true extraSpaceBetweenMinecraftShapes:100 ^
+task:edu.southwestern.tasks.evocraft.MinecraftLonerShapeTask ^
+minecraftContainsWholeMAPElitesArchive:false forceLinearArchiveLayoutInMinecraft:false interactWithMapElitesInWorld:false ^
+experiment:edu.southwestern.experiment.evolution.SteadyStateExperiment steadyStateIndividualsPerGeneration:100 ^
+mapElitesBinLabels:edu.southwestern.tasks.evocraft.characterizations.MinecraftMAPElitesPistonOrientationCountBinLabels minecraftPistonLabelSize:5 ^
+mapElitesQDBaseOffset:1 ^
+ea:edu.southwestern.evolution.mapelites.MAPElites ^
+mu:100 maxGens:60000 ^
+minecraftMandatoryWaitTime:10000 ^
+minecraftXRange:3 minecraftYRange:3 minecraftZRange:3 ^
+minecraftBlockSet:edu.southwestern.tasks.evocraft.blocks.MachineBlockSet ^
+spaceBetweenMinecraftShapes:22 ^
+minecraftClearWithGlass:false ^
+minecraftAccumulateChangeInCenterOfMass:true minecraftChangeCenterOfMassFitness:true base:minecraftcomplex log:MinecraftComplex-SmallFlyPOME saveTo:SmallFlyPOME
+
