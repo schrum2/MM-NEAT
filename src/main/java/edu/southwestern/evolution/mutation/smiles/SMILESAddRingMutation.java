241a5c49dc81ee2ae639a88072386c6bec0bf24c
Jacob Schrum <schrum2@southwestern.edu>
2024-07-10 11:04:25 -0500

    More comments
    Based on Steve's more recent paper that adds cycles

--- src/main/java/edu/southwestern/evolution/mutation/smiles/SMILESAddRingMutation.java
+++ src/main/java/edu/southwestern/evolution/mutation/smiles/SMILESAddRingMutation.java
@@ -3,7 +3,12 @@ package edu.southwestern.evolution.mutation.smiles;
 import edu.southwestern.parameters.Parameters;
 
 /**
- * Adds a ring to a SMILES string
+ * Adds a ring to a SMILES string by connecting two random atoms.
+ * In the string, the shared number 1 indicates the two link points.
+ * Molecules can only have at most one ring (is this true?)
+ * 
+ * Example mutation:
+ * C=C-N-O becomes C1=C-N-O1
  */
 public class SMILESAddRingMutation extends SMILESMutation{
 	public SMILESAddRingMutation() {
