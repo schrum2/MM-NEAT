92c6db653e85f6128fae4f52e68415ade706c201
Jacob Schrum <schrum2@southwestern.edu>
2024-07-17 14:30:54 -0500

    Not more watch

--- src/main/java/edu/southwestern/tasks/molecules/MoleculeTask.java
+++ src/main/java/edu/southwestern/tasks/molecules/MoleculeTask.java
@@ -117,7 +117,7 @@ public class MoleculeTask extends NoisyLonerTask<String> {
 //					 "moleculeTargetMeltingAndBoilingPointFitness:true").split(" "));
 		
 		// TODO: Make into MAP Elites
-		MMNEAT.main(("runNumber:1 randomSeed:1 watch:true trials:1 mu:10 base:molecules log:Molecules-TargetTypeCounts "+
+		MMNEAT.main(("runNumber:1 randomSeed:1 watch:false trials:1 mu:10 base:molecules log:Molecules-TargetTypeCounts "+
 				 "saveTo:TargetTypeCounts maxGens:50000 io:true netio:true mating:false "+
 			 	 "task:edu.southwestern.tasks.molecules.MoleculeTask cleanFrequency:-1 saveAllChampions:true "+
 				 "genotype:edu.southwestern.evolution.genotypes.SMILESStringGenotype "+
